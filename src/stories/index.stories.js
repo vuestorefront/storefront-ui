3c08e2c9ac8d3f20a02240a500b296b75f197ecd
Filip Rakowski <frakowski@divante.pl>
2019-03-11 17:48:14 +0100

    remove example story

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,23 +3,23 @@ import { storiesOf } from "@storybook/vue";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import MyButton from "../components/MyButton.vue";
+// import MyButton from "../components/MyButton.vue";
 
-storiesOf("Button", module)
-  .add("with text", () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action("clicked") }
-  }))
-  .add("with JSX", () => ({
-    components: { MyButton },
-    render() {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo("Button", "with some emoji") }
-  }))
-  .add("with some emoji", () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action("clicked") }
-  }));
+// storiesOf("Button", module)
+//   .add("with text", () => ({
+//     components: { MyButton },
+//     template: '<my-button @click="action">Hello Button</my-button>',
+//     methods: { action: action("clicked") }
+//   }))
+//   .add("with JSX", () => ({
+//     components: { MyButton },
+//     render() {
+//       return <my-button onClick={this.action}>With JSX</my-button>;
+//     },
+//     methods: { action: linkTo("Button", "with some emoji") }
+//   }))
+//   .add("with some emoji", () => ({
+//     components: { MyButton },
+//     template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+//     methods: { action: action("clicked") }
+//   }));
