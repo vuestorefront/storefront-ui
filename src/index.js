f21e2dbcca2be4db1fd1f3c4e191913b11d371ae
Filip Rakowski <frakowski@divante.pl>
2019-03-11 18:22:16 +0100

    sfui export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import Banner from './components/Banner/Banner.vue'
+import BottomNavigation from './components/BottomNavigation/BottomNavigation.vue'
+import Button from './components/Button/Button.vue'
+import CallToAction from './components/CallToAction/CallToAction'
 
 export default (StorefrontUI.install = function(Vue, options) {
   Vue.component('Banner', Banner)
-});
+  Vue.component('BottomNavigation', BottomNavigation)
+  Vue.component('Button', Button)
+  Vue.component('CallToAction', CallToAction)
+})
+
+export {
+  Banner,
+  BottomNavigation,
+  Button,
+  CallToAction
+}
