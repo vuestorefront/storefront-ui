f92f6a886546b6474dad64ce3f1b951150bb635f
Filip Rakowski <frakowski@divante.pl>
2019-03-12 11:56:32 +0100

    added partial unit tests for button

--- src/components/Button/Button.spec.ts
+++ src/components/Button/Button.spec.ts
@@ -2,16 +2,37 @@ import { shallowMount } from "@vue/test-utils";
 import Button from "@/components/Button/Button.vue";
 
 describe("HelloWorld.vue", () => {
-  let component
-  beforeEach(() => {
-    component = shallowMount(Button, {
-      slots: {
-        default: "Hello World!"
-      }
-    });
+
+  it("renders a button", () => {
+    const component = shallowMount(Button);
+    expect(component.contains('button')).toBe(true);
   });
 
-  it("renders prop text when passed", () => {
-    expect(component.find('Hello World')).toBe(true);
+  it("renders default prop text when passed", () => {
+    const msg = 'HelloWorld'
+    const component = shallowMount(Button, {
+      slots: {
+        default: msg
+      }
+    });
+    expect(component.find('.sf-button').text()).toMatch(msg);
   });
+
+  it('adds sf-button--full-width class when full-width prop is present', () => {
+    const component = shallowMount(Button, {
+      propsData: {
+        fullWidth: true
+      }
+    });
+    expect(component.classes()).toContain('sf-button--full-width');
+  })
+
+  it('adds sf-button--secondary class when "color" prop is set to "secondary"', () => {
+    const component = shallowMount(Button, {
+      propsData: {
+        color: "secondary"
+      }
+    });
+    expect(component.classes()).toContain('sf-button--secondary');
+  })
 });
