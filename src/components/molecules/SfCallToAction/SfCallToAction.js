2e00dde64d49e5edabe2ac7718beca0aa39930ce
Filip Rakowski <frakowski@divante.pl>
2019-03-14 11:01:05 +0100

    CTA SCSS vars and markup correction

--- src/components/molecules/SfCallToAction/SfCallToAction.js
+++ src/components/molecules/SfCallToAction/SfCallToAction.js
@@ -2,26 +2,6 @@ import SfButton from "../../atoms/SfButton/SfButton.vue";
 
 export default {
   name: "SfCallToAction",
-  data() {
-    return {
-      stylesObj: {
-        backgroundImage: this.bgImg,
-        backgroundColor: this.bgCol
-      }
-    };
-  },
-  props: {
-    bgImg: {
-      required: false,
-      type: String,
-      default: null
-    },
-    bgCol: {
-      required: false,
-      type: String,
-      default: "black"
-    }
-  },
   components: {
     SfButton
   }
