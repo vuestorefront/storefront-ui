9f82d137736b53149a87d8771bd6c8a62d8af37c
Leonardo Matos <leomp120894@gmail.com>
2019-03-21 19:35:38 -0300

    chore: starting with header mobile

---
+++ src/components/molecules/SfHeaderMobile/SfHeaderMobile.spec.ts
@@ -0,0 +1,21 @@
+import { shallowMount } from "@vue/test-utils";
+import SfHeaderMobile from "@/components/molecules/SfHeaderMobile/SfHeaderMobile.vue";
+
+describe("SfComponent.vue", () => {
+  // DOM Element render check, you can replace with with root component
+  // it("renders a button", () => {
+  //   const component = shallowMount(SfComponent);
+  //   expect(component.contains("button")).toBe(true);
+  // });
+
+  // Default slot check, you can replace `default` with any other
+  // it("renders default prop text when passed", () => {
+  //   const msg = "HelloWorld";
+  //   const component = shallowMount(SfComponent, {
+  //     slots: {
+  //       default: msg
+  //     }
+  //   });
+  //   expect(component.find(".sf-button").text()).toMatch(msg);
+  // });
+});
---
+++ src/components/molecules/SfHeaderMobile/SfHeaderMobile.vue
@@ -0,0 +1,10 @@
+<script>
+import instance from "./SfHeaderMobile.js";
+
+export default {
+  ...instance
+};
+</script>
+
+<template lang="html" src="./SfHeaderMobile.html"></template>
+<style lang="scss" src="./SfHeaderMobile.scss"></style>
