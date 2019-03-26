2c7198cd8b9785a9664ecb53f08ab8cc625d4905
Filip Rakowski <frakowski@divante.pl>
2019-03-26 17:03:22 +0100

    corrected  invalid remcorrected SfBanner

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <SfBanner>
-      <template slot="title"
-        >ECO SANDALS</template
-      >
-      <template slot="subtitle"
-        >Summer shoes</template
-      >
+      <template slot="title">
+        ECO SANDALS
+      </template>
+      <template slot="subtitle">
+        Summer shoes
+      </template>
       <template slot="description">
         The collection features formal and casual comfort shoes with a Danish
         design focus. Made from premium leathers and comfort.
