9e7e7945c137046ede115fad1345334fedbeaca5
Przemyslaw Rakowski <prakowski@divante.pl>
2019-03-26 10:20:42 +0100

    banner

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
-    <h1>It's a heading</h1>
-    <custom-button class="sf-button--secondary">Text</custom-button>
-    <sf-button>Text</sf-button>
+    <SfBanner style="background-image: url('Banner1.png')">
+      <div slot="title">ECO SANDALS</div>
+      <div slot="subtitle">Summer shoes</div>
+      <div slot="description">
+        The collection features formal and casual comfort shoes with a Danish
+        design focus. Made from premium leathers and comfort.
+      </div>
+      <template slot="call-to-action"
+        >shop now</template
+      >
+    </SfBanner>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import CustomButton from "./CustomButton.vue";
 import SfButton from "./components/atoms/SfButton/SfButton.vue";
 import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
 
 export default Vue.extend({
   name: "app",
   components: {
-    SfButton,
-    CustomButton
+    SfBanner
   }
 });
 </script>
