fe02edada441f68ebedfd36f66cf0e179ee8705d
Przemyslaw Rakowski <prakowski@divante.pl>
2019-03-27 16:23:55 +0100

    app.vue cleared

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <template>
   <div id="app">
-    <div style="display: flex; ">
-      <SfBanner class="sf-banner--right">
-        <template slot="title">
-          ECO SANDALS
-        </template>
-        <template slot="subtitle">
-          Summer shoes
-        </template>
-        <template slot="description">
-          The collection features formal and casual comfort shoes with a Danish
-          design focus. Made from premium leathers and comfort.
-        </template>
-        <template slot="button">
-          shop now
-        </template>
-      </SfBanner>
-    </div>
+    <SfButton></SfButton>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import SfButton from "./components/atoms/SfButton/SfButton.vue";
-import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
 
 export default Vue.extend({
   name: "app",
   components: {
-    SfBanner
+    SfButton
   }
 });
 </script>
