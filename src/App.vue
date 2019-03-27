e17fce89a21fd1d0badf0d5883c5e34562943610
llusiak <llusiak@divante.pl>
2019-03-27 10:10:34 +0100

    Mega Menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <h1>It's a heading</h1>
+    <!-- <h1>It's a heading</h1>
     <custom-button class="sf-button--secondary">Text</custom-button>
-    <sf-button>Text</sf-button>
+    <sf-button>Text</sf-button> -->
+    <sf-mega-menu>
+      Hello World, Mega Menu!
+    </sf-mega-menu>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import CustomButton from "./CustomButton.vue";
-import SfButton from "./components/atoms/SfButton/SfButton.vue";
-import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
+// import CustomButton from "./CustomButton.vue";
+// import SfButton from "./components/atoms/SfButton/SfButton.vue";
+// import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
+import SfMegaMenu from "./components/organisms/SfMegaMenu/SfMegaMenu.vue";
 
 export default Vue.extend({
   name: "app",
   components: {
-    SfButton,
-    CustomButton
+    SfMegaMenu
+    // SfButton,
+    // CustomButton
   }
 });
 </script>
