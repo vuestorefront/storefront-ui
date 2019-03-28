a33ba726b0051c5fd6689aefdb185732376d820f
Leonardo Matos <leomp120894@gmail.com>
2019-03-28 10:23:54 -0300

    feat: unit tests for call to action component

---
+++ src/components/molecules/SfCallToAction/SfCallToAction.spec.ts
@@ -0,0 +1,46 @@
+import { shallowMount } from "@vue/test-utils";
+import SfCallToAction from "@/components/organisms/SfCallToAction/SfCallToAction.vue";
+
+describe("SfCallToAction.vue", () => {
+  // DOM Element render check
+  it("renders a section", () => {
+     const component = shallowMount(SfCallToAction);
+     expect(component.contains("section")).toBe(true);
+  });
+
+  // default slot check
+  it("renders default slot text when passed", () => {
+    const msg = "HelloWorld";
+    const component = shallowMount(SfCallToAction, {
+      slots: {
+        default: msg
+      }
+    });
+    expect(component.find(".sf-call-to-action").text()).toMatch(msg);
+  });
+
+  // title message check from prop
+  it("renders message prop on h1 when passed", () => {
+    const msg = "H1Title";
+    const component = shallowMount(SfCallToAction, {
+      props: {
+        message: msg
+      }
+    });
+    const wrapper = component.find(".sf-call-to-action__message");
+    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.is('h1')).toBe(true);
+  });
+
+  // button text check from prop
+  it("renders button text from prop when passed", () => {
+    const msg = "ButtonText";
+    const component = shallowMount(SfCallToAction, {
+      props: {
+        buttonText: msg
+      }
+    });
+    const wrapper = component.find(".sf-call-to-action__button");
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
