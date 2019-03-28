5cd0c7b1c2d02aa565fbe30baa57c80796143cab
Filip Rakowski <frakowski@divante.pl>
2019-03-28 13:28:56 +0100

    added storybook example

---
+++ src/components/template/SfComponent.stories.js
@@ -0,0 +1,12 @@
+// /* eslint-disable import/no-extraneous-dependencies */
+// import { storiesOf } from "@storybook/vue";
+// import { action } from "@storybook/addon-actions";
+// import { linkTo } from "@storybook/addon-links";
+
+// import SfComponent from "./SfComponent.vue";
+
+// storiesOf("Component", module)
+//   .add("with text", () => ({
+//     components: { SfComponent },
+//     template: '<sf-component></sf-component>'
+//   }))
