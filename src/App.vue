fa72e0d7ee0cabd71c66cc44441426e770a26a19
Przemyslaw Rakowski <prakowski@divante.pl>
2019-03-29 14:48:17 +0100

    diffrent aproach to default icon

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <SfAlert
+      class="sf-alert--warning"
+      message="wiadomość przez propsa i slote-scope"
+    >
+    </SfAlert>
+    <SfAlert
+      class="sf-alert--info"
+      message="wiadomość przez propsa i slote-scope"
+    >
+    </SfAlert>
+    <SfAlert
+      class="sf-alert--alert"
+      message="wiadomość przez propsa i slote-scope"
+    >
+    </SfAlert>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SfAlert from "./components/atoms/SfAlert/SfAlert.vue";
 
 export default Vue.extend({
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      b: "a"
+    };
+  },
+  components: {
+    SfAlert
+  }
 });
 </script>
 
--- src/components/atoms/SfAlert/SfAlert.js
+++ src/components/atoms/SfAlert/SfAlert.js
@@ -1,11 +1,25 @@
 export default {
-  name: "SfComponent",
+  name: "SfAlert",
   props: {
     message: String,
     icon: String,
     displayIcon: {
       type: Boolean,
       default: true
+    }
+  },
+  data: () => ({
+    type: ""
+  }),
+  mounted() {
+    if (this.$el.className.includes("sf-alert--warning")) {
+      this.type = "warning";
+    }
+    if (this.$el.className.includes("sf-alert--info")) {
+      this.type = "info";
+    }
+    if (this.$el.className.includes("sf-alert--alert")) {
+      this.type = "alert";
     }
   }
 };
