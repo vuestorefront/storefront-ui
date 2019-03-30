6dba567ed21d3497a57abd3e0f44245722e0a204
Leonardo Matos <leomp120894@gmail.com>
2019-03-29 21:12:08 -0300

    fix: importing global styles for storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from "@storybook/vue";
 
+import styles from "../../src/css/styles.scss";
+
 const req = require.context("../../src/", true, /.stories.js$/);
 
 function loadStories() {
