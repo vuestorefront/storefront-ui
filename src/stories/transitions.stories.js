91c7155eafea5e95a2489c1dbf013ac26377bd3e
Leonardo Matos <leomp120894@gmail.com>
2019-03-31 16:52:01 -0300

    feat: add styles and stories for slide and fade/slide transitions

--- src/stories/transitions.stories.js
+++ src/stories/transitions.stories.js
@@ -10,9 +10,69 @@ export default storiesOf("Transitions", module)
     template: `
     <div>
       <sf-button v-on:click="show = !show">
-        Toggle
+        Fade in-out
       </sf-button>
       <transition name="fade">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>`,
+    data: () => ({
+      show: true
+    })
+  }))
+  .add("fade in slide out", () => ({
+    components: { SfButton },
+    template: `
+    <div>
+      <sf-button v-on:click="show = !show">
+        Fade in slide out
+      </sf-button>
+      <transition name="fade-slide">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>`,
+    data: () => ({
+      show: true
+    })
+  }))
+  .add("fade in slide y out", () => ({
+    components: { SfButton },
+    template: `
+    <div>
+      <sf-button v-on:click="show = !show">
+        Fade in slide y out
+      </sf-button>
+      <transition name="fade-slide-y">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>`,
+    data: () => ({
+      show: true
+    })
+  }))
+  .add("slide in-out", () => ({
+    components: { SfButton },
+    template: `
+    <div>
+      <sf-button v-on:click="show = !show">
+        Slide in-out
+      </sf-button>
+      <transition name="slide">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>`,
+    data: () => ({
+      show: true
+    })
+  }))
+  .add("slide y in-out", () => ({
+    components: { SfButton },
+    template: `
+    <div>
+      <sf-button v-on:click="show = !show">
+        Slide y in-out
+      </sf-button>
+      <transition name="slide-y">
         <p v-if="show">hello</p>
       </transition>
     </div>`,
