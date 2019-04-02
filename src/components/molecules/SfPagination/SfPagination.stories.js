3c7f2c59b9508e1268fc477f2d87aaf55a28253a
Leonardo Matos <leomp120894@gmail.com>
2019-04-02 15:00:07 -0300

    feat: stories for sfPagination component

--- src/components/molecules/SfPagination/SfPagination.stories.js
+++ src/components/molecules/SfPagination/SfPagination.stories.js
@@ -4,14 +4,39 @@ import { linkTo } from "@storybook/addon-links";
 
 import SfPagination from "./SfPagination.vue";
 
+const pagination = {
+  page: 2,
+  limit: 10,
+  totalSize: 120
+};
+
+const vm = {
+  components: { SfPagination },
+  data: () => pagination,
+  methods: {
+    setCurrentPage(page) {
+      this.page = page;
+    }
+  }
+};
+
 export default storiesOf("Pagination", module)
-  .add("pagination", () => ({
-    components: { SfPagination },
+  .add("default", () => ({
+    ...vm,
     template: `
-    <sf-pagination
-      :page="2"
-      :limit="10"
-      :totalSize="50"
-    />`,
-    methods: {}
+    <sf-pagination @update:page="(page) => { setCurrentPage(page) }"
+      :page="page"
+      :limit="limit"
+      :totalSize="totalSize"/>`
+  }))
+  .add("text for next and prev", () => ({
+    ...vm,
+    template: `
+    <sf-pagination @update:page="(page) => { setCurrentPage(page) }"
+      :page="page"
+      :limit="limit"
+      :totalSize="totalSize">
+      <template slot="prev">prev</template>
+      <template slot="next">next</template>
+    </sf-pagination>`
   }));
