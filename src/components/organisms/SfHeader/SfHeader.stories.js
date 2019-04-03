b7928e42389543504b1edb9b5f1b3b058c2daea9
Anna <amusial@divante.pl>
2019-04-03 14:16:24 +0200

    Adjust SfCallToAction storybook

--- src/components/organisms/SfHeader/SfHeader.stories.js
+++ src/components/organisms/SfHeader/SfHeader.stories.js
@@ -1,6 +1,5 @@
 import { storiesOf } from "@storybook/vue";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
+import { withKnobs, text, select } from "@storybook/addon-knobs";
 
 import SfHeader from "./SfHeader.vue";
 
