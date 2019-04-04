eacfc19bb59a1d2803a44060a2e6b5ec87b2f456
Anna Musial <ania.musial13@gmail.com>
2019-04-04 14:22:00 +0200

    49 adjust storybook stories (#50)
    * Adjust SfCallToAction storybook
    
    * Removed unused object
    
    * Removed knobs import from Header

--- src/components/organisms/SfHeader/SfHeader.stories.js
+++ src/components/organisms/SfHeader/SfHeader.stories.js
@@ -1,7 +1,4 @@
 import { storiesOf } from "@storybook/vue";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-
 import SfHeader from "./SfHeader.vue";
 
 export default storiesOf("Header", module)
