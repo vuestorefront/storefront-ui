12b0d9b27629ab62212efaaf8933a6622aa4c45d
martaradziszewska <martaradziszewskaa@gmail.com>
2019-04-04 19:34:30 +0200

    added test to check slot

--- src/components/atoms/SfCircleButton/SfCircleButton.spec.ts
+++ src/components/atoms/SfCircleButton/SfCircleButton.spec.ts
@@ -9,20 +9,31 @@ describe.only("SfCircleButton.vue", () => {
   });
 
   it("renders default slot with image", () => {
-    const component = shallowMount(SfCircleButton);
-    expect(component.contains('img')).toBe(true);
+    const component = shallowMount(SfCircleButton, {
+    slots: {
+      default: '<img class="sf-circle-button__icon" src="/assets/add_to_cart.svg" alt="icon" />'
+    }
+  });
+    expect(component.contains('.sf-circle-button__icon')).toBe(true);
   });
 
-  it("renders icon and alt props when passed", () => {
-    const msg = 'IamAltText';
+  it("renders icon props when passed", () => {
     const iconSrc = '../../../assets/add_to_cart.svg';
     const component = shallowMount(SfCircleButton, {
       propsData: {
-        icon: iconSrc,
+        icon: iconSrc
+      }
+    });
+    expect(component.find("img").attributes('src')).toEqual(iconSrc);
+  });
+
+  it("renders alt props when passed", () => {
+    const msg = 'IamAltText';
+    const component = shallowMount(SfCircleButton, {
+      propsData: {
         alt: msg
       }
     });
     expect(component.find("img").attributes('alt')).toEqual(msg);
-    expect(component.find("img").attributes('src')).toEqual(iconSrc);
   })
 });
