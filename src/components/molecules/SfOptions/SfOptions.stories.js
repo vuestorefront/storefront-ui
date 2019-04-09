ac687b23fbfaecdde8a331ad62b6bdda4a753306
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-04-09 13:38:37 +0700

    Remove headingId from options in stories

--- src/components/molecules/SfOptions/SfOptions.stories.js
+++ src/components/molecules/SfOptions/SfOptions.stories.js
@@ -12,31 +12,26 @@ storiesOf("Options", module).add("with text", () => ({
     return {
       options: [
         {
-          headingId: "option-label-size-1",
           id: "opt-size-xs",
           text: "XS",
           value: "XS"
         },
         {
-          headingId: "option-label-size-1",
           id: "opt-size-s",
           text: "S",
           value: "S"
         },
         {
-          headingId: "option-label-size-1",
           id: "opt-size-m",
           text: "M",
           value: "M"
         },
         {
-          headingId: "option-label-size-1",
           id: "opt-size-l",
           text: "L",
           value: "L"
         },
         {
-          headingId: "option-label-size-1",
           id: "opt-size-xl",
           text: "XL",
           value: "XL"
