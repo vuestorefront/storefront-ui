806c8b4ba9131de06dd688e8850fab6a96cf5d0d
Leonardo Matos <leomp120894@gmail.com>
2019-04-17 20:45:12 -0300

    style(input-number): js linter

--- src/components/molecules/SfInputNumber/SfInputNumber.js
+++ src/components/molecules/SfInputNumber/SfInputNumber.js
@@ -23,125 +23,131 @@ export default {
     },
     delimiter: {
       type: String,
-      default: '.'
+      default: "."
     },
     thousands: {
       type: String,
-      default: ','
+      default: ","
     },
     name: {
       type: String
     }
   },
 
-  data () {
+  data() {
     return {
       currentValue: this.parseNumber(this.value)
-    }
+    };
   },
 
   computed: {
     stringValue: {
       get() {
-        let str
+        let str;
         if (this.precision) {
-          str = this.currentValue.toFixed(this.precision)
-          if (this.delimiter !== '.') {
-            str = str.replace('.', this.delimiter)
+          str = this.currentValue.toFixed(this.precision);
+          if (this.delimiter !== ".") {
+            str = str.replace(".", this.delimiter);
           }
         } else {
-          str = parseInt(this.currentValue, 10).toString()
+          str = parseInt(this.currentValue, 10).toString();
         }
-        return str.replace(/\B(?=(\d{3})+(?!\d))/g, this.thousands)
+        return str.replace(/\B(?=(\d{3})+(?!\d))/g, this.thousands);
       },
       set(value) {
-        let num = this.parseNumber(value)
+        let num = this.parseNumber(value);
         if (isNaN(num)) {
-          num = 0
+          num = 0;
         }
         // force current number value to sync with input value
-        this.currentValue = num
+        this.currentValue = num;
         // check if number is valid
-        this.setCurrentValue(num)
+        this.setCurrentValue(num);
       }
     }
   },
 
   methods: {
-    parseNumber (value) {
+    parseNumber(value) {
       switch (typeof value) {
-        case 'string':
+        case "string":
           // remove thousands separator first
-          value = value.replace(new RegExp(this.thousands, 'g'), '')
+          value = value.replace(new RegExp(this.thousands, "g"), "");
           if (this.precision) {
-            let parts = value.split(this.delimiter, 2)
+            let parts = value.split(this.delimiter, 2);
             if (parts.length === 1) {
               // no decimals
-              return parseInt(value)
+              return parseInt(value);
             }
             // fix up to maximum precision on decimal places
-            let decimalPlaces = parts[1].length
+            let decimalPlaces = parts[1].length;
             if (decimalPlaces > this.precision) {
               // user may have added digit at the end of input
-              let digits = parts[0] + parts[1]
-              value = digits.slice(0, -this.precision) + '.' + digits.slice(-this.precision)
-            } else if (this.delimiter !== '.') {
+              let digits = parts[0] + parts[1];
+              value =
+                digits.slice(0, -this.precision) +
+                "." +
+                digits.slice(-this.precision);
+            } else if (this.delimiter !== ".") {
               // just fix the delimiter before parsing to float
-              value = value.replace(this.delimiter, '.')
+              value = value.replace(this.delimiter, ".");
             }
-            return parseFloat(value)
+            return parseFloat(value);
           }
           // no decimal precision: integer value
-          return parseInt(value, 10)
-        case 'number':
+          return parseInt(value, 10);
+        case "number":
           if (!isNaN(value)) {
-            return value
+            return value;
           }
       }
-      return 1
+      return 1;
     },
 
-    keypress (e) {
+    keypress(e) {
       if (e && e.key) {
         switch (e.key) {
-          case '0':
-          case '1':
-          case '2':
-          case '3':
-          case '4':
-          case '5':
-          case '6':
-          case '7':
-          case '8':
-          case '9':
-          case 'Enter':
-            return true
+          case "0":
+          case "1":
+          case "2":
+          case "3":
+          case "4":
+          case "5":
+          case "6":
+          case "7":
+          case "8":
+          case "9":
+          case "Enter":
+            return true;
           case this.delimiter:
             if (this.precision) {
-              return true
+              return true;
             }
         }
-        e.preventDefault()
+        e.preventDefault();
       }
     },
 
-    setCurrentValue (num) {
-      if (num >= this.min && (typeof this.max !== 'number' || num <= this.max)) {
+    setCurrentValue(num) {
+      if (
+        num >= this.min &&
+        (typeof this.max !== "number" || num <= this.max)
+      ) {
         // valid number
-        this.currentValue = num
-        this.$emit("change", num)
+        this.currentValue = num;
+        this.$emit("change", num);
       } else {
         // reset to default value
-        this.currentValue = this.parseNumber(this.value)
+        this.currentValue = this.parseNumber(this.value);
       }
     },
 
-    increase () {
-      this.setCurrentValue(this.currentValue + this.step)
+    increase() {
+      this.setCurrentValue(this.currentValue + this.step);
     },
 
-    decrease () {
-      this.setCurrentValue(this.currentValue - this.step)
+    decrease() {
+      this.setCurrentValue(this.currentValue - this.step);
     }
   }
 };
--- src/components/molecules/SfInputNumber/SfInputNumber.stories.js
+++ src/components/molecules/SfInputNumber/SfInputNumber.stories.js
@@ -1,50 +1,50 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, text, number, select } from "@storybook/addon-knobs";
-import notes from "./README.md"
+import notes from "./README.md";
 import SfInputNumber from "./SfInputNumber.vue";
 
 storiesOf("Molecules|InputNumber", module)
-   .addDecorator(withKnobs)
-   .add(
-     "Props / CSS Modifiers",
-     () => ({
-       props: {
-         value: {
-           default: number("value (prop)", 1)
-         },
-         min: {
-           default: number("min (prop)", 1)
-         },
-         max: {
-           default: number("max (prop)")
-         },
-         step: {
-           default: number("step (prop)", 1)
-         },
-         precision: {
-           default: number("precision (prop)", 0)
-         },
-         delimiter: {
-           default: text("delimiter (prop)", ".")
-         },
-         thousands: {
-           default: text("delimiter (prop)", ",")
-         },
-         name: {
-           default: text("name (prop)")
-         },
-         customClass: {
-           default: select(
-             "CSS Modifier",
-             ["null", "sf-input-number--large"],
-             "null",
-             "CSS-Modifiers"
-           )
-         }
-       },
-       components: { SfInputNumber },
-       template: `<SfInputNumber
+  .addDecorator(withKnobs)
+  .add(
+    "Props / CSS Modifiers",
+    () => ({
+      props: {
+        value: {
+          default: number("value (prop)", 1)
+        },
+        min: {
+          default: number("min (prop)", 1)
+        },
+        max: {
+          default: number("max (prop)")
+        },
+        step: {
+          default: number("step (prop)", 1)
+        },
+        precision: {
+          default: number("precision (prop)", 0)
+        },
+        delimiter: {
+          default: text("delimiter (prop)", ".")
+        },
+        thousands: {
+          default: text("delimiter (prop)", ",")
+        },
+        name: {
+          default: text("name (prop)")
+        },
+        customClass: {
+          default: select(
+            "CSS Modifier",
+            ["null", "sf-input-number--large"],
+            "null",
+            "CSS-Modifiers"
+          )
+        }
+      },
+      components: { SfInputNumber },
+      template: `<SfInputNumber
          :class="customClass"
          v-model="value"
          :min="min"
@@ -54,37 +54,37 @@ storiesOf("Molecules|InputNumber", module)
          :delimiter="delimiter"
          :thousands="thousands"
          :name="name" />`
-     }),
-     {
-       info: true,
-       notes
-     }
-   )
-   .add(
-     "[slot] up",
-     () => ({
-       components: { SfInputNumber },
-       template: `
+    }),
+    {
+      info: true,
+      notes
+    }
+  )
+  .add(
+    "[slot] up",
+    () => ({
+      components: { SfInputNumber },
+      template: `
        <SfInputNumber value="1">
          <template #up>+</template>
        </SfInputNumber>`
-     }),
-     {
-       info: true,
-       notes
-     }
-   )
-   .add(
-     "[slot] down",
-     () => ({
-       components: { SfInputNumber },
-       template: `
+    }),
+    {
+      info: true,
+      notes
+    }
+  )
+  .add(
+    "[slot] down",
+    () => ({
+      components: { SfInputNumber },
+      template: `
        <SfInputNumber value="1">
          <template #down>-</template>
        </SfInputNumber>`
-     }),
-     {
-       info: true,
-       notes
-     }
-   );
+    }),
+    {
+      info: true,
+      notes
+    }
+  );
