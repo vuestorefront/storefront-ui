42bb5c3ee3a7afc3288cd18b542614b05d808f92
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-04-20 00:28:47 +0700

    Add label slot and update add text option storybook

--- src/components/molecules/SfOptions/SfOptions.stories.js
+++ src/components/molecules/SfOptions/SfOptions.stories.js
@@ -2,41 +2,97 @@
 import { storiesOf } from "@storybook/vue";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
 import SfOptions from "./SfOptions.vue";
 
-storiesOf("Options", module).add("with text", () => ({
-  components: { SfOptions },
-  template: '<sf-options :options="options" />',
-  data() {
-    return {
-      options: [
-        {
-          id: "opt-size-xs",
-          text: "XS",
-          value: "XS"
+storiesOf("Options", module)
+  .addDecorator(withKnobs)
+  .add(
+    "Props text",
+    () => ({
+      components: { SfOptions },
+      props: {
+        label: {
+          default: text("Label", "Size")
         },
-        {
-          id: "opt-size-s",
-          text: "S",
-          value: "S"
-        },
-        {
-          id: "opt-size-m",
-          text: "M",
-          value: "M"
-        },
-        {
-          id: "opt-size-l",
-          text: "L",
-          value: "L"
-        },
-        {
-          id: "opt-size-xl",
-          text: "XL",
-          value: "XL"
+        options: {
+          default: [
+            {
+              text: "XS",
+              value: "XS"
+            },
+            {
+              text: "S",
+              value: "S"
+            },
+            {
+              text: "M",
+              value: "M"
+            },
+            {
+              text: "L",
+              value: "L"
+            },
+            {
+              text: "XL",
+              value: "XL"
+            }
+          ]
         }
-      ]
-    };
-  }
-}));
+      },
+      template:
+        '<sf-options :label="label" v-model="selected" :options="options" />',
+      data() {
+        return {
+          selected: "XS"
+        };
+      }
+    }),
+    {
+      info: true
+    }
+  )
+  .add(
+    "Slots text",
+    () => ({
+      components: { SfOptions },
+      template: `
+      <sf-options
+        v-model="selected"
+        :options="[
+          {
+            text: 'XS',
+            value: 'XS'
+          },
+          {
+            text: 'S',
+            value: 'S'
+          },
+          {
+            text: 'M',
+            value: 'M'
+          },
+          {
+            text: 'L',
+            value: 'L'
+          },
+          {
+            text: 'XL',
+            value: 'XL'
+          }
+        ]"
+      >
+        <template #label><h1>Size</h1></template>
+        <template #text="{ text }"><h2>{{ text }}</h2></template>
+      </sf-options>`,
+      data() {
+        return {
+          selected: "XS"
+        };
+      }
+    }),
+    {
+      info: true
+    }
+  );
