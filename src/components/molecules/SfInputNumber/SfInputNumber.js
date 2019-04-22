f4bfb2f9c51b4e2b2ccfc1a4c333fbb68b103dbf
Leonardo Matos <leomp120894@gmail.com>
2019-04-22 12:19:42 -0300

    feat(input-number): more props to handle generic input number

--- src/components/molecules/SfInputNumber/SfInputNumber.js
+++ src/components/molecules/SfInputNumber/SfInputNumber.js
@@ -7,20 +7,24 @@ export default {
       default: 1
     },
     value: {
-      type: Number,
-      default: 1
+      type: Number
+    },
+    // whether the input should be auto filled if left empty
+    autoFill: {
+      type: Boolean
     },
     min: {
-      type: Number,
-      default: 1
+      type: Number
     },
     max: {
       type: Number
     },
+    // decimal precision
     precision: {
       type: Number,
       default: 0
     },
+    // number format delimiters
     delimiter: {
       type: String,
       default: "."
@@ -29,40 +33,74 @@ export default {
       type: String,
       default: ","
     },
+    // whether to enable the control buttons
+    controls: {
+      type: Boolean,
+      default: true
+    },
+    // native input attributes
+    type: {
+      type: String,
+      default: "tel"
+    },
     name: {
       type: String
+    },
+    placeholder: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    },
+    required: {
+      type: Boolean
     }
   },
 
   data() {
     return {
-      currentValue: this.parseNumber(this.value)
+      currentValue: (this.value || this.autoFill)
+        ? this.parseNumber(this.value)
+        : null
     };
   },
 
   computed: {
     stringValue: {
       get() {
-        let str;
-        if (this.precision) {
-          str = this.currentValue.toFixed(this.precision);
-          if (this.delimiter !== ".") {
-            str = str.replace(".", this.delimiter);
+        if (typeof this.currentValue === 'number') {
+          let str;
+          if (this.precision) {
+            str = this.currentValue.toFixed(this.precision);
+            if (this.delimiter !== ".") {
+              str = str.replace(".", this.delimiter);
+            }
+          } else {
+            str = parseInt(this.currentValue, 10).toString();
           }
-        } else {
-          str = parseInt(this.currentValue, 10).toString();
+          if (typeof this.thousands === "string" && this.thousands !== "") {
+            // add thousands delimiter
+            return str.replace(/\B(?=(\d{3})+(?!\d))/g, this.thousands);
+          } else {
+            return str;
+          }
         }
-        return str.replace(/\B(?=(\d{3})+(?!\d))/g, this.thousands);
+        return this.currentValue;
       },
       set(value) {
-        let num = this.parseNumber(value);
-        if (isNaN(num)) {
-          num = 0;
+        if ((value !== null && value !== '') || this.autoFill) {
+          let num = this.parseNumber(value);
+          if (isNaN(num)) {
+            num = 0;
+          }
+          // force current number value to sync with input value
+          this.currentValue = num;
+          // check if number is valid
+          this.setCurrentValue(num);
+        } else {
+          // sey empty
+          this.setCurrentValue(null);
         }
-        // force current number value to sync with input value
-        this.currentValue = num;
-        // check if number is valid
-        this.setCurrentValue(num);
       }
     }
   },
@@ -71,8 +109,10 @@ export default {
     parseNumber(value) {
       switch (typeof value) {
         case "string":
-          // remove thousands separator first
-          value = value.replace(new RegExp(this.thousands, "g"), "");
+          if (typeof this.thousands === "string" && this.thousands !== "") {
+            // remove thousands separator first
+            value = value.replace(new RegExp(this.thousands, "g"), "");
+          }
           if (this.precision) {
             let parts = value.split(this.delimiter, 2);
             if (parts.length === 1) {
@@ -101,7 +141,7 @@ export default {
             return value;
           }
       }
-      return 1;
+      return this.min || 0;
     },
 
     keypress(e) {
@@ -128,16 +168,17 @@ export default {
       }
     },
 
-    setCurrentValue(num) {
-      if (num < this.min) {
-        this.currentValue = this.min;
-      } else if (typeof this.max === "number" && num > this.max) {
-        this.currentValue = this.max;
-      } else {
-        // valid number
-        this.currentValue = num;
-        this.$emit("change", num);
+    setCurrentValue(val) {
+      if (typeof val === 'number') {
+        // check minimum and maximum
+        if (typeof this.min === "number" && val < this.min) {
+          val = this.min;
+        } else if (typeof this.max === "number" && val > this.max) {
+          val = this.max;
+        }
       }
+      this.currentValue = val;
+      this.$emit("change", val);
     },
 
     increase() {
@@ -146,6 +187,15 @@ export default {
 
     decrease() {
       this.setCurrentValue(this.currentValue - this.step);
+    }
+  },
+
+  watch: {
+    value(val) {
+      if (val !== this.currentValue) {
+        // value prop externally changed
+        this.setCurrentValue(this.value);
+      }
     }
   }
 };
