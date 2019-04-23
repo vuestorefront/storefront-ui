a62cde80f87ce3abbe019124691988a7a4486b78
Anna <amusial@divante.pl>
2019-04-23 12:16:12 +0200

    Added tests for props

--- src/components/atoms/SfPrice/SfPrice.spec.ts
+++ src/components/atoms/SfPrice/SfPrice.spec.ts
@@ -5,6 +5,28 @@ describe("SfPrice.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfPrice);
     expect(component.contains(".sf-price")).toBe(true);
+  })
+  it("renders regular and special price props passed as String", () => {
+    const regPrice = "$3,99"
+    const spPrice = "$3,59"
+    const component = shallowMount(SfPrice, {
+      propsData: {
+        regularPrice: regPrice,
+        specialPrice: spPrice
+      }
+    });
+    expect(component.contains(".sf-price__old") && component.contains(".sf-price__special")).toBe(true);
+  })
+  it("renders regular and special price props passed as Number", () => {
+    const regPrice = 3.99
+    const spPrice = 3.59
+    const component = shallowMount(SfPrice, {
+      propsData: {
+        regularPrice: regPrice,
+        specialPrice: spPrice
+      }
+    });
+    expect(component.contains(".sf-price__old") && component.contains(".sf-price__special")).toBe(true);
   });
 
   // Default slot check, you can replace `default` with any other
