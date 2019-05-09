3e0a556a8057ef83dcdf15f00cc2222af48cc592
Leonardo Matos <leomp120894@gmail.com>
2019-05-09 14:02:15 -0300

    chore: edit vuepress sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,11 +11,12 @@ module.exports = {
       { text: 'GitHub', link: 'https://github.com/DivanteLtd/storefront-ui' }
     ],
     sidebar: [
+      ['/introduction', 'Introduction'],
       ['/customization', 'Customization'],
       ['/component-rules', 'Component Rules'],
       ['/creating-new-component', 'Creating New Component'],
-      ['/working-with-alpaca', 'Working With Alpaca'],
-      ['/working-with-designs', 'Working With Designs']
+      ['/working-with-designs', 'Working With Designs'],
+      ['/working-with-alpaca', 'Working With Alpaca']
     ]
   }
 }
