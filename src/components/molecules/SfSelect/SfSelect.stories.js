1a4f05b762410cf33f7b4a29315ee2bee8741b36
Przemyslaw Spaczek <pspaczek@divante.pl>
2019-05-10 14:36:31 +0200

    Update summary on storybook

--- src/components/molecules/SfSelect/SfSelect.stories.js
+++ src/components/molecules/SfSelect/SfSelect.stories.js
@@ -26,31 +26,20 @@ storiesOf("Molecules|[WIP] Select", module)
       `
     }),
     {
-      info: true,
-      notes
-    }
-  )
-  .add(
-    "Colors",
-    () => ({
-      components: { SfSelect },
-      data() {
-        return {
-          selected: "red"
-        };
+      info: {
+        summary: `
+        <h2> Description </h2>
+        <p>This component should be used for selecting an option. Use \`<SfSelect>\` as a wrapper for individual filters (\`<SfSelectOption>\`). Value passed to \`v-model\` holds an string of value properties from active option.</p>
+        <p> \`<SfSelectItem>\` has following properties:</p>
+        <ul>
+          <li> \`label\` - label that will be displayed in a component</li>
+          <li> \`value\` - unique identifier</li>
+          <li> \`color\` - (optional) color that will be displayed in a box on the left side (all valid CSS colors can be passed)</li>
+        </ul>
+        <h2> Usage </h2>
+        <pre><code>import SfSelect from "@storefrontui/vue/dist/SfSelect.vue"</code></pre>
+        `
       },
-      template: `
-        <div style="width: 300px;">
-          <SfSelect v-model="selected">
-            <SfSelectOption value="red" label="Red" color="red"></SfSelectOption>
-            <SfSelectOption value="blue" label="Blue" color="blue"></SfSelectOption>
-            <SfSelectOption value="pink" label="Pink" color="pink"></SfSelectOption>
-          </SfSelect>
-        </div>
-      `
-    }),
-    {
-      info: true,
       notes
     }
   );
