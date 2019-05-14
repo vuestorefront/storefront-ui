16d347286e561975fc2ca76c46b48a864d7b8f63
Leonardo Matos <leomp120894@gmail.com>
2019-05-14 03:13:58 -0300

    feat(gallery): bind image index on thumb slot

--- src/components/molecules/SfGallery/SfGallery.stories.js
+++ src/components/molecules/SfGallery/SfGallery.stories.js
@@ -1,62 +1,94 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-// import { action } from '@storybook/addon-actions'
-// import { linkTo } from '@storybook/addon-links'
-
+import {
+  withKnobs,
+  text,
+  number
+} from "@storybook/addon-knobs";
+import notes from "./README.md";
 import SfGallery from "./SfGallery.vue";
 
-const vm = {
-  components: { SfGallery },
-  data: () => {
-    return {
-      images: [
-        {
-          zoom: {
-            url:
-              "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-            alt: "Macbook PRO Apple"
-          },
-          small: {
-            url:
-              "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-            alt: "Macbook PRO Apple"
-          },
-          big: {
-            url:
-              "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/700px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-            alt: "Macbook PRO Apple"
-          }
+const data = () => {
+  return {
+    images: [
+      {
+        zoom: {
+          url:
+            "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+          alt: "Macbook PRO Apple"
         },
-        {
-          zoom: {
-            url:
-              "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858951531-teclado-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-            alt: "Macbook PRO Apple (keyboard)"
-          },
-          small: {
-            url:
-              "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858951531-teclado-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-            alt: "Macbook PRO Apple (keyboard)"
-          },
-          big: {
-            url:
-              "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/700px/@1550858951531-teclado-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-            alt: "Macbook PRO Apple (keyboard)"
-          }
+        small: {
+          url:
+            "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+          alt: "Macbook PRO Apple"
+        },
+        big: {
+          url:
+            "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/700px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+          alt: "Macbook PRO Apple"
         }
-      ]
-    };
-  },
-  methods: {}
+      },
+      {
+        zoom: {
+          url:
+            "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858951531-teclado-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+          alt: "Macbook PRO Apple (keyboard)"
+        },
+        small: {
+          url:
+            "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858951531-teclado-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+          alt: "Macbook PRO Apple (keyboard)"
+        },
+        big: {
+          url:
+            "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/700px/@1550858951531-teclado-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+          alt: "Macbook PRO Apple (keyboard)"
+        }
+      }
+    ]
+  };
 };
 
-export default storiesOf("Gallery", module).add(
-  "Props",
-  () => ({
-    ...vm,
-    template: `
-    <sf-gallery :images='images' />`
-  }),
-  {
-    info: true
-  }
-);
+storiesOf("Molecules|Gallery", module)
+  .addDecorator(withKnobs)
+  .add(
+    "Props / CSS Modifiers",
+    () => ({
+      props: {
+        autoplay: {
+          default: number("autoplay (prop)")
+        },
+        current: {
+          default: number("current (prop)", 1)
+        }
+      },
+      data,
+      components: { SfGallery },
+      template: `<SfGallery
+        :autoplay="autoplay"
+        :current.sync="current"
+        :images="images" />`
+    }),
+    {
+      info: true,
+      notes
+    }
+  )
+  .add(
+    "[slot] thumb",
+    () => ({
+      data,
+      components: { SfGallery },
+      template: `
+       <SfGallery :images="images">
+         <template #thumb="{ image, index }">
+           #{{ index }}
+           <img class="sf-gallery__thumb" :src="image.small.url" />
+         </template>
+       </SfGallery>`
+    }),
+    {
+      info: true,
+      notes
+    }
+  );
