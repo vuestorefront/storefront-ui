9b0842c67ea2096d709c115f05d2afa15800f5b3
Przemyslaw Spaczek <pspaczek@divante.pl>
2019-05-14 11:54:55 +0200

    style update

--- src/components/molecules/SfSelect/SfSelect.stories.js
+++ src/components/molecules/SfSelect/SfSelect.stories.js
@@ -23,16 +23,22 @@ storiesOf("Molecules|[WIP] Select", module)
         <div style="width: 300px">
           <SfSelect v-model="selected">
             <SfSelectOption value="amaranth" label="Amaranth pink" color="#F19CBB">
+              <div style="background-color:#F19CBB; width: 25px; height: 25px;"></div>
             </SfSelectOption>
             <SfSelectOption value="amber" label="Amber" color="#FFBF00">
+              <div style="background-color:#FFBF00; width: 25px; height: 25px;"></div>
             </SfSelectOption>
             <SfSelectOption value="arctiv" label="Arctic lime" color="#D0FF14">
+              <div style="background-color:#D0FF14; width: 25px; height: 25px;"></div>
             </SfSelectOption>
             <SfSelectOption value="bluetiful" label="Bluetiful" color="#3C69E7">
+              <div style="background-color:#3C69E7; width: 25px; height: 25px;"></div>
             </SfSelectOption>
             <SfSelectOption value="brilliant-rose" label="Brilliant rose" color="#FF55A3">
+              <div style="background-color:#FF55A3; width: 25px; height: 25px;"></div>
             </SfSelectOption>
             <SfSelectOption value="buff" label="Buff" color="#F0DC82">
+              <div style="background-color:#F0DC82; width: 25px; height: 25px;"></div>
             </SfSelectOption>
           </SfSelect>
         </div>
