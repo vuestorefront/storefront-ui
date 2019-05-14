39b229b07cad7f09f7cdd868e3cebccbb86fbffc
Leonardo Matos <leomp120894@gmail.com>
2019-05-14 02:39:50 -0300

    feat(gallery): handle slider with swipe and transitions with Glide.js

--- src/components/molecules/SfGallery/SfGallery.js
+++ src/components/molecules/SfGallery/SfGallery.js
@@ -1,3 +1,5 @@
+import Glide from '@glidejs/glide'
+
 export default {
   props: {
     images: {
@@ -12,22 +14,37 @@ export default {
 
   data() {
     return {
+      glide: null,
       activeIndex: this.current - 1
     };
-  },
-
-  computed: {
-    currentImage: {
-      get() {
-        return this.images[this.activeIndex];
-      }
-    }
   },
 
   methods: {
     setCurrentImage(index) {
       this.activeIndex = index;
       this.$emit("update:current", index + 1);
+      if (this.glide) {
+        this.glide.go('=' + index)
+      }
+    }
+  },
+
+  mounted () {
+    // handle slider with swipe and transitions with Glide.js
+    // https://glidejs.com/docs/
+    const glide = new Glide(this.$refs.glide, {
+      startAt: this.activeIndex
+    })
+    glide.on('run', () => {
+      this.setCurrentImage(glide.index)
+    })
+    glide.mount()
+    this.glide = glide
+  },
+
+  beforeDestroy () {
+    if (this.glide) {
+      this.glide.destroy()
     }
   }
 };
