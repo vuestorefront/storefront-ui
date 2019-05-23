d59b5e2b34cddfa9d78de32ba340cf88a50bfb6e
przemyslawspaczek <pspaczek@divante.com>
2019-05-23 12:51:37 +0200

    chore import SfProductOption to Select story

--- src/components/molecules/SfSelect/SfSelect.stories.js
+++ src/components/molecules/SfSelect/SfSelect.stories.js
@@ -4,6 +4,7 @@ import { withKnobs, text, select } from "@storybook/addon-knobs";
 import { withA11y } from "@storybook/addon-a11y";
 import notes from "./SfSelect.md";
 import SfSelect from "./SfSelect.vue";
+import SfProductOption from "../SfProductOption/SfProductOption.vue"
 
 storiesOf("Molecules|[WIP] Select", module)
   .addDecorator(withKnobs)
@@ -16,18 +17,30 @@ storiesOf("Molecules|[WIP] Select", module)
           selected: "amber"
         };
       },
-      components: { SfSelect },
+      components: { SfSelect, SfProductOption },
       template: `
       <div>
         <p><b>Selected: {{selected}}</b></p>
         <div style="width: 300px">
           <SfSelect v-model="selected">
-            <SfSelectOption value="amaranth">amaranth</SfSelectOption>
-            <SfSelectOption value="amber">amber</SfSelectOption>
-            <SfSelectOption value="arctiv">arctiv</SfSelectOption>
-            <SfSelectOption value="bluetiful">bluetiful</SfSelectOption>
-            <SfSelectOption value="brilliant-rose">brilliant-rose</SfSelectOption>
-            <SfSelectOption value="buff">buff</SfSelectOption>
+            <SfSelectOption value="amaranth">
+              <SfProductOption color="#E52B50" label="Amaranth"/>
+            </SfSelectOption>
+            <SfSelectOption value="amber">
+              <SfProductOption color="#FFBF00" label="Amber"/>
+            </SfSelectOption>
+            <SfSelectOption value="arctic-lime">
+              <SfProductOption color="#D0FF14" label="Arctic lime"/>
+            </SfSelectOption>
+            <SfSelectOption value="bluetiful">
+              <SfProductOption color="#3C69E7" label="Bluetiful"/>
+            </SfSelectOption>
+            <SfSelectOption value="brilliant-rose">
+              <SfProductOption color="#FF55A3" label="Brilliant rose"/>
+            </SfSelectOption>
+            <SfSelectOption value="buff">
+              <SfProductOption color="#F0DC82" label="Buff"/>
+            </SfSelectOption>
           </SfSelect>
         </div>
       </div>
