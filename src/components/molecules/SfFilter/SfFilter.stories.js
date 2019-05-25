63148ab13abf04aa20c936197af241a385629f2c
Filip Rakowski <frakowski@divante.pl>
2019-05-25 12:43:24 +0200

    filter stories update

--- src/components/molecules/SfFilter/SfFilter.stories.js
+++ src/components/molecules/SfFilter/SfFilter.stories.js
@@ -70,13 +70,13 @@ export default storiesOf("Molecules|Filter", module)
       info: {
         summary: `
           <h2> Description </h2>
-          <p>This component should be used for filtering products on listings. Use \`<SfFilter>\` as a wrapper for individual filters (\`<SfFilterItem>\`). Value passed to \`v-model\` holds an array (or string if multiple prop is set to false) of value properties from active filters.</p>
+          <p>This component should be used for filtering products on listings. Use <SfFilter> as a wrapper for individual filters (<SfFilterItem>). Value passed to v-model holds an array (or string if multiple prop is set to false) of value properties from active filters.</p>
           <p> \`<SfFilterItem>\` has following properties:</p>
           <ul>
-            <li> \`label\` - label that will be displayed in a component</li>
-            <li> \`value\` - unique identifier</li>
-            <li> \`count\` - (optional) number of items matching given filter</li>
-            <li> \`color\` - (optional) color that will be displayed in a box on the left side (all valid CSS colors can be passed)</li>
+            <li> label - label that will be displayed in a component</li>
+            <li> value - unique identifier</li>
+            <li> count - (optional) number of items matching given filter</li>
+            <li> color - (optional) color that will be displayed in a box on the left side (all valid CSS colors can be passed)</li>
           </ul>
           <h2> Usage </h2>
           <pre><code>import SfFilter from "@storefrontui/vue/dist/SfFilter.vue"</code></pre>
