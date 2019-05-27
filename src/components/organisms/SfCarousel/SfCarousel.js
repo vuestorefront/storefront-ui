21d2e734668ecf962f16b56d34417b2ad292ae84
przemyslawspaczek <pspaczek@divante.com>
2019-05-27 13:26:37 +0200

    Created Basict story

--- src/components/organisms/SfCarousel/SfCarousel.js
+++ src/components/organisms/SfCarousel/SfCarousel.js
@@ -1,8 +1,26 @@
 import Vue from "vue";
 import SfCarouselItem from "@/components/organisms/SfCarousel/_internal/SfCarouselItem.vue";
+import Glide, { Controls } from "@glidejs/glide";
 
 Vue.component("SfCarouselItem", SfCarouselItem);
 
 export default {
-  name: "SfCarousel"
+  name: "SfCarousel",
+  data() {
+    return {
+      glide: null
+    };
+  },
+  props: {
+    perPage: {
+      type: Number,
+      default: 1
+    }
+  },
+  mounted: function() {
+    const glide = new Glide(this.$refs.glide, {
+      perView: this.perPage
+    });
+    glide.mount({ Controls });
+  }
 };
--- src/components/organisms/SfCarousel/SfCarousel.stories.js
+++ src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -3,18 +3,31 @@ import { storiesOf } from "@storybook/vue";
 import { withKnobs, text, select } from "@storybook/addon-knobs";
 import notes from "./SfCarousel.md";
 import SfCarousel from "./SfCarousel.vue";
+import SfProductCard from "../../molecules/SfProductCard/SfProductCard.vue";
 
 storiesOf("Organisms|[WIP] Carousel", module)
   .addDecorator(withKnobs)
   .add(
     "Basic",
     () => ({
-      components: { SfCarousel },
-      template: `<SfCarousel>
-        <SfCarosuelItem>
-          Hi!
-        </SfCarosuelItem>
-      </SfCarousel>`
+      components: { SfCarousel, SfProductCard },
+      data() {
+        return {
+          products: [{title: "Product 1"}, {title: "Product 2"}, {title: "Product 3"}, {title: "Product 4"}]
+        };
+      },
+      template: `
+      <div style="width: 500px">
+        <SfCarousel :per-page="2">
+          <SfCarouselItem v-for="product in products">
+            <SfProductCard
+              :title="product.title"
+              :price="{ regularPrice: '10,99 $'}"
+              :rating="{ max: 5, score: 4 }"/>
+          </SfCarouselItem>
+        </SfCarousel>
+      </div>
+      `
     }),
     {
       info: true,
