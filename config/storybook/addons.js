3367898367697221216730d01b77284ef0b54a4b
martaradziszewska <martaradziszewskaa@gmail.com>
2019-05-27 21:18:00 +0200

    install addon backgrounds

--- config/storybook/addons.js
+++ config/storybook/addons.js
@@ -5,3 +5,4 @@ import "@storybook/addon-actions/register";
 import "@storybook/addon-knobs/register";
 import "@storybook/addon-links/register";
 import "@storybook/addon-viewport/register";
+import "@storybook/addon-backgrounds/register";
