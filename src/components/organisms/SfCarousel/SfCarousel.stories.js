facd33a46e358d7e6198dad6b800491a7b9d610f
przemyslawspaczek <pspaczek@divante.com>
2019-05-27 14:36:50 +0200

    Added ProductCard to Basic story

--- src/components/organisms/SfCarousel/SfCarousel.stories.js
+++ src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -13,12 +13,32 @@ storiesOf("Organisms|[WIP] Carousel", module)
       components: { SfCarousel, SfProductCard },
       data() {
         return {
-          products: [{title: "Product 1"}, {title: "Product 2"}, {title: "Product 3"}, {title: "Product 4"}]
+          products: [
+            { title: "Product 1" },
+            { title: "Product 2" },
+            { title: "Product 3" },
+            { title: "Product 4" },
+            { title: "Product 5" },
+            { title: "Product 6" },
+            { title: "Product 7" },
+            { title: "Product 8" },
+            { title: "Product 9" },
+            { title: "Product 10" },
+            { title: "Product 11" },
+            { title: "Product 12" }
+          ]
         };
       },
       template: `
-      <div style="width: 500px">
-        <SfCarousel :per-page="2">
+      <div style="width: 800px">
+        <SfCarousel
+          :per-page="4"
+          :breakpoints="{
+            480: {
+              perView: 2
+            }
+          }"
+          >
           <SfCarouselItem v-for="product in products">
             <SfProductCard
               :title="product.title"
