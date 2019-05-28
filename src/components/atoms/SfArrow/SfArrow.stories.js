30f20e4c483124b19bf8675248cd8207cc6626ab
przemyslawspaczek <pspaczek@divante.com>
2019-05-28 15:38:54 +0200

    Updated story

--- src/components/atoms/SfArrow/SfArrow.stories.js
+++ src/components/atoms/SfArrow/SfArrow.stories.js
@@ -14,14 +14,18 @@ storiesOf("Atoms|[WIP]Arrow", module)
         customClass: {
           default: options(
             "CSS Modifiers",
-            { null: "null", "sf-arrow--right": "sf-arrow--right", "sf-arrow--long": "sf-arrow--long" },
+            {
+              null: "null",
+              "sf-arrow--right": "sf-arrow--right",
+              "sf-arrow--long": "sf-arrow--long"
+            },
             "null",
             { display: "multi-select" }
           )
         }
       },
       template: `
-      <div style="padding: 1rem; background-color: #F1F2F3">
+      <div :style="{padding: '1rem', 'background-color': customClass.indexOf('sf-arrow--long') ? '' : '#F1F2F3'}">
       <SfArrow :class="customClass">
       </div>
       `
