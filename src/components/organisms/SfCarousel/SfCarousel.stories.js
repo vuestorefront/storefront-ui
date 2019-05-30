4d1e754d33b5c6613bc21e356697c63b916c8c7f
przemyslawspaczek <pspaczek@divante.com>
2019-05-30 10:19:09 +0200

    Updated story

--- src/components/organisms/SfCarousel/SfCarousel.stories.js
+++ src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -12,16 +12,46 @@ storiesOf("Organisms|[WIP] Carousel", module)
       components: { SfCarousel },
       data() {
         return {
-          products: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+          style: {
+            color: "#FFF",
+            display: "flex",
+            "justify-content": "center",
+            "font-size": "3rem",
+            "align-items": "center",
+            height: "300px",
+            "background-color": "#5ECE7B"
+          },
+          options: {
+            perView: 4
+          }
         };
       },
       template: `
       <div style="max-width: 1140px">
-        <SfCarousel>
-          <SfCarouselItem v-for="(product, key) in products">
-            <div :style="'color: #FFF; display: flex; justify-content: center; font-size: 72px; align-items: center; height: 300px; background: rgb('+ Math.random() * 255 +',' + Math.random() * 255 +', '+ Math.random() * 255 +')'">
-             {{key + 1}}
-            </div>
+        <SfCarousel :options="options">
+          <SfCarouselItem>
+            <div :style="style">1</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">2</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">3</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">4</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">5</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">6</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">7</div>
+          </SfCarouselItem>
+          <SfCarouselItem>
+            <div :style="style">8</div>
           </SfCarouselItem>
         </SfCarousel>
       </div>
