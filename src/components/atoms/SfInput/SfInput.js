c824f1566b709c55c08bee49495cc3e24ca36b18
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-06-03 20:08:57 +0700

    Bind attributes, listeners and value with add label props & slot

--- src/components/atoms/SfInput/SfInput.js
+++ src/components/atoms/SfInput/SfInput.js
@@ -1,3 +1,21 @@
 export default {
-  name: "SfInput"
+  name: "SfInput",
+  props: {
+    value: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: event => this.$emit("input", event.target.value)
+      };
+    }
+  }
 };
