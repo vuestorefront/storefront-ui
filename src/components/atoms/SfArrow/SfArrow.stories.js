92b2806d32d899a83470e1f3db9e0bd5b66d919b
przspa <pspaczek@divante.co>
2019-06-03 11:12:43 +0200

    Worked on component description

--- src/components/atoms/SfArrow/SfArrow.stories.js
+++ src/components/atoms/SfArrow/SfArrow.stories.js
@@ -1,10 +1,34 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, optionsKnob as options } from "@storybook/addon-knobs";
-import notes from "./SfArrow.md";
+import { generateStorybookTable } from "@/helpers";
 import SfArrow from "./SfArrow.vue";
 
-storiesOf("Atoms|[WIP]Arrow", module)
+const scssTableConfig = {
+  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
+  tableBodyConfig: [
+    ["$arrow__button-color", "$c-light-primary", ""],
+    ["$arrow__button-color--hover", "$c-dark-primary", ""],
+    ["$arrow__button-shadow", "0 5px 12px -6px rgba($c-dark-primary, 0)", ""],
+    [
+      "$arrow__button-shadow--right",
+      "0 -5px 12px -5px rgba($c-dark-primary, 0.6)",
+      ""
+    ],
+    ["$arrow__icon-color", "$c-white", ""],
+    ["$arrow__icon-color--hover", "$c-dark-primary", ""]
+  ]
+};
+
+const cssTableConfig = {
+  tableHeadConfig: ["NAME", "DESCRIPTION"],
+  tableBodyConfig: [
+    [".sf-arrow--long", "sets default arrow to long"],
+    [".sf-arrow--right", "transform default left arrow to right arrow"]
+  ]
+};
+
+storiesOf("Atoms|Arrow", module)
   .addDecorator(withKnobs)
   .add(
     "Basic",
