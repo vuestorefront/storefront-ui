83d1e213777ef5d20b601102223f7f4d7de11be4
Maya Shavin <maya@cloudinary.com>
2019-06-07 19:08:22 +0300

    add color & size css

--- src/components/atoms/SfIcon/README.md
+++ src/components/atoms/SfIcon/README.md
@@ -1,17 +1,26 @@
 # SfIcon
 
-## Events
+Icon Component for icon in general use.
 
-- `event` - description
+## Props
 
+- `path` - icon SVG path
+- `size` - custom size of the icon, it can be our standard sizes, or `12px` or `1.2rem`
+- `color` - custom color of the icon, it can be according to our standard colors, or `#fff` or nothing.
+
+## SCSS variables
+
+- `variablename` (defaultvalue) - description
 
 ## CSS Modifiers
 
 - `classname` - description
 
 
-## SCSS variables
+## Source files and Storybook demo
 
-- `variablename` (defaultvalue) - description
+- [@/src/components/atoms/SfCircleIcon](https://github.com/DivanteLtd/storefront-ui/tree/master/src/components/atoms/SfIcon)
+- [HTML template](https://github.com/DivanteLtd/storefront-ui/tree/master/src/components/atoms/SfIcon/SfIcon.html)
+- link to storybook
         
         
--- src/components/atoms/SfIcon/SfIcon.js
+++ src/components/atoms/SfIcon/SfIcon.js
@@ -1,3 +1,7 @@
+const HEX_REGEX = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+const RGB_REGEX = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
+const SF_SIZES = ["xxs", "xs", "sm", "md", "lg", "xl", "xxl", "xl3", "xl4"];
+
 export default {
   name: "SfIcon",
   props: {
@@ -8,9 +12,46 @@ export default {
       type: String,
       default: ""
     },
+    /**
+     * Icon SVG Size
+     */
     size: {
       type: String,
-      default: "small"
+      default: ""
+    },
+    /**
+     * Icon SVG Fill Color
+     */
+    color: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    isDecimalOrHexColor() {
+      const color = this.color.trim();
+      return RGB_REGEX.test(color) || HEX_REGEX.test(color);
+    },
+    isPresetSizes() {
+      const size = this.size.trim();
+      return SF_SIZES.includes(size);
+    },
+    iconColor() {
+      return !this.isDecimalOrHexColor
+        ? `sf-icon--color-${this.color.trim()}`
+        : this.color;
+    },
+    iconSize() {
+      return this.isPresetSizes ? `sf-icon--size-${this.size.trim()}` : "";
+    }
+  },
+  mounted() {
+    if (this.isDecimalOrHexColor) {
+      this.$refs.icon.style.setProperty("--icon-color", this.color);
+    }
+
+    if (!this.isPresetSizes) {
+      this.$refs.icon.style.setProperty("--icon-size", this.size);
     }
   }
 };
