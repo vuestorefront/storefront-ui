b10b0315767c17bd941215d72cb00e3ed830c6d8
Maya Shavin <maya@cloudinary.com>
2019-06-11 15:43:19 +0300

    fix css route  and remove readme

--- packages/vue/src/components/atoms/SfIcon/SfIcon.js
+++ packages/vue/src/components/atoms/SfIcon/SfIcon.js
@@ -6,21 +6,25 @@ export default {
   name: "SfIcon",
   props: {
     /**
-     * Icon SVG Path
+     * Icon SVG path
      */
     path: {
       type: String,
       default: ""
     },
     /**
-     * Icon SVG Size
+     * Custom size of the icon
+     * It can be our standard sizes, or `12px` or `1.2rem` or nothing.
+     * Standard sizes: `xxs`, `xs`, `sm`, `md`, `lg`, `xl`, `xxl`, `xl3`, `xl4`.
      */
     size: {
       type: String,
       default: ""
     },
     /**
-     * Icon SVG Fill Color
+     * Custom color of the icon
+     * It can be according to our standard colors, or `#fff` or `rgb(255,255,255)` or nothing.
+     * Standard colors: `primary`, `secondary`, `white`, `black`, `accent`, `gray-primary`, `gray-secondary`, `light-primary`, `light-secondary`, `pink-primary`, `pink-secondary`, `yellow-primary`, `yellow-secondary`, `blue-primary`, `blue-secondary`.
      */
     color: {
       type: String,
