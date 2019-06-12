df0cd5d49e321f672deac101f9504a9cd89cfd71
Filip Rakowski <frakowski@divante.pl>
2019-06-12 14:04:39 +0200

    update versioning scripts

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -62,3 +62,4 @@ export {
   SfMegaMenu,
   SfTopBar
 };
+
--- packages/vue/vue.config.js
+++ packages/vue/vue.config.js
@@ -1,6 +1,12 @@
+const FallbackDirectoryResolverPlugin = require('webpack-fallback-directory-resolver-plugin');
 
 module.exports = {
   devServer: {
     port: 3002
+  },
+  configureWebpack: {
+    plugins: [
+      new FallbackDirectoryResolverPlugin
+    ]
   }
 };
