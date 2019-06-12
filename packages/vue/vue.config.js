c3620deeb4553b790573dd5c497015bf2f726d70
Filip Rakowski <frakowski@divante.pl>
2019-06-12 14:07:24 +0200

    update config

--- packages/vue/vue.config.js
+++ packages/vue/vue.config.js
@@ -1,12 +1,6 @@
-const FallbackDirectoryResolverPlugin = require('webpack-fallback-directory-resolver-plugin');
 
 module.exports = {
   devServer: {
     port: 3002
-  },
-  configureWebpack: {
-    plugins: [
-      new FallbackDirectoryResolverPlugin
-    ]
   }
 };
