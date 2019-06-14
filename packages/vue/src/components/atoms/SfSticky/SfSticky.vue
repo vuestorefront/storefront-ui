eb015d3e3e00db8fe8ad97fa4b19956eb7cc5f07
przspa <pspaczek@divante.co>
2019-06-14 13:00:18 +0200

    Moved style to shared

--- packages/vue/src/components/atoms/SfSticky/SfSticky.vue
+++ packages/vue/src/components/atoms/SfSticky/SfSticky.vue
@@ -1,3 +1,5 @@
 <script src="./SfSticky.js"></script>
 <template lang="html" src="./SfSticky.html"></template>
-<style lang="scss" src="./SfSticky.scss"></style>
+<style lang="scss">
+  @import "~@storefrontui/shared/styles/components/SfSticky.scss";
+</style>
