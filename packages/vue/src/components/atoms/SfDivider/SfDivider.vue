8881ee9a7fcfd733e996c94b99317e3fbb740b47
fvillata <federica.villata@gmail.com>
2019-06-19 13:27:01 +0200

    Update packages/vue/src/components/atoms/SfDivider/SfDivider.vue
    Co-Authored-By: Filip Rakowski <f.rakowskI@hotmail.com>

--- packages/vue/src/components/atoms/SfDivider/SfDivider.vue
+++ packages/vue/src/components/atoms/SfDivider/SfDivider.vue
@@ -1,5 +1,5 @@
 <script src="./SfDivider.js"></script>
-<template lang="html" src="./SfDivider.html"></template>
+<template functional lang="html" src="./SfDivider.html"></template>
 <style lang="scss">
 @import "~@storefrontui/shared/styles/components/SfDivider.scss";
 </style>
