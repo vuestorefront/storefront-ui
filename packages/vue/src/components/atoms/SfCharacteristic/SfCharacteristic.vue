d879724ab8a0d47d4745f9dff8c7cb1b71944b07
Leonardo Matos <leomp120894@gmail.com>
2019-06-24 21:44:38 -0300

    chore: remove SfCharacteristic source files

--- packages/vue/src/components/atoms/SfCharacteristic/SfCharacteristic.vue
+++
@@ -1,5 +0,0 @@
-<script src="./SfCharacteristic.js"></script>
-<template functional lang="html" src="./SfCharacteristic.html"></template>
-<style lang="scss">
-  @import "~@storefrontui/shared/styles/components/SfCharacteristic.scss";
-</style>
