8cd986cbf1033ec79e42192b1f9dddad50aa9928
przspa <pspaczek@divante.co>
2019-06-25 11:55:44 +0200

    Added image component

---
+++ packages/vue/src/components/molecules/SfImage/SfImage.vue
@@ -0,0 +1,5 @@
+<script src="./SfImage.js"></script>
+<template lang="html" src="./SfImage.html"></template>
+<style lang="scss">
+@import "~@storefrontui/shared/styles/components/SfImage.scss";
+</style>
