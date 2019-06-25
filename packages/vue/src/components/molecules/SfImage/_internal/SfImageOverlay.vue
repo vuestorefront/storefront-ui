d84a85e826ca79a0eb82e6063d3d1fed454f51a7
przspa <pspaczek@divante.co>
2019-06-25 11:55:17 +0200

    Added custom overlay for image component

---
+++ packages/vue/src/components/molecules/SfImage/_internal/SfImageOverlay.vue
@@ -0,0 +1,5 @@
+<script src="./SfImageOverlay.js"></script>
+<template lang="html" src="./SfImageOverlay.html"></template>
+<style lang="scss">
+@import "~@storefrontui/shared/styles/components/SfImageOverlay.scss";
+</style>
