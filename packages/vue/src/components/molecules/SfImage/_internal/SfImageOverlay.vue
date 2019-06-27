9926a553e941beacbe908f8ef03f651bb641527f
przspa <pspaczek@divante.co>
2019-06-27 12:18:33 +0200

    Merged SfImageOverlay to SfImage component

--- packages/vue/src/components/molecules/SfImage/_internal/SfImageOverlay.vue
+++
@@ -1,5 +0,0 @@
-<script src="./SfImageOverlay.js"></script>
-<template lang="html" src="./SfImageOverlay.html"></template>
-<style lang="scss">
-@import "~@storefrontui/shared/styles/components/SfImageOverlay.scss";
-</style>
