25a6f6e3d0bcc4fd5421fe901c60088c99fcaa67
przspa <pspaczek@divante.co>
2019-06-27 16:09:47 +0200

    Adjusted banner section to design

--- packages/vue/src/examples/pages/home/Home.vue
+++ packages/vue/src/examples/pages/home/Home.vue
@@ -2,41 +2,41 @@
   <div id="home">
     <!-- SfHero -->
     <div class="banners-grid">
-      <div class="banners-grid__column">
+      <div class="banners-grid__column banners-grid__column--small">
         <SfBanner
           title="COCKTAIL & PARTY "
           subtitle="Scarves"
           description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands."
           button-text="SHOP NOW"
-          :image="bannerB"
+          :image="bannerF"
           class="sf-banner--left sf-banner--top"
         />
       </div>
-      <div class="banners-grid__column">
+      <div class="banners-grid__column banners-grid__column--medium">
         <SfBanner
           title="LINEN DRESSES"
           subtitle="Dresses"
           description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands."
           button-text="SHOP NOW"
           :image="bannerE"
-          class="sf-banner--left sf-banner--top"
+          class="sf-banner--left sf-banner--top sf-banner--small"
         />
       </div>
-      <div class="banners-grid__column banners-grid__column--vertical-tiles">
+      <div class="banners-grid__column banners-grid__column--vertical-tiles banners-grid__column--small">
         <div>
           <SfBanner
             title="THE OFFICE LIFE"
             subtitle="T-Shirts"
             :image="bannerC"
-            class="sf-banner--left"
+            class="sf-banner--left sf-banner--full"
           />
         </div>
         <div>
           <SfBanner
             title="ECO SANDALS"
             subtitle="Summer shoes"
-            :image="bannerA"
-            class="sf-banner--left"
+            :image="bannerG"
+            class="sf-banner--left sf-banner--full"
           />
         </div>
       </div>
@@ -192,6 +192,8 @@ import bannerB from "./assets/bannerB.png";
 import bannerC from "./assets/bannerC.png";
 import bannerD from "./assets/bannerD.png";
 import bannerE from "./assets/bannerE.png";
+import bannerF from "./assets/bannerF.png";
+import bannerG from "./assets/bannerG.png";
 
 import productA from "./assets/productA.png";
 import productB from "./assets/productB.png";
@@ -216,6 +218,8 @@ export default {
       bannerC,
       bannerD,
       bannerE,
+      bannerF,
+      bannerG,
       productA,
       productB,
       productC,
@@ -245,7 +249,7 @@ export default {
 
 #home {
   box-sizing: border-box;
-  max-width: 1200px;
+  max-width: 1240px;
   margin: auto;
   overflow: hidden;
 }
@@ -295,9 +299,14 @@ export default {
         margin-left: 40px;
       }
     }
+    &--small{
+      flex: 0 0 330px;
+    }
+    &--medium{
+      flex: 0 0 500px;
+    }
     &--vertical-tiles {
       flex-direction: column;
-
       & > div:not(:first-child) {
         margin-top: 10px;
         @media screen and (min-width: $desktop-min) {
@@ -341,6 +350,36 @@ export default {
     margin: $spacer-extra-big 0;
     @media screen and (min-width: $desktop-min) {
       margin: $spacer-extra-big * 2 0;
+    }
+  }
+}
+.sf-banner {
+  padding: 2.5rem;
+  /deep/ &__subtitle{
+    line-height: 1.167;
+    margin: 0;
+    padding: 0;
+  }
+  /deep/ &__title{
+    margin: 0;
+    padding: 0;
+    font-size: 2.25rem;
+    line-height: 1.6;
+  }
+  /deep/ &__container {
+    line-height: 1.875;
+    @media screen and (min-width: $desktop-min) {
+      width: 77%;
+    }
+  }
+  /deep/ &--full{
+    /deep/ .sf-banner__container {
+        width: 100%;
+    }
+  }
+  /deep/ &--small{
+    /deep/ .sf-banner__container {
+      width: 66%;
     }
   }
 }
