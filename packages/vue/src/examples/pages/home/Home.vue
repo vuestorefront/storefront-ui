454fe7be20987efcbba925e18ad7a9e97f44706b
przspa <pspaczek@divante.co>
2019-07-01 10:44:17 +0200

    Restyled banners-grid

--- packages/vue/src/examples/pages/home/Home.vue
+++ packages/vue/src/examples/pages/home/Home.vue
@@ -27,60 +27,60 @@
       ></SfHeroItem>
     </SfHero>
     <div class="banners-grid">
-      <div class="banners-grid__column banners-grid__column--small">
-        <SfBanner
-          title="COCKTAIL & PARTY "
-          subtitle="Scarves"
-          description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands."
-          button-text="SHOP NOW"
-          image="assets/storybook/homepage/bannerF.png"
-          class="sf-banner--left sf-banner--top"
-        />
-      </div>
-      <div class="banners-grid__column banners-grid__column--medium">
-        <SfBanner
-          title="LINEN DRESSES"
-          subtitle="Dresses"
-          description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands."
-          button-text="SHOP NOW"
-          image="assets/storybook/homepage/bannerE.png"
-          class="sf-banner--left sf-banner--top sf-banner--small"
-        />
-      </div>
-      <div
-        class="banners-grid__column banners-grid__column--vertical-tiles banners-grid__column--small"
-      >
-        <div>
+      <div class="banners-grid__row">
+        <div class="banners-grid__col">
           <SfBanner
-            title="THE OFFICE LIFE"
-            subtitle="T-Shirts"
-            image="assets/storybook/homepage/bannerC.png"
-            class="sf-banner--left sf-banner--full"
+            title="Coktail & party"
+            subtitle="Scarves"
+            description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands."
+            button-text="Shop now"
+            image="assets/storybook/homepage/bannerF.png"
+            class="sf-banner--left sf-banner--top"
           />
         </div>
-        <div>
+        <div class="banners-grid__col">
           <SfBanner
-            title="ECO SANDALS"
-            subtitle="Summer shoes"
-            image="assets/storybook/homepage/bannerG.png"
-            class="sf-banner--left sf-banner--full"
+            title="Linen dresses"
+            subtitle="Dresses"
+            description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands."
+            button-text="Shop now"
+            image="assets/storybook/homepage/bannerE.png"
+            class="sf-banner--left sf-banner--top"
           />
+        </div>
+        <div class="banners-grid__col">
+          <div class="banners-grid__row banners-grid__row--vertical">
+            <div class="banners-grid__col">
+              <SfBanner
+                title="The office life"
+                subtitle="T-Shirts"
+                image="assets/storybook/homepage/bannerC.png"
+                class="sf-banner--left"
+              />
+            </div>
+            <div class="banners-grid__col">
+              <SfBanner
+                title="Eco sandals"
+                subtitle="Summer shoes"
+                image="assets/storybook/homepage/bannerG.png"
+                class="sf-banner--left"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <SfCallToAction
       title="Subscribe to Newsletters"
-      button-text="SUBSCRIBE"
+      button-text="Subscribe"
       description="Be aware of upcoming sales and events. Receive gifts and special offers!"
-      class="callToAction"
     />
     <SfSection
       :heading="{
         title: 'Best Sellers'
       }"
-      class="section"
     >
-      <SfCarousel :settings="{ gap: 0 }" class="carousel">
+      <SfCarousel :settings="{ gap: 0 }">
         <SfCarouselItem>
           <SfProductCard
             title="Cream Beach Bag"
@@ -160,54 +160,46 @@
         title: 'Share Your Look',
         subtitle: '#YOURLOOK'
       }"
-      class="section"
     >
       <div class="images-grid">
-        <!-- TODO: test SfImage and fix it -->
-        <div class="images-grid__tiles">
-          <SfImage src="assets/storybook/homepage/imageA.png"
-            >maria_trn</SfImage
-          >
+        <div class="images-grid__row">
+          <div class="images-grid__col">
+            <SfImage src="assets/storybook/homepage/imageA.png"
+              >maria_trn</SfImage
+            >
+          </div>
+          <div class="images-grid__col">
+            <SfImage src="assets/storybook/homepage/imageB.png"
+              >katherina_trn</SfImage
+            >
+          </div>
         </div>
-        <div class="images-grid__tiles">
-          <SfImage src="assets/storybook/homepage/imageB.png"
-            >katherina_trn</SfImage
-          >
-        </div>
-      </div>
-      <div class="images-grid">
-        <div class="images-grid__tiles">
-          <SfImage src="assets/storybook/homepage/imageC.png"
-            >angelina_trn</SfImage
-          >
-        </div>
-        <div class="images-grid__tiles">
-          <SfImage src="assets/storybook/homepage/imageD.png"
-            >julia_trn</SfImage
-          >
+        <div class="images-grid__row">
+          <div class="images-grid__col">
+            <SfImage src="assets/storybook/homepage/imageC.png"
+              >angelina_trn</SfImage
+            >
+          </div>
+          <div class="images-grid__col">
+            <SfImage src="assets/storybook/homepage/imageD.png"
+              >julia_trn</SfImage
+            >
+          </div>
         </div>
       </div>
     </SfSection>
     <SfBanner
       subtitle="Fashion to Take Away"
       image="assets/storybook/homepage/bannerD.png"
-      class="sf-banner--left banner banner--download desktop-only"
+      class="sf-banner--left desktop-only"
     >
       <template v-slot:title>
         <h1>Download our application to your mobile</h1>
       </template>
       <template v-slot:call-to-action>
-        <div class="download">
-          <img
-            class="download__button"
-            src="assets/storybook/homepage/google.png"
-            alt="google play"
-          />
-          <img
-            class="download__button"
-            src="assets/storybook/homepage/apple.png"
-            alt="app store"
-          />
+        <div>
+          <img src="assets/storybook/homepage/google.png" alt="" />
+          <img src="assets/storybook/homepage/apple.png" alt="" />
         </div>
       </template>
     </SfBanner>
@@ -236,7 +228,18 @@ export default {
   }
 };
 </script>
-
+<style lang="scss">
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+body {
+  box-sizing: border-box;
+}
+</style>
 <style lang="scss" scoped>
 @import "../../../css/variables";
 @import "~@storefrontui/shared/styles/helpers/visibility";
@@ -245,37 +248,6 @@ export default {
   box-sizing: border-box;
   max-width: 1240px;
   margin: auto;
-  overflow: hidden;
-}
-.banner {
-  margin: $spacer-big 0;
-  @media screen and (min-width: $desktop-min) {
-    margin: $spacer-extra-big 0;
-  }
-  &--download {
-    margin-bottom: 0;
-    background-repeat: no-repeat;
-    background-size: contain;
-    background-position: 128%;
-  }
-}
-.callToAction {
-  margin: $spacer-extra-big 0;
-  @media screen and (min-width: $desktop-min) {
-    margin: $spacer-extra-big * 2 0;
-  }
-}
-.download {
-  &__button {
-    &:not(:first-child) {
-      margin-left: $spacer-big;
-    }
-  }
-}
-.product-card {
-  &:hover {
-    box-shadow: 0px 4px 20px rgba(168, 172, 176, 0.19);
-  }
 }
 
 .banners-grid {
@@ -284,67 +256,63 @@ export default {
     margin: $spacer-extra-big 0;
     display: flex;
   }
-  &__column {
-    flex: 1;
-    margin-top: 10px;
-    @media screen and (min-width: $desktop-min) {
-      display: flex;
-      margin-top: 0;
+  &__row {
+    display: flex;
+    &--vertical {
+      flex-direction: column;
+      .banners-grid {
+        &__col {
+          &:not(:first-child) {
+            margin-left: 0;
+            margin-top: $spacer-medium;
+            @media screen and (min-width: $desktop-min) {
+              margin-top: $spacer-extra-big;
+            }
+          }
+        }
+      }
     }
+  }
+  &__col {
+    display: flex;
     &:not(:first-child) {
       @media screen and (min-width: $desktop-min) {
         margin-left: 40px;
-      }
-    }
-    &--small {
-      @media screen and (min-width: $desktop-min) {
-        flex: 1 1 330px;
-      }
-    }
-    &--medium {
-      @media screen and (min-width: $desktop-min) {
-        flex: 1 1 500px;
-      }
-    }
-    &--vertical-tiles {
-      flex-direction: column;
-      margin-top: 10px;
-      @media screen and (min-width: $desktop-min) {
-        margin-top: 0px;
-      }
-      & > div:not(:first-child) {
-        margin-top: 10px;
-        @media screen and (min-width: $desktop-min) {
-          margin-top: 40px;
-        }
       }
     }
   }
 }
 .images-grid {
   display: flex;
-  justify-content: center;
-  margin-top: 10px;
-  @media screen and (min-width: $desktop-min) {
-    margin-top: 20px;
+  align-items: center;
+  flex-direction: column;
+  &__row {
+    flex: 1;
+    display: flex;
+    &:not(:first-child) {
+      margin-top: $spacer;
+      @media screen and (min-width: $desktop-min) {
+        margin-top: $spacer-big;
+      }
+    }
   }
-  &__tiles {
+  &__col {
     max-width: 470px;
     &:not(:first-child) {
-      margin-left: 10px;
+      margin-left: $spacer;
       @media screen and (min-width: $desktop-min) {
-        margin-left: 20px;
+        margin-left: $spacer-big;
       }
     }
   }
 }
 
-.sf-carousel {
-  margin: -20px 0;
-  /deep/ &__wrapper {
-    padding: 20px;
-  }
+.product-card {
+  /*TODO: unset in main styles? */
+  max-width: unset;
 }
+
+/* shame reset */
 .sf-section {
   margin: $spacer-extra-big 0;
   padding: 0;
@@ -357,40 +325,5 @@ export default {
       margin: $spacer-extra-big * 2 0;
     }
   }
-}
-.sf-banner {
-  @media screen and (min-width: $desktop-min) {
-    padding: 2.5rem;
-  }
-  /deep/ &__subtitle {
-    line-height: 1.167;
-    margin: 0;
-    padding: 0;
-  }
-  /deep/ &__title {
-    margin: 0;
-    padding: 0;
-    font-size: 2.25rem;
-    line-height: 1.6;
-  }
-  /deep/ &__container {
-    line-height: 1.875;
-    @media screen and (min-width: $desktop-min) {
-      width: 77%;
-    }
-  }
-  /deep/ &--full {
-    /deep/ .sf-banner__container {
-      width: 100%;
-    }
-  }
-  /deep/ &--small {
-    /deep/ .sf-banner__container {
-      width: 66%;
-    }
-  }
-}
-.sf-product-card {
-  max-width: unset;
 }
 </style>
