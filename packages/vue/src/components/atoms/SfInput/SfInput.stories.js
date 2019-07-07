3b026926231abe4dfa2adcd51b7472dfd3aab2c9
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-07-07 22:06:03 +0700

    Add slot default and errorMessage story in storybooks

--- packages/vue/src/components/atoms/SfInput/SfInput.stories.js
+++ packages/vue/src/components/atoms/SfInput/SfInput.stories.js
@@ -42,4 +42,68 @@ storiesOf("Atoms|Input", module)
         `
       }
     }
+  )
+  .add(
+    "[slot] default",
+    () => ({
+      components: { SfInput },
+      props: {
+        valid: {
+          default: boolean("Valid")
+        },
+        required: {
+          default: boolean("Required", true)
+        },
+        errorMessage: {
+          default: text("Error message", "Field is required.")
+        }
+      },
+      data() {
+        return {
+          value: ""
+        };
+      },
+      template: `
+        <div :style="{ width: '300px', margin: '50px' }">
+          <SfInput v-model="value" :valid="valid" :required="required">
+            <span style="color: salmon;">Address</span>
+          </SfInput>
+        </div>
+      `
+    }),
+    {
+      info: true
+    }
+  )
+  .add(
+    "[slot] with error message",
+    () => ({
+      components: { SfInput },
+      props: {
+        label: {
+          default: text("Label", "Name")
+        },
+        valid: {
+          default: boolean("Valid", false)
+        },
+        required: {
+          default: boolean("Required", true)
+        }
+      },
+      data() {
+        return {
+          value: ""
+        };
+      },
+      template: `
+        <div :style="{ width: '300px', margin: '50px' }">
+          <SfInput v-model="value" :label="label" :valid="valid" :required="required">
+            <span slot="errorMessage" style="color: orange;">This field is not correct.</span>
+          </SfInput>
+        </div>
+      `
+    }),
+    {
+      info: true
+    }
   );
