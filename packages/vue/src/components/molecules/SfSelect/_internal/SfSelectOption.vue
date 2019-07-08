165a629d127c78ac816cf771dc890c14d21e8097
Maciej Kopeć <dimitor115@gmail.com>
2019-07-08 22:36:53 +0200

    Merge internal SfSelect styles

--- packages/vue/src/components/molecules/SfSelect/_internal/SfSelectOption.vue
+++ packages/vue/src/components/molecules/SfSelect/_internal/SfSelectOption.vue
@@ -1,5 +1,5 @@
 <script src="./SfSelectOption.js"></script>
 <template lang="html" src="./SfSelectOption.html"></template>
 <style lang="scss">
-@import "~@storefrontui/shared/styles/components/SfSelectOption.scss";
+@import "~@storefrontui/shared/styles/components/SfSelect.scss";
 </style>
