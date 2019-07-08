0a1ab519b6f24d3ba333e2e9eda4547cc14ea7de
Maciej Kopeć <dimitor115@gmail.com>
2019-07-08 22:24:27 +0200

    Mere internal SfCarousel styles

--- packages/vue/src/components/organisms/SfCarousel/_internal/SfCarouselItem.vue
+++ packages/vue/src/components/organisms/SfCarousel/_internal/SfCarouselItem.vue
@@ -1,5 +1,5 @@
 <script src="./SfCarouselItem.js"></script>
 <template lang="html" src="./SfCarouselItem.html"></template>
 <style lang="scss">
-@import "~@storefrontui/shared/styles/components/SfCarouselItem.scss";
+@import "~@storefrontui/shared/styles/components/SfCarousel.scss";
 </style>
