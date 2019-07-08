8efd442ec2e931c6f68a3f8ccf71cc2469e26ee6
Maciej Kopeć <dimitor115@gmail.com>
2019-07-08 22:32:22 +0200

    Merge internal SfFilter styles

--- packages/vue/src/components/molecules/SfFilter/_internal/SfFilterItem.vue
+++ packages/vue/src/components/molecules/SfFilter/_internal/SfFilterItem.vue
@@ -1,5 +1,5 @@
 <script src="./SfFilterItem.js" />
 <template lang="html" src="./SfFilterItem.html"></template>
 <style lang="scss">
-@import "~@storefrontui/shared/styles/components/SfFilterItem.scss";
+@import "~@storefrontui/shared/styles/components/SfFilter.scss";
 </style>
