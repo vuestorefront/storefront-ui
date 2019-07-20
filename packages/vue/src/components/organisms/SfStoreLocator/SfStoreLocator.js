02a4a20939d6e61b6d00c20241c03a2eb5630bbb
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-07-20 17:37:08 +0200

    feat: store locator basic functionality

--- packages/vue/src/components/organisms/SfStoreLocator/SfStoreLocator.js
+++ packages/vue/src/components/organisms/SfStoreLocator/SfStoreLocator.js
@@ -21,12 +21,81 @@ export default {
     stores: {
       type: Array,
       default: () => []
+    },
+    markerIconSize: {
+      type: Array,
+      default: () => [21, 28]
+    },
+    markerIconAnchor: {
+      type: Array,
+      default: () => [10.5, 0]
+    },
+    mapOptions: {
+      type: Object,
+      default: () => ({})
+    },
+    tileLayerOptions: {
+      type: Object,
+      default: () => ({})
+    },
+    markerOptions: {
+      type: Object,
+      default: () => ({})
     }
   },
   data() {
     return {
-      loaded: false
+      loaded: false,
+      userPosition: null
     };
+  },
+  computed: {
+    parsedStores() {
+      if (this.userPosition) {
+        return this.stores.map(s => {
+          let distance = undefined;
+          if (s.latlng.length === 2) {
+            distance = this.getGeoDistance(this.userPosition, {
+              lat: s.latlng[0],
+              lng: s.latlng[1]
+            });
+            distance = distance / 1000;
+            distance = Math.round(distance * 100) / 100;
+          }
+          return {
+            ...s,
+            distance
+          };
+        });
+      }
+      return this.stores;
+    }
+  },
+  methods: {
+    mapReady(mapObject) {
+      mapObject.locate();
+    },
+    locationFound(location) {
+      this.userPosition = { ...location.latlng };
+      this.$emit("update:center", { ...location.latlng });
+    },
+    locationError(error) {
+      console.error(error);
+    },
+    getGeoDistance(start, end) {
+      const deg2rad = deg => deg * (Math.PI / 180);
+      const R = 6371;
+      const dLat = deg2rad(end.lat - start.lat);
+      const dLng = deg2rad(end.lng - start.lng);
+      const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(start.lat)) *
+          Math.cos(deg2rad(end.lat)) *
+          Math.sin(dLng / 2) *
+          Math.sin(dLng / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      return R * c * 1000;
+    }
   },
   async mounted() {
     const { LMap, LTileLayer, LMarker, LIcon } = await import("vue2-leaflet");
