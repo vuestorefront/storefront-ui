257bacba3185cbd80786efa9570e1c2441e50583
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-07-20 13:07:02 +0200

    feat: add more necessary components

--- packages/vue/src/components/organisms/SfStoreLocator/SfStoreLocator.js
+++ packages/vue/src/components/organisms/SfStoreLocator/SfStoreLocator.js
@@ -1,37 +1,44 @@
-import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
+import SfImage from "@/components/molecules/SfImage/SfImage.vue";
+import SfDivider from "@/components/atoms/SfDivider/SfDivider.vue";
 import SfIcon from "@/components/atoms/SfIcon/SfIcon.vue";
-import Vue from 'vue';
+import Vue from "vue";
 
 export default {
   name: "SfStoreLocator",
   props: {
     tileServerUrl: {
       type: String,
-      default: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      default: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
     },
     center: {
       type: [Array, Object],
-      default: [0,0]
+      default: () => [0, 0]
     },
     zoom: {
       type: Number,
       default: 6
+    },
+    stores: {
+      type: Array,
+      default: () => []
     }
   },
-  data () {
+  data() {
     return {
       loaded: false
-    }
+    };
   },
   async mounted() {
-    const { LMap, LTileLayer } = await import('vue2-leaflet')
-    Vue.component('l-map', LMap)
-    Vue.component('l-tilelayer', LTileLayer)
-    console.log(LMap)
+    const { LMap, LTileLayer, LMarker, LIcon } = await import("vue2-leaflet");
+    Vue.component("l-map", LMap);
+    Vue.component("l-tilelayer", LTileLayer);
+    Vue.component("l-marker", LMarker);
+    Vue.component("l-icon", LIcon);
     this.loaded = true;
   },
   components: {
-    SfCircleIcon,
+    SfImage,
+    SfDivider,
     SfIcon
   }
 };
