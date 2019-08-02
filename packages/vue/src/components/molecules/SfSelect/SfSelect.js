5c38838f97b68ea4e1fe6a197f4c38404641056a
przspa <pspaczek@divante.co>
2019-08-02 09:52:53 +0200

    Removed VSF object story from SfSelect

--- packages/vue/src/components/molecules/SfSelect/SfSelect.js
+++ packages/vue/src/components/molecules/SfSelect/SfSelect.js
@@ -87,7 +87,7 @@ export default {
 
     this.options = options;
     this.indexes = indexes;
-    if(!indexes[JSON.stringify(selected)]) return;
+    if (!indexes[JSON.stringify(selected)]) return;
     this.index = indexes[JSON.stringify(selected)];
   },
   beforeDestroy: function() {
