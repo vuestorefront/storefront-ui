f6bdb4b2f228d4f685b12001e0a9f4c1626e69f8
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2019-08-12 15:54:35 +0200

    Removed styles depended on css variables

--- packages/vue/src/components/atoms/SfChevron/SfChevron.js
+++ packages/vue/src/components/atoms/SfChevron/SfChevron.js
@@ -5,24 +5,6 @@ export default {
     isActive: {
       type: Boolean,
       default: false
-    },
-    /** Color of Chevron. Default is black.  */
-    color: {
-      type: String,
-      default: "#000000"
-    },
-    /** Size of Chevron component. Default is 40px.  */
-    size: {
-      type: String,
-      default: "40px"
-    }
-  },
-  computed: {
-    getStyle() {
-      return {
-        "--chevron-color": this.color,
-        "--chevron-size": this.size
-      };
     }
   }
 };
