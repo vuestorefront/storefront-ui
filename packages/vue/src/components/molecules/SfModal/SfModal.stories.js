b65fd2678248b2491bba3fbb966e5b50fef8d485
przspa <pspaczek@divante.co>
2019-08-13 13:14:30 +0200

    Cleaned SfModal

--- packages/vue/src/components/molecules/SfModal/SfModal.stories.js
+++ packages/vue/src/components/molecules/SfModal/SfModal.stories.js
@@ -46,32 +46,6 @@ storiesOf("Molecules|Modal", module)
         },
         persistent: {
           default: boolean("persistent (prop)", false)
-        },
-        transitionOverlay: {
-          default: select(
-            "transitionOverlay (prop)",
-            {
-              fade: "fade",
-              "collapse-top": "collapse-top",
-              "collapse-bottom": "collapse-bottom",
-              "fade-slide": "fade-slide",
-              "fade-collapse": "fade-collapse"
-            },
-            "fade"
-          )
-        },
-        transitionModal: {
-          default: select(
-            "transitionModal (prop)",
-            {
-              fade: "fade",
-              "collapse-top": "collapse-top",
-              "collapse-bottom": "collapse-bottom",
-              "fade-slide": "fade-slide",
-              "fade-collapse": "fade-collapse"
-            },
-            "fade"
-          )
         }
       },
       template: `
@@ -83,15 +57,14 @@ storiesOf("Molecules|Modal", module)
         :cross="cross"
         :overlay="overlay"
         :persistent="persistent"
-        :transitionOverlay="transitionOverlay"
-        :transitionModal="transitionModal"
+        transitionOverlay="fade"
+        transitionModal="fade"
       >Hello World!</SfModal>
       </div>`
     }),
     {
       info: {
-        summary: `<p>Component for modal.</p>
-       <h2>Usage</h2>
+        summary: `<h2>Usage</h2>
        <pre><code>import SfModal from "@storefrontui/vue/dist/SfModal.vue"</code></pre>
        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
        `
