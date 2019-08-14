ec33d3ae3ae48e14b3cd2f9632a638e3f09eced7
Filip Rakowski <frakowski@divante.pl>
2019-08-14 16:00:18 +0200

    bottom nav and cirle icon

---
+++ packages/vue/src/components/molecules/SfBottomNavigation/SfBottomNavigation.js
@@ -0,0 +1,10 @@
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
+
+export default {
+  name: "SfBottomNavigation",
+  components: {
+    SfIcon,
+    SfCircleIcon
+  }
+};
