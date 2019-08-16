f9e8c521a121721ffdff3c7f0c4453f1ef04d0cc
przspa <pspaczek@divante.co>
2019-08-16 17:17:31 +0200

    Refactored SfRadio

--- packages/vue/src/components/atoms/SfCheckbox/SfCheckbox.stories.js
+++ packages/vue/src/components/atoms/SfCheckbox/SfCheckbox.stories.js
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 import { generateStorybookTable } from "@/helpers";
 import SfCheckbox from "./SfCheckbox.vue";
 
@@ -7,12 +8,25 @@ const scssTableConfig = {
   tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
   tableBodyConfig: [
     ["$checkbox__label-font-size", "1.125rem", "font size for label"],
-    ["$checkbox__checkmark-transition", "background-color 0.25s cubic-bezier(1, 0.5, 0.8, 1), border-color 0.25s cubic-bezier(1, 0.5, 0.8, 1) !default;", "transition for checkmark"],
+    [
+      "$checkbox__checkmark-transition",
+      "background-color 0.25s cubic-bezier(1, 0.5, 0.8, 1), border-color 0.25s cubic-bezier(1, 0.5, 0.8, 1)",
+      "transition for checkmark"
+    ],
     ["$checkbox__checkmark-size", "1.4375rem ", "size for checkmark"],
-    ["$checkbox__checkmark-primary-color", "$c-green-primary", "primary color for checkmark"],
-    ["$checkbox__checkmark-secondary-color", "$c-gray-secondary", "secondary color for checkmark"]
+    [
+      "$checkbox__checkmark-primary-color",
+      "$c-green-primary",
+      "primary color for checkmark"
+    ],
+    [
+      "$checkbox__checkmark-secondary-color",
+      "$c-gray-secondary",
+      "secondary color for checkmark"
+    ]
   ]
 };
+
 storiesOf("Atoms|Checkbox", module)
   .addDecorator(withKnobs)
   .add(
@@ -42,7 +56,7 @@ storiesOf("Atoms|Checkbox", module)
       template: `<div>
         <p><b>Checked: {{checked}}</b></p>
         <p v-for="(checkbox, key) in checkboxes" :key="key">
-          <SfCheckbox v-model="checked" :disabled="checkbox.disabled" :name="checkbox.name" :value="checkbox.value" :label="checkbox.label"/>
+          <SfCheckbox v-model="checked" :disabled="checkbox.disabled" :name="checkbox.name" :value="checkbox.value" :label="checkbox.label" :class="customClass"/>
         </p>
       </div>`
     }),
