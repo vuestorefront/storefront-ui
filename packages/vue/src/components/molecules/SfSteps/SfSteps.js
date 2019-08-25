10f3a5dd3b7947b92c9c96e7d28a583e713e692f
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2019-08-25 17:30:22 +0200

    feat: basic component api

--- packages/vue/src/components/molecules/SfSteps/SfSteps.js
+++ packages/vue/src/components/molecules/SfSteps/SfSteps.js
@@ -1,3 +1,51 @@
 export default {
-  name: "SfSteps"
+  name: "SfSteps",
+  model: {
+    prop: "active",
+    event: "change"
+  },
+  props: {
+    /**
+     * Steps to show
+     */
+    steps: {
+      type: Array,
+      required: true
+    },
+    /**
+     * Current active step
+     */
+    active: {
+      type: Number,
+      default: 0
+    },
+    /**
+     * Disable clicking on  a past step
+     */
+    canGoBack: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    parsedSteps() {
+      if (this.steps) {
+        return this.steps.map((step, index) => ({
+          index,
+          step,
+          done: index < this.active,
+          disabled: !this.canGoBack && index < this.active,
+          current: index === this.active
+        }));
+      }
+      return [];
+    }
+  },
+  methods: {
+    stepClick({ index, disabled }) {
+      if (!disabled) {
+        this.$emit("change", index);
+      }
+    }
+  }
 };
