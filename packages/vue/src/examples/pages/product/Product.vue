1528c7bf4a21d1a38d8790e5d0739fa9baf04daf
przspa <pspaczek@divante.co>
2019-08-29 17:11:24 +0200

    Added SfBottomNavigation

--- packages/vue/src/examples/pages/product/Product.vue
+++ packages/vue/src/examples/pages/product/Product.vue
@@ -1,15 +1,50 @@
 <template>
-  <div id="home"></div>
+  <div id="product">
+    <SfBottomNavigation class="mobile-only">
+      <SfBottomNavigationItem>
+        <SfIcon icon="home" size="20px" />
+      </SfBottomNavigationItem>
+      <SfBottomNavigationItem>
+        <SfIcon icon="menu" size="20px" style="width: 25px" />
+      </SfBottomNavigationItem>
+      <SfBottomNavigationItem>
+        <SfIcon icon="heart" size="20px" />
+      </SfBottomNavigationItem>
+      <SfBottomNavigationItem>
+        <SfIcon icon="profile" size="20px" />
+      </SfBottomNavigationItem>
+      <SfBottomNavigationItem>
+        <SfCircleIcon
+          class="sf-bottom-navigation__floating-icon sf-circle-icon--big"
+        >
+          <SfIcon
+            icon="add_to_cart"
+            size="20px"
+            color="white"
+            style="margin-right: 4px;"
+          />
+        </SfCircleIcon>
+      </SfBottomNavigationItem>
+    </SfBottomNavigation>
+  </div>
 </template>
 <script>
-import {} from "@storefront-ui/vue";
+import {
+  SfBottomNavigation,
+  SfCircleIcon,
+  SfIcon
+} from "@storefront-ui/vue";
 
 export default {
   name: "Product",
   data() {
     return {};
   },
-  components: {},
+  components: {
+    SfBottomNavigation,
+    SfCircleIcon,
+    SfIcon
+  },
   mounted() {
     document.body.style.setProperty("margin", "0");
   }
