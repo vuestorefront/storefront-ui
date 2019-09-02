ba11244b1bc4397b07b2a3bc06058716c2a262a5
Talisson Ferreira <talissonf@gmail.com>
2019-09-02 01:07:31 -0300

    chore(SfCollectedProduct): setted component props

---
+++ packages/vue/src/components/molecules/SfCollectedProduct/SfCollectedProduct.js
@@ -0,0 +1,44 @@
+import SfPrice from "../../atoms/SfPrice/SfPrice.vue";
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
+import SfCounter from "../../molecules/SfCounter/SfCounter.vue";
+export default {
+  name: "SfCollectedProduct",
+  props: {
+    /**
+     * Product image
+     * It should be an url of the product
+     */
+    image: {
+      type: String,
+      default: "assets/storybook/product_thumb.png"
+    },
+    /**
+     * Product title
+     */
+    title: {
+      type: String,
+      required: true
+    },
+    /**
+     * Product regular price
+     */
+    regularPrice: {
+      type: [Number, String],
+      default: null
+    },
+    /**
+     * Product special price
+     */
+    specialPrice: {
+      type: [Number, String],
+      default: null
+    }
+  },
+  components: {
+    SfIcon,
+    SfCircleIcon,
+    SfPrice,
+    SfCounter
+  }
+};
