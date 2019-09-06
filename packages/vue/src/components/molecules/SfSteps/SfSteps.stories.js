fd2cda847e67744621a6f06a228ec39e2300f99a
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2019-09-06 13:11:23 +0200

    feat: storybook stories

--- packages/vue/src/components/molecules/SfSteps/SfSteps.stories.js
+++ packages/vue/src/components/molecules/SfSteps/SfSteps.stories.js
@@ -1,53 +1,141 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, text, select } from "@storybook/addon-knobs";
+import { withKnobs, boolean, array } from "@storybook/addon-knobs";
 import { generateStorybookTable } from "@/helpers";
 
 import SfSteps from "./SfSteps.vue";
 
+const info = () => ({
+  summary: `
+        <p>SfSteps allow to break the UI in steps it supports two modes, one with slots and one with events.</p>
+        <h2>Usage</h2>
+        <pre><code>import { SfSteps } from "@storefront-ui/vue"</code></pre>
+        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
+      `
+});
+
 // use this to document scss vars
 const scssTableConfig = {
   tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [["$component-size", "1.438rem", "size of checkmark"]]
+  tableBodyConfig: [
+    [
+      "$sf-steps__active-line-height-mobile",
+      "0.1875rem",
+      "Active line height mobile"
+    ],
+    [
+      "$sf-steps__active-line-height-desktop",
+      "0.125rem",
+      "Active line height desktop"
+    ],
+    ["$sf-steps__active-line-coilor", "#1d1f22", "Active line color"],
+    [
+      "$sf-steps__default-border",
+      "0.0625rem solid #f1f2f4",
+      "Default border of steps line"
+    ],
+    ["$sf-steps__step-font-weight", "500", "Step font weight"],
+    ["$sf-steps__step-font-size", "0.75rem", "Step font size"],
+    ["$sf-steps__step-font-color", "$c-gray-secondary", "Step font color"]
+  ]
 };
 
-// use this to document events
-const eventsTableConfig = {
-  tableHeadConfig: ["NAME", "DESCRIPTION"],
-  tableBodyConfig: [["input", "event emited when option is selected"]]
-};
+// // use this to document events
+// const eventsTableConfig = {
+//   tableHeadConfig: ["NAME", "DESCRIPTION"],
+//   tableBodyConfig: [["input", "event emited when option is selected"]]
+// };
 
-// storiesOf("Molecules|Steps", module)
-//   .addDecorator(withKnobs)
-//   .add(
-//     "[slot] default",
-//     () => ({
-//       props: {
-//         editableProp: {
-//           default: text("(prop) propname")
-//         },
-//         customClass: {
-//           default: select(
-//             "CSS Modifier",
-//             ["null", "sf-steps--modifier"],
-//             "null",
-//             "CSS-Modifiers"
-//           )
-//         }
-//       },
-//       components: { SfSteps },
-//       template: `<SfSteps
-//         :class="customClass"
-//       >
-//       </SfSteps>`
-//     }),
-//     {
-//       info: {
-//         summary: `<p>Component description.</p>
-//        <h2>Usage</h2>
-//        <pre><code>import { SfSteps } from "@storefront-ui/vue"</code></pre>
-//        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-//        ${generateStorybookTable(eventsTableConfig, "Events")}`
-//       }
-//     }
-//   );
+storiesOf("Molecules|Steps", module)
+  .addDecorator(withKnobs)
+  .add(
+    "[slot] default",
+    () => ({
+      props: {
+        steps: {
+          default: array("steps", ["One", "Two", "Three", "Four"])
+        },
+        canGoBack: {
+          default: boolean("canGoBack", true)
+        }
+      },
+      data() {
+        return {
+          active: 0
+        };
+      },
+      components: { SfSteps },
+      template: `
+        <SfSteps :steps="steps" :can-go-back="canGoBack" :active="active" @change="active = $event" >
+          <SfStep v-for="step in steps" :key="step" :name="step">
+            STEP {{ step }}
+          </SfStep>
+        </SfSteps>
+      `
+    }),
+    {
+      info: info()
+    }
+  )
+  .add(
+    "[slot] default with v-model",
+    () => ({
+      props: {
+        steps: {
+          default: array("steps", ["One", "Two", "Three", "Four"])
+        },
+        canGoBack: {
+          default: boolean("canGoBack", true)
+        }
+      },
+      data() {
+        return {
+          active: 0
+        };
+      },
+      components: { SfSteps },
+      template: `
+        <SfSteps :steps="steps" :can-go-back="canGoBack" v-model="active">
+          <SfStep v-for="step in steps" :key="step" :name="step">
+            STEP {{ step }}
+          </SfStep>
+        </SfSteps>
+      `
+    }),
+    {
+      info: info()
+    }
+  )
+  .add(
+    "Events usage",
+    () => ({
+      props: {
+        steps: {
+          default: array("steps", ["One", "Two", "Three", "Four"])
+        },
+        canGoBack: {
+          default: boolean("canGoBack", true)
+        }
+      },
+      data() {
+        return {
+          active: 0
+        };
+      },
+      components: { SfSteps },
+      template: `
+        <div>
+          <SfSteps :steps="steps" :can-go-back="canGoBack" v-model="active"></SfSteps>
+          <div v-if="active === 0">
+            FIRST
+          </div>
+          <div v-if="active === 1">
+            SECOND
+          </div>
+        </div>
+      `
+    }),
+    {
+      info: info()
+    }
+  );
