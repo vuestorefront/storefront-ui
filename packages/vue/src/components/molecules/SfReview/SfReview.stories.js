f105b21c4b8cdbfcb114fb14a8730ef3f87187d4
Petar Petrov <p.petrov@codecoda.com>
2019-09-13 13:48:11 +0300

    fix default message text

--- packages/vue/src/components/molecules/SfReview/SfReview.stories.js
+++ packages/vue/src/components/molecules/SfReview/SfReview.stories.js
@@ -137,7 +137,7 @@ storiesOf("Molecules|Review", module)
         message: {
           default: text(
             "message (prop)",
-            "Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here. Message here."
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
           )
         },
         rating: {
