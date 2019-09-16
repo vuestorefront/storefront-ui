659a784499968bb1c29f8d00f8a81e4f0f79c4db
Adam Pawlinski <apawlinski@divante.co>
2019-09-16 16:44:56 +0200

    adding storybook and closing event in progress

--- packages/vue/src/components/molecules/SfNotification/SfNotification.js
+++ packages/vue/src/components/molecules/SfNotification/SfNotification.js
@@ -2,50 +2,60 @@ import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
 import SfHeading from "../../atoms/SfHeading/SfHeading.vue";
 
 export default {
-  name: "SfNotification",
-  components: {
-    SfIcon,
-    SfHeading
-  },
-  props: {
-    // title of the message that will be showed as header 
-    title: {
-      type: String,
-      default: ""
+    name: "SfNotification",
+    components: {
+        SfIcon,
+        SfHeading
     },
-    level: {
-      type: Number,
-      default: 2
+    props: {
+        // visibility of notification
+        visible: {
+            type: Boolean,
+            default: () => false
+        },
+        // title of the message that will be showed as header 
+        title: {
+            type: String,
+            default: ""
+        },
+        level: {
+            type: Number,
+            default: 2
+        },
+        // the body of the message showed as subtitle
+        subtitle: {
+            type: String,
+            default: ""
+        },
+        // icon added to the message in desktop mode
+        icon: {
+            type: String,
+            default: "added_to_cart"
+        },
+        sizeIcon: {
+            type: String,
+            default: "lg"
+        },
+        colorIcon: {
+            type: String,
+            default: "white"
+        },
+        type: {
+            type: String,
+            default: "alert",
+            validator: function(value) {
+                return ["alert", "warning", "info", "success"].indexOf(value) !== -1;
+            }
+        }
     },
-    // the body of the message showed as subtitle
-    subtitle: {
-      type: String,
-      default: ""
+    computed: {
+        hasSubtitle() {
+            return this.subtitle || this.$slots.hasOwnProperty("subtitle");
+        }
     },
-    // icon added to the message in desktop mode
-    icon: {
-      type: String,
-      default: "added_to_cart"
-    },
-    sizeIcon: {
-      type: String,
-      default: "lg"
-    },
-    colorIcon: {
-      type: String,
-      default: "white"
-    },
-    type: {
-      type: String,
-      default: "alert",
-      validator: function(value) {
-        return ["alert", "warning", "info", "success"].indexOf(value) !== -1;
-      }
+    methods: {
+        close() {
+            this.$emit("close");
+        }
     }
-  },
-  computed: {
-    hasSubtitle() {
-      return !!this.subtitle || this.$slots.hasOwnProperty("subtitle");
-    }
-  }  
 };
--- packages/vue/src/components/molecules/SfNotification/SfNotification.stories.js
+++ packages/vue/src/components/molecules/SfNotification/SfNotification.stories.js
@@ -4,141 +4,181 @@ import { withKnobs, text, select } from "@storybook/addon-knobs";
 import { generateStorybookTable } from "@/helpers";
 
 import SfNotification from "./SfNotification.vue";
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+
 
 // use this to document scss vars
 const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    ["$notification__message-padding-left", "0.625rem", "left padding for text"],
-    ["$notification--info-color", "$c-dark-primary", "text and icon color for info version"],
-    ["$notification--warning-color", "#d12727", "text and icon color for warning version"],
-    ["$notification--alert-color", "#0468db", "text and icon color for alert version"],
-    ["$notification--success-color", "#128830", "text and icon color for success version"],
-    ["$notification-mobile-padding", "$spacer-big", "padding on mobile"],
-    ["$notification-background-size", "cover", "size of the background"],
-    ["$notification-background-position", "bottom left", "position of the background"],
-    ["$notification__title-font-size", "3rem", "font size of the title"]
-  ]
+    tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
+    tableBodyConfig: [
+        ["$notification__message-padding-left", "0.625rem", "left padding for text"],
+        ["$notification--info-color", "$c-dark-primary", "text and icon color for info version"],
+        ["$notification--warning-color", "#d12727", "text and icon color for warning version"],
+        ["$notification--alert-color", "#0468db", "text and icon color for alert version"],
+        ["$notification--success-color", "#128830", "text and icon color for success version"],
+        ["$notification-mobile-padding", "$spacer-big", "padding on mobile"],
+        ["$notification-background-size", "cover", "size of the background"],
+        ["$notification-background-position", "bottom left", "position of the background"],
+        ["$notification__title-font-size", "3rem", "font size of the title"]
+    ]
 };
 
 // use this to document events
-const eventsTableConfig = {
-  tableHeadConfig: ["NAME", "DESCRIPTION"],
-  tableBodyConfig: [
-    [".sf-notification--left", "aligns content to the left"],
-    [".sf-notification--right", "aligns content to the right"],
-    [".sf-notification--info", "to set blue color for notification message"],
-    [".sf-notification--alert", "to set red color for notification message"],
-    [".sf-notification--warning", "to set default color for notification message"],
-    [".sf-notification--success", "to set green color for notification message"]
-  ]
+const cssTableConfig = {
+    tableHeadConfig: ["NAME", "DESCRIPTION"],
+    tableBodyConfig: [
+        [".sf-notification--right", "aligns content to the right"],
+        [".sf-notification--info", "to set blue color for notification message"],
+        [".sf-notification--alert", "to set red color for notification message"],
+        [".sf-notification--warning", "to set default color for notification message"],
+        [".sf-notification--success", "to set green color for notification message"]
+    ]
 };
 
 storiesOf("Molecules|Notification", module)
-  .addDecorator(withKnobs)
-  .add(
-    "basic",
-    () => ({
-      props: {
-        title: {
-          default: text("title (prop)", "Added to Cart")
-        },
-        subtitle: {
-          default: text("subtitle (prop)", "This informative message for the user View cart")
-        },
-        // background: {
-        //   default: text("background (prop)", "#e1e3e2")
-        // },
-        icon: {
-          default: text("icon (prop)", "added_to_cart")
-        },
-        customClass: {
-          default: select(
-            "CSS Modifier",
-            [
-              "null",
-              "sf-notification--left",
-              "sf-notification--right",
-              "sf-notification--info",
-              "sf-notification--alert",
-              "sf-notification--warning",
-              "sf-notification--success"
-            ],
-            "null"
-          )
-        }
-      },
-      components: { SfNotification },
-      template: `<SfNotification
-        :title="title"
-        :subtitle="subtitle"
-        :background="backgroundColor"
-        :icon="icon"
-        :class="customClass"
-      />`        
-    }),
-    {
-      info: {
-        summary: `<p>Component description.</p>
+    .addDecorator(withKnobs)
+    .add(
+        "basic",
+        () => ({
+            data() {
+                return {
+                    isNotificationOpen: true
+                };
+            },
+            props: {
+                title: {
+                    default: text("title (prop)", "Added to Cart")
+                },
+                subtitle: {
+                    default: text("subtitle (prop)", "This is informative message for the user. View cart")
+                },
+                // level: {
+                //     default: number("level (prop)", 2)
+                // },
+                icon: {
+                    default: text("icon (prop)", "added_to_cart")
+                },
+                sizeIcon: {
+                    type: String,
+                    default: "lg"
+                },
+                colorIcon: {
+                    type: String,
+                    default: "white"
+                },
+                type: {
+                    default: select("type (prop)", [
+                        "alert",
+                        "warning",
+                        "info",
+                        "success"
+                    ])
+                }
+            },
+            components: { SfNotification, SfIcon },
+            template: `<SfNotification
+                    class="sf-notification sf-notification__container" 
+                    :visible="isNotificationOpen"
+                    @close="isNotificationOpen = false"
+                    :title="title"
+                    // :level="level"
+                    :subtitle="subtitle"
+                    :icon="icon"  
+                    :type="type"
+                    transitionNotification="fade"            
+                >
+                    <SfIcon class="sf-notification__close" icon="cross" color="white" size="14px" @click="close" /> 
+                </SfNotification>`
+        }), {
+            info: {
+                summary: `<p>Component description.</p>
        <h2>Usage</h2>
        <pre><code>import { SfNotification } from "@storefront-ui/vue"</code></pre>
        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-       ${generateStorybookTable(eventsTableConfig, "Events")}`
-      }
-    }
-  )
-  .add(
-    "[slot] title",
-    () => ({
-      components: { SfNotification },
-      template: `<SfNotification
-        title="Title prop"
-        subtitle="Subtitle prop"
-        icon="public/assets/added_to_cart.png">
-        <header name="message" class="sf-notification__heading">
-          <component :is="level" class="sf-notification__title">
-              <slot name="title" v-bind="{ title }">{{ title }}</slot>
-          </component>
-        </header>   
-      </SfNotification>`
-    }),
-    {
-      info: true
-    }
-  )
-  .add(
-    "[slot] subtitle",
-    () => ({
-      components: { SfNotification },
-      template: `<SfNotification
-        title="Title prop"
-        subtitle="Subtitle prop"
-        icon="public/assets/added_to_cart.png">
-        <header name="message" class="sf-notification__heading">          
-          <div v-if="hasSubtitle" class="sf-notification__subtitle">
-              <slot name="subtitle" v-bind="{ subtitle }">{{ subtitle }}</slot>
-          </div>
-        </header>   
-      </SfNotification>`
-    }),
-    {
-      info: true
-    }
-  )
-  .add(
-    "[slot] icon",
-    () => ({
-      components: { SfNotification },
-      template: `<SfNotification
-        title="Title prop"
-        subtitle="Subtitle prop"
-        icon="public/assets/added_to_cart.png">
-          <slot name="icon" v-bind="{ icon }">
-            <SfIcon src="assets/added_to_cart.svg" class="sf-notification--type"></SfIcon>
-          </slot>   
-      </SfNotification>`
-    }),
-    {
-      info: true
-    }
-  )
+       ${generateStorybookTable(cssTableConfig, "CSS modifiers")}`
+            }
+        }
+    )
+    .add(
+        "[slot] title",
+        () => ({
+            props: {
+                title: {
+                    default: text("title (prop)", "Added to Cart")
+                },
+                // level: {
+                //     default: number("level (prop)", 2)
+                // }
+            },
+            components: { SfNotification },
+            template: `<SfNotification>
+                <header name="message" class="sf-notification__heading">
+                    <component 
+                    // :is="{level}"
+                    class="sf-notification__title"> 
+                        <slot 
+                            name="title"
+                            v-bind="{ title }"> 
+                            {{ title }} 
+                        </slot> 
+                    </component> 
+                </header>    
+            </SfNotification>`
+        }), {
+            info: true
+        }
+    )
+    .add(
+        "[slot] subtitle",
+        () => ({
+            props: {
+                subtitle: {
+                    default: text("subtitle (prop)", "This is informative message for the user. View cart")
+                }
+            },
+            components: { SfNotification },
+            template: `<SfNotification>
+              <header name="message" class="sf-notification__heading">          
+                <div class="sf-notification__subtitle">
+                    <slot name="subtitle" v-bind="{ subtitle }">{{ subtitle }}</slot>
+                </div>
+              </header>   
+            </SfNotification>`
+        }), {
+            info: true
+        }
+    )
+    .add(
+        "[slot] icon",
+        () => ({
+            props: {
+                icon: {
+                    default: text("icon (prop)", "added_to_cart")
+                },
+                sizeIcon: {
+                    type: String,
+                    default: "lg"
+                },
+                colorIcon: {
+                    type: String,
+                    default: "white"
+                },
+                type: {
+                    default: select("type (prop)", [
+                        "alert",
+                        "warning",
+                        "info",
+                        "success"
+                    ])
+                }
+            },
+            components: { SfNotification, SfIcon },
+            template: `<SfNotification>
+                <slot name="icon" v-bind="{ icon, sizeIcon, colorIcon, type }">
+                    <SfIcon class="sf-notification__icon" :icon="icon" :size="sizeIcon" :color="colorIcon"></SfIcon>
+                </slot>  
+            </SfNotification>`
+        }), {
+            info: true
+        }
+    )
