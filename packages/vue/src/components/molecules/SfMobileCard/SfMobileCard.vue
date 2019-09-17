2c31705c477f03bf807810e311b1c98513df5929
przspa <pspaczek@divante.co>
2019-09-17 13:05:20 +0200

    Linted

--- packages/vue/src/components/molecules/SfMobileCard/SfMobileCard.vue
+++
@@ -1,5 +0,0 @@
-<script src="./SfMobileCard.js"></script>
-<template lang="html" src="./SfMobileCard.html"></template>
-<style lang="scss">
-@import "~@storefront-ui/shared/styles/components/SfMobileCard.scss";
-</style>
