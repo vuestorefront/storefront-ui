cb24a33357d64bca8f3f784369eb3b9bfd40e8a3
przspa <pspaczek@divante.co>
2019-09-18 15:55:54 +0200

    POC Mobile Slide

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
@@ -7,25 +7,71 @@ export default {
   data() {
     return {
       isActive: false,
+      viewportWidth: 0,
+      desktopMin: 1024,
       style: {}
     };
   },
+  watch: {
+    viewportWidth: {
+      handler: function(width) {
+        if (width >= this.desktopMin) {
+          this.isActive = false;
+          this.enableScroll();
+          return;
+        }
+        this.disableScroll();
+        window.addEventListener("touchend", this.openSliding, {passive: true})
+      },
+      immediate: true
+    },
+  },
   methods: {
-    toggleActive(event) {
+    preventDefault(e) {
+      e.preventDefault();
+    },
+    disableScroll() {
+      document.body.classList.add("sf-sliding-section-scroll--is-disabled");
+      window.addEventListener("touchmove", this.preventDefault, {
+        passive: false
+      });
+    },
+    enableScroll() {
+      document.body.classList.remove("sf-sliding-section-scroll--is-disabled");
+      window.removeEventListener("touchmove", this.preventDefault, {
+        passive: false
+      });
+    },
+    openSliding(){
       if (!this.style.height) {
         this.style = { height: this.$refs.top.offsetHeight + "px" };
       }
-      this.$nextTick(() => {
-        const { target } = event;
-        const mobileCross = this.$refs.cross.$el;
-        const offsetTop = window.pageYOffset;
-        if (
-          mobileCross.contains(target) ||
-          (!mobileCross.contains(target) && !this.isActive && offsetTop <= 10)
-        ) {
-          this.isActive = !this.isActive;
-        }
-      });
+      this.isActive = true;
+      this.enableScroll();
+      window.removeEventListener("touchend", this.openSliding, {passive: true})
+      window.addEventListener("touchend", this.closeHandler, {passive: true})
+    },
+    closeHandler() {
+      if(window.pageYOffset < -50){
+        this.isActive = false;
+        this.disableScroll();
+      }
     }
+  },
+  mounted() {
+    this.viewportWidth = Math.max(
+      document.documentElement.clientWidth,
+      window.innerWidth
+    );
+    window.addEventListener(
+      "resize",
+      () => {
+        this.viewportWidth = Math.max(
+          document.documentElement.clientWidth,
+          window.innerWidth
+        );
+      },
+      { passive: true }
+    );
   }
 };
