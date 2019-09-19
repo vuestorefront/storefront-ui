6a9979922439b9a7ec39a349a4e82bdabb901798
przspa <pspaczek@divante.co>
2019-09-19 09:40:32 +0200

    Refactored watchers behaviour

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
@@ -6,26 +6,69 @@ export default {
   },
   data() {
     return {
-      isActive: false,
+      isMobile: false,
+      isScrollLock: false,
+      isActive: true,
+      desktopMin: 1024,
       style: {}
     };
   },
-  methods: {
-    toggleActive(event) {
-      if (!this.style.height) {
-        this.style = { height: this.$refs.top.offsetHeight + "px" };
-      }
-      this.$nextTick(() => {
-        const { target } = event;
-        const mobileCross = this.$refs.cross.$el;
-        const offsetTop = window.pageYOffset;
-        if (
-          mobileCross.contains(target) ||
-          (!mobileCross.contains(target) && !this.isActive && offsetTop <= 10)
-        ) {
-          this.isActive = !this.isActive;
+  watch: {
+    isMobile: {
+      handler(mobile) {
+        this.isActive = !mobile;
+      },
+      immediate: true
+    },
+    isScrollLock: {
+      handler(scroll) {
+        if (!scroll) {
+          this.scrollUnlock();
+          return;
         }
-      });
+        this.scrollLock();
+      },
+      immediate: true
+    },
+    isActive: {
+      handler(active){
+        if(!active){
+          this.isScrollLock = true;
+          return;
+        }
+        // touch overload and close
+        this.isScrollLock = false;
+      },
+      immediate: true
     }
+  },
+  methods: {
+    preventDefault(e){
+      e.preventDefault();
+    },
+    scrollLock() {
+      document.body.style.setProperty("overflow", "hidden");
+      window.addEventListener("touchstart", this.preventDefault, {passive: false});
+    },
+    scrollUnlock() {
+      document.body.style.removeProperty("overflow");
+      window.removeEventListener("touchstart", this.preventDefault, {passive: false});
+    },
+  },
+  mounted() {
+    this.isMobile =
+      Math.max(document.documentElement.clientWidth, window.innerWidth || 0) <
+      this.desktopMin;
+    window.addEventListener(
+      "resize",
+      () => {
+        this.isMobile =
+          Math.max(
+            document.documentElement.clientWidth,
+            window.innerWidth || 0
+          ) < this.desktopMin;
+      },
+      { passive: true }
+    );
   }
 };
