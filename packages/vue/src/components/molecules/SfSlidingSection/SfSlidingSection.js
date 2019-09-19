30b11deeba3e010467bd7266574069f077c3027b
przspa <pspaczek@divante.co>
2019-09-19 11:14:38 +0200

    Mobile POC

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
@@ -8,7 +8,7 @@ export default {
     return {
       isMobile: false,
       isScrollLock: false,
-      isActive: true,
+      isActive: false,
       desktopMin: 1024,
       style: {}
     };
@@ -16,9 +16,19 @@ export default {
   watch: {
     isMobile: {
       handler(mobile) {
-        this.isActive = !mobile;
-      },
-      immediate: true
+        this.isActive = false;
+        if (!mobile) {
+          this.isScrollLock = false;
+          // unregister eventsA & eventsB
+          console.log("unregister eventsA & eventsB");
+          this.clearMobileEvents();
+          return;
+        }
+        this.isScrollLock = true;
+        // register eventsA
+        console.log("register eventsA");
+        this.notActiveEvents();
+      }
     },
     isScrollLock: {
       handler(scroll) {
@@ -27,33 +37,82 @@ export default {
           return;
         }
         this.scrollLock();
-      },
-      immediate: true
+      }
     },
     isActive: {
-      handler(active){
-        if(!active){
-          this.isScrollLock = true;
+      handler(active) {
+        if (!active) {
+          this.isScrollLock = this.isMobile;
+          // register eventsA
+          console.log("register eventsA");
+          this.notActiveEvents();
           return;
         }
-        // touch overload and close
         this.isScrollLock = false;
-      },
-      immediate: true
+        // register eventsB
+        console.log("register eventsB");
+        this.activeEvents();
+      }
     }
   },
   methods: {
-    preventDefault(e){
+    preventDefault(e) {
       e.preventDefault();
     },
     scrollLock() {
       document.body.style.setProperty("overflow", "hidden");
-      window.addEventListener("touchstart", this.preventDefault, {passive: false});
+      window.addEventListener("touchstart", this.preventDefault, {
+        passive: false
+      });
+      window.addEventListener("touchmove", this.preventDefault, {
+        passive: false
+      });
     },
     scrollUnlock() {
       document.body.style.removeProperty("overflow");
-      window.removeEventListener("touchstart", this.preventDefault, {passive: false});
+      window.removeEventListener("touchstart", this.preventDefault, {
+        passive: false
+      });
+      window.removeEventListener("touchmove", this.preventDefault, {
+        passive: false
+      });
     },
+    open() {
+      this.isActive = true;
+    },
+    close() {
+      this.isActive = false;
+    },
+    scrollOverload(e) {
+      const { pageYOffset } = window;
+      if (pageYOffset < 0) {
+        this.close();
+      }
+    },
+    // events A
+    notActiveEvents() {
+      // unregister events B
+      window.removeEventListener("touchend", this.scrollOverload, {
+        passive: true
+      });
+      // register events A
+      window.addEventListener("touchend", this.open, { passive: true });
+      window.addEventListener("wheel", this.open, { passive: true });
+    },
+    // events B
+    activeEvents() {
+      // unregister events A
+      window.removeEventListener("touchend", this.open, { passive: true });
+      window.removeEventListener("wheel", this.open, { passive: true });
+      // register events B
+      window.addEventListener("touchend", this.scrollOverload, {
+        passive: true
+      });
+    },
+    clearMobileEvents() {
+      //unregiser events A
+      // uregister events B
+    }
   },
   mounted() {
     this.isMobile =
