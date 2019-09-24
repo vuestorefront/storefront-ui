5767bf9d324ea4433350e2a08e9b94c25fd0a1f0
Przemek Spaczek <pspaczek@divante.co>
2019-09-24 09:46:05 +0200

    Updated story

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.stories.js
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.stories.js
@@ -8,13 +8,7 @@ import SfSlidingSection from "./SfSlidingSection.vue";
 // use this to document scss vars
 const scssTableConfig = {
   tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [["$component-size", "1.438rem", "size of checkmark"]]
-};
-
-// use this to document events
-const eventsTableConfig = {
-  tableHeadConfig: ["NAME", "DESCRIPTION"],
-  tableBodyConfig: [["input", "event emited when option is selected"]]
+  tableBodyConfig: [["$card-margin-left-desktop", "$spacer-big * 5", "size of desktop column spacer"]]
 };
 
 storiesOf("Molecules|SlidingSection", module)
@@ -49,8 +43,9 @@ storiesOf("Molecules|SlidingSection", module)
           <template #static>
             <div :style="static"></div>
           </template>
-          <template #sliding>
+          <template #sliding="{isActive}">
             <h1>Cashmere Sweater</h1>
+            <small></small>
             <p>Find stunning women cocktail and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.</p>
             <p>The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common "problem" areas. Find stunning women's cocktail dresses and party dresses.</p>
             <h2>Brand</h2>
@@ -67,8 +62,7 @@ storiesOf("Molecules|SlidingSection", module)
         summary: `<p>Component description.</p>
        <h2>Usage</h2>
        <pre><code>import { SfSlidingSection } from "@storefront-ui/vue"</code></pre>
-       ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-       ${generateStorybookTable(eventsTableConfig, "Events")}`
+       ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
       }
     }
   );
