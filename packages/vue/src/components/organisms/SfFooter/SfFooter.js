838b3e12e90e002acd298c05c210436d601103d8
Petar <p.petrov@codecoda.com>
2019-09-27 15:43:06 +0300

    Remove props for column items

--- packages/vue/src/components/organisms/SfFooter/SfFooter.js
+++ packages/vue/src/components/organisms/SfFooter/SfFooter.js
@@ -4,21 +4,5 @@ import SfFooterColumn from "./_internal/SfFooterColumn.vue";
 Vue.component("SfFooterColumn", SfFooterColumn);
 
 export default {
-  name: "SfFooter",
-  props: {
-    /**
-     * Column items
-     */
-    columnItems: {
-      type: Array,
-      default: () => []
-    },
-    /**
-     * Social items
-     */
-    socialItems: {
-      type: Object,
-      default: () => ({})
-    }
-  }
+  name: "SfFooter"
 };
