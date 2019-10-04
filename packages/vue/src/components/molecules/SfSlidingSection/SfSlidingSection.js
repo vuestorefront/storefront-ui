fe7596bfeb028bb77599b7d25d207e55437f6383
Przemek Spaczek <pspaczek@divante.co>
2019-10-04 14:41:55 +0200

    added scroll unlock to beforeDestroy

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.js
@@ -98,6 +98,7 @@ export default {
     window.addEventListener("resize", this.isMobileHandler, { passive: true });
   },
   beforeDestroy() {
+    this.scrollUnlock();
     this.hammer.destroy();
   }
 };
