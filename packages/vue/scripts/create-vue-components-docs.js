04276b5894a319ee0b8d381a820261d8830240c5
Eduard Dopler <kontakt@eduard-dopler.de>
2019-10-05 08:33:40 +0200

    Add possibility to ignore CSS modifiers
    By using the no-docs annotation instead of the modifier annotation, you
    can exclude the CSS modifier from the final list of CSS modifiers.

--- packages/vue/scripts/create-vue-components-docs.js
+++ packages/vue/scripts/create-vue-components-docs.js
@@ -311,7 +311,12 @@ function extractCssModifiers(contentScssFile) {
       if (!lines[j].endsWith("{")) {
         continue;
       }
-      // opening-brace found; if the line after it doesn't contain a @modifier annotation, break
+      // opening-brace found; but if the line after it contains a @no-docs annotation, remove it again from the found modifiers
+      if (lines[j + 1].trim().includes("@no-docs")) {
+        uniqueModifiers.delete(lastModifierFound);
+        break;
+      }
+      // if it doesn't contain a @modifier annotation, keep it in the found modifiers, but break (don't look for description)
       if (!lines[j + 1].trim().includes("@modifier")) {
         break;
       }
