079f2c4ff18e476c791a43e5a96fd1ee52ab3584
Eduard Dopler <kontakt@eduard-dopler.de>
2019-10-09 19:38:29 +0200

    Fix copy paste error for SfSlidingSection.spec

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.spec.ts
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.spec.ts
@@ -1,9 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import SfCard from "@/components/molecules/SfCard.vue";
+import SfSlidingSection from "@/components/molecules/SfSlidingSection/SfSlidingSection.vue";
 
-describe("SfCard.vue", () => {
+describe("SfSlidingSection.vue", () => {
   it("renders a component", () => {
-    const component = shallowMount(SfCard);
-    expect(component.contains(".sf-card")).toBe(true);
+    const component = shallowMount(SfSlidingSection);
+    expect(component.contains(".sf-sliding-section")).toBe(true);
   });
 });
