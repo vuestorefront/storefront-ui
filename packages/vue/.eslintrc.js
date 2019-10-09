86254af148e1965624dc32a8274a486e69e7c576
Eduard Dopler <kontakt@eduard-dopler.de>
2019-10-09 21:32:21 +0200

    Remove typescript support

--- packages/vue/.eslintrc.js
+++ packages/vue/.eslintrc.js
@@ -3,12 +3,9 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "@typescript-eslint/parser"
   }
 };
--- packages/vue/jest.config.js
+++ packages/vue/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "json", "vue", "ts", "tsx"],
+  moduleFileExtensions: ["js", "json", "vue"],
   transform: {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
-    "^.+\\.tsx?$": "ts-jest",
     "^.+\\.js$": "babel-jest"
   },
   transformIgnorePatterns: ["/node_modules/"],
@@ -13,11 +12,5 @@ module.exports = {
   },
   modulePathIgnorePatterns: ["scripts"],
   snapshotSerializers: ["jest-serializer-vue"],
-  testMatch: ["**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"],
-  testURL: "http://localhost/",
-  globals: {
-    "ts-jest": {
-      babelConfig: false
-    }
-  }
+  testMatch: ["**/*.spec.js"]
 };
