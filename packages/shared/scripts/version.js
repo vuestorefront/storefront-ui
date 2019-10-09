c926f0cf53ddd0d044654ab93a19f8ddc98acb63
Przemek Spaczek <pspaczek@divante.co>
2019-10-09 12:07:05 +0200

    typo

--- packages/shared/scripts/version.js
+++ packages/shared/scripts/version.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 const path = require("path");
 const stylesPath = path.resolve("../styles/_variables.scss");
 
-fs.readFile(pathVariables, "utf8", function(err, data) {
+fs.readFile(stylesPath, "utf8", function(err, data) {
   if (err) return console.error(err);
   const result = data.replace("../../vue/sfui.scss", "../../../../sfui.scss");
-  fs.writeFile(pathVariables, result, "utf8", function(err) {
+  fs.writeFile(stylesPath, result, "utf8", function(err) {
     if (err) return console.error(err);
     console.log(result);
   });
