61cc64ad5bba5cb18c53ded46e0c8bcabd616089
Przemek Spaczek <pspaczek@divante.co>
2019-10-11 16:20:29 +0200

    moved sfui path replacer to postinstall

--- packages/shared/scripts/postinstall.js
+++ packages/shared/scripts/postinstall.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const currentDir = path.dirname(__dirname);
-const stylesPath = path.resolve("../../../sfui.scss");
+const stylesPath = path.resolve(__dirname, "../../../sfui.scss");
+const variablePath = path.resolve(__dirname, "../styles/_variables.scss");
 const isDependency = currentDir.includes("node_modules");
-
 const stylesContent = `/* Here you can override global SCSS variables */
   
 // global override excluding scoped components
@@ -11,16 +11,16 @@ const stylesContent = `/* Here you can override global SCSS variables */
 // global override including scoped componentss
 // $button-desktop-padding: 50px;`;
 
-const playgroundContent = `<template>
-  <div id="playground"></div>
-</template>
-  
-<script>
-// Use this component to play with other components
-export default {};
-</script>`;
 
 if (isDependency && !fs.existsSync(stylesPath)) {
+  fs.readFile(variablePath, "utf8", function(err, data) {
+    if (err) return console.error(err);
+    const result = data.replace("../../vue/sfui.scss", "../../../../sfui.scss");
+    fs.writeFile(variablePath, result, "utf8", function(err) {
+      if (err) return console.error(err);
+      console.log(result);
+    });
+  });
   fs.appendFile("../../../sfui.scss", stylesContent, err => {
     if (err) throw err;
   });
