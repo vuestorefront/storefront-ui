8f98b6b0ff5b1af70a4602d5b11dc8719bdc23f2
Przemek Spaczek <pspaczek@divante.co>
2019-10-16 16:31:29 +0200

    cleared SfLoader

--- packages/vue/src/components/atoms/SfLoader/SfLoader.stories.js
+++ packages/vue/src/components/atoms/SfLoader/SfLoader.stories.js
@@ -1,49 +1,47 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, select } from "@storybook/addon-knobs";
-import SfLoader from "./SfLoader.vue";
 import { generateStorybookTable } from "@/helpers";
+
+import SfLoader from "./SfLoader.vue";
 
 const scssTableConfig = {
   tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
   tableBodyConfig: [
-    ["$loader__spinner-color", "$c-accent-primary", "color of spinner"],
-    ["$loader__overlay-color", "white", "color of overlay"]
+    ["$loader__spinner-color", "$c-accent-primary", "color for loader spinner"],
+    ["$loader__overlay-color", "white", "color for loader overlay"]
   ]
 };
 
 storiesOf("Atoms|Loader", module)
   .addDecorator(withKnobs)
   .add(
-    "Basic",
+  "Basic",
     () => ({
-      data() {
-        return {
+      components: { SfLoader },
+      data: () => ({
           isLoading: select("isLoading (prop)", [true, false], true),
           style: { width: "400px", height: "200px", border: "1px solid #eee" }
-        };
-      },
+      }),
       created() {
         setTimeout(() => {
           this.isLoading = false;
         }, 3000);
       },
-      components: { SfLoader },
-      template: `
-      <!-- it adjusts to any wrapper -->
-      <div :style="style">
-        <SfLoader :loading="isLoading">
+
+      template: `<div 
+      :style="style">
+        <SfLoader 
+          :loading="isLoading">
           <p>Content is loaded</p>
         </SfLoader>
       </div>`
     }),
     {
       info: {
-        summary: `<p>Component to indicate loading state. Use it to cover content until data is fetched. Set **isLoading** to false once data is fetched.</p>
-        <h2> Usage </h2>
+        summary: `<h2> Usage </h2>
         <pre><code>import { SfLoader } from "@storefront-ui/vue"</code></pre>
-        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-        `
+        ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
       }
     }
   );
