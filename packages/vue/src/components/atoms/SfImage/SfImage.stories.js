8e2458cdee8c04a2ab283e43ff6bae6c82ea7506
Przemek Spaczek <pspaczek@divante.co>
2019-10-16 15:40:29 +0200

    cleaned SfImage

--- packages/vue/src/components/atoms/SfImage/SfImage.stories.js
+++ packages/vue/src/components/atoms/SfImage/SfImage.stories.js
@@ -5,41 +5,38 @@ import { generateStorybookTable } from "@/helpers";
 
 import SfImage from "./SfImage.vue";
 
-// use this to document scss vars
 const scssTableConfig = {
   tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
   tableBodyConfig: [
     [
-      "image__overlay-justify-content",
+      "$image__overlay-justify-content",
       "center",
-      "overlay content horizontal justify"
+      "justify content for image overlay"
     ],
-    ["$image__overlay-align-items", "center", "overlay content vertical align"],
+    ["$image__overlay-align-items", "center", "align items fro image overlay"],
     [
       "$image__overlay-background-color",
       "rgba(29, 31, 34, 0.8)",
-      "overlay background color"
+      "background color for image overlay"
     ],
-    ["$image__overlay-color", "$c-white", "overlay content color"]
+    ["$image__overlay-color", "$c-white", "color for image overlay"]
   ]
 };
 
-const data = () => {
-  return {
-    src: {
-      normal: {
-        url:
-          "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-        alt: "Macbook PRO Apple"
-      },
-      small: {
-        url:
-          "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-        alt: "Macbook PRO Apple"
-      }
+const data = () => ({
+  src: {
+    normal: {
+      url:
+        "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+      alt: "Macbook PRO Apple"
+    },
+    small: {
+      url:
+        "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
+      alt: "Macbook PRO Apple"
     }
-  };
-};
+  }
+});
 
 storiesOf("Atoms|Image", module)
   .addDecorator(withKnobs)
@@ -68,30 +65,22 @@ storiesOf("Atoms|Image", module)
       },
       data,
       components: { SfImage },
-      template: `<SfImage
-        :src="src"
-        />`
+      template: `<SfImage :src="src" />`
     }),
     {
       info: {
         summary: `<h2>Usage</h2>
         <pre><code>import { SfImage } from "@storefrontui/vue"</code></pre>
-        To render a picture tag with different img sizes for mobile and desktop
-        you should pass an object with the format below:
-        <code>
-        let src = {
-          small: { url, alt },
-          normal: { url, alt }
-        }
-        </code>
-        <br>
-        <br>
-        To render a simple img tag you should pass
-        an src string with the image url:
-        <code>let src = "assets/storybook/product_thumb.png"</code>
-        <br>
-        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-       `
+        <p>
+          To render a picture tag with different img sizes for mobile and desktop
+          you should pass an object with the format below:
+          <code>let src = { small: { url, alt }, normal: { url, alt }}</code>
+        </p>
+        <p>
+          To render a simple img tag you should pass an src string with the image url:
+          <code>let src = "assets/storybook/product_thumb.png"</code>
+        </p>
+        ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
       }
     }
   )
@@ -124,14 +113,15 @@ storiesOf("Atoms|Image", module)
         :src="src"
         :alt="alt"
         :transition="transition"
-      >angelina_trn</SfImage>`
+        >
+        angelina_trn
+      </SfImage>`
     }),
     {
       info: {
         summary: `<h2>Usage</h2>
-       <pre><code>import { SfImage } from "@storefront-ui/vue"</code></pre>
-       ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-       `
+        <pre><code>import { SfImage } from "@storefront-ui/vue"</code></pre>
+        ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
       }
     }
   );
