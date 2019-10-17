a1c9ef48285a421efb7b57272090ec5c6b4096cb
Przemek Spaczek <pspaczek@divante.co>
2019-10-17 15:53:06 +0200

    rewrote SfProperty stories

--- packages/vue/src/components/atoms/SfProperty/SfProperty.stories.js
+++ packages/vue/src/components/atoms/SfProperty/SfProperty.stories.js
@@ -1,14 +1,26 @@
+// /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import SfProperty from "./SfProperty.vue";
-import { withKnobs, text } from "@storybook/addon-knobs";
+import { withKnobs, text, optionsKnob as options } from "@storybook/addon-knobs";
 
-export default storiesOf("Atoms|Property", module)
+import SfProperty from "./SfProperty.vue";
+
+storiesOf("Atoms|Property", module)
   .addDecorator(withKnobs)
   .add(
-    "Basic",
+    "Default",
     () => ({
       components: { SfProperty },
       props: {
+        customClass: {
+          default: options(
+            "CSS modifier",
+            {
+              "sf-property--full-width": "sf-property--full-width"
+            },
+            "",
+            { display: "multi-select" }
+          )
+        },
         name: {
           default: text("name (prop)", "Material")
         },
@@ -16,20 +28,73 @@ export default storiesOf("Atoms|Property", module)
           default: text("value (prop)", "Cotton")
         }
       },
-      template: `
-      <div>
-        <SfProperty
-          :name="name"
-          :value="value"
-        />
-      </div>
-    `
-    }),
-    {
-      info: {
-        summary: `<p>Simple label and data component created for product description</p>
-        <h2> Usage </h2>
-        <pre><code>import { SfProperty } from "@storefront-ui/vue"</code></pre>`
-      }
-    }
+      template: `<SfProperty
+        :class="customClass"
+        :name="name"
+        :value="value" />`
+    })
+  )
+  .add(
+    "[slot] name",
+    () => ({
+      components: { SfProperty },
+      props: {
+        customClass: {
+          default: options(
+            "CSS modifier",
+            {
+              "sf-property--full-width": "sf-property--full-width"
+            },
+            "",
+            { display: "multi-select" }
+          )
+        },
+        name: {
+          default: text("name (prop)", "Material")
+        },
+        value: {
+          default: text("value (prop)", "Cotton")
+        }
+      },
+      template: `<SfProperty
+        :class="customClass"
+        :name="name"
+        :value="value">
+        <template #name>
+          <span></span>
+        </template>
+      </SfProperty>`
+    })
+  )
+  .add(
+    "[slot] value",
+    () => ({
+      components: { SfProperty },
+      props: {
+        customClass: {
+          default: options(
+            "CSS modifier",
+            {
+              "sf-property--full-width": "sf-property--full-width"
+            },
+            "",
+            { display: "multi-select" }
+          )
+        },
+        name: {
+          default: text("name (prop)", "Material")
+        },
+        value: {
+          default: text("value (prop)", "Cotton")
+        }
+      },
+      template: `<SfProperty
+        :class="customClass"
+        :name="name"
+        :value="value">
+        <template #value>
+          <span></span>
+        </template>
+      </SfProperty>`
+    })
   );
