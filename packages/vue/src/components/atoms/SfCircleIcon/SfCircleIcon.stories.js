78369873a4eddab3223522a5f095b298ff66565a
Przemek Spaczek <pspaczek@divante.co>
2019-10-17 13:03:38 +0200

    rewrote SfCircleIcon stories

--- packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.stories.js
+++ packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.stories.js
@@ -1,54 +1,22 @@
+// /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { generateStorybookTable } from "@/helpers";
-import {
-  withKnobs,
-  text,
-  optionsKnob as options
-} from "@storybook/addon-knobs";
+import { withKnobs, select, optionsKnob as options } from "@storybook/addon-knobs";
+import { icons } from "@storefront-ui/shared/icons/icons";
 
 import SfCircleIcon from "./SfCircleIcon.vue";
 
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    [
-      "$circle-icon-background",
-      "$c-green-primary",
-      "background color for circle icon"
-    ],
-    ["$circle-icon-size", "3.25rem", "size for circle icon"],
-    ["$circle-icon-size-small", "1.625rem", "size for small circle icon"],
-    ["$circle-icon-size-big", "4rem", "size for big circle icon"]
-  ]
-};
-const cssTableConfig = {
-  tableHeadConfig: ["NAME", "DESCRIPTION"],
-  tableBodyConfig: [
-    ["sf-circle-button--small", "transform normal circle icon to small"],
-    ["sf-circle-button--big", "transform normal circle icon to big"]
-  ]
-};
-
-const summary = `<h2> Usage </h2>
-<pre><code>import { SfCircleIcon } from "@storefront-ui/vue"</code></pre>
-${generateStorybookTable(scssTableConfig, "SCSS variables")}
-${generateStorybookTable(cssTableConfig, "CSS modifiers")}`;
+const icon =  Object.keys(icons);
 
 storiesOf("Atoms|CircleIcon", module)
   .addDecorator(withKnobs)
   .add(
-    "Basic",
+    "Default",
     () => ({
+      components: { SfCircleIcon },
       props: {
-        icon: {
-          default: text("icon (prop)", "home")
-        },
-        colorIcon: {
-          default: text("colorIcon (prop)", "white")
-        },
         customClass: {
           default: options(
-            "CSS Modifiers",
+            "CSS modifier",
             {
               "sf-circle-icon--small": "sf-circle-icon--small",
               "sf-circle-icon--big": "sf-circle-icon--big",
@@ -62,30 +30,53 @@ storiesOf("Atoms|CircleIcon", module)
             "",
             { display: "multi-select" }
           )
+        },
+        icon: {
+          default: select(
+            "(prop) icon",
+            icon,
+            "home",
+          )
         }
       },
-      components: { SfCircleIcon },
       template: `<SfCircleIcon
-      :icon="icon"
-      :class="customClass" />`
-    }),
-    {
-      info: {
-        summary
-      }
-    }
+        :class="customClass"
+        :icon="icon" />`
+    })
   )
   .add(
     "[slot] default",
     () => ({
       components: { SfCircleIcon },
-      template: `<SfCircleIcon>
-        <img src='assets/storybook/menu.svg'>
+      props: {
+        customClass: {
+          default: options(
+            "CSS modifier",
+            {
+              "sf-circle-icon--small": "sf-circle-icon--small",
+              "sf-circle-icon--big": "sf-circle-icon--big",
+              "color-primary": "color-primary",
+              "color-secondary": "color-secondary",
+              "color-warning": "color-warning",
+              "color-danger": "color-danger",
+              "color-info": "color-info",
+              "color-success": "color-success"
+            },
+            "",
+            { display: "multi-select" }
+          )
+        },
+        icon: {
+          default: select(
+            "(prop) icon",
+            icon,
+            "home",
+          )
+        }
+      },
+      template: `<SfCircleIcon
+        :class="customClass">
+        <span></span>
       </SfCircleIcon>`
-    }),
-    {
-      info: {
-        summary
-      }
-    }
-  );
+    })
+  )
