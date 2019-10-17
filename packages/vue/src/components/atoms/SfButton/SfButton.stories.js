21aa6ea2bc1a9c5f3b454da3dd922705ca91a55e
Przemek Spaczek <pspaczek@divante.co>
2019-10-17 12:19:39 +0200

    rewrote SfButton stories

--- packages/vue/src/components/atoms/SfButton/SfButton.stories.js
+++ packages/vue/src/components/atoms/SfButton/SfButton.stories.js
@@ -1,75 +1,21 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, optionsKnob as options } from "@storybook/addon-knobs";
-import { generateStorybookTable } from "@/helpers";
+import {withKnobs, text, optionsKnob as options} from "@storybook/addon-knobs";
 
 import SfButton from "./SfButton.vue";
-
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    ["$button-padding", "1rem 2.5rem", "padding for button"],
-    [
-      "$button-padding-desktop",
-      "0.9375rem 2.5rem",
-      "padding for button on desktop"
-    ],
-    ["$button-color", "$c-text-on-dark", "color for button"],
-    [
-      "$button-background-color",
-      "$c-green-primary",
-      "background-color for button"
-    ],
-    [
-      "$button-font-family",
-      "$body-font-family-secondary",
-      "font family for button"
-    ],
-    [
-      "$button-font-size",
-      "$font-size-small-mobile",
-      "font size for button"
-    ],
-    [
-      "$button-font-size-desktop",
-      "$font-size-small-desktop",
-      "font size for button on mobile"
-    ],
-    [
-      "$button-font-weight",
-      "$body-font-weight-secondary",
-      "font weight for btton"
-    ],
-    ["$button-line-height", "1.6", "line height for button"],
-    [
-      "$button--outline-border",
-      "2px",
-      "border width for outline button"
-    ]
-  ]
-};
-
-const cssTableConfig = {
-  tableHeadConfig: ["NAME", "DESCRIPTION"],
-  tableBodyConfig: [
-    [".sf-button--full-width", "change short button to full width button"],
-    [".sf-button--outline", "transform fill button to outline button"]
-  ]
-};
 
 storiesOf("Atoms|Button", module)
   .addDecorator(withKnobs)
   .add(
-    "Basic",
+    "[slot] default",
     () => ({
-      components: { SfButton },
       props: {
         customClass: {
           default: options(
-            "CSS Modifiers",
+            "CSS modifier",
             {
-              "sf-button--full-width": "sf-button--full-width",
               "sf-button--outline": "sf-button--outline",
+              "sf-button--full-width": "sf-button--full-width",
               "color-primary": "color-primary",
               "color-secondary": "color-secondary",
               "color-warning": "color-warning",
@@ -80,16 +26,14 @@ storiesOf("Atoms|Button", module)
             "",
             { display: "multi-select" }
           )
-        }
+        },
+        customLabel: {
+          default: text("(slot) default", "Shop now")
+        },
       },
-      template: `<SfButton :class="customClass">Hello world</SfButton>`
-    }),
-    {
-      info: {
-        summary: `<h2>Usage</h2>
-        <pre><code>import { SfButton } from "@storefront-ui/vue"</code></pre>
-        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-        ${generateStorybookTable(cssTableConfig, "CSS modifiers")}`
-      }
-    }
+      components: { SfButton },
+      template: `<SfButton
+        :class="customClass"
+      >{{customLabel}}</SfButton>`
+    })
   );
