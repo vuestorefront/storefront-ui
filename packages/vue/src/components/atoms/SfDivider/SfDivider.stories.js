2bcd6401cf8a98fd5b009b08b1c437b7cc778a6f
Przemek Spaczek <pspaczek@divante.co>
2019-10-17 13:09:40 +0200

    rewrote SfDivider stories

--- packages/vue/src/components/atoms/SfDivider/SfDivider.stories.js
+++ packages/vue/src/components/atoms/SfDivider/SfDivider.stories.js
@@ -1,28 +1,13 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { generateStorybookTable } from "@/helpers";
 
 import SfDivider from "./SfDivider.vue";
 
-// use this to document scss vars
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    ["$divider-border-color", "$c-light-primary", "border color for divider"]
-  ]
-};
-
-storiesOf("Atoms|Divider", module).add(
-  "Basic",
-  () => ({
-    components: { SfDivider },
-    template: `<SfDivider />`
-  }),
-  {
-    info: {
-      summary: `<h2>Usage</h2>
-      <pre><code>import { SfDivider } from "@storefront-ui/vue"</code></pre>
-      ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
-    }
-  }
-);
+storiesOf("Atoms|Divider", module)
+  .add(
+    "Default",
+    () => ({
+      components: { SfDivider },
+      template: `<SfDivider />`
+    })
+  );
