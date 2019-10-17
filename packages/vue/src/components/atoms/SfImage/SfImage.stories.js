8c420d69bca67ead2e4f6ca9fef55bf1d21dbdcf
Przemek Spaczek <pspaczek@divante.co>
2019-10-17 14:57:18 +0200

    rewrote SfImage stories

--- packages/vue/src/components/atoms/SfImage/SfImage.stories.js
+++ packages/vue/src/components/atoms/SfImage/SfImage.stories.js
@@ -1,127 +1,88 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, text, select } from "@storybook/addon-knobs";
-import { generateStorybookTable } from "@/helpers";
+import {withKnobs, text, boolean, number, object} from "@storybook/addon-knobs";
 
 import SfImage from "./SfImage.vue";
-
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    [
-      "$image__overlay-justify-content",
-      "center",
-      "justify content for image overlay"
-    ],
-    ["$image__overlay-align-items", "center", "align items fro image overlay"],
-    [
-      "$image__overlay-background-color",
-      "rgba(29, 31, 34, 0.8)",
-      "background color for image overlay"
-    ],
-    ["$image__overlay-color", "$c-white", "color for image overlay"]
-  ]
-};
-
-const data = () => ({
-  src: {
-    normal: {
-      url:
-        "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-      alt: "Macbook PRO Apple"
-    },
-    small: {
-      url:
-        "https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1550858949523-frontal-macbook-pro-apple-13-intel-core-i5-128gb-mpxq2bz-a.jpg",
-      alt: "Macbook PRO Apple"
-    }
-  }
-});
 
 storiesOf("Atoms|Image", module)
   .addDecorator(withKnobs)
   .add(
-    "Basic",
+    "Default",
     () => ({
+      components: { SfImage },
       props: {
-        alt: {
-          default: text("alt (prop)", "angelina_trn")
+        src: {
+          default: object( "(prop) src", {
+            small: {
+              url: "/assets/storybook/product-109x164.png",
+              alt: "Vila stripe maxi shirt dress"
+            },
+            normal: {
+              url: "/assets/storybook/product-216x326.png",
+              alt: "Vila stripe maxi shirt dress"
+            }
+          })
+        },
+        placeholder: {
+          default: text("(prop) placeholder", "/assets/placeholder.png")
         },
         transition: {
-          default: select(
-            "transition (prop)",
-            {
-              fade: "fade",
-              "slide-left": "slide-left",
-              "slide-right": "slide-right",
-              "collapse-top": "collapse-top",
-              "collapse-bottom": "collapse-bottom",
-              "fade-slide": "fade-slide",
-              "fade-collapse": "fade-collapse"
-            },
-            "fade"
-          )
-        }
-      },
-      data,
-      components: { SfImage },
-      template: `<SfImage :src="src" />`
-    }),
-    {
-      info: {
-        summary: `<h2>Usage</h2>
-        <pre><code>import { SfImage } from "@storefrontui/vue"</code></pre>
-        <p>
-          To render a picture tag with different img sizes for mobile and desktop
-          you should pass an object with the format below:
-          <code>let src = { small: { url, alt }, normal: { url, alt }}</code>
-        </p>
-        <p>
-          To render a simple img tag you should pass an src string with the image url:
-          <code>let src = "assets/storybook/product_thumb.png"</code>
-        </p>
-        ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
-      }
-    }
-  )
-  .add(
-    "[slot] default",
-    () => ({
-      props: {
-        alt: {
-          default: text("alt (prop)", "angelina_trn")
+          default: text("(prop) transition", "fade")
         },
-        transition: {
-          default: select(
-            "transition (prop)",
-            {
-              fade: "fade",
-              "slide-left": "slide-left",
-              "slide-right": "slide-right",
-              "collapse-top": "collapse-top",
-              "collapse-bottom": "collapse-bottom",
-              "fade-slide": "fade-slide",
-              "fade-collapse": "fade-collapse"
-            },
-            "fade"
-          )
+        lazy: {
+          default: boolean("(prop) lazy", true)
+        },
+        pictureBreakpoint: {
+          default: number("(prop) pictureBreakpoint", 576)
         }
       },
-      data,
-      components: { SfImage },
       template: `<SfImage
         :src="src"
         :alt="alt"
         :transition="transition"
-        >
-        angelina_trn
+        :lazy="lazy"
+        :placeholder="placeholder"
+        :picture-breakpoint="pictureBreakpoint" />`
+    })
+  )
+  .add(
+    "[slot] default",
+    () => ({
+      components: { SfImage },
+      props: {
+        src: {
+          default: object( "(prop) src", {
+            small: {
+              url: "/assets/storybook/product-109x164.png",
+              alt: "Vila stripe maxi shirt dress"
+            },
+            normal: {
+              url: "/assets/storybook/product-216x326.png",
+              alt: "Vila stripe maxi shirt dress"
+            }
+          })
+        },
+        placeholder: {
+          default: text("(prop) placeholder", "/assets/placeholder.png")
+        },
+        transition: {
+          default: text("(prop) transition", "fade")
+        },
+        lazy: {
+          default: boolean("(prop) lazy", true)
+        },
+        pictureBreakpoint: {
+          default: number("(prop) pictureBreakpoint", 576)
+        }
+      },
+      template: `<SfImage
+        :src="src"
+        :alt="alt"
+        :transition="transition"
+        :lazy="lazy"
+        :placeholder="placeholder"
+        :picture-breakpoint="pictureBreakpoint">
+        <span></span>
       </SfImage>`
-    }),
-    {
-      info: {
-        summary: `<h2>Usage</h2>
-        <pre><code>import { SfImage } from "@storefront-ui/vue"</code></pre>
-        ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
-      }
-    }
+    })
   );
