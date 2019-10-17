83e85f7be35007a0696f918401f68d3df0ecba65
Przemek Spaczek <pspaczek@divante.co>
2019-10-17 15:26:21 +0200

    rewrote SfLoader stories

--- packages/vue/src/components/atoms/SfLoader/SfLoader.stories.js
+++ packages/vue/src/components/atoms/SfLoader/SfLoader.stories.js
@@ -1,47 +1,26 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, select } from "@storybook/addon-knobs";
-import { generateStorybookTable } from "@/helpers";
 
 import SfLoader from "./SfLoader.vue";
 
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    ["$loader__spinner-color", "$c-accent-primary", "color for loader spinner"],
-    ["$loader__overlay-color", "white", "color for loader overlay"]
-  ]
-};
-
 storiesOf("Atoms|Loader", module)
-  .addDecorator(withKnobs)
   .add(
-  "Basic",
+    "[slot] default",
     () => ({
       components: { SfLoader },
-      data: () => ({
-          isLoading: select("isLoading (prop)", [true, false], true),
-          style: { width: "400px", height: "200px", border: "1px solid #eee" }
-      }),
-      created() {
-        setTimeout(() => {
-          this.isLoading = false;
-        }, 3000);
+      data(){
+        return {
+          isLoading: true
+        }
       },
-
-      template: `<div 
-      :style="style">
-        <SfLoader 
-          :loading="isLoading">
-          <p>Content is loaded</p>
-        </SfLoader>
-      </div>`
-    }),
-    {
-      info: {
-        summary: `<h2> Usage </h2>
-        <pre><code>import { SfLoader } from "@storefront-ui/vue"</code></pre>
-        ${generateStorybookTable(scssTableConfig, "SCSS variables")}`
-      }
-    }
+      mounted(){
+        window.setTimeout(()=>{
+          this.isLoading = false
+        }, 3000)
+      },
+      template: `<SfLoader
+        :loading="isLoading">
+        <span></span>
+      </SfLoader>`
+    })
   );
