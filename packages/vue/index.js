24f25e3fb2cf6236dd5e1d129b82ffd490c40733
Petar <p.petrov@codecoda.com>
2019-10-17 17:24:22 +0300

    Update index and js files

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,4 +1,4 @@
-/** Package root */
+// Auto-generated file by create-index-files.js. Do not edit manually
 import SfArrow from "./src/components/atoms/SfArrow/SfArrow.vue";
 import SfBadge from "./src/components/atoms/SfBadge/SfBadge.vue";
 import SfBreadcrumbs from "./src/components/atoms/SfBreadcrumbs/SfBreadcrumbs.vue";
@@ -10,13 +10,13 @@ import SfCircleIcon from "./src/components/atoms/SfCircleIcon/SfCircleIcon.vue";
 import SfDivider from "./src/components/atoms/SfDivider/SfDivider.vue";
 import SfHeading from "./src/components/atoms/SfHeading/SfHeading.vue";
 import SfIcon from "./src/components/atoms/SfIcon/SfIcon.vue";
+import SfImage from "./src/components/atoms/SfImage/SfImage.vue";
 import SfInput from "./src/components/atoms/SfInput/SfInput.vue";
 import SfLoader from "./src/components/atoms/SfLoader/SfLoader.vue";
 import SfOverlay from "./src/components/atoms/SfOverlay/SfOverlay.vue";
 import SfPrice from "./src/components/atoms/SfPrice/SfPrice.vue";
 import SfProperty from "./src/components/atoms/SfProperty/SfProperty.vue";
 import SfRating from "./src/components/atoms/SfRating/SfRating.vue";
-
 import SfAddToCart from "./src/components/molecules/SfAddToCart/SfAddToCart.vue";
 import SfAlert from "./src/components/molecules/SfAlert/SfAlert.vue";
 import SfBanner from "./src/components/molecules/SfBanner/SfBanner.vue";
@@ -25,9 +25,9 @@ import SfCharacteristic from "./src/components/molecules/SfCharacteristic/SfChar
 import SfCounter from "./src/components/molecules/SfCounter/SfCounter.vue";
 import SfFilter from "./src/components/molecules/SfFilter/SfFilter.vue";
 import SfGallery from "./src/components/molecules/SfGallery/SfGallery.vue";
-import SfImage from "./src/components/molecules/SfImage/SfImage.vue";
 import SfMenuItem from "./src/components/molecules/SfMenuItem/SfMenuItem.vue";
 import SfModal from "./src/components/molecules/SfModal/SfModal.vue";
+import SfNotification from "./src/components/molecules/SfNotification/SfNotification.vue";
 import SfOptions from "./src/components/molecules/SfOptions/SfOptions.vue";
 import SfPagination from "./src/components/molecules/SfPagination/SfPagination.vue";
 import SfProductOption from "./src/components/molecules/SfProductOption/SfProductOption.vue";
@@ -37,13 +37,15 @@ import SfSearchBar from "./src/components/molecules/SfSearchBar/SfSearchBar.vue"
 import SfSection from "./src/components/molecules/SfSection/SfSection.vue";
 import SfSelect from "./src/components/molecules/SfSelect/SfSelect.vue";
 import SfSlidingSection from "./src/components/molecules/SfSlidingSection/SfSlidingSection.vue";
+import SfSteps from "./src/components/molecules/SfSteps/SfSteps.vue";
 import SfSticky from "./src/components/molecules/SfSticky/SfSticky.vue";
-
 import SfAccordion from "./src/components/organisms/SfAccordion/SfAccordion.vue";
 import SfBannerGrid from "./src/components/organisms/SfBannerGrid/SfBannerGrid.vue";
 import SfBottomNavigation from "./src/components/organisms/SfBottomNavigation/SfBottomNavigation.vue";
 import SfCarousel from "./src/components/organisms/SfCarousel/SfCarousel.vue";
+import SfCollectedProduct from "./src/components/organisms/SfCollectedProduct/SfCollectedProduct.vue";
 import SfFooter from "./src/components/organisms/SfFooter/SfFooter.vue";
+import SfHeader from "./src/components/organisms/SfHeader/SfHeader.vue";
 import SfHero from "./src/components/organisms/SfHero/SfHero.vue";
 import SfList from "./src/components/organisms/SfList/SfList.vue";
 import SfMegaMenu from "./src/components/organisms/SfMegaMenu/SfMegaMenu.vue";
@@ -54,8 +56,8 @@ import SfTabs from "./src/components/organisms/SfTabs/SfTabs.vue";
 import SfTopBar from "./src/components/organisms/SfTopBar/SfTopBar.vue";
 
 export {
-  SfBadge,
   SfArrow,
+  SfBadge,
   SfBreadcrumbs,
   SfBullets,
   SfButton,
@@ -65,6 +67,7 @@ export {
   SfDivider,
   SfHeading,
   SfIcon,
+  SfImage,
   SfInput,
   SfLoader,
   SfOverlay,
@@ -79,9 +82,9 @@ export {
   SfCounter,
   SfFilter,
   SfGallery,
-  SfImage,
   SfMenuItem,
   SfModal,
+  SfNotification,
   SfOptions,
   SfPagination,
   SfProductOption,
@@ -91,12 +94,15 @@ export {
   SfSection,
   SfSelect,
   SfSlidingSection,
+  SfSteps,
   SfSticky,
   SfAccordion,
   SfBannerGrid,
   SfBottomNavigation,
   SfCarousel,
+  SfCollectedProduct,
   SfFooter,
+  SfHeader,
   SfHero,
   SfList,
   SfMegaMenu,
