7cfc14e05423f7c8b5d775acbd2dc1902046461b
Przemek Spaczek <pspaczek@divante.co>
2019-10-18 16:45:38 +0200

    added story source addon to storybook

--- packages/vue/config/storybook/addons.js
+++ packages/vue/config/storybook/addons.js
@@ -5,4 +5,5 @@ import "@storybook/addon-knobs/register";
 import "@storybook/addon-a11y/register";
 import "@storybook/addon-links/register";
 import "@storybook/addon-viewport/register";
-import "@storybook/addon-backgrounds/register";
+// import "@storybook/addon-backgrounds/register";
+import "@storybook/addon-storysource/register";
