2be9a7e4156fec4e3bc1d7a869cce86b71d4d8be
Przemek Spaczek <pspaczek@divante.co>
2019-10-22 16:01:42 +0200

    add SfContentPages stories

--- packages/vue/src/components/organisms/SfContentPages/SfContentPages.stories.js
+++ packages/vue/src/components/organisms/SfContentPages/SfContentPages.stories.js
@@ -1,53 +1,85 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, text, select } from "@storybook/addon-knobs";
-import { generateStorybookTable } from "@/helpers";
 
 import SfContentPages from "./SfContentPages.vue";
+import SfTabs from "../SfTabs/SfTabs.vue";
 
-// use this to document scss vars
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [["$component-size", "1.438rem", "size of checkmark"]]
-};
-
-// use this to document events
-const eventsTableConfig = {
-  tableHeadConfig: ["NAME", "DESCRIPTION"],
-  tableBodyConfig: [["input", "event emited when option is selected"]]
-};
-
-// storiesOf("Organisms|ContentPages", module)
-//   .addDecorator(withKnobs)
-//   .add(
-//     "[slot] default",
-//     () => ({
-//       props: {
-//         editableProp: {
-//           default: text("(prop) propname")
-//         },
-//         customClass: {
-//           default: select(
-//             "CSS Modifier",
-//             ["null", "sf-content-pages--modifier"],
-//             "null",
-//             "CSS-Modifiers"
-//           )
-//         }
-//       },
-//       components: { SfContentPages },
-//       template: `<SfContentPages
-//         :class="customClass"
-//       >
-//       </SfContentPages>`
-//     }),
-//     {
-//       info: {
-//         summary: `<p>Component description.</p>
-//        <h2>Usage</h2>
-//        <pre><code>import { SfContentPages } from "@storefront-ui/vue"</code></pre>
-//        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-//        ${generateStorybookTable(eventsTableConfig, "Events")}`
-//       }
-//     }
-//   );
+storiesOf("Organisms|ContentPages", module).add("[slot] default", () => ({
+  components: {
+    SfContentPages,
+    SfTabs
+  },
+  props: {},
+  data() {
+    return {
+      activePage: "About us"
+    };
+  },
+  template: `<div>
+      <SfContentPages
+        :active="activePage"
+        @click:change="updateActive"
+        title="Help">
+        <SfContentPage title="About us">
+          <SfTabs :open-tab="1">
+            <SfTab title="Security">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only covers
+              information collected at this website, and does not cover any
+              information collected offline by Luma. All Luma websites are covered
+              by this privacy policy.
+            </SfTab>
+            <SfTab title="Privacy Policy">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only covers
+              information collected at this website, and does not cover any
+              information collected offline by Luma. All Luma websites are covered
+              by this privacy policy.
+            </SfTab>
+            <SfTab title="Additional Information ">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only covers
+              information collected at this website, and does not cover any
+              information collected offline by Luma. All Luma websites are covered
+              by this privacy policy.
+            </SfTab>
+          </SfTabs>
+        </SfContentPage>
+        <SfContentPage title="Customer service">
+          <SfTabs :open-tab="1">
+            <SfTab title="Size guide">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only covers
+              information collected at this website, and does not cover any
+              information collected offline by Luma. All Luma websites are covered
+              by this privacy policy.
+            </SfTab>
+            <SfTab title="Where's my order?">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only covers
+              information collected at this website, and does not cover any
+              information collected offline by Luma. All Luma websites are covered
+              by this privacy policy.
+            </SfTab>
+          </SfTabs>
+        </SfContentPage>
+        <SfContentPage title="Store locator">
+          This website ("website") is operated by Luma Inc., which includes
+          Luma stores, and Luma Private Sales. This privacy policy only covers
+          information collected at this website, and does not cover any
+          information collected offline by Luma. All Luma websites are covered
+          by this privacy policy.
+        </SfContentPage>
+      </SfContentPages>
+      <button 
+        v-if="activePage" 
+        @click="activePage = ''" 
+        style="position: absolute; bottom: .5rem; left: .5rem; border: 0; background-color: transparent" 
+        class="mobile-only">back</button>
+    </div>`,
+  methods: {
+    updateActive(title) {
+      this.activePage = title;
+    }
+  }
+}));
