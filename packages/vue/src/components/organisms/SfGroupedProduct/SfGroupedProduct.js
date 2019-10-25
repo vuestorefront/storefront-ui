5be94c5b24e8a26444370b2a23746cd584ff7434
Przemek Spaczek <pspaczek@divante.co>
2019-10-25 10:00:35 +0200

    create first DOM draft

--- packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.js
+++ packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.js
@@ -1,3 +1,60 @@
+import SfPrice from "../../atoms/SfPrice/SfPrice.vue";
+import SfImage from "../../atoms/SfImage/SfImage.vue";
+import SfInput from "../../atoms/SfInput/SfInput.vue";
+
 export default {
-  name: "SfGroupedProduct"
+  name: "SfGroupedProduct",
+  components: {
+    SfImage,
+    SfPrice,
+    SfInput
+  },
+  props: {
+    /**
+     * Product image
+     * It should be an url of the product
+     */
+    image: {
+      type: String,
+      default: "assets/storybook/product_thumb.jpg"
+    },
+    /**
+     * Product title
+     */
+    title: {
+      type: String,
+      required: true
+    },
+    /**
+     * Product regular price
+     */
+    regularPrice: {
+      type: [Number, String],
+      default: null
+    },
+    /**
+     * Product special price
+     */
+    specialPrice: {
+      type: [Number, String],
+      default: null
+    },
+    /**
+     * Selected quantity
+     */
+    qty: {
+      type: String,
+      default: "1"
+    },
+    /**
+     * Stock quantity of product
+     */
+    stock: {
+      type: Number,
+      default: 1
+    }
+  },
+  model: {
+    prop: "qty"
+  }
 };
--- packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.stories.js
+++ packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.stories.js
@@ -1,34 +1,40 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import {
-  withKnobs,
-  text,
-  optionsKnob as options
-} from "@storybook/addon-knobs";
+import { withKnobs, text, number } from "@storybook/addon-knobs";
 
 import SfGroupedProduct from "./SfGroupedProduct.vue";
 
 storiesOf("Organisms|GroupedProduct", module)
   .addDecorator(withKnobs)
   .add("[slot] default", () => ({
+    components: { SfGroupedProduct },
     props: {
-      customClass: {
-        default: options(
-          "CSS modifier",
-          {
-            "sf-grouped-product--modifier":
-              "sf-grouped-product--modifier"
-          },
-          "",
-          { display: "multi-select" }
-        )
+      image: {
+        default: text("image (prop)", "/assets/storybook/homepage/productB.jpg")
+      },
+      title: {
+        default: text("title (prop)", "Product name")
+      },
+      regularPrice: {
+        default: text("regularPrice (prop)", "$10,99")
+      },
+      specialPrice: {
+        default: text("specialPrice (prop)", "$5,99")
+      },
+      stock: {
+        default: number("stock (prop)", 99)
       }
     },
-    editableProp: {
-      default: text("propname (prop)", "propvalue")
+    data() {
+      return {
+        qty: "1"
+      };
     },
-    components: { SfGroupedProduct },
     template: `<SfGroupedProduct
-      :class="customClass" />`
+      :image="image"
+      :title="title"
+      :specialPrice="specialPrice"
+      :regularPrice="regularPrice"
+      :stock="stock"
+      v-model="qty" />`
   }));
-
