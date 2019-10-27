7010f2bf66e861e3663b5d5b641b6f49afcbbf86
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-10-28 01:02:20 +0700

    Add cartIcon, wishlistIcon, and accountIcon props with style and SfIcon

--- packages/vue/src/components/organisms/SfHeader/SfHeader.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.js
@@ -1,7 +1,11 @@
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
 const DEFAULT_LOGO = "/assets/logo.svg";
 
 export default {
   name: "SfHeader",
+  components: {
+    SfIcon
+  },
   props: {
     logo: {
       type: String,
@@ -10,11 +14,28 @@ export default {
     navigations: {
       type: Array,
       default: () => []
+    },
+    cartIcon: {
+      type: String,
+      default: "empty_cart"
+    },
+    wishlistIcon: {
+      type: String,
+      default: "heart"
+    },
+    accountIcon: {
+      type: String,
+      default: "profile"
     }
   },
   computed: {
     isDefaultLogo() {
       return this.logo === DEFAULT_LOGO;
+    }
+  },
+  methods: {
+    onClickEmit(name) {
+      this.$emit(`click:${name}`);
     }
   }
 };
