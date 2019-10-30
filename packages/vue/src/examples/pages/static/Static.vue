ad20a39779dc91239626a8039b8bbf64bd15f796
Przemek Spaczek <pspaczek@divante.co>
2019-10-30 16:50:41 +0100

    add Static example page

---
+++ packages/vue/src/examples/pages/static/Static.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="static"></div>
+</template>
+<script>
+export default {
+  name: "Static"
+};
+</script>
+<style lang="scss" scoped>
+@import "~@storefront-ui/vue/styles";
+
+@mixin for-desktop {
+  @media screen and (min-width: $desktop-min) {
+    @content;
+  }
+}
+
+#static {
+  box-sizing: border-box;
+  @include for-desktop {
+    max-width: 1240px;
+    margin: auto;
+  }
+}
+</style>
