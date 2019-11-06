ad7a7db28f0e4dc24c6b66416a8e098f6062e5a6
Przemek Spaczek <pspaczek@divante.co>
2019-11-06 12:19:51 +0100

    add Cart page stories

---
+++ packages/vue/src/examples/pages/cart/Cart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="cart">
+    <SfSidebar :visible="true" class="sf-sidebar--right">
+      <div class="my-cart">
+        My Cart
+      </div>
+    </SfSidebar>
+  </div>
+</template>
+
+<script>
+import { SfSidebar } from "../../../../index.js";
+export default {
+  name: "Cart",
+  components: {
+    SfSidebar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@storefront-ui/vue/styles";
+
+@mixin for-desktop {
+  @media screen and (min-width: $desktop-min) {
+    @content;
+  }
+}
+
+#cart {
+  box-sizing: border-box;
+  @include for-desktop {
+    max-width: 1240px;
+    margin: auto;
+  }
+}
+</style>
