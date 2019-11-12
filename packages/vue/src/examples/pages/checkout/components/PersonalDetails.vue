726aaa286ec30224cbd77bef5a8087255dd0688c
Przemek Spaczek <pspaczek@divante.co>
2019-11-12 12:06:08 +0100

    add style to form

--- packages/vue/src/examples/pages/checkout/components/PersonalDetails.vue
+++ packages/vue/src/examples/pages/checkout/components/PersonalDetails.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="personal-details">
-    <SfHeading level="2" title="1. Personal Details" class="sf-heading--left" />
-    <SfInput label="First name" name="first-name" required />
-    <SfInput label="Last name" name="last-name" required />
-    <SfInput label="Your email" name="email" required />
-    <SfCheckbox label="I want to create an account" name="create-account" />
-    <span>+info</span>
-    <SfButton>Continue to shopping</SfButton>
-    <span>or log in to your account</span>
+    <SfHeading
+      :level="2"
+      title="1. Personal Details"
+      class="sf-heading--left sf-heading--no-underline"
+    />
+    <div class="form">
+      <SfInput
+        class="form__element"
+        label="First name"
+        name="first-name"
+        required
+      />
+      <SfInput
+        class="form__element form__element--half"
+        label="Last name"
+        name="last-name"
+        required
+      />
+      <SfInput
+        class="form__element form__element--half"
+        label="Your email"
+        name="email"
+        required
+      />
+      <SfCheckbox
+        class="form__element"
+        label="I want to create an account"
+        name="create-account"
+      />
+      <SfButton class="form__element form__element--half"
+        >Continue to shopping</SfButton
+      >
+      <span class="form__element form__element--half"
+        >or log in to your account</span
+      >
+    </div>
   </div>
 </template>
 
@@ -29,4 +57,21 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import "~@storefront-ui/vue/styles";
+.form {
+  display: flex;
+  flex-wrap: wrap;
+  &__element {
+    flex: 0 0 100%;
+    margin: $spacer-big 0;
+    align-self: center;
+    &--half {
+      flex: auto;
+      & + & {
+        margin-left: $spacer-extra-big;
+      }
+    }
+  }
+}
+</style>
