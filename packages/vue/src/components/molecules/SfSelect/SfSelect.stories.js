89668cd12fb5444ffb365b474bbc6a135fb8008f
Przemek Spaczek <pspaczek@divante.co>
2019-11-13 20:05:28 +0100

    update SfSelect stories

--- packages/vue/src/components/molecules/SfSelect/SfSelect.stories.js
+++ packages/vue/src/components/molecules/SfSelect/SfSelect.stories.js
@@ -13,7 +13,7 @@ import SfProductOption from "../SfProductOption/SfProductOption.vue";
 
 storiesOf("Molecules|Select", module)
   .addDecorator(withKnobs)
-  .add("Default", () => ({
+  .add("[slot] default", () => ({
     components: { SfSelect, SfProductOption },
     props: {
       customClass: {
@@ -64,6 +64,66 @@ storiesOf("Molecules|Select", module)
         :required="required"
         :valid="valid"
         :error-message="errorMessage">
+        <SfSelectOption v-for="(option, key) in options" :key="key" :value="option.value">
+          <SfProductOption :color="option.color" :label="option.label"></SfProductOption>
+        </SfSelectOption>
+      </SfSelect>
+    </div>`
+  }))
+  .add("[slot] label", () => ({
+    components: { SfSelect, SfProductOption },
+    props: {
+      customClass: {
+        default: options(
+          "CSS modifier",
+          {
+            "sf-select--bordered": "sf-select--bordered",
+            "sf-select--underlined": "sf-select--underlined"
+          },
+          "sf-select--underlined",
+          { display: "multi-select" }
+        )
+      },
+      label: {
+        default: text("label (prop)", "Color")
+      },
+      size: {
+        default: number("size (prop)", 5)
+      },
+      required: {
+        default: boolean("required (prop)", false)
+      },
+      valid: {
+        default: boolean("valid (prop)", true)
+      },
+      errorMessage: {
+        default: text("label (prop)", "Color")
+      }
+    },
+    data() {
+      return {
+        selected: "",
+        options: [
+          { value: "amaranth", color: "#E52B50", label: "Amaranth" },
+          { value: "amber", color: "#FFBF00", label: "Amber" },
+          { value: "arctic-lime", color: "#D0FF14", label: "Arctic lime" },
+          { value: "bluetiful", color: "#3C69E7", label: "Bluetiful" },
+          { value: "buff", color: "#F0DC82", label: "Buff" }
+        ]
+      };
+    },
+    template: `<div style="max-width: 18.75rem">
+      <SfSelect
+        v-model="selected"
+        :class="customClass"
+        :label="label"
+        :size="size"
+        :required="required"
+        :valid="valid"
+        :error-message="errorMessage">
+        <template #label>
+          <!-- slot content -->
+        </template>
         <SfSelectOption v-for="(option, key) in options" :key="key" :value="option.value">
           <SfProductOption :color="option.color" :label="option.label"></SfProductOption>
         </SfSelectOption>
