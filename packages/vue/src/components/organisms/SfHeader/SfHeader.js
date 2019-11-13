07577bcdd121771be2a794b0f44a0586eabc2cf0
Anna <amusial@divante.pl>
2019-11-13 12:20:34 +0100

    Added comments

--- packages/vue/src/components/organisms/SfHeader/SfHeader.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.js
@@ -6,22 +6,37 @@ export default {
     SfIcon
   },
   props: {
+    /**
+     * Heading logo
+     */
     logo: {
       type: String,
       default: ""
     },
+    /**
+     * Heading navigations
+     */
     navigations: {
       type: Array,
       default: () => []
     },
+    /**
+     * Heading cartIcon
+     */
     cartIcon: {
       type: String,
       default: "empty_cart"
     },
+    /**
+     * Heading wishlistIcon
+     */
     wishlistIcon: {
       type: String,
       default: "heart"
     },
+    /**
+     * Heading accountIcon
+     */
     accountIcon: {
       type: String,
       default: "profile"
