7206f436ffced5b789dd0312f21e7eed0adb56e1
Anna <amusial@divante.pl>
2019-11-13 13:33:42 +0100

    Fixed tests

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -50,23 +50,3 @@ storiesOf("Organisms/Header", module)
     template: `<SfHeader 
       :logo="logo" />`
   }));
-
-// export default storiesOf("Organisms/Header", module)
-//   .add("mobile with icons", () => ({
-//     components: { SfHeader },
-//     template: `
-//     <sf-header>
-//       <img src="arrow_left.svg" alt="arrow" slot="left"/>
-//       <img src="arrow_right.svg" alt="arrow" slot="right"/>
-//     </sf-header>`,
-//     methods: {}
-//   }))
-//   .add("mobile with text", () => ({
-//     components: { SfHeader },
-//     template: `
-//     <sf-header>
-//       <img src="arrow_left.svg" alt="arrow" slot="left"/>
-//       <template slot="middle">Women</template>
-//     </sf-header>`,
-//     methods: {}
-//   }));
