7a9348634ef131bd4cacb910e830ca566a177ba9
Anna <amusial@divante.pl>
2019-11-14 10:09:59 +0100

    Added remaining stories

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -143,5 +143,117 @@ storiesOf("Organisms/Header", module)
       :navigations="navigations"
       :cart-icon="cartIcon" 
       :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon" />`
+      :account-icon="accountIcon">
+      <template slot="search">
+          <img src="assets/storybook/doge.svg" style="height: 25px; margin-left: auto;"/>
+      </template>
+      </SfHeader>`
+  }))
+  .add("[slot] accountIcon", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      navigations: {
+        default: array("(prop) navigations", [
+          {
+            text: "WOMEN",
+            href: ""
+          },
+          {
+            text: "MEN",
+            href: ""
+          }
+        ])
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "mail")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigations="navigations"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      </SfHeader>`
+  }))
+  .add("[slot] cartIcon", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      navigations: {
+        default: array("(prop) navigations", [
+          {
+            text: "WOMEN",
+            href: ""
+          },
+          {
+            text: "MEN",
+            href: ""
+          }
+        ])
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "cross")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "profile")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigations="navigations"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      </SfHeader>`
+  }))
+  .add("[slot] wishlistIcon", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      navigations: {
+        default: array("(prop) navigations", [
+          {
+            text: "WOMEN",
+            href: ""
+          },
+          {
+            text: "MEN",
+            href: ""
+          }
+        ])
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "marker")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "profile")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigations="navigations"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      </SfHeader>`
   }));
