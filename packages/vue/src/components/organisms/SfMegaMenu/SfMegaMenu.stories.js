07ed99c15bddc12f965842bc8fc34b03242ea0df
Anna <amusial@divante.pl>
2019-11-14 13:56:12 +0100

    Cleaned stories and styles

--- packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.stories.js
+++ packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.stories.js
@@ -2,16 +2,9 @@ import { storiesOf } from "@storybook/vue";
 
 import SfMegaMenu from "./SfMegaMenu.vue";
 
-storiesOf("Organisms|MegaMenu", module).add(
-  "Basic",
-  () => ({
-    components: { SfMegaMenu },
-    template: "<SfMegaMenu>Hello Mega-Menu</SfMegaMenu>"
-  }),
-  {
-    info: {
-      summary: `<h2> Usage </h2>
-      <pre><code>import { SfMegaMenu } from "@storefront-ui/vue"</code></pre>`
-    }
-  }
-);
+storiesOf("Organisms|MegaMenu", module).add("Default", () => ({
+  components: { SfMegaMenu },
+  template: `<SfMegaMenu>
+    Hello
+    </SfMegaMenu>`
+}));
