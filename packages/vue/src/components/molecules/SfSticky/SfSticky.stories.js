d68d690eca748cd85274e8ea008d1e903e7abed9
Przemek Spaczek <pspaczek@divante.co>
2019-11-14 10:03:54 +0100

    move stories placeholder and wrapper to component

--- packages/vue/src/components/molecules/SfSticky/SfSticky.stories.js
+++ packages/vue/src/components/molecules/SfSticky/SfSticky.stories.js
@@ -1,21 +1,30 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
 
-import SfSticky from "./SfSticky.vue";
-
-storiesOf("Molecules|Sticky", module).add("[slot] default", () => ({
-  components: { SfSticky },
+const StoriesWrapper = {
   template: `<div style="display: flex;">
     <div style="flex: 1; margin-right: 1.25rem">
       <div style="height: 25rem; border: 1px solid #f2f2f2;"></div>
       <div style="height: 25rem; margin-top: 1.25rem; border: 1px solid #f2f2f2;"></div>
     </div>
     <div style="flex: 1">
-      <SfSticky>
-        <div style="display: flex; flex: 1; align-items:center; justify-content:center; height: 18.75rem; background-color: #f2f2f2;">
-          [#default slot content]
-        </div>
-      </SfSticky>
+      <slot/>
     </div>
   </div>`
+};
+const StoriesPlaceholder = {
+  template: `<div style="display: flex; flex: 1; align-items:center; justify-content:center; height: 18.75rem; background-color: #f2f2f2;">
+        [#default slot content]
+      </div>`
+};
+
+import SfSticky from "./SfSticky.vue";
+
+storiesOf("Molecules|Sticky", module).add("[slot] default", () => ({
+  components: { SfSticky, StoriesWrapper, StoriesPlaceholder },
+  template: `<StoriesWrapper>
+    <SfSticky>
+      <StoriesPlaceholder/>
+    </SfSticky>
+  </StoriesWrapper>`
 }));
