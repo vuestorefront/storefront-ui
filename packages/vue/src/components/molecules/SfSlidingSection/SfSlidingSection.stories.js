e754eb61303940148394b624740576b9e74c8099
Przemek Spaczek <pspaczek@divante.co>
2019-11-15 10:56:07 +0100

    update SfSlidingSection stories

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.stories.js
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.stories.js
@@ -1,21 +1,28 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
 
+const StoriesPlaceholderStatic = {
+  template: `<div style="display: flex; align-items:center; justify-content:center; height: 34.6875rem; background-color: #f2f2f2;">[#static slot content]</div>`
+};
+const StoriesPlaceholderSliding = {
+  template: `<div style="display: flex; align-items:center; justify-content:center; height: 18.75rem; background-color: #f2f2f2;">[#sliding slot content]</div>`
+};
+
 import SfSlidingSection from "./SfSlidingSection.vue";
 
 storiesOf("Molecules|SlidingSection", module).add("Default", () => ({
-  components: { SfSlidingSection },
+  components: {
+    SfSlidingSection,
+    StoriesPlaceholderStatic,
+    StoriesPlaceholderSliding
+  },
   template: `<div style="max-width: 64rem; margin: auto">
       <SfSlidingSection>
         <template #static>
-          <div style="display: flex; align-items:center; justify-content:center; height: 34.6875rem; background-color: #f2f2f2;">
-            [#static slot content]
-          </div>
+          <StoriesPlaceholderStatic />
         </template>
         <template #sliding>
-          <div style="display: flex; align-items:center; justify-content:center; height: 18.75rem; background-color: #f2f2f2;">
-            [#sliding slot content]
-          </div>
+          <StoriesPlaceholderSliding />
         </template>
       </SfSlidingSection>
     </div>`
