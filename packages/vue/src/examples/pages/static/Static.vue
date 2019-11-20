1dea27ba95a2f88cc211cb86f926d519b5a7d8a9
Przemek Spaczek <pspaczek@divante.co>
2019-11-20 13:10:18 +0100

    adjust Static example

--- packages/vue/src/examples/pages/static/Static.vue
+++ packages/vue/src/examples/pages/static/Static.vue
@@ -27,56 +27,58 @@
       title="Help"
       class="static-content"
     >
-      <SfContentPage title="About us">
-        <SfTabs :open-tab="1">
-          <SfTab title="Security">
-            This website ("website") is operated by Luma Inc., which includes
-            Luma stores, and Luma Private Sales. This privacy policy only covers
-            information collected at this website, and does not cover any
-            information collected offline by Luma. All Luma websites are covered
-            by this privacy policy.
-          </SfTab>
-          <SfTab title="Privacy Policy">
-            This website ("website") is operated by Luma Inc., which includes
-            Luma stores, and Luma Private Sales. This privacy policy only covers
-            information collected at this website, and does not cover any
-            information collected offline by Luma. All Luma websites are covered
-            by this privacy policy.
-          </SfTab>
-          <SfTab title="Additional Information ">
-            This website ("website") is operated by Luma Inc., which includes
-            Luma stores, and Luma Private Sales. This privacy policy only covers
-            information collected at this website, and does not cover any
-            information collected offline by Luma. All Luma websites are covered
-            by this privacy policy.
-          </SfTab>
-        </SfTabs>
-      </SfContentPage>
-      <SfContentPage title="Customer service">
-        <SfTabs :open-tab="1">
-          <SfTab title="Size guide">
-            This website ("website") is operated by Luma Inc., which includes
-            Luma stores, and Luma Private Sales. This privacy policy only covers
-            information collected at this website, and does not cover any
-            information collected offline by Luma. All Luma websites are covered
-            by this privacy policy.
-          </SfTab>
-          <SfTab title="Where's my order?">
-            This website ("website") is operated by Luma Inc., which includes
-            Luma stores, and Luma Private Sales. This privacy policy only covers
-            information collected at this website, and does not cover any
-            information collected offline by Luma. All Luma websites are covered
-            by this privacy policy.
-          </SfTab>
-        </SfTabs>
-      </SfContentPage>
-      <SfContentPage title="Store locator">
-        This website ("website") is operated by Luma Inc., which includes Luma
-        stores, and Luma Private Sales. This privacy policy only covers
-        information collected at this website, and does not cover any
-        information collected offline by Luma. All Luma websites are covered by
-        this privacy policy.
-      </SfContentPage>
+      <SfContentCategory>
+        <SfContentPage title="About us">
+          <SfTabs :open-tab="1">
+            <SfTab title="Security">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only
+              covers information collected at this website, and does not cover
+              any information collected offline by Luma. All Luma websites are
+              covered by this privacy policy.
+            </SfTab>
+            <SfTab title="Privacy Policy">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only
+              covers information collected at this website, and does not cover
+              any information collected offline by Luma. All Luma websites are
+              covered by this privacy policy.
+            </SfTab>
+            <SfTab title="Additional Information ">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only
+              covers information collected at this website, and does not cover
+              any information collected offline by Luma. All Luma websites are
+              covered by this privacy policy.
+            </SfTab>
+          </SfTabs>
+        </SfContentPage>
+        <SfContentPage title="Customer service">
+          <SfTabs :open-tab="1">
+            <SfTab title="Size guide">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only
+              covers information collected at this website, and does not cover
+              any information collected offline by Luma. All Luma websites are
+              covered by this privacy policy.
+            </SfTab>
+            <SfTab title="Where's my order?">
+              This website ("website") is operated by Luma Inc., which includes
+              Luma stores, and Luma Private Sales. This privacy policy only
+              covers information collected at this website, and does not cover
+              any information collected offline by Luma. All Luma websites are
+              covered by this privacy policy.
+            </SfTab>
+          </SfTabs>
+        </SfContentPage>
+        <SfContentPage title="Store locator">
+          This website ("website") is operated by Luma Inc., which includes Luma
+          stores, and Luma Private Sales. This privacy policy only covers
+          information collected at this website, and does not cover any
+          information collected offline by Luma. All Luma websites are covered
+          by this privacy policy.
+        </SfContentPage>
+      </SfContentCategory>
     </SfContentPages>
   </div>
 </template>
