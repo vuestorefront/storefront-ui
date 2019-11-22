6176605fc6722e7590f611fd32c3ce35071b23ad
Anna <amusial@divante.pl>
2019-11-22 13:51:11 +0100

    Finished styling for desktop

--- packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.stories.js
+++ packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.stories.js
@@ -6,22 +6,62 @@ import SfMegaMenuColumn from "./_internal/SfMegaMenuColumn.vue";
 import SfImage from "../../atoms/SfImage/SfImage.vue";
 
 storiesOf("Organisms|MegaMenu", module).add("Default", () => ({
+  data: () => {
+    return {
+      categories: [
+        {
+          header: "Clothing",
+          items: [
+            { label: "Skirts" },
+            { label: "Sweaters" },
+            { label: "Dresses" },
+            { label: "TShirts" },
+            { label: "Pants" },
+            { label: "Underwear" },
+            { label: "Jackets" },
+            { label: "Blouses" }
+          ]
+        },
+        {
+          header: "Accessories",
+          items: [
+            { label: "Bags & Purses" },
+            { label: "Belts" },
+            { label: "Gloves" },
+            { label: "Hats" }
+          ]
+        },
+        {
+          header: "Shoes",
+          items: [
+            { label: "Boots" },
+            { label: "Heels" },
+            { label: "Flat shoes" },
+            { label: "Loafers" },
+            { label: "Sandals" },
+            { label: "Slippers" },
+            { label: "Trainers" }
+          ]
+        }
+      ]
+    };
+  },
   components: { SfMegaMenu, SfMenuItem, SfMegaMenuColumn, SfImage },
   template: `<SfMegaMenu>
-    <SfMegaMenuColumn>
-    <div>
-      <h3>Clothing</h3>
-      <SfMenuItem label="Skirts"></SfMenuItem>
-    </div>
+    <SfMegaMenuColumn style="padding: 0 6rem 0 2rem;">
+      <div v-for="category in categories">
+        <h3>{{ category.header }}</h3>
+        <SfMenuItem style="margin: 1rem 0;" v-for="item in category.items" :label="item.label"></SfMenuItem>
+      </div>
     </SfMegaMenuColumn>
-    <SfMegaMenuColumn>
+    <SfMegaMenuColumn style="padding: 0 2rem 0 6rem;">
       <div>
         <h3>LAST PAIRS LEFT</h3>
-        <SfImage src="assets/storybook/megamenu/bannerSandals.jpg"/>
+        <SfImage style="margin: 1rem 1rem 0 0 ;" src="assets/storybook/megamenu/bannerSandals.jpg"/>
       </div>
       <div>
         <h3>BEACH BAGS 2=1</h3>
-        <SfImage src="assets/storybook/megamenu/bannerBeachBag.jpg"/>
+        <SfImage style="margin: 1rem 1rem 0 0;" src="assets/storybook/megamenu/bannerBeachBag.jpg"/>
       </div>
     </SfMegaMenuColumn>
     </SfMegaMenu>`
