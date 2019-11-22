62bb459e968afd9806f12b6f866fea03669e4cef
Anna <amusial@divante.pl>
2019-11-22 12:15:07 +0100

    Created template for mega-menu content

--- packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.stories.js
+++ packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.stories.js
@@ -1,10 +1,28 @@
 import { storiesOf } from "@storybook/vue";
 
 import SfMegaMenu from "./SfMegaMenu.vue";
+import SfMenuItem from "../../molecules/SfMenuItem/SfMenuItem.vue";
+import SfMegaMenuColumn from "./_internal/SfMegaMenuColumn.vue";
+import SfImage from "../../atoms/SfImage/SfImage.vue";
 
 storiesOf("Organisms|MegaMenu", module).add("Default", () => ({
-  components: { SfMegaMenu },
+  components: { SfMegaMenu, SfMenuItem, SfMegaMenuColumn, SfImage },
   template: `<SfMegaMenu>
-    Hello
+    <SfMegaMenuColumn>
+    <div>
+      <h3>Clothing</h3>
+      <SfMenuItem label="Skirts"></SfMenuItem>
+    </div>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn>
+      <div>
+        <h3>LAST PAIRS LEFT</h3>
+        <SfImage src="assets/storybook/megamenu/bannerSandals.jpg"/>
+      </div>
+      <div>
+        <h3>BEACH BAGS 2=1</h3>
+        <SfImage src="assets/storybook/megamenu/bannerBeachBag.jpg"/>
+      </div>
+    </SfMegaMenuColumn>
     </SfMegaMenu>`
 }));
---
+++ packages/vue/src/components/organisms/SfMegaMenu/_internal/SfMegaMenuColumn.vue
@@ -0,0 +1,2 @@
+<script src="./SfMegaMenuColumn.js"></script>
+<template lang="html" src="./SfMegaMenuColumn.html"></template>
