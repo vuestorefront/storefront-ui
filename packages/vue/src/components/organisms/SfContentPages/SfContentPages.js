df3a2b958f4af79a3ca26b392f29afedaae283ee
Przemek Spaczek <pspaczek@divante.co>
2019-11-24 11:31:52 +0100

    fix

--- packages/vue/src/components/organisms/SfContentPages/SfContentPages.js
+++ packages/vue/src/components/organisms/SfContentPages/SfContentPages.js
@@ -80,7 +80,7 @@ export default {
         this.$emit("click:change", "");
         return;
       }
-      this.$emit("click:change", this.categories[0].pages[0].title);
+      this.$emit("click:change", this.categories[0].items[0].title);
     }
   },
   methods: {
