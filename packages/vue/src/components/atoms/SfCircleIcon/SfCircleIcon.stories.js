431a4ecab7509b2ac38d57d2575e554c4e37b656
Przemek Spaczek <pspaczek@divante.co>
2019-12-02 16:07:00 +0100

    refactor SfCircleIcon

--- packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.stories.js
+++ packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.stories.js
@@ -2,6 +2,7 @@
 import { storiesOf } from "@storybook/vue";
 import {
   withKnobs,
+  text,
   select,
   optionsKnob as options
 } from "@storybook/addon-knobs";
@@ -35,11 +36,15 @@ storiesOf("Atoms|CircleIcon", module)
       },
       icon: {
         default: select("icon (prop)", iconsNames, "home")
+      },
+      iconSize: {
+        default: text("icon-size (prop)", "20px")
       }
     },
     template: `<SfCircleIcon
         :class="customClass"
-        :icon="icon" />`
+        :icon="icon" 
+        :icon-size="iconSize"/>`
   }))
   .add("[slot] default", () => ({
     components: { SfCircleIcon },
@@ -48,8 +53,6 @@ storiesOf("Atoms|CircleIcon", module)
         default: options(
           "CSS modifier",
           {
-            "sf-circle-icon--small": "sf-circle-icon--small",
-            "sf-circle-icon--big": "sf-circle-icon--big",
             "color-primary": "color-primary",
             "color-secondary": "color-secondary",
             "color-warning": "color-warning",
@@ -68,6 +71,6 @@ storiesOf("Atoms|CircleIcon", module)
     template: `<SfCircleIcon
       :class="customClass"
       :icon="icon">
-      <span style="font-size: 1.5rem">1</span>
+      <span style="font-size: 1.5rem; width: 20px; height: 20px; line-height: 0; display: flex; justify-content: center; align-items: center">1</span>
     </SfCircleIcon>`
   }));
