68ef8fa55e0eb02baf02bddc9ee216845cb9ce86
Przemek Spaczek <pspaczek@divante.co>
2019-12-06 10:58:04 +0100

    add all components to view

--- packages/vue/src/examples/pages/sign-in/SignIn.vue
+++ packages/vue/src/examples/pages/sign-in/SignIn.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="sign-in">
     <SfModal :visible="true">
+      <div>
+        <SfInput label="Your email"/>
+        <SfInput label="First Name"/>
+        <SfInput label="Last Name"/>
+        <SfInput label="Password" type="password"/>
+        <SfCheckbox label="I want to create an account"/>
+        <SfButton>Create an account</SfButton>
+        <div>or <SfButton class="sf-button--text">login in to your account</SfButton></div>
+      </div>
+      <div>
+        <SfInput label="Your email"/>
+        <SfInput label="Password" type="password"/>
+        <SfCheckbox label="Remember me"/>
+        <SfButton>Log in</SfButton>
+        <SfButton class="sf-button--text">Forgotten password?</SfButton>
+        <div>
+          Don't have and account yet?
+          <SfButton class="sf-button--text">Register today?</SfButton>
+        </div>
+      </div>
     </SfModal>
   </div>
 </template>
 
 <script>
-import { SfModal, Sfnput, SfButton, SfCheckbox } from "../../../../index.js";
+import { SfModal, SfInput, SfButton, SfCheckbox } from "../../../../index.js";
 export default {
   name: "SignIn",
-  components: { SfModal, Sfnput, SfButton, SfCheckbox }
+  components: { SfModal, SfInput, SfButton, SfCheckbox }
 };
 </script>
 
