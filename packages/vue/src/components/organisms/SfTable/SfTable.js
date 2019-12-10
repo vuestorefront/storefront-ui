f87eb9d80bfd7115aa914ca661ecf46de1cec594
Anna <amusial@divante.pl>
2019-12-10 16:33:01 +0100

    Registered components

--- packages/vue/src/components/organisms/SfTable/SfTable.js
+++ packages/vue/src/components/organisms/SfTable/SfTable.js
@@ -1,8 +1,14 @@
 import SfTableRow from "./_internal/SfTableRow";
+import SfTableData from "./_internal/SfTableData";
+import Vue from "vue";
+
+Vue.component("SfTableRow", SfTableRow);
+Vue.component("SfTableData", SfTableData);
 
 export default {
   name: "SfTable",
   components: {
-    SfTableRow
+    SfTableRow,
+    SfTableData
   }
 };
