237bddb7a6a4dcd670029240c3405931c39d50e8
Anna <amusial@divante.pl>
2019-12-12 13:10:13 +0100

    Added download icon, download button and views

--- packages/vue/src/components/organisms/SfTable/SfTable.js
+++ packages/vue/src/components/organisms/SfTable/SfTable.js
@@ -13,5 +13,8 @@ export default {
     SfButton,
     SfTableRow,
     SfTableData
+  },
+  mounted() {
+    console.log(this.$slots.default.length);
   }
 };
--- packages/vue/src/components/organisms/SfTable/SfTable.stories.js
+++ packages/vue/src/components/organisms/SfTable/SfTable.stories.js
@@ -2,11 +2,13 @@ import { storiesOf } from "@storybook/vue";
 import { withKnobs } from "@storybook/addon-knobs";
 
 import SfTable from "./SfTable.vue";
+import SfButton from "../../atoms/SfButton/SfButton.vue";
+import SfImage from "../../atoms/SfImage/SfImage.vue";
 
 storiesOf("Organisms/Table", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
-    components: { SfTable },
+    components: { SfTable, SfButton, SfImage },
     props: {
       headers: {
         type: Array,
@@ -28,14 +30,19 @@ storiesOf("Organisms/Table", module)
       }
     },
     template: `<SfTable>
-    <template #headers>
-    <SfTableRow>
-      <SfTableData :is-header="true" v-for="header in headers">{{ header }}</SfTableData>
-    </SfTableRow>
-    </template>
-    <template #content>
+    <template>
+      <SfTableRow>
+        <SfTableData :is-header="true" v-for="header in headers">{{ header }}</SfTableData>
+        <SfTableData :is-header="true">
+          <SfButton class="desktop-only">DOWNLOAD ALL</SfButton>
+        </SfTableData>
+      </SfTableRow>
       <SfTableRow v-for="data in content">
         <SfTableData v-for="item in data">{{ item }}</SfTableData>
+        <SfTableData>
+          <span>VIEW</span>
+          <SfImage class="desktop-only" src="assets/storybook/download.svg"/>
+        </SfTableData>
       </SfTableRow>
     </template>
     </SfTable>`
