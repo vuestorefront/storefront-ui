5a2c3dfeb4bbcd2c258b8ce2f55dc7a6d74431eb
Przemek Spaczek <pspaczek@divante.co>
2019-12-12 12:50:55 +0100

    add SfContentPages items

--- packages/vue/src/examples/pages/my-account/MyAccount.vue
+++ packages/vue/src/examples/pages/my-account/MyAccount.vue
@@ -1,12 +1,75 @@
 <template>
-  <div id="my-account"></div>
+  <div id="my-account">
+    <SfBreadcrumbs
+      class="breadcrumbs desktop-only"
+      :breadcrumbs="breadcrumbs"
+    />
+    <SfContentPages
+      title="My Account"
+      :active="activePage"
+      @click:change="changeActivePage"
+    >
+      <SfContentCategory title="Personal Details">
+        <SfContentPage title="My profile">
+          My profile
+        </SfContentPage>
+        <SfContentPage title="Shipping details">
+          Shipping details
+        </SfContentPage>
+        <SfContentPage title="Loyalty Card">
+          Loyalty Card
+        </SfContentPage>
+        <SfContentPage title="My newsletter">
+          My newsletter
+        </SfContentPage>
+      </SfContentCategory>
+      <SfContentCategory title="Order details">
+        <SfContentPage title="Order history">
+          Order history
+        </SfContentPage>
+        <SfContentPage title="My reviews">
+          My reviews
+        </SfContentPage>
+      </SfContentCategory>
+      <SfContentPage title="Log out" />
+    </SfContentPages>
+  </div>
 </template>
 <script>
+import { SfBreadcrumbs, SfContentPages } from "../../../../index.js";
 export default {
   name: "MyAccount",
-  components: {},
+  components: {
+    SfBreadcrumbs,
+    SfContentPages
+  },
   data() {
-    return {};
+    return {
+      activePage: "My profile",
+      breadcrumbs: [
+        {
+          text: "Home",
+          route: {
+            link: "#"
+          }
+        },
+        {
+          text: "My Account",
+          route: {
+            link: "#"
+          }
+        }
+      ]
+    };
+  },
+  methods: {
+    changeActivePage(title){
+      if(title === "Log out"){
+        alert("You are logged out!");
+        return;
+      }
+      this.activePage = title;
+    }
   }
 };
 </script>
@@ -25,5 +88,8 @@ export default {
     max-width: 1240px;
     margin: auto;
   }
+}
+.breadcrumbs {
+  padding: $spacer-big $spacer-extra-big $spacer-extra-big;
 }
 </style>
