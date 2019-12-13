c849c2305edea757c9dbc4ecb3df69ca8002e671
Anna <amusial@divante.pl>
2019-12-13 13:53:03 +0100

    semantic fixes

--- packages/vue/src/components/organisms/SfTable/_internal/SfTableRow.js
+++ packages/vue/src/components/organisms/SfTable/_internal/SfTableRow.js
@@ -1,10 +1,5 @@
 export default {
   name: "SfTableRow",
-  data() {
-    return {
-      columnsCount: 0
-    };
-  },
   mounted() {
     this.$parent.updateColumnsCount(
       this.$slots.default.filter(node => node.tag).length
