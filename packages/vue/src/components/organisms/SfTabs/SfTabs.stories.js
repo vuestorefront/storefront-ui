70b137bed6f979f93593b0d7cbae439523c9425c
Luke Romanowicz <lromanowicz1@gmail.com>
2019-12-15 19:54:08 +0100

    Improve SfTabs stories

--- packages/vue/src/components/organisms/SfTabs/SfTabs.stories.js
+++ packages/vue/src/components/organisms/SfTabs/SfTabs.stories.js
@@ -49,8 +49,8 @@ storiesOf("Organisms|Tabs", module)
       <SfTab title="Additional Information">
         <h3 style="padding: 0">Brand</h3>
         <p>
-        <u>Brand name</u> is the perfect pairing of quality and design. This label creates major everyday vibes with its collection of modern brooches, silver and gold jewellery, or clips it back with hair accessories in geo styles.
-</p>
+          <u>Brand name</u> is the perfect pairing of quality and design. This label creates major everyday vibes with its collection of modern brooches, silver and gold jewellery, or clips it back with hair accessories in geo styles.
+        </p>
       </SfTab>
     </SfTabs>`
     }),
@@ -62,7 +62,6 @@ storiesOf("Organisms|Tabs", module)
         <h2> Usage </h2>
         <pre>
           <code>
-            import { SfTabs } from "@storefront-ui/vue"
             import { SfTabs } from "@storefront-ui/vue"
           </code>
         </pre>
