12b732966760103683bda97942b4c2587e2c35a1
Anna <amusial@divante.pl>
2019-12-16 11:52:28 +0100

    Added SfTableHeader

--- packages/vue/src/components/organisms/SfTable/_internal/SfTableData.js
+++ packages/vue/src/components/organisms/SfTable/_internal/SfTableData.js
@@ -1,14 +1,3 @@
 export default {
-  name: "SfTableData",
-  props: {
-    isHeader: {
-      type: Boolean,
-      default: false
-    }
-  },
-  computed: {
-    tableTag() {
-      return this.isHeader ? "th" : "td";
-    }
-  }
+  name: "SfTableData"
 };
