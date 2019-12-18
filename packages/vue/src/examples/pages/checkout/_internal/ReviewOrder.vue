271dfce9738270420708d2a675981941249f7cc7
Przemek Spaczek <pspaczek@divante.co>
2019-12-18 16:15:10 +0100

    fix _internal ReviewOrder step

--- packages/vue/src/examples/pages/checkout/_internal/ReviewOrder.vue
+++ packages/vue/src/examples/pages/checkout/_internal/ReviewOrder.vue
@@ -6,49 +6,79 @@
     />
     <SfTable>
       <SfTableRow class="sf-table__row--header">
-        <SfTableHeader v-for="tableHeader in tableHeaders" :key="tableHeader">{{
+        <SfTableHeader v-for="tableHeader in tableHeaders" :key="tableHeader" class="table__header">{{
           tableHeader
         }}</SfTableHeader>
         <SfTableHeader></SfTableHeader>
       </SfTableRow>
       <SfTableRow>
         <SfTableData>
-          SfImage
+          <SfImage src="/assets/storybook/checkoutpage/product.png" />
+        </SfTableData>
+        <SfTableData class="table__data table__data--left">
+          <div class="product-title">Summer Bag</div>
+          <div class="product-sku">MSD23-345-324</div>
+        </SfTableData>
+        <SfTableData class="table__data">Cream</SfTableData>
+        <SfTableData class="table__data">One size</SfTableData>
+        <SfTableData class="table__data">1</SfTableData>
+        <SfTableData class="table__data">
+          <SfPrice regular="$50" class="product-price" />
         </SfTableData>
         <SfTableData>
-          <div>Summer Bag</div>
-          <div>MSD23-345-324</div>
+          <SfIcon icon="cross" size="xxs" color="#BEBFC4" role="button" />
         </SfTableData>
-        <SfTableData>Cream</SfTableData>
-        <SfTableData>One size</SfTableData>
-        <SfTableData>1</SfTableData>
-        <SfTableData>$50</SfTableData>
-        <SfTableData>SfIcon</SfTableData>
       </SfTableRow>
       <SfTableRow>
         <SfTableData>
-          SfImage
+          <SfImage src="/assets/storybook/checkoutpage/product.png" />
+        </SfTableData>
+        <SfTableData class="table__data table__data--left">
+          <div class="product-title">Summer Bag</div>
+          <div class="product-sku">MSD23-345-324</div>
+        </SfTableData>
+        <SfTableData class="table__data">Cream</SfTableData>
+        <SfTableData class="table__data">One size</SfTableData>
+        <SfTableData class="table__data">1</SfTableData>
+        <SfTableData class="table__data">
+          <SfPrice regular="$50" class="product-price" />
         </SfTableData>
         <SfTableData>
-          <div>Summer Bag</div>
-          <div>MSD23-345-324</div>
+          <SfIcon icon="cross" size="xxs" color="#BEBFC4" role="button" />
         </SfTableData>
-        <SfTableData>Cream</SfTableData>
-        <SfTableData>One size</SfTableData>
-        <SfTableData>1</SfTableData>
-        <SfTableData>$50</SfTableData>
-        <SfTableData>SfIcon</SfTableData>
       </SfTableRow>
     </SfTable>
-    <div>
-      <SfCheckbox>
+    <div class="summary">
+      <SfCheckbox v-model="terms" class="summary__column">
         <template #label>
           <div class="sf-checkbox__label">
             I agree to <a href="#">Terms and conditions</a>
           </div>
         </template>
       </SfCheckbox>
-      <div class="summary"></div>
+      <div class="summary__column summary__column--small ">
+        <SfProperty
+          name="Subtotal"
+          value="$150.00"
+          class="sf-property--full-width property"
+        >
+          <template #name><span class="property__name">Subtotal</span></template>
+        </SfProperty>
+        <SfProperty
+          name="Shipping"
+          value="$9.90"
+          class="sf-property--full-width property"
+        >
+          <template #name><span class="property__name">Shipping</span></template>
+        </SfProperty>
+        <SfProperty
+          name="Total"
+          value="$150.00"
+          class="sf-property--full-width property property--huge summary__total"
+        >
+          <template #name>TOTAL</template>
+        </SfProperty>
+      </div>
     </div>
     <div class="form">
       <SfButton class="sf-button--text form__action form__action--left"
@@ -63,13 +93,27 @@ import {
   SfHeading,
   SfTable,
   SfCheckbox,
-  SfButton
+  SfButton,
+  SfImage,
+  SfIcon,
+  SfPrice,
+  SfProperty
 } from "../../../../../index.js";
 export default {
   name: "ReviewOrder",
-  components: { SfHeading, SfTable, SfCheckbox, SfButton },
+  components: {
+    SfHeading,
+    SfTable,
+    SfCheckbox,
+    SfButton,
+    SfImage,
+    SfIcon,
+    SfPrice,
+    SfProperty
+  },
   data() {
     return {
+      terms: false,
       tableHeaders: [
         "Item",
         "Description",
@@ -82,7 +126,6 @@ export default {
   }
 };
 </script>
-
 <style lang="scss" scoped>
 @import "~@storefront-ui/vue/styles";
 @mixin for-desktop {
@@ -105,5 +148,57 @@ export default {
       margin-right: auto;
     }
   }
+}
+.table {
+  margin-bottom: $spacer-big;
+  &__header{
+    font-size: $font-size-regular-desktop;
+    font-weight: $body-font-weight-primary;
+  }
+  &__data {
+    justify-content: flex-start;
+    font-size: $font-size-small-desktop;
+    &--left {
+      align-items: flex-start;
+    }
+  }
+}
+.summary{
+  display: flex;
+  margin-bottom: $spacer-extra-big  ;
+  &__column{
+    flex: 1;
+    &--small{
+      flex: 0 0 18.75rem;
+    }
+  }
+  &__total{
+    margin-top: $spacer-big;
+  }
+}
+.property{
+  font-size: $font-size-small-desktop;
+  line-height: 1.6;
+  &--huge{
+    text-transform: uppercase;
+    font-size: $font-size-regular-desktop;
+    font-weight: 500;
+  }
+  &__name{
+    color: $c-text-muted;
+  }
+}
+.product-title,
+.product-sku {
+  line-height: 1.6;
+}
+.product-title {
+}
+.product-sku {
+  color: $c-text-muted;
+  font-size: $font-size-extra-small-desktop;
+}
+.product-price {
+  font-size: $font-size-small-desktop;
 }
 </style>
