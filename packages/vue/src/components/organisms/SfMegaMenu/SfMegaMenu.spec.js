a99633d9f9f155ffea371fa315e3b77012b11ee2
Przemek Spaczek <pspaczek@divante.co>
2019-12-21 00:38:31 +0100

    fix unit test

--- packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.spec.js
+++ packages/vue/src/components/organisms/SfMegaMenu/SfMegaMenu.spec.js
@@ -5,7 +5,7 @@ import SfMegaMenu from "./SfMegaMenu.vue";
 describe("SfMegaMenu.vue", () => {
   it("renders a Mega Menu", () => {
     const component = shallowMount(SfMegaMenu, {
-      propsData: { parentMenu: "Man" }
+      propsData: { visible: true }
     });
     expect(component.contains(".sf-mega-menu")).toBe(true);
   });
