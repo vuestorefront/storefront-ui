2ed01c7b5af28981508a0bc5351ae92d11f081f7
Przemek Spaczek <pspaczek@divante.co>
2019-12-23 20:51:08 +0100

    refactor(atoms): merge SfButton

--- packages/vue/src/components/atoms/SfButton/SfButton.vue
+++ packages/vue/src/components/atoms/SfButton/SfButton.vue
@@ -1,5 +1,14 @@
-<script src="./SfButton.js"></script>
-<template lang="html" src="./SfButton.html"></template>
+<template>
+  <button class="sf-button" v-bind="$attrs" v-on="$listeners">
+    <!--@slot Use this slot to place content inside the button.-->
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  name: "SfButton"
+};
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfButton.scss";
 </style>
