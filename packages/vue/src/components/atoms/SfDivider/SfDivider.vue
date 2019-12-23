9136f40de95d5534b25e3838f685fa0ec6220e15
Przemek Spaczek <pspaczek@divante.co>
2019-12-23 20:57:44 +0100

    refactor(atoms): lint SfDivider

--- packages/vue/src/components/atoms/SfDivider/SfDivider.vue
+++ packages/vue/src/components/atoms/SfDivider/SfDivider.vue
@@ -1,9 +1,10 @@
-<template><hr class="sf-divider" /></template>
+<template>
+  <hr class="sf-divider" />
+</template>
 <script>
-  export default {
-    name: "SfDivider"
-  };
-
+export default {
+  name: "SfDivider"
+};
 </script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfDivider.scss";
