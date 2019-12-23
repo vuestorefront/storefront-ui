b73502d5adde10f24917e228b24e00e195c6bb0d
Przemek Spaczek <pspaczek@divante.co>
2019-12-23 20:47:44 +0100

    refactor(atoms): merge SfBadge

--- packages/vue/src/components/atoms/SfBadge/SfBadge.vue
+++ packages/vue/src/components/atoms/SfBadge/SfBadge.vue
@@ -1,5 +1,14 @@
-<script src="./SfBadge.js"></script>
-<template lang="html" src="./SfBadge.html"></template>
+<template>
+  <div class="sf-badge">
+    <!--@slot Use this slot to place content inside the badge-->
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: "SfBadge"
+};
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfBadge.scss";
 </style>
