05622a8a1ac5074ac0229100691afc355d07651e
Przemek Spaczek <pspaczek@divante.co>
2019-12-23 20:56:10 +0100

    refactor(atoms): merge SfDivider

--- packages/vue/src/components/atoms/SfDivider/SfDivider.vue
+++ packages/vue/src/components/atoms/SfDivider/SfDivider.vue
@@ -1,5 +1,10 @@
-<script src="./SfDivider.js"></script>
-<template lang="html" src="./SfDivider.html"></template>
+<template><hr class="sf-divider" /></template>
+<script>
+  export default {
+    name: "SfDivider"
+  };
+
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfDivider.scss";
 </style>
