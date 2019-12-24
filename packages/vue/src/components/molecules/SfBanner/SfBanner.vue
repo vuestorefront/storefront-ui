d876cff60c7b64ad5bb5a94dd7236ddca0f5f257
Przemek Spaczek <pspaczek@divante.co>
2019-12-24 08:03:56 +0100

    refactor(molecules): merge SfBanner

--- packages/vue/src/components/molecules/SfBanner/SfBanner.vue
+++ packages/vue/src/components/molecules/SfBanner/SfBanner.vue
@@ -1,5 +1,116 @@
-<script src="./SfBanner.js"></script>
-<template lang="html" src="./SfBanner.html"></template>
+<template>
+  <section class="sf-banner">
+    <div class="sf-banner__container">
+      <slot name="subtitle" v-bind="{ subtitle }">
+        <h2 v-if="subtitle" class="sf-banner__subtitle">
+          {{ subtitle }}
+        </h2>
+      </slot>
+      <slot name="title" v-bind="{ title }">
+        <h1 v-if="title" class="sf-banner__title">
+          {{ title }}
+        </h1>
+      </slot>
+
+      <slot name="description" v-bind="{ description }">
+        <p v-if="description" class="sf-banner__description">
+          {{ description }}
+        </p>
+      </slot>
+
+      <slot name="call-to-action" v-bind="{ buttonText }">
+        <SfButton
+          v-if="buttonText"
+          class="sf-banner__call-to-action color-secondary"
+        >
+          {{ buttonText }}
+        </SfButton>
+      </slot>
+
+      <slot />
+    </div>
+  </section>
+</template>
+<script>
+import SfButton from "../../atoms/SfButton/SfButton.vue";
+
+export default {
+  name: "SfBanner",
+  components: {
+    SfButton
+  },
+  props: {
+    /**
+     * Banner title
+     */
+    title: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Banner subtitle (at the top)
+     */
+    subtitle: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+    /** text that will be displayed inside the button. You can replace the button  with "call-to-action" slot */
+    buttonText: {
+      type: String,
+      default: ""
+    },
+    /** Background color in HEX (eg #FFFFFF) */
+    background: {
+      type: String,
+      default: ""
+    },
+    /** Background image. Influenced by $banner-background-size, $banner-background-position CSS props. */
+    image: {
+      type: [String, Object],
+      default: ""
+    }
+  },
+  watch: {
+    image: {
+      handler(image) {
+        if (typeof window === "undefined") return;
+        this.$nextTick(() => {
+          if (typeof image === "object") {
+            this.$el.style.setProperty(
+              "--background-image",
+              `url(${image.small})`
+            );
+            this.$el.style.setProperty(
+              "--background-image-desktop",
+              `url(${image.normal})`
+            );
+          } else {
+            this.$el.style.setProperty("--background-image", `url(${image})`);
+            this.$el.style.setProperty(
+              "--background-image-desktop",
+              `url(${image})`
+            );
+          }
+        });
+      },
+      immediate: true
+    },
+    background: {
+      handler(background) {
+        if (typeof window === "undefined") return;
+        this.$nextTick(() => {
+          this.$el.style.setProperty("--background-color", background);
+        });
+      },
+      immediate: true
+    }
+  }
+};
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfBanner.scss";
 </style>
