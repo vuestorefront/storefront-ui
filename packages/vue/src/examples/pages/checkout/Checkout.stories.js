768d2d390ed23f4485a1259d8f9c6c696fdf1fde
Przemek Spaczek <pspaczek@divante.co>
2019-12-27 13:51:39 +0100

    chore: update stories

--- packages/vue/src/examples/pages/checkout/Checkout.stories.js
+++ packages/vue/src/examples/pages/checkout/Checkout.stories.js
@@ -1,26 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
-
 import Checkout from "./Checkout.vue";
 
 storiesOf("Pages|Checkout", module)
-  .addDecorator(withKnobs)
-  .add(
-    "Checkout",
-    () => ({
-      components: { Checkout },
-      mounted() {
-        document.body.style.setProperty("margin", "0");
-      },
-      template: `
-      <Checkout/>
-    `
-    }),
-    {
-      info: {
-        summary:
-          "Check https://github.com/DivanteLtd/storefront-ui/blob/master/packages/vue/src/examples/pages/checkout/Checkout.vue to see full code"
-      }
-    }
-  );
+  .add("Common",() => ({
+    components: { Checkout },
+    template: `<Checkout />`
+  }));
