f02f5a52ed684a05934cabf94c87059f02dc03bf
Przemek Spaczek <pspaczek@divante.co>
2019-12-27 13:47:42 +0100

    chore: update stories

--- packages/vue/src/examples/pages/my-account/MyAccount.stories.js
+++ packages/vue/src/examples/pages/my-account/MyAccount.stories.js
@@ -2,13 +2,8 @@
 import { storiesOf } from "@storybook/vue";
 import MyAccount from "./MyAccount";
 
-storiesOf("Pages|My account", module).add("Default", () => ({
-  components: { MyAccount },
-  mounted() {
-    document.body.style.setProperty("margin", "0");
-  },
-  beforeDestroy() {
-    document.body.style.removeProperty("margin");
-  },
-  template: `<MyAccount />`
-}));
+storiesOf("Pages|My account", module)
+  .add("Common", () => ({
+    components: { MyAccount },
+    template: `<MyAccount />`
+  }));
