4468eb6cdaa11c798a74c4f0decb3c84c7c59b0d
Przemek Spaczek <pspaczek@divante.co>
2019-12-27 19:14:05 +0100

    chore: linted

--- packages/vue/src/components/atoms/SfImage/SfImage.js
+++ packages/vue/src/components/atoms/SfImage/SfImage.js
@@ -6,7 +6,7 @@ export default {
   props: {
     src: {
       type: [String, Object],
-      default: () => ({})
+      default: ""
     },
     alt: {
       type: String,
--- packages/vue/src/components/atoms/SfImage/SfImage.spec.js
+++ packages/vue/src/components/atoms/SfImage/SfImage.spec.js
@@ -3,7 +3,7 @@ import SfImage from "./SfImage.vue";
 
 describe("SfImage.vue", () => {
   it("renders a component", () => {
-    const component = shallowMount(SfImage);
+    const component = shallowMount(SfImage, { propsData: {} });
     expect(component.contains(".sf-image")).toBe(true);
   });
 });
