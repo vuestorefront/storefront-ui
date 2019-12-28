716d13fafdba02b5a2eaf2c14ae4cb38013ef5a5
Anna <amusial@divante.pl>
2019-12-28 13:49:20 +0100

    Added unit tests

---
+++ packages/vue/src/components/atoms/SfColor/SfColor.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import SfColor from "./SfColor.vue";
+
+describe.only("SfColor.vue", () => {
+  it("renders a chevron", () => {
+    const component = shallowMount(SfColor);
+    expect(component.contains(".sf-color")).toBe(true);
+  });
+});
