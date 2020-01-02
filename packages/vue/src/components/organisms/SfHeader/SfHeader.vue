47dc7e4a3a1bdf705079935029a3ead891abf76d
Przemek Spaczek <pspaczek@divante.co>
2020-01-02 22:48:11 +0100

    fix(linter): The "SfIcon" component has been registered but not used

--- packages/vue/src/components/organisms/SfHeader/SfHeader.vue
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.vue
@@ -56,7 +56,6 @@
 <script>
 // @vue/component
 import SfHeaderNavigationItem from "./_internal/SfHeaderNavigationItem.vue";
-import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
 import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
 import SfImage from "../../atoms/SfImage/SfImage.vue";
 import SfSearchBar from "../../molecules/SfSearchBar/SfSearchBar.vue";
@@ -65,7 +64,6 @@ Vue.component("SfHeaderNavigationItem", SfHeaderNavigationItem);
 export default {
   name: "SfHeader",
   components: {
-    SfIcon,
     SfCircleIcon,
     SfImage,
     SfSearchBar
