cc431c5734e835ddc44cab90b1707a9e554244f4
Przemek Spaczek <pspaczek@divante.co>
2020-01-02 21:35:22 +0100

    refactor(organisms): merge SfCollectedProduct

--- packages/vue/src/components/organisms/SfCollectedProduct/SfCollectedProduct.vue
+++ packages/vue/src/components/organisms/SfCollectedProduct/SfCollectedProduct.vue
@@ -1,5 +1,128 @@
-<script src="./SfCollectedProduct.js"></script>
-<template lang="html" src="./SfCollectedProduct.html"></template>
+<template>
+  <div class="sf-collected-product">
+    <slot name="remove" v-bind="{ removeHandler }">
+      <SfCircleIcon
+        @click="removeHandler"
+        class="sf-collected-product__remove"
+        icon="cross"
+        icon-color="white"
+        icon-size="8px"
+      />
+    </slot>
+    <div class="sf-collected-product__aside">
+      <slot name="image" v-bind="{ image, title }">
+        <SfImage
+          :alt="title"
+          :src="image"
+          class="sf-collected-product__image"
+        />
+      </slot>
+      <slot name="input">
+        <SfQuantitySelector
+          :min="qtyMin"
+          :qty="qty"
+          :stock="stock"
+          @input="$emit('input', $event)"
+          class="sf-collected-product__quantity-selector"
+        />
+      </slot>
+    </div>
+    <div class="sf-collected-product__main">
+      <slot name="title" v-bind="{ title }">
+        <div class="sf-collected-product__title">{{ title }}</div>
+      </slot>
+      <slot name="price" v-bind="{ specialPrice, regularPrice }">
+        <SfPrice
+          :regular="regularPrice"
+          :special="specialPrice"
+          v-if="regularPrice"
+        />
+      </slot>
+      <div class="sf-collected-product__details">
+        <slot name="configuration"> </slot>
+        <slot name="actions"> </slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import SfPrice from "../../atoms/SfPrice/SfPrice.vue";
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+import SfImage from "../../atoms/SfImage/SfImage.vue";
+import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
+import SfQuantitySelector from "../../atoms/SfQuantitySelector/SfQuantitySelector.vue";
+
+export default {
+  name: "SfCollectedProduct",
+  components: {
+    SfIcon,
+    SfImage,
+    SfCircleIcon,
+    SfPrice,
+    SfQuantitySelector
+  },
+  model: {
+    prop: "qty"
+  },
+  props: {
+    /**
+     * Product image
+     * It should be an url of the product
+     */
+    image: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Product title
+     */
+    title: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Product regular price
+     */
+    regularPrice: {
+      type: [Number, String],
+      default: null
+    },
+    /**
+     * Product special price
+     */
+    specialPrice: {
+      type: [Number, String],
+      default: null
+    },
+    /**
+     * Selected quantity
+     */
+    qty: {
+      type: [Number, String],
+      default: 1
+    },
+    /**
+     * Stock quantity of product
+     */
+    stock: {
+      type: Number,
+      default: 1
+    },
+    /**
+     * Minimum product quantity
+     */
+    qtyMin: {
+      type: Number,
+      default: 1
+    }
+  },
+  methods: {
+    removeHandler() {
+      this.$emit("click:remove");
+    }
+  }
+};
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfCollectedProduct.scss";
 </style>
