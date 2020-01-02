4da5417fdaa0fb90767d1cc4311b912521b407f1
Przemek Spaczek <pspaczek@divante.co>
2020-01-02 22:15:24 +0100

    refactor(organisms): merge SfTabs

--- packages/vue/src/components/organisms/SfTabs/SfTabs.vue
+++ packages/vue/src/components/organisms/SfTabs/SfTabs.vue
@@ -1,5 +1,47 @@
-<script src="./SfTabs.js"></script>
-<template lang="html" src="./SfTabs.html"></template>
+<template>
+  <div class="sf-tabs">
+    <!--@slot Default. Here you should pass your tabs-->
+    <slot></slot>
+  </div>
+</template>
+<script src="./SfTabs.js">
+import SfTab from "./_internal/SfTab.vue";
+import Vue from "vue";
+
+Vue.component("SfTab", SfTab);
+
+export default {
+  name: "SfTabs",
+  components: {
+    SfTab
+  },
+  props: {
+    /** Which tab should be open at the beginning  */
+    openTab: {
+      type: Number,
+      required: false
+    }
+  },
+    mounted() {
+      this.$on("toggle", this.toggle);
+      if (this.openTab) this.openChild();
+    },
+    methods: {
+      toggle(id) {
+        this.$children.forEach(child => {
+          child._uid === id
+            ? (child.isActive = !child.isActive)
+            : (child.isActive = false);
+        });
+      },
+      openChild() {
+        if (this.openTab < this.$children.length + 1) {
+          this.$children[this.openTab - 1].isActive = true;
+        }
+      }
+    }
+};
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfTabs.scss";
 </style>
