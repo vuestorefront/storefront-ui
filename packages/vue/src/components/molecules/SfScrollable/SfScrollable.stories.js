75e7c1ae96ebdf087ad7d659c80bce8976ed2df5
Przemek Spaczek <pspaczek@divante.co>
2020-01-06 11:39:38 +0100

    feat: set content height by CSS stories

--- packages/vue/src/components/molecules/SfScrollable/SfScrollable.stories.js
+++ packages/vue/src/components/molecules/SfScrollable/SfScrollable.stories.js
@@ -62,4 +62,60 @@ storiesOf("Molecules|Scrollable", module)
           :label="option.label"
           :style="{marginBottom: key < options.length - 1 ? '8px' : undefined}"/>
       </SfScrollable>`
+  }))
+  .add("Content height by CSS", () => ({
+    components: { SfScrollable, SfProductOption },
+    props: {
+      showText: {
+        default: text("showText", "View all colors", "Props")
+      },
+      hideText: {
+        default: text("hideText", "Hide colors", "Props")
+      },
+      maxContentHeight: {
+        default: text("maxContentHeight", "6.875rem", "Props")
+      }
+    },
+    data() {
+      return {
+        options: [
+          {
+            color: "red",
+            label: "Red"
+          },
+          {
+            color: "blue",
+            label: "Blue"
+          },
+          {
+            color: "green",
+            label: "Green"
+          },
+          {
+            color: "black",
+            label: "Black"
+          },
+          {
+            color: "navy",
+            label: "Navy"
+          },
+          {
+            color: "pink",
+            label: "Pink"
+          }
+        ]
+      };
+    },
+    template: `<SfScrollable
+      :show-text="showText"
+      :hide-text="hideText"
+      style="max-width: 13.75rem; --max-height: 1.375rem"
+    >
+      <SfProductOption
+        v-for="(option, key) in options"
+        :key="option.color"
+        :color="option.color"
+        :label="option.label"
+        :style="{marginBottom: key < options.length - 1 ? '8px' : undefined}"/>
+    </SfScrollable>`
   }));
