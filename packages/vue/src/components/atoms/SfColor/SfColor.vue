8452a1528a97df7336523d34b53a15f8057902ff
Przemek Spaczek <pspaczek@divante.co>
2020-01-09 23:14:54 +0100

    chore: update component to new convention

--- packages/vue/src/components/atoms/SfColor/SfColor.vue
+++ packages/vue/src/components/atoms/SfColor/SfColor.vue
@@ -1,5 +1,41 @@
-<script src="./SfColor.js"></script>
-<template lang="html" src="./SfColor.html"></template>
+<template>
+  <button
+    class="sf-color"
+    :class="{ 'sf-color--active': isActive }"
+    :aria-label="ariaLabel"
+    :aria-pressed="isActive"
+    v-on="$listeners"
+  >
+    <!-- @slot Custom color markup -->
+    <slot>
+      <div v-if="color" class="sf-color__fill" :style="colorStyle" />
+    </slot>
+  </button>
+</template>
+<script>
+export default {
+  name: "SfColor",
+  props: {
+    color: {
+      type: String,
+      default: ""
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    },
+    ariaLabel: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    colorStyle() {
+      return "background-color:" + this.color;
+    }
+  }
+};
+</script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/SfColor.scss";
 </style>
