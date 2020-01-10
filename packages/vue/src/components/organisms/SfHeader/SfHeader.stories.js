617ccd5550c29eebba87dfd07a4f75173b0fa7c5
Przemek Spaczek <pspaczek@divante.co>
2020-01-10 12:03:34 +0100

    fix: stories after merge

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -7,10 +7,13 @@ import {
   boolean
 } from "@storybook/addon-knobs";
 import SfHeader from "./SfHeader.vue";
+const StoriesPlaceholder = {
+  template: `<div style="box-sizing: border-box; display: flex; justify-content: center; align-items: center; width: 100%; height: 151vh; background-color: #5ECE7B; color: #FFF">[page content]</div>`
+};
 storiesOf("Organisms|Header", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
-    components: { SfHeader },
+    components: { SfHeader, StoriesPlaceholder },
     props: {
       title: {
         default: text("title", "Storefront UI", "Props")
@@ -47,24 +50,31 @@ storiesOf("Organisms|Header", module)
       },
       searchPlaceholder: {
         default: text("searchPlaceholder", "Search for items", "Props")
+      },
+      sticky: {
+        default: boolean("sticky", true, "Props")
       }
     },
-    template: `<SfHeader
-      :title="title" 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon"
-      :active-icon="activeIcon"
-      :has-mobile-search="hasMobileSearch"
-      :search-placeholder="searchPlaceholder"
-     >
-      <template #navigation>
-        <SfHeaderNavigationItem>CLOTHES</SfHeaderNavigationItem>
-        <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
-        <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
-      </template>
-    </SfHeader>`
+    template: `<div>
+      <SfHeader
+        :title="title" 
+        :logo="logo"
+        :cart-icon="cartIcon" 
+        :wishlist-icon="wishlistIcon" 
+        :account-icon="accountIcon"
+        :active-icon="activeIcon"
+        :has-mobile-search="hasMobileSearch"
+        :search-placeholder="searchPlaceholder"
+        :sticky="sticky"
+       >
+        <template #navigation>
+          <SfHeaderNavigationItem>CLOTHES</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
+        </template>
+      </SfHeader>
+      <StoriesPlaceholder/>
+    </div>`
   }))
   .add("[slot] logo", () => ({
     components: { SfHeader },
@@ -106,25 +116,29 @@ storiesOf("Organisms|Header", module)
         default: text("searchPlaceholder", "Search for items", "Props")
       }
     },
-    template: `<SfHeader
-      :title="title"  
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon"
-      :active-icon="activeIcon"
-      :has-mobile-search="hasMobileSearch"
-      :search-placeholder="searchPlaceholder"
-    >
-      <template #logo>
-        CUSTOM LOGO 
-      </template>
-      <template #navigation>
-      <SfHeaderNavigationItem>WOMEN</SfHeaderNavigationItem>
-      <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
-      <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
-    </template>
-    </SfHeader>`
+    template: `<div>
+      <SfHeader
+        :title="title"  
+        :logo="logo"
+        :cart-icon="cartIcon" 
+        :wishlist-icon="wishlistIcon" 
+        :account-icon="accountIcon"
+        :active-icon="activeIcon"
+        :has-mobile-search="hasMobileSearch"
+        :search-placeholder="searchPlaceholder"
+        :sticky="sticky"
+      >
+        <template #logo>
+          CUSTOM LOGO 
+        </template>
+        <template #navigation>
+          <SfHeaderNavigationItem>WOMEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
+        </template>
+      </SfHeader>
+      <StoriesPlaceholder/>
+    </div>`
   }))
   .add("[slot] navigation", () => ({
     components: { SfHeader },
@@ -159,22 +173,29 @@ storiesOf("Organisms|Header", module)
       },
       searchPlaceholder: {
         default: text("searchPlaceholder", "Search for items", "Props")
+      },
+      sticky: {
+        default: boolean("sticky", true, "Props")
       }
     },
-    template: `<SfHeader
-      :title="title" 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon"
-      :active-icon="activeIcon"
-      :has-mobile-search="hasMobileSearch"
-      :search-placeholder="searchPlaceholder"
-    >
-      <template #navigation>
-        CUSTOM NAVIGATION
-      </template>
-    </SfHeader>`
+    template: `<div>
+      <SfHeader
+        :title="title" 
+        :logo="logo"
+        :cart-icon="cartIcon" 
+        :wishlist-icon="wishlistIcon" 
+        :account-icon="accountIcon"
+        :active-icon="activeIcon"
+        :has-mobile-search="hasMobileSearch"
+        :search-placeholder="searchPlaceholder"
+        :sticky="sticky"
+      >
+        <template #navigation>
+          CUSTOM NAVIGATION
+        </template>
+      </SfHeader>
+      <StoriesPlaceholder/>
+    </div>`
   }))
   .add("[slot] search", () => ({
     components: { SfHeader },
@@ -209,27 +230,34 @@ storiesOf("Organisms|Header", module)
       },
       searchPlaceholder: {
         default: text("searchPlaceholder", "Search for items", "Props")
+      },
+      sticky: {
+        default: boolean("sticky", true, "Props")
       }
     },
-    template: `<SfHeader
-      :title="title"  
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon"
-      :active-icon="activeIcon"
-      :has-mobile-search="hasMobileSearch"
-      :search-placeholder="searchPlaceholder"
-    >
-      <template #navigation>
-        <SfHeaderNavigationItem>WOMEN</SfHeaderNavigationItem>
-        <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
-        <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
-      </template>
-      <template #search>
-        CUSTOM SEARCH
-      </template>
-    </SfHeader>`
+    template: `<div>
+      <SfHeader
+        :title="title"  
+        :logo="logo"
+        :cart-icon="cartIcon" 
+        :wishlist-icon="wishlistIcon" 
+        :account-icon="accountIcon"
+        :active-icon="activeIcon"
+        :has-mobile-search="hasMobileSearch"
+        :search-placeholder="searchPlaceholder"
+        :sticky="sticky"
+      >
+        <template #navigation>
+          <SfHeaderNavigationItem>WOMEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
+        </template>
+        <template #search>
+         CUSTOM SEARCH
+        </template>
+      </SfHeader>
+      <StoriesPlaceholder/>
+    </div>`
   }))
   .add("[slot] icons", () => ({
     components: { SfHeader },
@@ -269,25 +297,32 @@ storiesOf("Organisms|Header", module)
       },
       searchPlaceholder: {
         default: text("searchPlaceholder", "Search for items", "Props")
+      },
+      sticky: {
+        default: boolean("sticky", true, "Props")
       }
     },
-    template: `<SfHeader
-      :title="title" 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon"
-      :active-icon="activeIcon"
-      :has-mobile-search="hasMobileSearch"
-      :search-placeholder="searchPlaceholder"
-    >
-      <template #navigation>
-        <SfHeaderNavigationItem>WOMEN</SfHeaderNavigationItem>
-        <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
-        <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
-      </template>
-      <template #header-icons>
-        CUSTOM ICONS
-      </template>
-    </SfHeader>`
+    template: `<div>
+      <SfHeader
+        :title="title" 
+        :logo="logo"
+        :cart-icon="cartIcon" 
+        :wishlist-icon="wishlistIcon" 
+        :account-icon="accountIcon"
+        :active-icon="activeIcon"
+        :has-mobile-search="hasMobileSearch"
+        :search-placeholder="searchPlaceholder"
+        :sticky="sticky"
+      >
+        <template #navigation>
+          <SfHeaderNavigationItem>WOMEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>MEN</SfHeaderNavigationItem>
+          <SfHeaderNavigationItem>KIDS</SfHeaderNavigationItem>
+        </template>
+        <template #header-icons>
+          CUSTOM ICONS
+        </template>
+      </SfHeader>
+      <StoriesPlaceholder/>
+    </div>`
   }));
