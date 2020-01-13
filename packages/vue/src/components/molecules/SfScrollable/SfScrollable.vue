1aada70c5dc072617997fcfed513d85de8e0c8ff
Przemek Spaczek <pspaczek@divante.co>
2020-01-13 13:35:08 +0100

    chore: revert SfScrollable to SFUI

---
+++ packages/vue/src/components/molecules/SfScrollable/SfScrollable.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="sf-scrollable" :class="{ 'sf-scrollable--is-open': !isHidden }">
+    <Simplebar ref="content" class="sf-scrollable__content" :style="style">
+      <slot />
+    </Simplebar>
+    <SfButton
+      v-if="hasScroll"
+      class="sf-scrollable__button sf-button--text"
+      @click="isHidden = !isHidden"
+    >
+      <span v-if="isHidden">{{ showText }}</span>
+      <span v-else>{{ hideText }}</span>
+    </SfButton>
+  </div>
+</template>
+<script>
+import Simplebar from "simplebar-vue";
+import SfButton from "../../atoms/SfButton/SfButton.vue";
+export default {
+  name: "SfScrollable",
+  components: {
+    Simplebar,
+    SfButton
+  },
+  props: {
+    maxContentHeight: {
+      type: String,
+      default: ""
+    },
+    showText: {
+      type: String,
+      default: "Show"
+    },
+    hideText: {
+      type: String,
+      default: "Hide"
+    }
+  },
+  data() {
+    return {
+      isHidden: true,
+      hasScroll: false,
+      contentEl: undefined
+    };
+  },
+  computed: {
+    style() {
+      return {
+        "--max-height": this.maxContentHeight.trim
+          ? this.maxContentHeight
+          : undefined
+      };
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.contentEl = this.$refs.content.$el.querySelector(
+        ".simplebar-content"
+      );
+      if (typeof MutationObserver === "undefined") return;
+      const observer = new MutationObserver(this.sizeCalc);
+      this.sizeCalc();
+      observer.observe(this.contentEl, { childList: true });
+    });
+  },
+  methods: {
+    sizeCalc() {
+      const containerHeight = this.$refs.content.$el.offsetHeight;
+      const contentHeight = this.contentEl.offsetHeight;
+      this.hasScroll = contentHeight > containerHeight;
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "~@storefront-ui/shared/styles/components/SfScrollable.scss";
+</style>
