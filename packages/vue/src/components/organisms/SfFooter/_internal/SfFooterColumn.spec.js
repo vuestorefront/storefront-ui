9d8ac758fcac88c3a07fe60c0f4c334c9044d513
peoray <peoray@yahoo.com>
2020-01-15 19:26:41 +0100

    add render test to components

--- packages/vue/src/components/organisms/SfFooter/_internal/SfFooterColumn.spec.js
+++ packages/vue/src/components/organisms/SfFooter/_internal/SfFooterColumn.spec.js
@@ -1,4 +1,4 @@
-import "./matchMedia.mock";
+import "../matchMedia.mock";
 import { shallowMount } from "@vue/test-utils";
 import SfFooterColumn from "./SfFooterColumn.vue";
 describe("SfFooterColumn.vue", () => {
