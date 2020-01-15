ce01ef2c41917aea3eb8ec5b129033781eba4ee3
Anna <amusial@divante.pl>
2020-01-15 14:14:43 +0100

    fix: fixed issue with provided/inject

--- packages/vue/src/components/organisms/SfContentPages/SfContentPages.spec.js
+++ packages/vue/src/components/organisms/SfContentPages/SfContentPages.spec.js
@@ -1,8 +1,21 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import SfContentPages from "@/components/organisms/SfContentPages/SfContentPages.vue";
+import SfContentPage from "./_internal/SfContentPage.vue";
+
+const v = createLocalVue();
+v.component("SfContentPage", SfContentPage, {
+  inject: ["provided"]
+});
+
 describe("SfContentPages.vue", () => {
   it("renders a component", () => {
-    const component = shallowMount(SfContentPages);
+    const component = shallowMount(SfContentPages, {
+      provide: {
+        provided() {
+          return { active: "true" };
+        }
+      }
+    });
     expect(component.contains(".sf-content-pages")).toBe(true);
   });
 });
--- packages/vue/src/components/organisms/SfContentPages/_internal/SfContentPage.spec.js
+++ packages/vue/src/components/organisms/SfContentPages/_internal/SfContentPage.spec.js
@@ -1,8 +1,16 @@
 import { shallowMount } from "@vue/test-utils";
 import SfContentPage from "./SfContentPage.vue";
+
 describe("SfContentPage.vue", () => {
   it("renders a component", () => {
-    const component = shallowMount(SfContentPage);
+    const component = shallowMount(SfContentPage, {
+      inject: ["provided"],
+      provide: {
+        provided() {
+          return { active: "true" };
+        }
+      }
+    });
     expect(component.contains(".sf-content-page")).toBe(true);
   });
 });
