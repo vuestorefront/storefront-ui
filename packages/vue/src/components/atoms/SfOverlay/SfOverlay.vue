75b6907b47a040d7ac24155c7c692e6452700b76
Przemek Spaczek <pspaczek@divante.co>
2020-01-16 12:36:20 +0100

    refactor: remove Portal from SfOverlay

--- packages/vue/src/components/atoms/SfOverlay/SfOverlay.vue
+++ packages/vue/src/components/atoms/SfOverlay/SfOverlay.vue
@@ -1,23 +1,17 @@
 <template>
-  <Portal>
-    <transition :name="transition">
-      <div
-        v-if="visible"
-        ref="overlay"
-        class="sf-overlay"
-        :class="[staticClass, className]"
-        @click="$emit('click')"
-      ></div>
-    </transition>
-  </Portal>
+  <transition :name="transition">
+    <div
+      v-if="visible"
+      ref="overlay"
+      class="sf-overlay"
+      :class="[staticClass, className]"
+      @click="$emit('click')"
+    ></div>
+  </transition>
 </template>
 <script>
-import { Portal } from "@linusborg/vue-simple-portal";
 export default {
   name: "SfOverlay",
-  components: {
-    Portal
-  },
   props: {
     /**
      * Transition effect to apply when overlay visibility is changed
