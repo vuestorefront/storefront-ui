1cd221bd0155dd9c3f7d5a8ec1ba310ed6e07eb0
Przemek Spaczek <pspaczek@divante.co>
2020-01-17 09:03:13 +0100

    docs: add new modifier to stories

--- packages/vue/src/components/atoms/SfColor/SfColor.stories.js
+++ packages/vue/src/components/atoms/SfColor/SfColor.stories.js
@@ -1,11 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, optionsKnob as options } from "@storybook/addon-knobs";
 import SfColor from "./SfColor.vue";
 storiesOf("Atoms|Color", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
     components: { SfColor },
+    props: {
+      customClass: {
+        default: options(
+          "CSS modifiers",
+          {
+            "sf-color--rounded": "sf-color--rounded"
+          },
+          "",
+          { display: "multi-select" },
+          "CSS Modifiers"
+        )
+      }
+    },
     data() {
       return {
         colors: [
@@ -29,6 +42,13 @@ storiesOf("Atoms|Color", module)
       };
     },
     template: `<div>
-        <SfColor v-for="color in colors" :color="color.label" :is-active="color.active" :aria-label="color.label" @click="color.active= !color.active" style="margin: 10px;"/>
+        <SfColor 
+          v-for="color in colors"
+          :color="color.label" 
+          :is-active="color.active" 
+          :aria-label="color.label"  
+          style="margin: 10px;"
+          :class="customClass"
+          @click="color.active= !color.active"/>
     </div>`
   }));
