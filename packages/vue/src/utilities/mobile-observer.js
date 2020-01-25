1732b326e886139bbd7b3ff42925a258ebb35e41
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-01-25 22:59:27 +0100

    feat: mobile observer utility

---
+++ packages/vue/src/utilities/mobile-observer.js
@@ -0,0 +1,75 @@
+import Vue from "vue";
+
+let observer;
+const desktopMin = 1024;
+
+export const onMediaMatch = e => {
+  observer.isMobile = e.matches;
+};
+
+export const setupListener = () => {
+  if (
+    typeof window === "undefined" ||
+    typeof document === "undefined" ||
+    !window.matchMedia
+  ) {
+    return;
+  }
+  observer.isMobile =
+    Math.max(document.documentElement.clientWidth, window.innerWidth) <
+    desktopMin;
+  window.matchMedia(`(max-width: ${desktopMin}px)`).addListener(onMediaMatch);
+  observer.isInitialized = true;
+};
+
+export const tearDownListener = () => {
+  if (
+    typeof window !== "undefined" &&
+    typeof document !== "undefined" &&
+    window.matchMedia
+  ) {
+    window
+      .matchMedia(`(max-width: ${desktopMin}px)`)
+      .removeListener(onMediaMatch);
+  }
+};
+
+export const mapMobileObserver = () => {
+  if (!observer) {
+    observer = Vue.observable({
+      isMobile: false,
+      clients: 0,
+      isInitialized: false
+    });
+  }
+  observer.clients += 1;
+
+  return {
+    isMobile: {
+      get() {
+        if (!observer.isInitialized) {
+          setupListener();
+        }
+        return observer.isMobile;
+      }
+    },
+    clients: {
+      get() {
+        return observer.clients;
+      }
+    },
+    isInitialized: {
+      get() {
+        return observer.isInitialized;
+      }
+    }
+  };
+};
+
+export const unMapMobileObserver = () => {
+  observer.clients -= 1;
+  if (observer.clients === 0) {
+    observer = null;
+    tearDownListener();
+  }
+};
