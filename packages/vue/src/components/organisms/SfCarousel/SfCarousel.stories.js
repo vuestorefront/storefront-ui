bc0b1467f9160c186d94739da13f7000b9aeedf5
Przemek Spaczek <pspaczek@divante.co>
2020-01-27 14:11:20 +0100

    refactor: clean SfCarousel stories

--- packages/vue/src/components/organisms/SfCarousel/SfCarousel.stories.js
+++ packages/vue/src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -1,138 +1,73 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, number, object } from "@storybook/addon-knobs";
 import SfCarousel from "./SfCarousel.vue";
-const data = () => {
-  return {
-    style: {
-      color: "#FFF",
-      display: "flex",
-      "justify-content": "center",
-      "font-size": "3rem",
-      "align-items": "center",
-      height: "300px",
-      "background-color": "#5ECE7B"
-    },
-    options: {
-      perView: 4
-    }
-  };
+const StoriesPlaceholder = {
+  props: ["index"],
+  template: `<div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">{{index}}</div>`
 };
 storiesOf("Organisms|Carousel", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
-    components: { SfCarousel },
-    data,
-    template: `
-      <div style="max-width: 1140px">
-        <SfCarousel :options="options">
-          <SfCarouselItem>
-            <div :style="style">1</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">2</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">3</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">4</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">5</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">6</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">7</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">8</div>
-          </SfCarouselItem>
-        </SfCarousel>
-      </div>
-      `
-  }))
-  .add("[slot] prev", () => ({
-    components: { SfCarousel },
-    data,
-    template: `
-      <div style="max-width: 1140px">
-        <SfCarousel :options="options">
-          <SfCarouselItem>
-            <div :style="style">1</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">2</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">3</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">4</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">5</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">6</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">7</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">8</div>
-          </SfCarouselItem>
-
-          <template v-slot:prev="{ go }">
-            <div @click="go">
-              <span style="margin: 12px; cursor: pointer; font-weight: 900; font-size: 18px;">&lt; PREV</span>
-            </div>
-          </template>
-
-        </SfCarousel>
-      </div>
-      `
+    components: { SfCarousel, StoriesPlaceholder },
+    props: {
+      settings: {
+        default: object("settings", { perView: 4 }, "Props")
+      },
+      slidesNumber: {
+        default: number("slidesNumber", 8, {}, "Stories")
+      }
+    },
+    template: `<SfCarousel
+        :settings="settings"
+        :style="{maxWidth: '1140px'}"
+      >
+        <SfCarouselItem v-for="index in slidesNumber">
+          <StoriesPlaceholder :index="index"/>
+        </SfCarouselItem>
+      </SfCarousel>`
   }))
   .add("[slot] next", () => ({
-    components: { SfCarousel },
-    data,
-    template: `
-      <div style="max-width: 1140px">
-        <SfCarousel :options="options">
-          <SfCarouselItem>
-            <div :style="style">1</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">2</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">3</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">4</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">5</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">6</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">7</div>
-          </SfCarouselItem>
-          <SfCarouselItem>
-            <div :style="style">8</div>
-          </SfCarouselItem>
-
-          <template v-slot:next="{ go }">
-            <div @click="go">
-              <span style="margin: 12px; cursor: pointer; font-weight: 900; font-size: 18px;">NEXT &gt;</span>
-            </div>
-          </template>
-
-        </SfCarousel>
-      </div>
-      `
+    components: { SfCarousel, StoriesPlaceholder },
+    props: {
+      settings: {
+        default: object("settings", { perView: 4 }, "Props")
+      },
+      slidesNumber: {
+        default: number("slidesNumber", 8, {}, "Stories")
+      }
+    },
+    template: `<SfCarousel
+        :settings="settings"
+        :style="{maxWidth: '1140px'}"
+    >
+      <template #next="{go}">
+        <button @click="go">NEXT</button>
+      </template>
+      <SfCarouselItem v-for="index in slidesNumber">
+        <StoriesPlaceholder :index="index"/>
+      </SfCarouselItem>
+    </SfCarousel>`
+  }))
+  .add("[slot] prev", () => ({
+    components: { SfCarousel, StoriesPlaceholder },
+    props: {
+      settings: {
+        default: object("settings", { perView: 4 }, "Props")
+      },
+      slidesNumber: {
+        default: number("slidesNumber", 8, {}, "Stories")
+      }
+    },
+    template: `<SfCarousel
+        :settings="settings"
+        :style="{maxWidth: '1140px'}"
+    >
+      <template #prev="{go}">
+        <button @click="go">PREV</button>
+      </template>
+      <SfCarouselItem v-for="index in slidesNumber">
+        <StoriesPlaceholder :index="index"/>
+      </SfCarouselItem>
+    </SfCarousel>`
   }));
