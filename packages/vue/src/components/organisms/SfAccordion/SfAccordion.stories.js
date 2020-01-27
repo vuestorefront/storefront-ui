7a7bedf5b6e7920383aba1ed02d88ae94875c049
Przemek Spaczek <pspaczek@divante.co>
2020-01-27 12:52:49 +0100

    refactor: clean SfAccordion stories

--- packages/vue/src/components/organisms/SfAccordion/SfAccordion.stories.js
+++ packages/vue/src/components/organisms/SfAccordion/SfAccordion.stories.js
@@ -7,61 +7,147 @@ import SfMenuItem from "../../molecules/SfMenuItem/SfMenuItem.vue";
 storiesOf("Organisms|Accordion", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
-    data: () => {
-      return {
-        accordions: [
-          {
-            header: "About Us",
-            items: [
-              { label: "About us (Magento CMS)", count: "280" },
-              { label: "Store locator", count: "34" }
-            ]
-          },
-          {
-            header: "Departaments",
-            items: [
-              { label: "Women fashion", count: "2" },
-              { label: "Men fashion", count: "56" },
-              { label: "Kidswear", count: "16" },
-              { label: "Home", count: "166" },
-              { label: "Dogswear", count: "24" }
-            ]
-          },
-          {
-            header: "Help",
-            items: [
-              { label: "Customer service", count: "54" },
-              { label: "Size guide", count: "4" },
-              { label: "Contact us", count: "76" }
-            ]
-          }
-        ]
-      };
-    },
+    components: { SfAccordion, SfList, SfMenuItem },
     props: {
-      multiple: {
-        default: boolean("multiple", true, "Props")
-      },
       firstOpen: {
         default: boolean("firstOpen", true, "Props")
       },
+      multiple: {
+        default: boolean("multiple", false, "Props")
+      },
       showChevron: {
-        default: boolean("showChevron", true, "Props")
+        default: boolean("showChevron", false, "Props")
       },
       transition: {
         default: text("transition", "fade", "Props")
       }
     },
+    data() {
+      return {
+        accordions: [
+          {
+            header: "Clothing",
+            items: [
+              { label: "All", count: "280" },
+              { label: "Skirts", count: "11" },
+              { label: "Dresses", count: "32" }
+            ]
+          },
+          {
+            header: "Accessories",
+            items: [
+              { label: "All", count: "80" },
+              { label: "Belts", count: "101" },
+              { label: "Bag", count: "2" }
+            ]
+          },
+          {
+            header: "Shoes",
+            items: [
+              { label: "All", count: "2" },
+              { label: "Trainers", count: "22" },
+              { label: "Sandals", count: "55" }
+            ]
+          }
+        ]
+      };
+    },
+    template: `<SfAccordion 
+        :first-open="firstOpen" 
+        :multiple="multiple"
+        :show-chevron="showChevron"
+        :transition="transition"
+    :style="{maxWidth: '186px', padding: '1rem'}">
+      <SfAccordionItem 
+        v-for="accordion in accordions" 
+        :key="accordion.header" 
+        :header="accordion.header"
+      >
+        <SfList>
+          <SfListItem
+            v-for="item in accordion.items"
+            :key="item.label"
+            :style="{margin: '10px 0'}">
+            <SfMenuItem 
+              :label="item.label" 
+              :count="item.count"
+            />
+          </SfListItem>
+        </SfList>
+      </SfAccordionItem>
+      </SfAccordion>`
+  }))
+  .add("[slot] header", () => ({
     components: { SfAccordion, SfList, SfMenuItem },
-    template: `<div :style="{width: '300px', padding: '1rem'}">
-        <SfAccordion :multiple="multiple" :firstOpen="firstOpen" :showChevron="showChevron" :transition="transition">
-          <SfAccordionItem v-for="(accordion, i) of accordions" :header="accordion.header" :key="i">
-            <SfList>
-              <SfListItem v-for="(item, j) of accordion.items" :key="j">
-                <SfMenuItem :label="item.label" :count="item.count"/>
-              </SfListItem>
-            </SfList>
-          </SfAccordionItem>
-        </SfAccordion>
-      </div>`
+    props: {
+      firstOpen: {
+        default: boolean("firstOpen", true, "Props")
+      },
+      multiple: {
+        default: boolean("multiple", false, "Props")
+      },
+      showChevron: {
+        default: boolean("showChevron", false, "Props")
+      },
+      transition: {
+        default: text("transition", "fade", "Props")
+      }
+    },
+    data() {
+      return {
+        accordions: [
+          {
+            header: "Clothing",
+            items: [
+              { label: "All", count: "280" },
+              { label: "Skirts", count: "11" },
+              { label: "Dresses", count: "32" }
+            ]
+          },
+          {
+            header: "Accessories",
+            items: [
+              { label: "All", count: "80" },
+              { label: "Belts", count: "101" },
+              { label: "Bag", count: "2" }
+            ]
+          },
+          {
+            header: "Shoes",
+            items: [
+              { label: "All", count: "2" },
+              { label: "Trainers", count: "22" },
+              { label: "Sandals", count: "55" }
+            ]
+          }
+        ]
+      };
+    },
+    template: `<SfAccordion
+        :first-open="firstOpen"
+        :multiple="multiple"
+        :show-chevron="showChevron"
+        :transition="transition"
+        :style="{maxWidth: '186px', padding: '1rem'}">
+      <SfAccordionItem
+          v-for="accordion in accordions"
+          :key="accordion.header"
+          :header="accordion.header"
+      >
+        <template #header="{header, isOpen, accordionClick, showChevron}">
+          <div @click="accordionClick" :style="{cursor: 'pointer'}">CUSTOM HEADER</div>
+        </template>
+        <SfList>
+          <SfListItem
+              v-for="item in accordion.items"
+              :key="item.label"
+              :style="{margin: '10px 0'}">
+            <SfMenuItem
+                :label="item.label"
+                :count="item.count"
+            />
+          </SfListItem>
+        </SfList>
+      </SfAccordionItem>
+    </SfAccordion>`
   }));
