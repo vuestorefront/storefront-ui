ce1b83b05d08cc79237cdd455b08c2a0e2184035
Anna <amusial@divante.pl>
2020-01-28 11:30:42 +0100

    feat: added disabled watcher

--- packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.vue
+++ packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.vue
@@ -68,34 +68,50 @@ export default {
       };
     }
   },
+  watch: {
+    disabled: {
+      handler(value) {
+        if (!value) {
+          this.$nextTick(() => {
+            this.enableGlide();
+          });
+        }
+      }
+    }
+  },
   mounted() {
     this.$nextTick(() => {
       if (!this.$slots.default) return;
       if (!this.disabled) {
-        const glide = new Glide(this.$refs.glide, this.glideSettings);
-        glide.mount();
-        glide.on("run.before", move => {
-          const { perView, slidePerPage, rewind } = this.glide.settings,
-            { index } = this.glide,
-            { direction } = move,
-            length = this.glide._c.Sizes.length;
-          let page, steps;
-          if (direction === "=" || !slidePerPage || perView <= 1) return;
-          page = Math.ceil(index / perView);
-          steps = page * perView + (direction === ">" ? perView : -perView);
-          if (steps === length - 1) {
-            steps = steps - 1;
-          } else if (steps >= length) {
-            steps = rewind ? 0 : index;
-          } else if (steps < 0) {
-            steps = 0;
-          }
-          move.direction = "=";
-          move.steps = steps;
-        });
-        this.glide = glide;
+        this.enableGlide();
       }
     });
+  },
+  methods: {
+    enableGlide() {
+      const glide = new Glide(this.$refs.glide, this.glideSettings);
+      glide.mount();
+      glide.on("run.before", move => {
+        const { perView, slidePerPage, rewind } = this.glide.settings,
+          { index } = this.glide,
+          { direction } = move,
+          length = this.glide._c.Sizes.length;
+        let page, steps;
+        if (direction === "=" || !slidePerPage || perView <= 1) return;
+        page = Math.ceil(index / perView);
+        steps = page * perView + (direction === ">" ? perView : -perView);
+        if (steps === length - 1) {
+          steps = steps - 1;
+        } else if (steps >= length) {
+          steps = rewind ? 0 : index;
+        } else if (steps < 0) {
+          steps = 0;
+        }
+        move.direction = "=";
+        move.steps = steps;
+      });
+      this.glide = glide;
+    }
   }
 };
 </script>
