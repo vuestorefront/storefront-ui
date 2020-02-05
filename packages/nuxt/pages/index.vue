f0ab9eac9b6c8f49feb0a4a2f9ea2b5cc425d0b7
Przemek Spaczek <pspaczek@divante.co>
2020-02-05 12:50:43 +0100

    feat: add nuxt playground for storefront-ui

---
+++ packages/nuxt/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <SfButton>Button</SfButton>
+  </div>
+</template>
+
+<script>
+import {SfButton} from '@storefront-ui/vue'
+
+export default {
+  components: {
+    SfButton
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~@storefront-ui/vue/styles.scss";
+</style>
