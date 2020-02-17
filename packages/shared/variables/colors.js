18ef34fca3625570dfb98c1b1ae4c806ffd021be
Claudia Pascual Gea <clau.p.gea@gmail.com>
2020-02-17 14:40:51 +0100

    docs: addjust docs to CSS custom properties (#748)
    * fix: fix color variables
    
    * fix: fix docs links
    
    * fix: change sfui.scss import order
    
    * fix: fix color variables
    
    * docs: add css vars docs
    
    * docs: add colors css vars docs
    
    * docs: add colors variables doc
    
    * docs: fix doc sizes
    
    * docs: fix vue components docs script
    
    * docs: fix getComponentInfoFromScss function
    
    * docs: add css vars table generation
    
    * docs: fix css modifiers generation
    
    * docs: add CSS vars auto generated doc

--- packages/shared/variables/colors.js
+++ packages/shared/variables/colors.js
@@ -1,4 +1,4 @@
-export const colors = {
+export const iconColors = {
   greenPrimary: "green-primary",
   greenSecondary: "green-secondary",
   black: 'black',
@@ -17,35 +17,16 @@ export const colors = {
   blueSecondary: "blue-secondary",
   accent: "accent",
 };
-export const palettes = {
-  primary: {
-    bg: '#5ece7b',
-    variant: '#9ee2b0',
-  },
-  light: {
-    bg: '#f1f2f3',
-    variant: '#ffffff',
-  },
-  dark: {
-    bg: '#1d1f22',
-    variant: '#737680',
-  },
-  info: {
-    bg: '#6699cc',
-    variant: '#e1f4fe',
-  },
-  success: {
-    bg: '#5ece7b',
-    variant: '#9ee2b0',
-  },
-  warning: {
-    bg: '#ffdf3d',
-    variant: '#fffceb',
-  },
-  danger: {
-    bg: '#e22225',
-    variant: '#fff6f5',
-  }
+export const colors = {
+  primary: "color-primary",
+  secondary: "color-secondary",
+  light: "color-light",
+  dark: "color-dark",
+  info: "color-info",
+  success: "color-success",
+  warning: 'color-warning',
+  danger: 'color-danger'
 };
 export const colorsValues = Object.values(colors);
+export const iconColorsValues = Object.values(iconColors);
 export default colors;
