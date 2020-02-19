3e90f5fb817ee845e5abfc8c38bbe5f88695a968
Przemek Spaczek <pspaczek@divante.co>
2020-02-19 17:02:22 +0100

    fix: docs components

--- packages/vue/docs/.vuepress/components/SfDocsColors.vue
+++ packages/vue/docs/.vuepress/components/SfDocsColors.vue
@@ -13,12 +13,6 @@
     </div>
   </div>
 </template>
-<style lang="scss">
-@import "~@storefront-ui/shared/styles/variables/scss/_functions.scss";
-@import "~@storefront-ui/shared/styles/variables/scss/_colors.scss";
-@import "~@storefront-ui/shared/styles/helpers/colors";
-@import "~@storefront-ui/shared/styles/variables/css/_colors.scss";
-</style>
 <script>
 import { colors, iconColors } from '@storefront-ui/shared/variables/colors';
 import SfDocsColorPalette from './SfDocsColorPalette';
@@ -42,31 +36,33 @@ export default {
   },
 }
 </script>
+<style lang="scss">
+  @import "~@storefront-ui/shared/styles/variables/colors";
+</style>
 <style lang="scss" scoped>
-/*@import "~@storefront-ui/shared/styles/variables/scss/_colors.scss";*/
-.sf-docs-colors--list {
-  display: flex;
-  flex-flow: wrap;
-  text-align: center;
-}
-.sf-docs-colors--list__item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 1rem;
-}
-.sf-docs-colors--list__item--color {
-  width: 80px;
-  height: 80px;
-  margin: 1rem;
-}
-.sf-docs-colors--list__item--label {
-  font-size: 0.8rem;
-}
-
-/*@each $pallete, $color in $icon-colors {
-  .#{$pallete} {
-    background-color: #{$color};
+  @import "~@storefront-ui/shared/styles/helpers";
+  .sf-docs-colors--list {
+    display: flex;
+    flex-flow: wrap;
+    text-align: center;
   }
-}*/
+  .sf-docs-colors--list__item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+  .sf-docs-colors--list__item--color {
+    width: 80px;
+    height: 80px;
+    margin: 1rem;
+  }
+  .sf-docs-colors--list__item--label {
+    font-size: 0.8rem;
+  }
+  @each $pallete, $color in $icon-colors {
+    .#{$pallete} {
+      background: #{$color};
+    }
+  }
 </style>
--- packages/vue/docs/.vuepress/components/SfDocsSizes.vue
+++ packages/vue/docs/.vuepress/components/SfDocsSizes.vue
@@ -10,7 +10,6 @@
 </template>
 <script>
   import {typographySizes} from '@storefront-ui/shared/variables/sizes';
-
   export default {
     data() {
       return {
@@ -20,42 +19,41 @@
     },
   }
 </script>
-<style scoped>
-  /*h3 {
-    text-transform: capitalize;
-  }
-  .sf-docs-sizes--container-list__item-wrapper {
-    display: flex;
-    flex-flow: wrap;
-  }
-  .sf-docs-sizes--container-list__item {
-    margin: 0.5rem;
-    padding: 0.5rem 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    min-width: 100px;
-    justify-content: flex-end;
-    border: 1px solid #bebfc4;
-  }
-  .sf-docs-sizes--container-list__item-label {
-    margin: 0.5rem 0.5rem 0;
-    padding-top: 0.5rem;
-    border-top: 1px solid #bebfc4;
-    width: 100%;
-    text-align: center;
-    font-family: monospace;
-    color: #6699cc;
-    font-size: 0.8rem;
-  }*/
+<style>
+  h3 {
+  text-transform: capitalize;
+}
+.sf-docs-sizes--container-list__item-wrapper {
+  display: flex;
+  flex-flow: wrap;
+}
+.sf-docs-sizes--container-list__item {
+  margin: 0.5rem;
+  padding: 0.5rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-width: 100px;
+  justify-content: flex-end;
+  border: 1px solid #bebfc4;
+}
+.sf-docs-sizes--container-list__item-label {
+  margin: 0.5rem 0.5rem 0;
+  padding-top: 0.5rem;
+  border-top: 1px solid #bebfc4;
+  width: 100%;
+  text-align: center;
+  font-family: monospace;
+  color: #6699cc;
+  font-size: 0.8rem;
+}
 </style>
-<style lang="scss">
-  /*@import "~@storefront-ui/shared/styles/variables";
-  @import "~@storefront-ui/shared/styles/variables/css/typography.scss";
-
+<style lang="scss" >
+  @import "~@storefront-ui/shared/styles/variables/colors";
+  @import "~@storefront-ui/shared/styles/helpers";
   @each $name, $size in $typography-map {
     .font-size-#{$name} {
       font-size: #{$size};
     }
-  }*/
+  }
 </style>
