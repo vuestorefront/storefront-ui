c04bb4de36edb9905ca2b63f3284a91d6a78f1f3
Przemek Spaczek <pspaczek@divante.co>
2020-02-21 22:01:00 +0100

    fix: stories for SfTabs

--- packages/vue/src/components/organisms/SfTabs/SfTabs.stories.js
+++ packages/vue/src/components/organisms/SfTabs/SfTabs.stories.js
@@ -3,31 +3,30 @@ import { storiesOf } from "@storybook/vue";
 
 import SfTabs from "./SfTabs.vue";
 
-storiesOf("Organisms|Tabs", module)
-  .add("Common", () => ({
-    components: { SfTabs },
-    data() {
-      return {
-        tabs: [
-          {
-            title: "Description",
-            content:
-              "The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
-          },
-          {
-            title: "Read reviews",
-            content:
-              "The Larissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
-          },
-          {
-            title: "Additional Information",
-            content:
-              "The Marissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
-          }
-        ]
-      };
-    },
-    template: `<SfTabs
+storiesOf("Organisms|Tabs", module).add("Common", () => ({
+  components: { SfTabs },
+  data() {
+    return {
+      tabs: [
+        {
+          title: "Description",
+          content:
+            "The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
+        },
+        {
+          title: "Read reviews",
+          content:
+            "The Larissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
+        },
+        {
+          title: "Additional Information",
+          content:
+            "The Marissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
+        }
+      ]
+    };
+  },
+  template: `<SfTabs
         :open-tab="openTab">
         <SfTab 
             v-for="tab in tabs" 
@@ -37,41 +36,4 @@ storiesOf("Organisms|Tabs", module)
           {{tab.content}}
         </SfTab>
       </SfTabs>`
-  }))
-  .add("[slot] title", () => ({
-    components: { SfTabs },
-    data() {
-      return {
-        tabs: [
-          {
-            title: "Description",
-            content:
-              "The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
-          },
-          {
-            title: "Read reviews",
-            content:
-              "The Larissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
-          },
-          {
-            title: "Additional Information",
-            content:
-              "The Marissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
-          }
-        ]
-      };
-    },
-    template: `<SfTabs
-        :open-tab="openTab">
-        <SfTab 
-            v-for="tab in tabs" 
-            :key="tab.title" 
-            :title="tab.title"
-        >
-          <template #title={tabClick}>
-            <div @click="tabClick" :style="{margin: '0 1rem', cursor: 'pointer'}">CUSTOM TITLE</div>
-          </template>
-          {{tab.content}}
-        </SfTab>
-      </SfTabs>`
-  }));
+}));
