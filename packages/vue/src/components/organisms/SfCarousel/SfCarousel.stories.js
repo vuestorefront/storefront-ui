af2c5e2c776c921e8f433a443d51a111cf386152
Przemek Spaczek <pspaczek@divante.co>
2020-02-21 21:40:30 +0100

    fix: remove knobs from SfCarousel

--- packages/vue/src/components/organisms/SfCarousel/SfCarousel.stories.js
+++ packages/vue/src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, number, object } from "@storybook/addon-knobs";
 
 import SfCarousel from "./SfCarousel.vue";
 
@@ -10,66 +9,38 @@ const StoriesPlaceholder = {
 };
 
 storiesOf("Organisms|Carousel", module)
-  .addDecorator(withKnobs)
   .add("Common", () => ({
     components: { SfCarousel, StoriesPlaceholder },
-    props: {
-      settings: {
-        default: object("settings", { perView: 4 }, "Props")
-      },
-      slidesNumber: {
-        default: number("slidesNumber", 8, {}, "Stories")
-      }
-    },
     template: `<SfCarousel
-        :settings="settings"
         :style="{maxWidth: '1140px', margin: 'auto'}"
       >
-        <SfCarouselItem v-for="index in slidesNumber">
+        <SfCarouselItem v-for="index in 24">
           <StoriesPlaceholder :index="index"/>
         </SfCarouselItem>
       </SfCarousel>`
   }))
   .add("[slot] next", () => ({
     components: { SfCarousel, StoriesPlaceholder },
-    props: {
-      settings: {
-        default: object("settings", { perView: 4 }, "Props")
-      },
-      slidesNumber: {
-        default: number("slidesNumber", 8, {}, "Stories")
-      }
-    },
     template: `<SfCarousel
-        :settings="settings"
         :style="{maxWidth: '1140px', margin: 'auto'}"
     >
       <template #next="{go}">
         <button @click="go">NEXT</button>
       </template>
-      <SfCarouselItem v-for="index in slidesNumber">
+      <SfCarouselItem v-for="index in 12">
         <StoriesPlaceholder :index="index"/>
       </SfCarouselItem>
     </SfCarousel>`
   }))
   .add("[slot] prev", () => ({
     components: { SfCarousel, StoriesPlaceholder },
-    props: {
-      settings: {
-        default: object("settings", { perView: 4 }, "Props")
-      },
-      slidesNumber: {
-        default: number("slidesNumber", 8, {}, "Stories")
-      }
-    },
     template: `<SfCarousel
-        :settings="settings"
         :style="{maxWidth: '1140px', margin: 'auto'}"
     >
       <template #prev="{go}">
         <button @click="go">PREV</button>
       </template>
-      <SfCarouselItem v-for="index in slidesNumber">
+      <SfCarouselItem v-for="index in 12">
         <StoriesPlaceholder :index="index"/>
       </SfCarouselItem>
     </SfCarousel>`
