a89b96a88161b9c7c6ed0a459671cb53e69d5029
Anna Musial <amusial@divante.pl>
2020-02-25 09:31:59 +0100

    fix: set initial slide in SfGallery (#760)
    * fix: fixed issue with choosing current image in gallery
    
    * fix: current on SfGallery stories
    
    * refactor: conditional in updatedSliderOptions computed property
    
    Co-authored-by: Przemek Spaczek <pspaczek@divante.co>

--- packages/vue/src/components/molecules/SfGallery/SfGallery.vue
+++ packages/vue/src/components/molecules/SfGallery/SfGallery.vue
@@ -147,6 +147,9 @@ export default {
         mobile: desktop,
         desktop: big
       }));
+    },
+    updatedSliderOptions() {
+      return { ...this.sliderOptions, startAt: this.activeIndex };
     }
   },
   mounted() {
@@ -154,7 +157,7 @@ export default {
       // handle slider with swipe and transitions with Glide.js
       // https://glidejs.com/docs/
       if (this.images.length < 1) return;
-      const glide = new Glide(this.$refs.glide, this.sliderOptions);
+      const glide = new Glide(this.$refs.glide, this.updatedSliderOptions);
       glide.on("run", () => {
         this.go(glide.index);
       });
