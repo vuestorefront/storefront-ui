71c1fea7ccab469b4757d71168557fe66bddfdbd
Przemek Spaczek <pspaczek@divante.co>
2020-02-26 11:30:28 +0100

    refactor: remove sfui scripts

--- packages/vue/scripts/postinstall.js
+++ packages/vue/scripts/postinstall.js
@@ -1,11 +1,9 @@
 "use strict";
 
 const { createPlaygroundVue } = require("./create-playground-vue");
-const { createRootSfuiScss } = require("./create-root-sfui-scss");
 
 function runPostInstall() {
   createPlaygroundVue();
-  createRootSfuiScss();
 }
 
 runPostInstall();
