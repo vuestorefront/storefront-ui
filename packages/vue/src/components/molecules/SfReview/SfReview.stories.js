485ed94c527ca220760690bd437769da03cca9ac
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-03-06 09:58:49 +0100

    refactor: add story with icon

--- packages/vue/src/components/molecules/SfReview/SfReview.stories.js
+++ packages/vue/src/components/molecules/SfReview/SfReview.stories.js
@@ -45,6 +45,52 @@ storiesOf("Molecules|Review", module)
       :read-more-text="readMoreText"
       :hide-full-text="hideFullText" />`
   }))
+  .add("[slot] icon", () => ({
+    components: { SfReview },
+    props: {
+      author: {
+        default: text("author", "Jane D.Smith", "Props")
+      },
+      date: {
+        default: text("date", "April 2019", "Props")
+      },
+      message: {
+        default: text(
+          "message",
+          "I was looking for a bright light for the kitchen but wanted some item more modern than a strip light. this one is perfect, very bright and looks great. I can comment on interlation as I had an electrition instal it. Would recommend.",
+          "Props"
+        )
+      },
+      maxRating: {
+        default: number("maxRating", 5, {}, "Props")
+      },
+      rating: {
+        default: number("rating", 4, {}, "Props")
+      },
+      charLimit: {
+        default: number("charLimit", 250, {}, "Props")
+      },
+      readMoreText: {
+        default: text("readMoreText", "Read more", "Props")
+      },
+      hideFullText: {
+        default: text("hideFullText", "Read less", "Props")
+      }
+    },
+    template: `<SfReview
+      :author="author"
+      :date="date"
+      :message="message"
+      :max-rating="maxRating"
+      :rating="rating"
+      :char-limit="charLimit"
+      :read-more-text="readMoreText"
+      :hide-full-text="hideFullText">
+      <template #icon>
+        🥑
+      </template>  
+    </SfReview>`
+  }))
   .add("[slot] author", () => ({
     components: { SfReview },
     props: {
