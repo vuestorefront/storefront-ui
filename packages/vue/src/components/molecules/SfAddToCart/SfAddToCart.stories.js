293152fc813c940b0b6e00c3a11b9fad6f2ea088
Justyna <jgieracka@divante.pl>
2020-03-09 07:18:51 +0100

    refactor: w/o quantity selector

--- packages/vue/src/components/molecules/SfAddToCart/SfAddToCart.stories.js
+++ packages/vue/src/components/molecules/SfAddToCart/SfAddToCart.stories.js
@@ -16,7 +16,7 @@ storiesOf("Molecules|AddToCart", module)
         qty: 1
       };
     },
-    template: `<div style="max-width: 21.25rem">
+    template: `<div>
       <SfAddToCart 
         v-model="qty"
         :disabled="disabled"
@@ -35,7 +35,7 @@ storiesOf("Molecules|AddToCart", module)
         qty: 1
       };
     },
-    template: `<div style="max-width: 21.25rem">
+    template: `<div>
         <SfAddToCart
           v-model="qty"
           :disabled="disabled"
@@ -59,7 +59,7 @@ storiesOf("Molecules|AddToCart", module)
         qty: 1
       };
     },
-    template: `<div style="max-width: 21.25rem">
+    template: `<div>
         <SfAddToCart 
           :disabled="disabled"
           @click="()=>{}">
