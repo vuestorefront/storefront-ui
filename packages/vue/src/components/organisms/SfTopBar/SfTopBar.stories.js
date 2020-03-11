b4c386784c16825fca2e046d0a12e932ea506700
Przemek Spaczek <pspaczek@divante.co>
2020-03-11 14:26:24 +0100

    feat: improve SfTopBar

--- packages/vue/src/components/organisms/SfTopBar/SfTopBar.stories.js
+++ packages/vue/src/components/organisms/SfTopBar/SfTopBar.stories.js
@@ -3,45 +3,68 @@ import { storiesOf } from "@storybook/vue";
 
 import SfTopBar from "./SfTopBar.vue";
 import SfButton from "../../atoms/SfButton/SfButton.vue";
+import SfImage from "../../atoms/SfImage/SfImage.vue"
 
 storiesOf("Organisms|TopBar", module)
   .add("Common", () => ({
-    components: { SfTopBar, SfButton },
+    components: { SfTopBar, SfButton, SfImage },
     template: `<SfTopBar>
-    <template #center>
-      <p style="margin-right: 5px;">Download our application.</p>
-      <SfButton class="sf-button--text">Find out more</SfButton>
+      <template #center>
+        <p style="margin-right: 5px;">Download our application.</p>
+        <SfButton class="sf-button--text">Find out more</SfButton>
       </template>
       <template #left>
         <SfButton class="sf-button--text">Help & FAQs</SfButton>
       </template>
       <template #right>
         <div style="margin-right: 27px">Location:</div>
-        <img src="assets/storybook/SfTopBar/flag.png" alt="flag"/>
+        <SfImage src="/assets/storybook/SfTopBar/flag.png" alt="flag"/>
       </template>
-    </SfTopBar>`
-  }))
-  .add("[slot] default", () => ({
-    components: { SfTopBar },
-    template: `<SfTopBar>
-      CUSTOM DEFAULT
     </SfTopBar>`
   }))
   .add("[slot] left", () => ({
-    components: { SfTopBar },
+    components: { SfTopBar, SfButton, SfImage },
     template: `<SfTopBar>
+      <template #center>
+        <p style="margin-right: 5px;">Download our application.</p>
+        <SfButton class="sf-button--text">Find out more</SfButton>
+      </template>
       <template #left>
         CUSTOM LEFT
       </template>
-      Download our application. Find out more.
+      <template #right>
+        <div style="margin-right: 27px">Location:</div>
+        <SfImage src="/assets/storybook/SfTopBar/flag.png" alt="flag"/>
+      </template>
+    </SfTopBar>`
+  }))
+  .add("[slot] center", () => ({
+    components: { SfTopBar, SfButton, SfImage },
+    template: `<SfTopBar>
+      <template #center>
+        CUSTOM CENTER
+      </template>
+      <template #left>
+        <SfButton class="sf-button--text">Help & FAQs</SfButton>
+      </template>
+      <template #right>
+        <div style="margin-right: 27px">Location:</div>
+        <SfImage src="/assets/storybook/SfTopBar/flag.png" alt="flag"/>
+      </template>
     </SfTopBar>`
   }))
   .add("[slot] right", () => ({
-    components: { SfTopBar },
+    components: { SfTopBar, SfButton, SfImage },
     template: `<SfTopBar>
+      <template #center>
+        <p style="margin-right: 5px;">Download our application.</p>
+        <SfButton class="sf-button--text">Find out more</SfButton>
+      </template>
+      <template #left>
+        <SfButton class="sf-button--text">Help & FAQs</SfButton>
+      </template>
       <template #right>
         CUSTOM RIGHT
       </template>
-      Download our application. Find out more.
     </SfTopBar>`
-  }));
+  }))
