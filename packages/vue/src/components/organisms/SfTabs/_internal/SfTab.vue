39a6b3625286e41f843bc4b52fcb1405b6794085
Justyna <jgieracka@divante.pl>
2020-03-13 10:36:01 +0100

    refactor: tabs w/o transition

--- packages/vue/src/components/organisms/SfTabs/_internal/SfTab.vue
+++ packages/vue/src/components/organisms/SfTabs/_internal/SfTab.vue
@@ -11,7 +11,7 @@
       >
         {{ title }}
         <div class="sf-tabs__chevron">
-          <SfChevron :class="{ 'sf-chevron--top': isActive }" />
+          <SfChevron :class="{ 'sf-chevron--right': !isActive }" />
         </div>
       </div>
     </slot>
@@ -59,26 +59,3 @@ export default {
   }
 };
 </script>
-<style>
-.slide-left-enter-active {
-  -webkit-animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-  animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-}
-.slide-left-leave-active {
-  -webkit-animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
-    reverse;
-  animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both reverse;
-}
-@keyframes slide-left {
-  0% {
-    -webkit-transform: translateX(-1000px);
-    transform: translateX(-1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateX(0);
-    transform: translateX(0);
-    opacity: 1;
-  }
-}
-</style>
