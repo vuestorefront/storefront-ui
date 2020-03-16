b9d38338aca8f3f7300a15d33032d751970c90a9
Anna <amusial@divante.pl>
2020-03-16 13:29:42 +0100

    chore: adjusted spacers and paddings

--- packages/vue/src/components/organisms/SfAccordion/SfAccordion.stories.js
+++ packages/vue/src/components/organisms/SfAccordion/SfAccordion.stories.js
@@ -69,7 +69,7 @@ storiesOf("Organisms|Accordion", module)
           <SfListItem
             v-for="item in accordion.items"
             :key="item.label"
-            :style="{padding: '1.25rem 2rem 1.25rem 2rem'}">
+            >
             <SfMenuItem 
               :label="item.label" 
               :count="item.count"
