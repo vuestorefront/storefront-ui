a351bf21d78721dcf46fdcf12808785ff15e7b55
Anna <amusial@divante.pl>
2020-03-18 22:10:57 +0100

    fix: adjusted directive

--- packages/vue/src/utilities/directives/focus-directive.js
+++ packages/vue/src/utilities/directives/focus-directive.js
@@ -3,21 +3,17 @@ export const focus = {
     el.tabindex = "0";
   },
   bind: function(el) {
-    el.addEventListener("mousedown", function() {
+    el.addEventListener("mousedown", function bindMouse() {
       el.style.outline = "none";
       el.blur();
     });
-    el.addEventListener("keyup", function() {
+    el.addEventListener("keyup", function bindKey() {
       el.style.outline = "";
       el.focus();
     });
   },
   unbind: function(el) {
-    el.removeEventListener("mousedown", function() {
-      el.style.outline = "";
-    });
-    el.removeEventListener("keyup", function() {
-      el.blur();
-    });
+    el.removeEventListener("mousedown", el.bindMouse());
+    el.removeEventListener("keyup", el.bindKey());
   }
 };
