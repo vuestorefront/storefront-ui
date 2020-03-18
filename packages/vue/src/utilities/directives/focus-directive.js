e1f119cde4491b4db2a04733e9ca75032551a4fb
Anna <amusial@divante.pl>
2020-03-18 22:14:49 +0100

    fix: adjust naming

--- packages/vue/src/utilities/directives/focus-directive.js
+++ packages/vue/src/utilities/directives/focus-directive.js
@@ -3,17 +3,17 @@ export const focus = {
     el.tabindex = "0";
   },
   bind: function(el) {
-    el.addEventListener("mousedown", function bindMouse() {
+    el.addEventListener("mousedown", function handleMouse() {
       el.style.outline = "none";
       el.blur();
     });
-    el.addEventListener("keyup", function bindKey() {
+    el.addEventListener("keyup", function handleKey() {
       el.style.outline = "";
       el.focus();
     });
   },
   unbind: function(el) {
-    el.removeEventListener("mousedown", el.bindMouse());
-    el.removeEventListener("keyup", el.bindKey());
+    el.removeEventListener("mousedown", el.handleMouse());
+    el.removeEventListener("keyup", el.handleKey());
   }
 };
