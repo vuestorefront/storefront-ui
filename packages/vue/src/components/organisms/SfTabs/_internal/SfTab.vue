74ae264e3df3e3dda91571a83152c18144e5348a
Przemek Spaczek <pspaczek@divante.co>
2020-03-19 17:47:18 +0100

    fix: remove transition

--- packages/vue/src/components/organisms/SfTabs/_internal/SfTab.vue
+++ packages/vue/src/components/organisms/SfTabs/_internal/SfTab.vue
@@ -16,12 +16,10 @@
       </div>
     </slot>
     <div class="sf-tabs__content">
-      <transition name="slide-left" mode="out-in">
-        <div v-if="isActive" class="sf-tabs__content__tab">
-          <!--@slot Default. Here you should pass your tab content -->
-          <slot />
-        </div>
-      </transition>
+      <div v-if="isActive" class="sf-tabs__content__tab">
+        <!--@slot Default. Here you should pass your tab content -->
+        <slot />
+      </div>
     </div>
   </Fragment>
 </template>
@@ -63,26 +61,3 @@ export default {
   }
 };
 </script>
-<style scoped>
-.slide-left-enter-active {
-  -webkit-animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-  animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-}
-.slide-left-leave-active {
-  -webkit-animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
-    reverse;
-  animation: slide-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both reverse;
-}
-@keyframes slide-left {
-  0% {
-    -webkit-transform: translateX(-1000px);
-    transform: translateX(-1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateX(0);
-    transform: translateX(0);
-    opacity: 1;
-  }
-}
-</style>
