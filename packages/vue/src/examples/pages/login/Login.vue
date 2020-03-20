c12722a7a5736bae2240ea3110c7808e64ab2b10
Adam Pawliński <apawlinski@divante.pl>
2020-03-20 20:16:28 +0100

    refactor: login example adjust style

--- packages/vue/src/examples/pages/login/Login.vue
+++ packages/vue/src/examples/pages/login/Login.vue
@@ -1,110 +1,194 @@
 <template>
   <div>
-    <SfModal :visible="true" title="Log in">
+    <SfModal :visible="true" :cross="false" title="My Account">
+      <!-- <transition name="fade" mode="out-in"> -->
       <transition name="fade" mode="out-in">
-        <div v-if="isLogin" key="log-in">
-          <div class="form">
-            <SfInput
-              v-model="email"
-              name="email"
-              label="Your email"
-              class="form__element"
+        <!-- <SfBar title="My Account" :close="false" /> -->
+        <div class="login-container">
+          <SfImage :src="src" />
+          <SfSection title-heading="Come on in!" :level-heading="1">
+            <p>
+              View orders and update your details.<br />
+              Make your checkout fast and easy!
+            </p>
+          </SfSection>
+          <SfButton
+            class="sf-button login-button__signin"
+            @click="isSignIn = true"
+            >SIGN IN</SfButton
+          >
+          <SfButton
+            class="sf-button login-button__login"
+            @click="isLogin = true"
+            >JOIN</SfButton
+          >
+          <SfBottomNavigation>
+            <SfBottomNavigationItem
+              v-for="(item, key) in items"
+              :key="key"
+              :icon="item.icon"
+              :label="item.label"
+              icon-size="20px"
             />
-            <SfInput
-              v-model="password"
-              name="password"
-              label="Password"
-              type="password"
-              class="form__element"
-            />
-            <SfCheckbox
-              v-model="rememberMe"
-              name="remember-me"
-              label="Remember me"
-              class="form__element"
-            />
-            <SfButton class="sf-button--full-width form__button"
-              >Login</SfButton
+            <SfBottomNavigationItem
+              label="Basket"
+              icon="add_to_cart"
+              :is-floating="isFloating"
             >
-          </div>
-          <div class="action">
-            <SfButton class="sf-button--text color-secondary"
-              >Forgotten password?</SfButton
-            >
-          </div>
-          <div class="bottom">
-            Don't have and account yet?
-            <SfButton
-              class="sf-button--text color-secondary"
-              @click="isLogin = false"
-              >Register today?</SfButton
-            >
-          </div>
-        </div>
-        <div v-else key="sign-up" class="form">
-          <div class="from">
-            <SfInput
-              v-model="email"
-              name="email"
-              label="Your email"
-              class="form__element"
-            />
-            <SfInput
-              v-model="firstName"
-              name="first-name"
-              label="First Name"
-              class="form__element"
-            />
-            <SfInput
-              v-model="lastName"
-              name="last-name"
-              label="Last Name"
-              class="form__element"
-            />
-            <SfInput
-              v-model="password"
-              name="password"
-              label="Password"
-              type="password"
-              class="form__element"
-            />
-            <SfCheckbox
-              v-model="createAccount"
-              name="create-account"
-              label="I want to create an account"
-              class="form__element"
-            />
-            <SfButton class="sf-button--full-width form__button"
-              >Create an account</SfButton
-            >
-          </div>
-          <div class="action">
-            or
-            <SfButton
-              class="sf-button--text color-secondary"
-              @click="isLogin = true"
-              >login in to your account</SfButton
-            >
-          </div>
+              <template #icon>
+                <SfCircleIcon>
+                  <SfIcon
+                    icon="add_to_cart"
+                    color="white"
+                    size="28px"
+                    :style="{ margin: '0 0 0 -2px' }"
+                  />
+                </SfCircleIcon>
+              </template>
+            </SfBottomNavigationItem>
+          </SfBottomNavigation>
         </div>
       </transition>
+      <div v-if="isLogin" key="log-in">
+        <div class="form">
+          <SfInput
+            v-model="email"
+            name="email"
+            label="Your email"
+            class="form__element"
+          />
+          <SfInput
+            v-model="password"
+            name="password"
+            label="Password"
+            type="password"
+            class="form__element"
+          />
+          <SfCheckbox
+            v-model="rememberMe"
+            name="remember-me"
+            label="Remember me"
+            class="form__element"
+          />
+          <SfButton class="sf-button--full-width form__button">Login</SfButton>
+        </div>
+        <div class="action">
+          <SfButton class="sf-button--text color-secondary"
+            >Forgotten password?</SfButton
+          >
+        </div>
+        <div class="bottom">
+          Don't have and account yet?
+          <!-- <SfButton
+                class="sf-button--text color-secondary"
+                @click="isLogin = false"
+                >Register today?</SfButton
+              > -->
+        </div>
+      </div>
+      <div v-if="isSignIn" key="sign-up" class="form">
+        <div class="from">
+          <SfInput
+            v-model="email"
+            name="email"
+            label="Your email"
+            class="form__element"
+          />
+          <SfInput
+            v-model="firstName"
+            name="first-name"
+            label="First Name"
+            class="form__element"
+          />
+          <SfInput
+            v-model="lastName"
+            name="last-name"
+            label="Last Name"
+            class="form__element"
+          />
+          <SfInput
+            v-model="password"
+            name="password"
+            label="Password"
+            type="password"
+            class="form__element"
+          />
+          <SfCheckbox
+            v-model="createAccount"
+            name="create-account"
+            label="I want to create an account"
+            class="form__element"
+          />
+          <SfButton class="sf-button--full-width form__button"
+            >Create an account</SfButton
+          >
+        </div>
+        <div class="action">
+          or
+          <!-- <SfButton
+                class="sf-button--text color-secondary"
+                @click="isLogin = true"
+                >login in to your account</SfButton
+              > -->
+        </div>
+      </div>
+      <!-- </transition> -->
     </SfModal>
   </div>
 </template>
 <script>
-import { SfModal, SfInput, SfButton, SfCheckbox } from "@storefront-ui/vue";
+import {
+  // SfBar,
+  SfImage,
+  SfSection,
+  SfModal,
+  SfInput,
+  SfButton,
+  SfCheckbox,
+  SfBottomNavigation,
+  SfCircleIcon,
+  SfIcon
+} from "@storefront-ui/vue";
 export default {
   name: "Login",
-  components: { SfModal, SfInput, SfButton, SfCheckbox },
+  components: {
+    // SfBar,
+    SfImage,
+    SfSection,
+    SfModal,
+    SfInput,
+    SfButton,
+    SfCheckbox,
+    SfBottomNavigation,
+    SfCircleIcon,
+    SfIcon
+  },
   data() {
     return {
-      isLogin: true,
+      isLogin: false,
+      isSignIn: false,
       email: "",
       password: "",
       createAccount: false,
       rememberMe: false,
       firstName: "",
-      lastName: ""
+      lastName: "",
+      src: {
+        mobile: {
+          url: "/assets/storybook/Login/EmptyHistory.png"
+        },
+        desktop: {
+          url: "/assets/storybook/Login/EmptyHistory.png"
+        }
+      },
+      items: [
+        { icon: "menu", label: "Menu" },
+        { icon: "heart", label: "Heart" },
+        { icon: "profile", label: "Profile" }
+      ],
+      label: "Home",
+      icon: "home",
+      isFloating: true
     };
   },
   watch: {
@@ -121,7 +205,27 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "~@storefront-ui/vue/styles";
-
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.sf-heading__title {
+  color: var(--c-primary);
+  font-weight: var(--font-semibold);
+}
+.sf-section__content {
+  color: var(--c-secondary-variant);
+  font-family: var(--font-family-secondary);
+  font-size: var(--font-base);
+}
+.login-button__login {
+  --button-background: var(--c-secondary);
+  margin: var(--spacer-sm);
+}
+.login-button__signin {
+  --button-background: var(--c-primary);
+}
 .form {
   &__element {
     margin: 0 0 var(--spacer-extra-big) 0;
