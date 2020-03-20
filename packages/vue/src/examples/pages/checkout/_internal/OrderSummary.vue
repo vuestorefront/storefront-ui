c0029eb9752c9bc723be7206f1394989fc5d9892
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-03-20 16:24:33 +0100

    refactor: removed products, adjusted styles

--- packages/vue/src/examples/pages/checkout/_internal/OrderSummary.vue
+++ packages/vue/src/examples/pages/checkout/_internal/OrderSummary.vue
@@ -1,58 +1,10 @@
 <template>
-  <div>
-    <div class="highlighted">
-      <SfHeading
-        title="Order summary"
-        class="sf-heading--left sf-heading--no-underline title"
-      />
-      <div class="total-items">
-        <h3>Total items: {{ totalItems }}</h3>
-        <SfButton
-          class="sf-button--text color-secondary"
-          @click="listIsHidden = !listIsHidden"
-          >{{ listIsHidden ? "Show" : "Hide" }} items list
-        </SfButton>
-      </div>
-      <transition name="fade">
-        <div v-if="!listIsHidden" class="collected-product-list">
-          <SfCollectedProduct
-            v-for="(product, index) in products"
-            :key="index"
-            v-model="product.qty"
-            :image="product.image"
-            :title="product.title"
-            :regular-price="product.price.regular"
-            :special-price="product.price.special"
-            :stock="product.stock"
-            class="collected-product"
-            @click:remove="removeProduct(index)"
-          >
-            <template #configuration>
-              <div class="collected-product__properties">
-                <SfProperty
-                  v-for="(property, key) in product.configuration"
-                  :key="key"
-                  :name="property.name"
-                  :value="property.value"
-                  class="collected-product__property"
-                />
-              </div>
-            </template>
-            <template #actions>
-              <div>
-                <div class="collected-product__action">{{ product.sku }}</div>
-                <div class="collected-product__action">
-                  Quantity: <span class="product__qty">{{ product.qty }}</span>
-                </div>
-              </div>
-            </template>
-            <template #input>
-              <span></span>
-            </template>
-          </SfCollectedProduct>
-        </div>
-      </transition>
-    </div>
+  <div id="order-summary">
+    <SfHeading
+      title="Totals"
+      :level="3"
+      class="sf-heading--left sf-heading--no-underline title"
+    />
     <div class="highlighted highlighted--total">
       <SfProperty
         name="Products"
@@ -69,29 +21,33 @@
         :value="shippingMethod.price"
         class="sf-property--full-width property"
       />
+      <SfDivider class="divider" />
       <SfProperty
-        name="Total"
+        name="Total price"
         :value="total"
-        class="sf-property--full-width property-total"
+        class="sf-property--full-width property"
       />
     </div>
     <div class="highlighted promo-code">
+      <SfInput
+        v-model="promoCode"
+        name="promoCode"
+        placeholder="Enter promo code"
+        class="sf-input--filled promo-code__input"
+      />
+      <SfCircleIcon class="promo-code__circle-icon" icon="check" />
+    </div>
+    <div class="actions">
       <SfButton
-        class="sf-button--text promo-code__button "
-        @click="showPromoCode = !showPromoCode"
-        >{{ showPromoCode ? "-" : "+" }} Promo Code
-      </SfButton>
-      <transition name="fade">
-        <div v-if="showPromoCode">
-          <SfInput
-            v-model="promoCode"
-            name="promoCode"
-            label="Enter promo code"
-            class="promo-code__input"
-          />
-          <SfButton class="sf-button--full-width">Apply code</SfButton>
-        </div>
-      </transition>
+        class="sf-button--full-width actions__button"
+        @click="$emit('click:next')"
+        >{{ buttonName }}</SfButton
+      >
+      <SfButton
+        class="sf-button--text actions__button"
+        @click="$emit('click:back')"
+        >Go back</SfButton
+      >
     </div>
     <div class="highlighted">
       <SfCharacteristic
@@ -110,7 +66,8 @@
 import {
   SfHeading,
   SfButton,
-  SfCollectedProduct,
+  SfCircleIcon,
+  SfDivider,
   SfProperty,
   SfCharacteristic,
   SfInput
@@ -121,7 +78,8 @@ export default {
   components: {
     SfHeading,
     SfButton,
-    SfCollectedProduct,
+    SfDivider,
+    SfCircleIcon,
     SfProperty,
     SfCharacteristic,
     SfInput
@@ -138,31 +96,21 @@ export default {
     paymentMethods: {
       type: Array,
       default: () => []
+    },
+    characteristics: {
+      type: Array,
+      default: () => []
+    },
+    buttonName: {
+      type: String,
+      default: ""
     }
   },
   data() {
     return {
       promoCode: "",
       showPromoCode: false,
-      listIsHidden: false,
-      characteristics: [
-        {
-          title: "Safety",
-          description: "It carefully packaged with a personal touch",
-          icon: "safety"
-        },
-        {
-          title: "Easy shipping",
-          description:
-            "You’ll receive dispatch confirmation and an arrival date",
-          icon: "shipping"
-        },
-        {
-          title: "Changed your mind?",
-          description: "Rest assured, we offer free returns within 30 days",
-          icon: "return"
-        }
-      ]
+      listIsHidden: false
     };
   },
   computed: {
@@ -230,87 +178,44 @@ export default {
 <style lang="scss" scoped>
 @import "~@storefront-ui/vue/styles";
 
-.highlighted {
-  box-sizing: border-box;
-  width: 100%;
-  background: #f1f2f3;
-  padding: var(--spacer-extra-big);
-  margin: 0 0 var(--spacer-big) 0;
-  font: 300 var(--font-size-mall) / 1.6 var(--font-family-secondary);
-  &:last-child {
-    margin: 0;
-  }
-}
 .title {
-  --heading-title-margin: 0 0 var(--spacer-extra-big) 0;
+  --heading-title-margin: 0 0 var(--spacer-lg) 0;
 }
-.total-items {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font: 400 var(--font-size-big) / 1.6 var(--font-family-secondary);
-  h3 {
-    font: inherit;
-  }
-}
-.collected-product-list {
-  flex: 1;
-  margin: var(--spacer-big) calc(var(--spacer-big) * -1);
-}
-.collected-product {
-  margin: var(--spacer-big) 0;
-  --collected-product-title-font: 300 var(--font-size-small) / 1.6
-    var(--font-family-secondary);
-  &:last-child {
-    margin: var(--spacer-big) 0 0 0;
-  }
-  &__properties {
-    margin: var(--spacer-big) 0 0 0;
-  }
-  &__property {
-    --property-name-font: 300 var(--font-size-extra-small) / 1.6
-      var(--font-family-secondary);
-    --property-value-font: 300 var(--font-size-extra-small) / 1.6
-      var(--font-family-secondary);
-  }
-  &__actions {
-    transition: opacity 150ms ease-in-out;
-    opacity: var(--cp-actions-opacity, 0);
-  }
-  &__action {
-    --button-padding: 0;
-    font: 300 var(--font-size-extra-small) / 1.6 var(--font-family-secondary);
-  }
-  &:hover {
-    --cp-actions-opacity: 1;
-  }
-}
+
 .property {
-  --property-name-text-transform: none;
+  --property-name-font-size: var(--font-xl);
+  --property-value-font-size: var(--font-xl);
+  margin: var(--spacer-base) 0;
 }
-.property-total {
-  margin: var(--spacer-extra-big) 0 0 0;
-  --property-name-font: 500 var(--font-size-extra-big) / 1.6
-    var(--font-family-secondary);
-  --property-name-color: var(--c-text);
-  --property-value-font: 500 var(--font-size-extra-big) / 1.6
-    var(--font-family-secondary);
+
+.divider {
+  --divider-border-color: var(--c-white);
+  --divider-border-width: 2px;
+  --divider-margin: calc(var(--spacer-base) * 2) 0 0 0;
 }
+
 .promo-code {
-  &__button {
-    --button-padding: 0;
-    --button-text-decoration: none;
-    --button-text-transform: uppercase;
-    --button-font-size: var(--font-size-big);
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: var(--spacer-lg) 0 var(--spacer-base) 0;
+  &__circle-icon {
+    --button-size: 2rem;
+    --icon-size: 0.6875rem;
   }
   &__input {
-    margin: var(--spacer-big) 0;
+    flex: 1;
+    --input-background: var(--c-white);
+    margin: 0 var(--spacer-lg) 0 0;
   }
 }
 .characteristic {
-  margin: 0 0 var(--spacer-big) 0;
-  &:last-child {
-    margin: 0;
+  margin: var(--spacer-base) 0 var(--spacer-base) var(--spacer-xs);
+}
+
+.actions {
+  &__button {
+    margin: var(--spacer-sm) 0;
   }
 }
 </style>
