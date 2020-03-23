5ee7260096667b3547bee6d06929a838e6c80b57
Justyna <jgieracka@divante.pl>
2020-03-23 20:22:45 +0100

    refactor: select added

--- packages/vue/src/examples/pages/product/Product.vue
+++ packages/vue/src/examples/pages/product/Product.vue
@@ -26,9 +26,6 @@
         :is-sticky="false"
         account-icon="profile"
         cart-items-qty="0"
-        @click:cart="alert('@click:cart')"
-        @click:wishlist="alert('@click:wishlist')"
-        @click:account="alert('@click:account')"
       >
         <template #navigation>
           <SfHeaderNavigationItem v-for="item in navigation" :key="item">
@@ -42,62 +39,100 @@
       </SfHeader>
       <SfBreadcrumbs :breadcrumbs="breadcrumbs" />
     </div>
-    <main class="product__details">
-      <SfChevron
-        class="sf-chevron--left product__details__chevron mobile-only"
-      />
+    <main class="product">
+      <SfChevron class="sf-chevron--left product__chevron mobile-only" />
       <SfGallery
         :images="product.images"
         image-width="400"
         image-height="500"
         class="mobile-only"
       />
-      <div class="product__details__images desktop-only">
+      <div class="product__images desktop-only">
         <SfImage :src="product.images[1].big.url" />
         <SfImage :src="product.images[2].big.url" />
       </div>
-      <div class="product__details__info">
-        <div class="header">
+      <div class="product__info">
+        <div class="product__info__header">
           <SfHeading
             :title="product.name"
             :level="3"
-            class="sf-heading--no-underline sf-heading--left product-details__heading"
+            class="sf-heading--no-underline sf-heading--left"
           />
-          <SfIcon icon="drag" class="mobile-only" />
+          <SfIcon
+            icon="drag"
+            size="xl"
+            color="gray-secondary"
+            class="mobile-only"
+          />
         </div>
-        <div class="product__details__price-and-review">
+        <div class="product__price-and-review">
           <SfPrice :regular="product.price" />
           <div>
-            <SfRating :score="product.rating.rate" :max="product.rating.max" />
+            <div class="product__price-and-review__rating">
+              <SfRating
+                :score="product.rating.rate"
+                :max="product.rating.max"
+              />
+              <a
+                v-if="!!product.reviews"
+                href="#"
+                class="product__price-and-review__count"
+                @click="$emit('click:reviews')"
+              >
+                ({{ product.reviews.length }})
+              </a>
+            </div>
             <SfButton class="sf-button--text desktop-only"
               >Read all reviews</SfButton
             >
           </div>
         </div>
         <div>
-          <p class="desktop-only">
+          <p class="product__description desktop-only">
             {{ product.description }}
           </p>
-          <SfButton class="sf-button--text product__details btn__guide">
+          <SfButton class="sf-button--text desktop-only btn__guide">
             Size guide
           </SfButton>
-          <!-- TODO: ADD SfSelect -->
-          <div class="product__details__colors desktop-only">
-            <div>Color</div>
+          <SfSelect
+            v-model="selectedSize"
+            label="Size"
+            :valid="true"
+            class="sf-select--underlined product__select"
+          >
+            <SfSelectOption
+              v-for="(size, key) in product.sizes"
+              :key="key"
+              :value="size"
+            >
+              <SfProductOption :label="size"></SfProductOption>
+            </SfSelectOption>
+          </SfSelect>
+          <div class="product__colors desktop-only">
+            <p class="product__colors__label">Color:</p>
             <div v-for="(color, i) in product.colors" :key="i">
-              <SfColor :color="color" class="product__color" />
+              <SfColor :color="color" class="product__colors__color" />
             </div>
           </div>
-          <SfAddToCart />
-          <SfButton class="sf-button--text product__details btn__save">
+          <SfAddToCart
+            v-model="qty"
+            class="product__add-to-cart desktop-only"
+            @click="addToCart"
+          />
+          <SfButton class="sf-button--text desktop-only btn__save">
             Save for later
           </SfButton>
-          <SfButton class="sf-button--text product__details btn__compare">
+          <SfButton class="sf-button--text desktop-only btn__compare">
             Add to compare
           </SfButton>
         </div>
-        <SfTabs :open-tab="1" style="margin: 40px auto 80px">
-          <SfTab v-for="tab in tabs" :key="tab.title" :title="tab.title">
+        <SfTabs :open-tab="1" class="product__tabs">
+          <SfTab
+            v-for="tab in tabs"
+            :key="tab.title"
+            :title="tab.title"
+            class="product__tabs__tab"
+          >
             {{ tab.content }}
             <div v-if="tab.title === 'Description'">
               <SfProperty
@@ -105,9 +140,10 @@
                 :key="i"
                 :name="detailed.name"
                 :value="detailed.value"
+                class="product__tabs__property"
               />
             </div>
-            <div v-if="tab.title === 'Read reviews'">
+            <div v-else-if="tab.title === 'Read reviews'">
               <div v-for="(review, i) in product.reviews" :key="i">
                 <SfReview
                   :author="review.author"
@@ -119,7 +155,23 @@
                   read-more-text="Read more"
                   hide-full-text="Read less"
                 />
+                <SfDivider class="product__tabs__divider" />
               </div>
+            </div>
+            <div
+              v-else-if="tab.title === 'Additional Information'"
+              class="product__additional-info"
+            >
+              <p class="product__additional-info__title">Brand</p>
+              <p>{{ product.brand }}</p>
+              <p class="product__additional-info__title">Take care of me</p>
+              <p class="product__additional-info__paragraph">
+                Just here for the care instructions?
+              </p>
+              <p class="product__additional-info__paragraph">
+                Yeah, we thought so
+              </p>
+              <p>{{ product.careInstructions }}</p>
             </div>
           </SfTab>
         </SfTabs>
@@ -138,20 +190,11 @@
         @click="currentIcon = item.iconActive"
       />
       <SfBottomNavigationItem
-        label="Basket"
-        icon="add_to_cart"
+        label="Add to cart"
+        icon="plus"
         :is-floating="true"
+        @click="addToCart"
       >
-        <template #icon>
-          <SfCircleIcon aria-label="Add to cart">
-            <SfIcon
-              icon="add_to_cart"
-              color="white"
-              size="25px"
-              :style="{ margin: '0 0 0 -2px' }"
-            />
-          </SfCircleIcon>
-        </template>
       </SfBottomNavigationItem>
     </SfBottomNavigation>
   </div>
@@ -167,7 +210,6 @@ import {
   SfTabs,
   SfProperty,
   SfBottomNavigation,
-  SfCircleIcon,
   SfChevron,
   SfTopBar,
   SfButton,
@@ -175,12 +217,15 @@ import {
   SfBreadcrumbs,
   SfReview,
   SfAddToCart,
-  SfColor
+  SfColor,
+  SfSelect,
+  SfProductOption,
+  SfDivider
 } from "@storefront-ui/vue";
 import SfTab from "@storefront-ui/vue/src/components/organisms/SfTabs/_internal/SfTab";
 import SfBottomNavigationItem from "@storefront-ui/vue/src/components/organisms/SfBottomNavigation/_internal/SfBottomNavigationItem";
 import SfHeaderNavigationItem from "@storefront-ui/vue/src/components/organisms/SfHeader/_internal/SfHeaderNavigationItem";
-
+import SfSelectOption from "@storefront-ui/vue/src/components/molecules/SfSelect/_internal/SfSelectOption";
 export default {
   name: "Product",
   components: {
@@ -195,7 +240,6 @@ export default {
     SfProperty,
     SfBottomNavigation,
     SfBottomNavigationItem,
-    SfCircleIcon,
     SfChevron,
     SfTopBar,
     SfButton,
@@ -204,10 +248,16 @@ export default {
     SfReview,
     SfAddToCart,
     SfColor,
-    SfHeaderNavigationItem
+    SfHeaderNavigationItem,
+    SfSelect,
+    SfSelectOption,
+    SfProductOption,
+    SfDivider
   },
   data() {
     return {
+      selectedSize: "",
+      qty: 1,
       product: {
         name: "Cashmere Sweater",
         description:
@@ -227,6 +277,11 @@ export default {
             mobile: { url: "assets/storybook/Product/productB.jpg" },
             desktop: { url: "assets/storybook/Product/productB.jpg" },
             big: { url: "assets/storybook/Product/productB.jpg" }
+          },
+          {
+            mobile: { url: "assets/storybook/Product/productZ.jpg" },
+            desktop: { url: "assets/storybook/Product/productZ.jpg" },
+            big: { url: "assets/storybook/Product/productZ.jpg" }
           }
         ],
         price: "$50.00",
@@ -253,6 +308,10 @@ export default {
             value: "Poland"
           }
         ],
+        sizes: ["32", "34", "36", "38", "40", "42"],
+        careInstructions: "Do not wash!",
+        brand:
+          "Brand name is the perfect pairing of quality and design. This label creates major everyday vibes with its collection of modern brooches, silver and gold jewellery, or clips it back with hair accessories in geo styles.",
         reviews: [
           {
             author: "Jane D.Smith",
@@ -293,11 +352,15 @@ export default {
         },
         {
           title: "Additional Information",
-          content:
-            "The Marissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses."
+          content: ""
         }
       ],
       bottomNavigationItems: [
+        {
+          icon: "home",
+          iconActive: "home_fill",
+          label: "Home"
+        },
         {
           icon: "menu",
           iconActive: "",
@@ -306,18 +369,24 @@ export default {
         {
           icon: "heart",
           iconActive: "heart_fill",
-          label: "Heart"
+          label: "Wishlist"
         },
         {
           icon: "profile",
           iconActive: "profile_fill",
-          label: "Profile"
+          label: "Account"
         }
       ],
       currentIcon: "heart_fill",
-      isMobile: false,
       navigation: ["women", "man", "kids"]
     };
+  },
+  methods: {
+    addToCart() {
+      console.log(
+        `${this.qty} x ${this.product.name} (size: ${this.selectedSize}) has been added to cart`
+      );
+    }
   }
 };
 </script>
@@ -326,45 +395,143 @@ export default {
 
 .topbar {
   &__center {
-    margin: 0 5px 0 0;
+    margin: 0 var(--spacer-2xs) 0 0;
   }
   &__right {
-    margin: 0 27px 0 0;
+    margin: 0 var(--spacer-base) 0 0;
   }
 }
 
-.product__details {
+.product {
   &__chevron {
     --chevron-size: 14px;
     position: absolute;
     top: 0;
     left: 0;
     z-index: 1;
-    margin: 24px 0 0 24px;
+    margin: var(--spacer-base) 0 0 var(--spacer-base);
   }
   &__images {
     display: flex;
     flex-direction: column;
   }
   &__info {
-    margin: 32px auto 8px;
+    margin: var(--spacer-sm) auto var(--spacer-xs);
+    &__header {
+      margin: 0 var(--spacer-sm);
+      display: flex;
+      justify-content: space-between;
+      @include for-desktop {
+        margin: 0 auto;
+      }
+    }
+    @include for-desktop {
+      max-width: 32.625rem;
+      margin: 0 0 0 7.5rem;
+    }
+  }
+  &__price-and-review {
+    margin: var(--spacer-xs) var(--spacer-sm) var(--spacer-base);
+    align-items: center;
+    &__rating {
+      display: flex;
+      align-items: center;
+      margin: var(--spacer-xs) 0 0 0;
+    }
+    &__count {
+      @include font(
+        --count-font,
+        var(--font-normal),
+        var(--font-sm),
+        1.4,
+        var(--font-family-secondary)
+      );
+      color: var(--c-text);
+      text-decoration: none;
+      margin: 0 0 0 var(--spacer-xs);
+    }
+    @include for-desktop {
+      display: flex;
+      justify-content: space-between;
+      margin: var(--spacer-sm) 0 var(--spacer-lg) 0;
+    }
+  }
+  &__description {
+    color: var(--c-link);
+    @include font(
+      --product-description-font,
+      var(--font-light),
+      var(--font-base),
+      1.6,
+      var(--font-family-primary)
+    );
+  }
+  &__select {
+    margin: 0 var(--spacer-sm);
+    @include for-desktop {
+      margin: 0;
+    }
   }
   &__colors {
+    @include font(
+      --product-color-font,
+      var(--font-normal),
+      var(--font-lg),
+      1.6,
+      var(--font-family-secondary)
+    );
     display: flex;
+    align-items: center;
+    &__label {
+      margin: 0 var(--spacer-lg) 0 0;
+    }
+    &__color {
+      margin: 0 var(--spacer-2xs);
+    }
+  }
+  &__add-to-cart {
+    margin-top: var(--spacer-2xl);
+  }
+  &__tabs {
+    margin: var(--spacer-lg) auto var(--spacer-2xl);
+    &__property {
+      margin-top: var(--spacer-lg);
+    }
+    &__divider {
+      margin: var(--spacer-base) auto;
+      @include for-desktop {
+        margin-bottom: var(--spacer-base);
+      }
+    }
+
+    @include for-desktop {
+      margin-top: var(--spacer-2xl);
+      --tabs-content-tab-padding: 3.5rem 0 0 0;
+    }
+  }
+  &__additional-info {
+    @include font(
+      --additional-info-font,
+      var(--font-light),
+      var(--font-base),
+      1.6,
+      var(--font-family-primary)
+    );
+    &__title {
+      font-weight: var(--font-bold);
+      margin-bottom: var(--spacer-sm);
+      &:not(:first-child) {
+        margin-top: 58px;
+      }
+    }
+    &__paragraph {
+      margin: 0;
+    }
   }
   @include for-desktop {
     display: flex;
-    max-width: 1240px;
+    max-width: 77.5rem;
     margin: 0 auto;
-    &__info {
-      max-width: 544px;
-      margin: 0 0 0 123px;
-    }
-    &__price-and-review {
-      display: flex;
-      justify-content: space-between;
-      margin: 26px 0 0 0;
-    }
   }
 }
 
@@ -373,26 +540,15 @@ export default {
   &__compare,
   &__save {
     display: block;
-    margin: 40px 0 24px auto;
+    margin: var(--spacer-xl) 0 var(--spacer-base) auto;
+  }
+  &__compare {
+    margin-top: 0;
   }
 }
 
 .sf-breadcrumbs {
-  max-width: 1240px;
-  margin: 32px auto 24px;
-}
-
-.product__color {
-  margin: 4px 4px 40px;
-}
-
-.product {
-  display: flex;
-  flex-direction: column;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
+  max-width: 77.5rem;
+  margin: var(--spacer-lg) auto var(--spacer-base);
 }
 </style>
