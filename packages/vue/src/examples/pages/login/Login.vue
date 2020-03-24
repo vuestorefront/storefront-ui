bd455d9f4cf5220345d1e3d15cc54b77ee169d83
Adam Pawliński <apawlinski@divante.pl>
2020-03-24 01:34:03 +0100

    refactor: login example style adjust

--- packages/vue/src/examples/pages/login/Login.vue
+++ packages/vue/src/examples/pages/login/Login.vue
@@ -2,54 +2,44 @@
   <div>
     <SfModal :visible="true" :cross="false" title="My Account">
       <!-- <transition name="fade" mode="out-in"> -->
-      <transition name="fade" mode="out-in">
-        <!-- <SfBar title="My Account" :close="false" /> -->
-        <div class="login-container">
-          <SfImage :src="src" />
-          <SfSection title-heading="Come on in!" :level-heading="1">
-            <p>
+      <template #modal-bar>
+        <SfBar
+          class="login-bar"
+          :title="onScreenChange()"
+          :back="true"
+          :close="false"
+        />
+      </template>
+      <div v-if="isMyAccount" class="login-container">
+        <SfImage :src="src" />
+        <SfHeading
+          class="login-heading"
+          :title="titleText"
+          :subtitle="subtitleText"
+          :level="loginHeadinglevel"
+        >
+          <template #title="{title}">
+            <p class="login-heading__title">{{ title }}</p>
+          </template>
+          <template #subtitle>
+            <p class="login-heading__subtitle">
               View orders and update your details.<br />
               Make your checkout fast and easy!
             </p>
-          </SfSection>
-          <SfButton
-            class="sf-button login-button__signin"
-            @click="isSignIn = true"
-            >SIGN IN</SfButton
-          >
-          <SfButton
-            class="sf-button login-button__login"
-            @click="isLogin = true"
-            >JOIN</SfButton
-          >
-          <SfBottomNavigation>
-            <SfBottomNavigationItem
-              v-for="(item, key) in items"
-              :key="key"
-              :icon="item.icon"
-              :label="item.label"
-              icon-size="20px"
-            />
-            <SfBottomNavigationItem
-              label="Basket"
-              icon="add_to_cart"
-              :is-floating="isFloating"
-            >
-              <template #icon>
-                <SfCircleIcon>
-                  <SfIcon
-                    icon="add_to_cart"
-                    color="white"
-                    size="28px"
-                    :style="{ margin: '0 0 0 -2px' }"
-                  />
-                </SfCircleIcon>
-              </template>
-            </SfBottomNavigationItem>
-          </SfBottomNavigation>
-        </div>
-      </transition>
-      <div v-if="isLogin" key="log-in">
+          </template>
+        </SfHeading>
+        <SfButton
+          class="sf-button login-button login-button--signin"
+          @click="onBtnClick('signIn')"
+          >SIGN IN</SfButton
+        >
+        <SfButton
+          class="sf-button login-button login-button--login"
+          @click="onBtnClick('join')"
+          >JOIN</SfButton
+        >
+      </div>
+      <div v-else-if="isLogin" key="log-in" class="form-container">
         <div class="form">
           <SfInput
             v-model="email"
@@ -63,12 +53,13 @@
             label="Password"
             type="password"
             class="form__element"
+            :has-show-password="true"
           />
           <SfCheckbox
             v-model="rememberMe"
             name="remember-me"
             label="Remember me"
-            class="form__element"
+            class="form__element form__element--checkbox"
           />
           <SfButton class="sf-button--full-width form__button">Login</SfButton>
         </div>
@@ -78,26 +69,24 @@
           >
         </div>
         <div class="bottom">
-          Don't have and account yet?
-          <!-- <SfButton
-                class="sf-button--text color-secondary"
-                @click="isLogin = false"
-                >Register today?</SfButton
-              > -->
+          <SfHeading
+            title="Don't have an account yet?"
+            class="bottom-heading"
+            :level="bottomHeadingLevel"
+          />
+          <SfButton
+            class="sf-button--text color-secondary"
+            @click="onBtnClick('signIn')"
+            >Register now</SfButton
+          >
         </div>
       </div>
-      <div v-if="isSignIn" key="sign-up" class="form">
-        <div class="from">
-          <SfInput
-            v-model="email"
-            name="email"
-            label="Your email"
-            class="form__element"
-          />
+      <div v-else-if="isSignIn" key="sign-up" class="form-container">
+        <div class="form">
           <SfInput
             v-model="firstName"
             name="first-name"
-            label="First Name"
+            label="Name"
             class="form__element"
           />
           <SfInput
@@ -107,16 +96,16 @@
             class="form__element"
           />
           <SfInput
+            v-model="email"
+            name="email"
+            label="Your email"
+            class="form__element"
+          />
+          <SfInput
             v-model="password"
             name="password"
             label="Password"
             type="password"
-            class="form__element"
-          />
-          <SfCheckbox
-            v-model="createAccount"
-            name="create-account"
-            label="I want to create an account"
             class="form__element"
           />
           <SfButton class="sf-button--full-width form__button"
@@ -124,27 +113,51 @@
           >
         </div>
         <div class="action">
-          or
-          <!-- <SfButton
-                class="sf-button--text color-secondary"
-                @click="isLogin = true"
-                >login in to your account</SfButton
-              > -->
+          <SfButton
+            class="sf-button--text color-secondary"
+            @click="onBtnClick('join')"
+            >or Log In To Your Account</SfButton
+          >
         </div>
       </div>
       <!-- </transition> -->
     </SfModal>
+    <SfBottomNavigation>
+      <SfBottomNavigationItem
+        v-for="(item, key) in items"
+        :key="key"
+        :icon="item.icon"
+        :label="item.label"
+        icon-size="20px"
+      />
+      <SfBottomNavigationItem
+        label="Basket"
+        icon="add_to_cart"
+        :is-floating="isFloating"
+      >
+        <template #icon>
+          <SfCircleIcon>
+            <SfIcon
+              icon="add_to_cart"
+              color="white"
+              size="28px"
+              :style="{ margin: '0 0 0 -2px' }"
+            />
+          </SfCircleIcon>
+        </template>
+      </SfBottomNavigationItem>
+    </SfBottomNavigation>
   </div>
 </template>
 <script>
 import {
-  // SfBar,
+  SfBar,
   SfImage,
-  SfSection,
   SfModal,
   SfInput,
   SfButton,
   SfCheckbox,
+  SfHeading,
   SfBottomNavigation,
   SfCircleIcon,
   SfIcon
@@ -152,21 +165,27 @@ import {
 export default {
   name: "Login",
   components: {
-    // SfBar,
+    SfBar,
     SfImage,
-    SfSection,
     SfModal,
     SfInput,
     SfButton,
     SfCheckbox,
+    SfHeading,
     SfBottomNavigation,
     SfCircleIcon,
     SfIcon
   },
   data() {
     return {
+      isMyAccount: true,
       isLogin: false,
       isSignIn: false,
+      loginHeadinglevel: 1,
+      titleText: "Come on in!",
+      subtitleText: `View orders and update your details.
+         Make your checkout fast and easy!`,
+      bottomHeadingLevel: 3,
       email: "",
       password: "",
       createAccount: false,
@@ -200,55 +219,111 @@ export default {
       this.firstName = "";
       this.lastName = "";
     }
+  },
+  methods: {
+    onBtnClick(btn) {
+      this.isMyAccount = false;
+      if (btn === "signIn") {
+        this.isSignIn = true;
+        this.isLogin = false;
+      } else if (btn === "join") {
+        this.isLogin = true;
+        this.isSignIn = false;
+      }
+    },
+    onScreenChange() {
+      if (this.isSignIn === true) {
+        return "Join Vue Storefront";
+      } else if (this.isLogin === true) {
+        return "Log In";
+      } else {
+        return "My Account";
+      }
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
 @import "~@storefront-ui/vue/styles";
+.sf-modal {
+  --modal-height: 100%;
+  --modal-content-padding: var(--spacer-lg) var(--spacer-base) var(--spacer-2xl)
+    var(--spacer-base);
+}
+.login-bar {
+  height: 3.125rem;
+}
 .login-container {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-.sf-heading__title {
-  color: var(--c-primary);
-  font-weight: var(--font-semibold);
-}
-.sf-section__content {
-  color: var(--c-secondary-variant);
-  font-family: var(--font-family-secondary);
-  font-size: var(--font-base);
-}
-.login-button__login {
-  --button-background: var(--c-secondary);
-  margin: var(--spacer-sm);
-}
-.login-button__signin {
-  --button-background: var(--c-primary);
-}
-.form {
-  &__element {
-    margin: 0 0 var(--spacer-extra-big) 0;
+  .sf-image {
+    margin: 0 0 0 var(--spacer-2xl);
   }
 }
+// .sf-heading {
+//   --heading-padding: var(--spacer-2xl) 0 var(--spacer-xl) 0;
+//   &__title {
+//     color: var(--c-primary);
+//   }
+// }
+.login-heading {
+  margin: var(--spacer-2xl) 0 var(--spacer-xl) 0;
+  &__title {
+    color: var(--c-primary);
+    font-family: var(--font-family-secondary);
+    font-size: var(--h1-font-size);
+    font-weight: var(--font-semibold);
+  }
+  &__subtitle {
+    color: var(--c-secondary-variant);
+    font-family: var(--font-family-primary);
+    font-size: var(--font-base);
+  }
+}
+.login-button {
+  --button-width: 100%;
+  --button-height: 4.0625rem;
+  &--login {
+    margin: var(--spacer-sm);
+    --button-background: var(--c-secondary);
+  }
+  &--signin {
+    --button-background: var(--c-primary);
+  }
+}
+
+.form-container {
+  margin: 0 0 var(--spacer-2xl) 0;
+  .form {
+    margin: var(--spacer-xl) 0 var(--spacer-lg) 0;
+    &__element {
+      margin: 0 0 var(--spacer-sm) 0;
+      &--checkbox {
+        margin: var(--spacer-lg) 0 var(--spacer-2xl);
+      }
+    }
+    &__button {
+      margin: var(--spacer-xl) 0 0 0;
+    }
+  }
+}
+
 .action,
 .bottom {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: var(--spacer-big) 0 var(--spacer-big) 0;
-  font: 300 var(--font-size-regular) / 1.6 var(--font-family-secondary);
-  & > * {
-    margin: 0 0 0 var(--spacer);
-  }
 }
 .action {
-  margin: var(--spacer-big) 0 var(--spacer-big) 0;
+  margin: 0 0 var(--spacer-xl) 0;
 }
 .bottom {
   padding: var(--spacer-extra-big) 0 0 0;
   margin: var(--spacer-extra-big) 0 0 0;
-  border: 1px solid var(--c-light);
-  border-width: 1px 0 0 0;
+  &-heading {
+    color: var(--c-primary);
+  }
 }
 </style>
