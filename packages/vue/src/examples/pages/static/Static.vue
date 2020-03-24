e6360b7c483920c3908b4358e783d73c4a4f78f3
Justyna <jgieracka@divante.pl>
2020-03-24 16:19:44 +0100

    refactor: content added

--- packages/vue/src/examples/pages/static/Static.vue
+++ packages/vue/src/examples/pages/static/Static.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="static">
-    <SfBreadcrumbs
-      class="breadcrumbs desktop-only"
-      :breadcrumbs="breadcrumbs"
-    />
     <SfContentPages
       :active="activePage"
       title="Help"
-      class="static-content"
       @click:change="updateActive"
     >
       <SfContentPage title="About us">
@@ -18,6 +13,19 @@
             information collected at this website, and does not cover any
             information collected offline by Luma. All Luma websites are covered
             by this privacy policy.
+            <h3 class="title">Luma Security</h3>
+            <p>
+              Personal information provided on the website and online credit
+              card transactions are transmitted through a secure server. We are
+              committed to handling your personal information with high
+              standards of information security. We take appropriate physical,
+              electronic, and administrative steps to maintain the security and
+              accuracy of personally identifiable information we collect,
+              including limiting the number of people who have physical access
+              to our database servers, as well as employing electronic security
+              systems and password protections that guard against unauthorized
+              access.
+            </p>
           </SfTab>
           <SfTab title="Privacy Policy">
             This website ("website") is operated by Luma Inc., which includes
@@ -60,32 +68,50 @@
         information collected offline by Luma. All Luma websites are covered by
         this privacy policy.
       </SfContentPage>
+      <SfContentPage title="Delivery">
+        This website ("website") is operated by Luma Inc., which includes Luma
+        stores, and Luma Private Sales. This privacy policy only covers
+        information collected at this website, and does not cover any
+        information collected offline by Luma. All Luma websites are covered by
+        this privacy policy.
+      </SfContentPage>
+      <SfContentPage title="Return policy">
+        This website ("website") is operated by Luma Inc., which includes Luma
+        stores, and Luma Private Sales. This privacy policy only covers
+        information collected at this website, and does not cover any
+        information collected offline by Luma. All Luma websites are covered by
+        this privacy policy.
+      </SfContentPage>
+      <SfContentPage title="Size guide">
+        This website ("website") is operated by Luma Inc., which includes Luma
+        stores, and Luma Private Sales. This privacy policy only covers
+        information collected at this website, and does not cover any
+        information collected offline by Luma. All Luma websites are covered by
+        this privacy policy.
+      </SfContentPage>
+      <SfContentPage title="Contact us">
+        This website ("website") is operated by Luma Inc., which includes Luma
+        stores, and Luma Private Sales. This privacy policy only covers
+        information collected at this website, and does not cover any
+        information collected offline by Luma. All Luma websites are covered by
+        this privacy policy.
+      </SfContentPage>
     </SfContentPages>
   </div>
 </template>
 <script>
-import { SfBreadcrumbs, SfContentPages, SfTabs } from "@storefront-ui/vue";
+import { SfContentPages, SfTabs } from "@storefront-ui/vue";
 
 export default {
   name: "Static",
   components: {
-    SfBreadcrumbs,
     SfContentPages,
     SfTabs
   },
   data() {
     return {
-      activePage: "About us",
-      breadcrumbs: [
-        { text: "Home", route: { link: "#" } },
-        { text: "Help", route: { link: "#" } }
-      ]
+      activePage: "About us"
     };
-  },
-  computed: {
-    barTitle() {
-      return this.activePage ? this.activePage : "Home";
-    }
   },
   methods: {
     updateActive(title) {
@@ -100,12 +126,15 @@ export default {
 #static {
   box-sizing: border-box;
   @include for-desktop {
-    max-width: 1240px;
+    max-width: 77.5rem;
     margin: 0 auto;
   }
 }
-.breadcrumbs {
-  padding: var(--spacer-big) var(--spacer-extra-big) var(--spacer-extra-big)
-    var(--spacer-extra-big);
+
+.title {
+  font-size: var(--h3-font-size);
+  font-family: var(--font-family-secondary);
+  font-weight: var(--font-medium);
+  margin-top: 3.5rem;
 }
 </style>
