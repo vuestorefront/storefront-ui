30a69ca3db4182cd7469cc800b704ef4ac5f456e
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-03-31 11:59:56 +0200

    chore: add vue-next

--- test-vue3/src/main.js
+++ test-vue3/src/main.js
@@ -1,9 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue'
 import "@storefront-ui/vue/styles.scss";
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
