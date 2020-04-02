dee82379ec030208065ad8f269f2014577499402
Adam Pawliński <apawlinski@divante.pl>
2020-04-02 19:52:25 +0200

    refactor: home example fix after CR and fix align right modifier on hero

--- packages/vue/src/examples/pages/home/Home.vue
+++ packages/vue/src/examples/pages/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <SfHero class="home__hero">
+  <div id="home">
+    <SfHero class="hero">
       <SfHeroItem
         v-for="(img, index) in heroes"
         :key="index"
@@ -8,17 +8,15 @@
         :title="img.title"
         :button-text="img.buttonText"
         :background="img.background"
+        :class="img.className"
       />
     </SfHero>
-    <SfHeading
-      class="home__heading sf-heading--underline"
-      :level="headingLevel"
-    >
+    <SfHeading class="heading sf-heading--underline" :level="3">
       <template #title>
-        <span class="home__heading__title mobile-only">
+        <span class="heading__title mobile-only">
           {{ headingTitle.mobile }}
         </span>
-        <span class="home__heading__title desktop-only">
+        <span class="heading__title desktop-only">
           {{ headingTitle.desktop }}
         </span>
       </template>
@@ -28,11 +26,11 @@
         </SfButton>
       </template>
     </SfHeading>
-    <SfCarousel class="home__carousel">
+    <SfCarousel class="carousel">
       <SfCarouselItem
         v-for="(product, index) in products"
         :key="index"
-        class="home__carousel-item"
+        class="carousel__item"
       >
         <SfProductCard
           :image="product.image"
@@ -50,7 +48,7 @@
         />
       </SfCarouselItem>
     </SfCarousel>
-    <SfBannerGrid class="home__banner-grid">
+    <SfBannerGrid class="banner-grid">
       <template v-for="banner in banners" #[banner.slot]>
         <SfBanner
           :key="banner.slot"
@@ -64,45 +62,43 @@
       </template>
     </SfBannerGrid>
     <SfCallToAction
-      class="home__call-to-action"
+      class="call-to-action"
       title="Subscribe to Newsletters"
       description="Be aware of upcoming sales and events. Receive gifts and special offers!"
       button-text="subscribe"
       image="/assets/storybook/Home/newsletter.jpg"
     />
     <SfSection
-      class="home__section-instagram-feed"
+      class="section"
       title-heading="Share Your Look"
       subtitle-heading="#YOURLOOK"
       :level-heading="sectionLevel"
     >
-      <div class="home__instagram-feed">
+      <div class="images-grid">
         <SfImage
           v-for="(img, index) in instagramFeed"
           :key="index"
           :src="img"
-          :class="'home__instagram-feed__' + index"
-          ><span class="home__instagram-feed--mobile">{{
-            img.mobile.content
-          }}</span
-          ><span class="home__instagram-feed--desktop">{{
+          :class="'images-grid__' + index"
+          ><span class="images-grid--mobile">{{ img.mobile.content }}</span
+          ><span class="images-grid--desktop">{{
             img.desktop.content
           }}</span></SfImage
         >
       </div>
     </SfSection>
     <SfBanner
-      class="home__bottom-banner"
+      class="banner"
       title="Download our application to your mobile"
       subtitle="fashion to take away"
       image="/assets/storybook/Home/bannerD.png"
     >
       <template #call-to-action>
-        <div class="home__bottom-banner__call-to-action">
-          <SfButton class="home__bottom-banner__button">
+        <div class="banner__call-to-action">
+          <SfButton class="banner__button">
             <SfImage src="/assets/storybook/Home/apple.png" />
           </SfButton>
-          <SfButton class="home__bottom-banner__button">
+          <SfButton class="banner__button">
             <SfImage src="/assets/storybook/Home/google.png" />
           </SfButton>
         </div>
@@ -139,7 +135,6 @@ export default {
   },
   data() {
     return {
-      headingLevel: 3,
       headingTitle: {
         mobile: "Match it with",
         desktop: "Bestsellers"
@@ -149,15 +144,15 @@ export default {
           title: "Colorful summer dresses are already in store",
           subtitle: "SUMMER COLLECTION 2020",
           buttonText: "Learn more",
-          background: "#fcede8",
+          background: "rgb(236, 239, 241)",
           image: "/assets/storybook/Home/hero.png"
         },
         {
           title: "Colorful summer dresses are already in store",
           subtitle: "SUMMER COLLECTION 2020",
           buttonText: "Learn more",
-          background: "#9ee2b0",
-          image: "/assets/storybook/Home/hero.png",
+          background: "rgb(239, 235, 233)",
+          image: "/assets/storybook/Home/bannerHM.jpg",
           className:
             "sf-hero-item--position-bg-top-left sf-hero-item--align-right"
         },
@@ -165,7 +160,7 @@ export default {
           title: "Colorful summer dresses are already in store",
           subtitle: "SUMMER COLLECTION 2020",
           buttonText: "Learn more",
-          background: "#f6e389",
+          background: "rgb(252, 228, 236)",
           image: "/assets/storybook/Home/hero.png"
         }
       ],
@@ -342,136 +337,132 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "~@storefront-ui/vue/styles";
-.home {
+#home {
   display: flex;
   flex-direction: column;
-  box-sizing: border-box;
+  max-width: 1240px;
+  margin: 0 auto;
   padding: 0 var(--spacer-sm);
-  &__hero {
-    display: flex;
-    order: 1;
-    margin: var(--spacer-xl) 0 0 0;
-  }
-  &__heading {
-    display: flex;
-    order: 2;
-    justify-content: space-between;
-    margin: var(--spacer-base) 0;
-    &__title {
-      font-size: var(--h3-font-size);
-      font-weight: var(--font-medium);
-      font-family: var(--font-family-secondary);
-    }
-  }
-  &__carousel {
-    --image-height: 16.125rem;
-    display: flex;
-    justify-content: center;
-    order: 3;
-  }
-  &__banner-grid {
-    display: flex;
-    flex-direction: column;
-    order: 4;
-    margin: var(--spacer-xl) 0 var(--spacer-base) 0;
-  }
-  &__call-to-action {
-    display: flex;
-    order: 5;
-  }
-  &__section-instagram-feed {
-    display: flex;
-    flex-direction: column;
-    order: 6;
-    margin: var(--spacer-2xl) 0 var(--spacer-xl) 0;
-  }
-  &__instagram-feed {
-    --image-width: 100%;
-    --image-height: 100%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(4, 1fr);
-    grid-column-gap: var(--spacer-xs);
-    grid-row-gap: var(--spacer-xs);
-    &__imageA,
-    &__imageB {
-      grid-row: 1/3;
-    }
-    &__imageC,
-    &__imageD {
-      grid-row: 3/5;
-    }
-    &--desktop {
-      display: none;
-    }
-  }
-  &__bottom-banner,
-  &__newsletter-subscribe {
-    display: none;
-  }
-  &__footer-social {
-    display: flex;
-    flex-direction: column;
+  box-sizing: border-box;
+}
+.hero {
+  display: flex;
+  order: 1;
+}
+.heading {
+  display: flex;
+  order: 2;
+  justify-content: space-between;
+  margin: var(--spacer-base) 0;
+  &__title {
+    font-size: var(--h3-font-size);
+    font-weight: var(--font-medium);
+    font-family: var(--font-family-secondary);
   }
 }
+.carousel {
+  --image-height: 100%;
+  display: flex;
+  justify-content: center;
+  order: 3;
+}
+.banner-grid {
+  display: flex;
+  flex-direction: column;
+  order: 4;
+  margin: var(--spacer-xl) 0 var(--spacer-base) 0;
+}
+.call-to-action {
+  display: flex;
+  order: 5;
+}
+.section {
+  display: flex;
+  flex-direction: column;
+  order: 6;
+  margin: var(--spacer-3xl) 0 var(--spacer-2xl) 0;
+}
+.images-grid {
+  --image-width: 100%;
+  --image-height: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: var(--spacer-xs);
+  grid-row-gap: var(--spacer-xs);
+  &__imageA,
+  &__imageB {
+    grid-row: 1/3;
+  }
+  &__imageC,
+  &__imageD {
+    grid-row: 3/5;
+  }
+  &--desktop {
+    display: none;
+  }
+}
+.banner {
+  display: none;
+}
 @include for-desktop {
-  .home {
-    padding: 0 var(--spacer-2xl);
-    &__heading {
-      justify-content: center;
-      order: 4;
-      margin: var(--spacer-3xl) 0 var(--spacer-2xl) 0;
+  #home {
+    padding: 0;
+  }
+  .heading {
+    justify-content: center;
+    order: 4;
+    margin: var(--spacer-3xl) 0 var(--spacer-2xl) 0;
+  }
+  .banner-grid {
+    order: 2;
+    margin: var(--spacer-2xl) 0;
+  }
+  .call-to-action {
+    order: 3;
+  }
+  .carousel {
+    order: 5;
+  }
+  .images-grid {
+    grid-column-gap: var(--spacer-base);
+    grid-row-gap: var(--spacer-base);
+    &__imageA {
+      grid-column: 1/2;
+      grid-row: 1/4;
     }
-    &__banner-grid {
-      order: 2;
-      margin: var(--spacer-2xl) 0;
+    &__imageB {
+      grid-column: 2/3;
+      grid-row: 1/2;
     }
+    &__imageC {
+      grid-column: 1/2;
+      grid-row: 4/5;
+    }
+    &__imageD {
+      grid-column: 2/3;
+      grid-row: 2/5;
+    }
+    &--mobile {
+      display: none;
+    }
+    &--desktop {
+      display: block;
+    }
+  }
+  .banner {
+    --banner-padding: 0 var(--spacer-2xl);
+    --banner-title-margin: var(--spacer-base) 0 var(--spacer-xl) 0;
+    --banner-title-font-weight: 600;
+    --banner-title-text-transform: capitalize;
+    display: flex;
+    order: 7;
     &__call-to-action {
-      order: 3;
-    }
-    &__carousel {
-      order: 5;
-    }
-    &__instagram-feed {
-      grid-column-gap: var(--spacer-base);
-      grid-row-gap: var(--spacer-base);
-      &__imageA {
-        grid-column: 1/2;
-        grid-row: 1/4;
-      }
-      &__imageB {
-        grid-column: 2/3;
-        grid-row: 1/2;
-      }
-      &__imageC {
-        grid-column: 1/2;
-        grid-row: 4/5;
-      }
-      &__imageD {
-        grid-column: 2/3;
-        grid-row: 2/5;
-      }
-      &--mobile {
-        display: none;
-      }
-      &--desktop {
-        display: block;
-      }
-    }
-    &__bottom-banner {
-      --banner-padding: 0 var(--spacer-2xl);
-      --banner-title-margin: var(--spacer-base) 0 var(--spacer-xl) 0;
-      --banner-title-font-weight: 600;
-      --banner-title-text-transform: capitalize;
+      --button-background: var(--c-white);
       display: flex;
-      order: 7;
-      &__call-to-action {
-        --button-background: var(--c-white);
-        display: flex;
-      }
-      &__button {
-        --button-padding: 0 var(--spacer-base) 0 0;
-      }
+    }
+    &__button {
+      --button-padding: 0 var(--spacer-base) 0 0;
     }
   }
 }
