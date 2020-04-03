3bf0d45d817f2a7bd20d0d951cd0c46e69982f35
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-04-03 12:35:12 +0200

    fix: remove unnecessary methods

--- packages/vue/src/examples/pages/detailed-cart/DetailedCart.stories.js
+++ packages/vue/src/examples/pages/detailed-cart/DetailedCart.stories.js
@@ -3,11 +3,5 @@ import { storiesOf } from "@storybook/vue";
 import DetailedCart from "./DetailedCart";
 storiesOf("Pages|DetailedCart", module).add("Common", () => ({
   components: { DetailedCart },
-  mounted() {
-    document.body.style.setProperty("margin", "0");
-  },
-  beforeDestroy() {
-    document.body.style.removeProperty("margin");
-  },
   template: `<DetailedCart />`
 }));
