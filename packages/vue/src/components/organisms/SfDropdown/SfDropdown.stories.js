d6828f2b4b5d2ffd3eb37713fd7b1a0fbd696671
Justyna <jgieracka@divante.pl>
2020-04-03 16:18:53 +0200

    feat: open and close buttons added

--- packages/vue/src/components/organisms/SfDropdown/SfDropdown.stories.js
+++ packages/vue/src/components/organisms/SfDropdown/SfDropdown.stories.js
@@ -3,36 +3,40 @@ import { storiesOf } from "@storybook/vue";
 import { withKnobs, boolean, text } from "@storybook/addon-knobs";
 
 import SfDropdown from "./SfDropdown.vue";
-import SfButton from "../../atoms/SfButton/SfButton.vue";
+import { SfButton, SfChevron } from "@storefront-ui/vue";
 
 storiesOf("Organisms|Dropdown", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
-    components: { SfDropdown, SfButton },
+    components: { SfDropdown, SfButton, SfChevron },
     props: {
-      isOpen: {
-        default: boolean("isOpen", true, "Props")
-      },
       transition: {
         default: text("transition", "fade", "Props")
+      },
+      dropdownUp: {
+        default: boolean("dropdownUp", false, "Props")
+      },
+      label: {
+        default: text("label", "Label", "Props")
+      },
+      cancelLabel: {
+        default: text("cancelLabel", "Cancel", "Props")
       }
     },
     data() {
       return {
-        items: [
-          {
-            name: "item1"
-          },
-          {
-            name: "item2"
-          },
-          {
-            name: "item3"
-          }
-        ]
+        isOpen: false,
+        items: ["item1", "item2", "item3", "item3", "item3"]
       };
     },
-    template: `<SfDropdown :is-open="isOpen" :transition="transition">
-        <SfButton v-for="item in items" :key="item" style="width: 100%">{{ item.name }}</SfButton>
-    </SfDropdown>`
+    template: `<SfDropdown 
+  :is-open="isOpen" 
+  :label="label"
+  :cancel-label="cancelLabel"
+  :dropdownUp="dropdownUp" 
+  :transition="transition" 
+  @triggerDropdown="isOpen = !isOpen" 
+  @close="isOpen = false">
+    <SfButton class="sf-button--underlined" v-for="(item, i) in items" :key="i" style="width: 100%; --button-background: var(--c-white)">{{ item }}</SfButton>
+   </SfDropdown>`
   }));
--- packages/vue/src/components/organisms/SfDropdown/SfDropdown.vue
+++ packages/vue/src/components/organisms/SfDropdown/SfDropdown.vue
@@ -1,22 +1,73 @@
 <template>
-  <transition :name="transition">
-    <div v-if="isOpen" class="sf-dropdown">
-      <slot />
-    </div>
-  </transition>
+  <div class="sf-dropdown">
+    <SfButton
+      class="sf-button--underlined sf-dropdown__button sf-dropdown__button--trigger "
+      :class="isOpen ? 'sf-dropdown__button--trigger--active' : ''"
+      @click="triggerDropdown"
+    >
+      {{ label }}
+      <SfChevron :class="dropdownUp ? 'sf-chevron--top' : ''" />
+    </SfButton>
+    <SfOverlay :visible="isOpen" class="mobile-only" @click="closeDropdown" />
+    <transition :name="transition">
+      <div v-if="isOpen" class="sf-dropdown__list ">
+        <SfButton
+          class="sf-dropdown__button sf-dropdown__button--mobile-label mobile-only"
+        >
+          {{ label }}
+        </SfButton>
+        <div :class="dropdownUp ? 'sf-dropdown__list--up' : ''">
+          <slot />
+        </div>
+        <SfButton
+          class="sf-dropdown__button sf-dropdown__button--cancell mobile-only"
+          @click="closeDropdown"
+        >
+          {{ cancelLabel }}
+        </SfButton>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
+import { SfButton, SfOverlay, SfChevron } from "@storefront-ui/vue";
+
 export default {
   name: "SfDropdown",
+  components: {
+    SfButton,
+    SfOverlay,
+    SfChevron
+  },
   props: {
     isOpen: {
       type: Boolean,
-      reqired: true
+      default: false
     },
     transition: {
       type: String,
       default: "fade"
+    },
+    dropdownUp: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: "Name"
+    },
+    cancelLabel: {
+      type: String,
+      default: "Cancel"
+    }
+  },
+  methods: {
+    closeDropdown() {
+      this.$emit("close");
+    },
+    triggerDropdown() {
+      this.$emit("triggerDropdown");
     }
   }
 };
