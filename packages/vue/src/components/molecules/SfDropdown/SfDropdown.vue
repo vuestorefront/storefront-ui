daba0e26139a58e587a8f38030579e5b29a5427a
Przemek Spaczek <pspaczek@divante.co>
2020-04-10 12:49:51 +0200

    docs: add docs descriptions

--- packages/vue/src/components/molecules/SfDropdown/SfDropdown.vue
+++ packages/vue/src/components/molecules/SfDropdown/SfDropdown.vue
@@ -3,10 +3,13 @@
     <SfOverlay :visible="isOpen" class="sf-dropdown__overlay" @click="close" />
     <transition name="sf-dropdown">
       <div v-show="isOpen" class="sf-dropdown__container">
+        <!--@slot Use this slot to replace title. -->
         <slot name="title">
           <div v-if="title" class="sf-dropdown__title">{{ title }}</div>
         </slot>
+        <!--@slot Use this slot to place content inside the dropdown.-->
         <slot />
+        <!--@slot Use this slot to replace cancel button. -->
         <slot name="cancel">
           <SfButton
             class="sf-button--full-width sf-dropdown__cancel"
@@ -28,10 +31,16 @@ export default {
     SfButton
   },
   props: {
+    /**
+     * Dropdown is open
+     */
     isOpen: {
       type: Boolean,
       default: false
     },
+    /**
+     * Title for dropdown content, visible on mobile.
+     */
     title: {
       type: String,
       default: ""
