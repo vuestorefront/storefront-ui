b17e96a7fa8cf8962b9b83287b72c5bd6deac61d
Przemek Spaczek <pspaczek@divante.co>
2020-04-15 13:28:39 +0200

    chore: temporary disabled console rulses

--- packages/vue/.eslintrc.js
+++ packages/vue/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     "@vue/prettier",
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-multiple-empty-lines": ["error", { max: 2 }],
   },
