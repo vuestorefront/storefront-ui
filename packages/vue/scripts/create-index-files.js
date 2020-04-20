e4445f88ac84fb1a0fe9f9bf7655ddaaceba2f06
Przemek Spaczek <pspaczek@divante.co>
2020-04-20 15:43:49 +0200

    feat(stories): importable Storybook stories

--- packages/vue/scripts/create-index-files.js
+++ packages/vue/scripts/create-index-files.js
@@ -5,16 +5,18 @@ const glob = require("glob");
 const path = require("path");
 
 const pathTargetIndexJs = path.resolve(__dirname, "..", "index.js");
-
+const pathTargetStoriesJs = path.resolve(__dirname, "..", "stories.js");
 const pathVueComponentsRoot = path.resolve(__dirname, "..", "src/components");
 const pathsVueComponents = glob.sync("*/*/Sf*.vue", {
-  cwd: pathVueComponentsRoot
+  cwd: pathVueComponentsRoot,
 });
 
 function createIndexFiles() {
   const filesContent = generateFilesContent();
   saveIndexJs(filesContent.contentIndexJs);
   console.log("index.js created");
+  saveStoriesJs(filesContent.contentStoriesJs);
+  console.log("stories.js created");
 }
 
 function generateFilesContent() {
@@ -34,11 +36,14 @@ function generateFilesContent() {
     "export {\n" +
     exports.join(",\n") +
     "\n};\n";
-  const contentJsJs = contentIndexJs.replace(/\.vue";/g, '.js";');
-
+  const contentStoriesJs = contentIndexJs
+    .replace(/import/g, "const")
+    .replace(/ from /g, " = require(")
+    .replace(/\.vue";/g, ".stories.js\");")
+    .replace(/export/g, "module.exports =")
   return {
     contentIndexJs,
-    contentJsJs
+    contentStoriesJs
   };
 }
 
@@ -46,8 +51,11 @@ function saveIndexJs(contentIndexJs) {
   fs.writeFileSync(pathTargetIndexJs, contentIndexJs);
 }
 
+function saveStoriesJs(contentStoriesJs) {
+  fs.writeFileSync(pathTargetStoriesJs, contentStoriesJs);
+}
 module.exports = {
-  createIndexFiles
+  createIndexFiles,
 };
 
 if (require.main === module) {
