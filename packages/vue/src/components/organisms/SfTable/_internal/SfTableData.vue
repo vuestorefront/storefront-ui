2eb8cd38bc84fc8d7e8c5a4c4b2ded33a137c324
Justyna <jgieracka@divante.pl>
2020-04-20 16:06:56 +0200

    fix: table with v-focus

--- packages/vue/src/components/organisms/SfTable/_internal/SfTableData.vue
+++ packages/vue/src/components/organisms/SfTable/_internal/SfTableData.vue
@@ -1,10 +1,12 @@
 <template>
-  <td class="sf-table__data">
+  <td v-focus tabindex="0" class="sf-table__data">
     <slot />
   </td>
 </template>
 <script>
+import { focus } from "../../../../utilities/directives/focus-directive.js";
 export default {
   name: "SfTableData",
+  directives: { focus },
 };
 </script>
--- packages/vue/src/components/organisms/SfTable/_internal/SfTableHeader.vue
+++ packages/vue/src/components/organisms/SfTable/_internal/SfTableHeader.vue
@@ -1,10 +1,12 @@
 <template>
-  <th class="sf-table__header">
+  <th v-focus tabindex="0" class="sf-table__header">
     <slot />
   </th>
 </template>
 <script>
+import { focus } from "../../../../utilities/directives/focus-directive.js";
 export default {
   name: "SfTableHeader",
+  directives: { focus },
 };
 </script>
