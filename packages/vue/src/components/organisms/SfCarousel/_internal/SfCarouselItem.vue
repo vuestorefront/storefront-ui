f94a94b958c6d87c5eb5b07d6f699b7f935ceb49
Justyna <jgieracka@divante.pl>
2020-04-20 13:27:12 +0200

    fix: carousel with v-focus

--- packages/vue/src/components/organisms/SfCarousel/_internal/SfCarouselItem.vue
+++ packages/vue/src/components/organisms/SfCarousel/_internal/SfCarouselItem.vue
@@ -1,10 +1,12 @@
 <template>
-  <li class="sf-carousel-item glide__slide">
+  <li tabindex="0" v-focus class="sf-carousel-item glide__slide">
     <slot />
   </li>
 </template>
 <script>
+import { focus } from "../../../../utilities/directives/focus-directive.js";
 export default {
   name: "SfCarouselItem",
+  directives: { focus },
 };
 </script>
