1dc0f9d9fa2f04ecfe7f91eeb4ce2545a62bd009
Justyna <jgieracka@divante.pl>
2020-04-21 11:01:32 +0200

    fix: v-focus for banner

--- packages/vue/src/components/molecules/SfBanner/SfBanner.vue
+++ packages/vue/src/components/molecules/SfBanner/SfBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="sf-banner" :style="style" v-on="isMobile ? $listeners : {}">
+  <section tabindex="0" v-focus class="sf-banner" :style="style" v-on="isMobile ? $listeners : {}">
     <div class="sf-banner__container">
       <slot name="subtitle" v-bind="{ subtitle }">
         <h2 v-if="subtitle" class="sf-banner__subtitle">
@@ -34,8 +34,10 @@ import {
   mapMobileObserver,
   unMapMobileObserver,
 } from "../../../utilities/mobile-observer";
+import { focus } from "../../../utilities/directives/focus-directive.js";
 export default {
   name: "SfBanner",
+  directives: { focus },
   components: {
     SfButton,
   },
