dd0b01f7833fb8e1c440e8bf97a48a64e2bcd8d6
Marta Radziszewska <marta.radziszewskaa@gmail.com>
2020-04-27 13:15:14 +0200

    fix(a11y): v-focus in molecules (#1073)
    * feat: add v-focus SfSteps
    
    * feat: use v-focus on SfSearchBar
    
    * fix: customClass default value
    
    * feat: add aria-label, v-focus SfScrollable
    
    * feat: add focus SfRadio
    
    * feat: add v-focus SfPagination
    
    * fix: change steps to button, remove tabindex
    
    * fix: remove v-focus, remove aria-label
    
    * fix: steps styles
    
    * fix: add v-focus on checked
    
    * fix: radio fix, scrollable clean
    
    * fix: sf-link added
    
    * fix: sf-link for chevrons
    
    * refactor(a11y): use CSS to move focus from native to custom element
    
    Co-authored-by: Justyna <jgieracka@divante.pl>
    Co-authored-by: Przemek Spaczek <pspaczek@divante.pl>
    Co-authored-by: Przemek Spaczek <pspaczek@divante.co>

--- packages/vue/src/components/molecules/SfSearchBar/SfSearchBar.stories.js
+++ packages/vue/src/components/molecules/SfSearchBar/SfSearchBar.stories.js
@@ -17,7 +17,7 @@ storiesOf("Molecules|SearchBar", module)
             "sf-search-bar--position-center": "sf-search-bar--position-center",
             "sf-search-bar--no-icon": "sf-search-bar--no-icon",
           },
-          "null",
+          "",
           { display: "multi-select" },
           "CSS Modifiers"
         ),
@@ -47,7 +47,7 @@ storiesOf("Molecules|SearchBar", module)
             "sf-search-bar--position-center": "sf-search-bar--position-center",
             "sf-search-bar--no-icon": "sf-search-bar--no-icon",
           },
-          "null",
+          "",
           { display: "multi-select" },
           "CSS Modifiers"
         ),
