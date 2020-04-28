0ced97fbe4222a362be0d7b4b3faa4ae74ef6840
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-04-28 11:45:25 +0200

    fix: filter elements with hidden parent

--- packages/vue/src/utilities/directives/focus-trap-directive.js
+++ packages/vue/src/utilities/directives/focus-trap-directive.js
@@ -1,26 +1,32 @@
-import { getFocusableChildren, isFocusable, trap } from "./focus-trap/focus-trap.js";
-import Vue from 'vue';
+import {
+  getFocusableChildren,
+  isFocusable,
+  moveFocus,
+} from "./focus-trap/focus-trap.js";
+import Vue from "vue";
 
 export const focusTrap = {
   bind(el) {
-    el._focusableChildrenElements = getFocusableChildren(el);
+    Vue.nextTick(() => {
+      el._focusableChildrenElements = getFocusableChildren(el);
+    });
     el._keyHandler = function (e) {
       if (e.key === "Tab") {
         if (!isFocusable(e, el._focusableChildrenElements)) {
           el._lastFocusedElement = e.target;
         }
       }
-      trap(e, el._focusableChildrenElements);
+      moveFocus(e, el._focusableChildrenElements);
     };
     document.addEventListener("keydown", el._keyHandler);
   },
-  componentUpdated(el) { 
+  componentUpdated(el) {
     Vue.nextTick(() => {
       el._focusableChildrenElements = getFocusableChildren(el);
-    })
+    });
   },
   unbind(el) {
-    if(el._lastFocusedElement) el._lastFocusedElement.focus()
+    if (el._lastFocusedElement) el._lastFocusedElement.focus();
     document.removeEventListener("keydown", el._keyHandler);
   },
 };
--- packages/vue/src/utilities/directives/focus-trap/focus-trap.js
+++ packages/vue/src/utilities/directives/focus-trap/focus-trap.js
@@ -1,26 +1,34 @@
 const focusableElements = [
   "a[href]",
   "button:not([disabled])",
+  "button:not([hidden])",
   "[tabindex]:not([disabled])",
   "input:not([disabled])",
   "select",
   "textarea",
 ];
 
+const isHidden = (selector) => selector.offsetParent === null;
+
 export const getFocusableChildren = (el) => {
-  return el.querySelectorAll(focusableElements) || [];
+  return (
+    Array.from(el.querySelectorAll(focusableElements)).filter(
+      (selector) => !isHidden(selector)
+    ) || []
+  );
 };
 
-export const isFocusable = (e, focusableChildrenElements) => Array.from(focusableChildrenElements).some(
-    (el) => el === e.target
-  );
+export const isFocusable = (e, focusableChildrenElements) =>
+  Array.from(focusableChildrenElements).some((el) => el === e.target);
 
-export const trap = (e, focusableChildrenElements) => {
+export const moveFocus = (e, focusableChildrenElements) => {
   if (!focusableChildrenElements.length || e.key !== "Tab") return;
+
   if (!isFocusable(e, focusableChildrenElements)) {
     e.preventDefault();
     focusableChildrenElements[0].focus();
   }
+
   if (focusableChildrenElements.length === 1) {
     e.preventDefault();
     return;
@@ -30,7 +38,6 @@ export const trap = (e, focusableChildrenElements) => {
   const isLastElement =
     e.target === focusableChildrenElements[lastElementIndex];
   const isFirstElement = e.target === focusableChildrenElements[0];
-
   const isGoingForward = e.shiftKey === false;
 
   if (isGoingForward && isLastElement) {
