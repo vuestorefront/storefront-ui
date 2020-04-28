3718a0a20e25d63ac124fcd1688fca6d3469eea0
Justyna <jgieracka@divante.pl>
2020-04-28 13:16:25 +0200

    fix: tabindex deleted

--- packages/vue/src/components/molecules/SfSelect/_internal/SfSelectOption.vue
+++ packages/vue/src/components/molecules/SfSelect/_internal/SfSelectOption.vue
@@ -1,23 +1,19 @@
 <template>
   <li
-    v-focus
     class="sf-select-option"
     :class="{ 'sf-select-option--is-active': selected === value }"
     role="option"
     :aria-selected="selected === value ? 'true' : 'false'"
     @click="clicked"
     @keyup.space.enter="clicked"
-    tabindex="0"
   >
     <!-- @slot -->
     <slot />
   </li>
 </template>
 <script>
-import { focus } from "../../../../utilities/directives/focus-directive.js";
 export default {
   name: "SfSelectOption",
-  directives: { focus },
   props: {
     value: {
       type: [String, Number, Object],
