39f5d189dab33a09475171a4387ec8c7830abdf9
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-05-01 17:41:37 +0200

    feat: change to SfBottomModal, add styles

--- packages/vue/src/components/molecules/SfBottomModal/SfBottomModal.vue
+++ packages/vue/src/components/molecules/SfBottomModal/SfBottomModal.vue
@@ -1,51 +1,38 @@
 <template>
-  <div class="sf-language-selector">
+  <div class="sf-bottom-modal">
     <SfOverlay
       :visible="isOpen"
-      class="sf-language-selector__overlay mobile-only"
+      class="sf-bottom-modal__overlay"
       @click="close"
     />
-    <transition name="sf-language-selector">
+    <transition name="sf-bottom-modal">
       <nav
         v-show="isOpen"
         role="dialog"
         aria-modal="true"
         aria-labelledby="Language Selector"
-        class="sf-language-selector__container"
+        class="sf-bottom-modal__container"
       >
         <slot name="title">
-          <div v-if="title" class="sf-language-selector__title">
-            {{ title }}
-          </div>
+          <SfHeading
+            v-if="title"
+            :level="3"
+            :title="title"
+            class="sf-bottom-modal__title"
+          />
         </slot>
         <slot name="close-desktop">
           <SfCircleIcon
-            class="sf-circle-icon--small sf-language-selector__close-desktop desktop-only"
+            class="sf-circle-icon--small sf-bottom-modal__close-desktop desktop-only"
             aria-label="Close"
             icon="cross"
             @click="close"
           />
         </slot>
-        <SfList class="sf-language-selector__list">
-          <SfListItem
-            v-for="item in items"
-            :key="item"
-            class="sf-language-selector__list-item"
-          >
-            <SfButton
-              class="sf-button--full-width sf-button--underlined sf-language-selector__language-button"
-              :aria-label="item.language"
-              @click="isOpen = !isOpen"
-            >
-              <slot name="icon" />
-              {{ item.country }}
-              <span>{{ item.language }}</span>
-            </SfButton>
-          </SfListItem>
-        </SfList>
+        <slot />
         <slot name="close-mobile">
           <SfButton
-            class="sf-button--full-width mobile-only sf-language-selector__close"
+            class="sf-button--full-width mobile-only sf-bottom-modal__close"
             aria-label="Close"
             @click="close"
             >Cancel</SfButton
@@ -57,31 +44,22 @@
 </template>
 <script>
 import SfOverlay from "../../atoms/SfOverlay/SfOverlay.vue";
+import SfHeading from "../../atoms/SfHeading/SfHeading.vue";
 import SfButton from "../../atoms/SfButton/SfButton.vue";
 import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
-import SfList from "../../organisms/SfList/SfList.vue";
 export default {
-  name: "SfLanguageSelector",
-  components: { SfList, SfOverlay, SfButton, SfCircleIcon },
+  name: "SfBottomModal",
+  components: { SfOverlay, SfButton, SfCircleIcon, SfHeading },
   props: {
     /**
-     * Language Selector is open
+     * Bottom Modal is open
      */
     isOpen: {
       type: Boolean,
       default: false,
     },
     /**
-     * Items
-     */
-    items: {
-      type: [Array, Object],
-      default: () => {
-        [];
-      },
-    },
-    /**
-     * Title for Language Selector content.
+     * Bottom Modal title
      */
     title: {
       type: String,
@@ -115,5 +93,5 @@ export default {
 };
 </script>
 <style lang="scss">
-@import "~@storefront-ui/shared/styles/components/molecules/SfLanguageSelector.scss";
+@import "~@storefront-ui/shared/styles/components/molecules/SfBottomModal.scss";
 </style>
--- packages/vue/src/components/molecules/SfLanguageSelector/SfLanguageSelector.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import SfLanguageSelector from "./SfLanguageSelector.vue";
-
-describe("SfLanguageSelector.vue", () => {
-  it("renders a component", () => {
-    const component = shallowMount(SfLanguageSelector);
-    expect(component.contains(".sf-language-selector")).toBe(true);
-  });
-});
