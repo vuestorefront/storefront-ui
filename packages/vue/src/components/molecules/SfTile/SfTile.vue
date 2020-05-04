209af2d638426226d633de19ea2d3f5f65ac7e2e
Justyna <jgieracka@divante.pl>
2020-05-04 11:54:24 +0200

    fix: class names fixed

--- packages/vue/src/components/molecules/SfTile/SfTile.vue
+++ packages/vue/src/components/molecules/SfTile/SfTile.vue
@@ -2,17 +2,13 @@
   <div class="sf-tile" :style="style">
     <!-- @slot Use this slot to replace title -->
     <slot name="title" v-bind="{ title }">
-      <h1 v-if="title" class="sf-tile__title">
+      <h3 v-if="title" class="sf-tile__title">
         {{ title }}
-      </h1>
+      </h3>
     </slot>
   </div>
 </template>
 <script>
-import {
-  mapMobileObserver,
-  unMapMobileObserver,
-} from "../../../utilities/mobile-observer";
 export default {
   name: "SfAlert",
   props: {
@@ -39,26 +35,18 @@ export default {
     },
   },
   computed: {
-    ...mapMobileObserver(),
     style() {
       const image = this.image;
       const background = this.background;
-      return this.isMobile
-        ? {
-            "--tile-background-image": image.mobile
-              ? `url(${image.mobile})`
-              : `url(${image})`,
-            "--tile-background-color": background,
-          }
-        : {
-            "--_tile-background-desktop-image":
-              image.desktop && `url(${image.desktop})`,
-            "--tile-background-color": background,
-          };
+      return {
+        "--_tile-background-image": image.mobile
+          ? `url(${image.mobile})`
+          : `url(${image})`,
+        "--_tile-background-desktop-image":
+          image.desktop && `url(${image.desktop})`,
+        "--tile-background-color": background,
+      };
     },
-  },
-  beforeDestroy() {
-    unMapMobileObserver();
   },
 };
 </script>
