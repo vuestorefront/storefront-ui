603210e0e80ebf91f92ed1d3288e1fcc792a7307
Przemek Spaczek <pspaczek@divante.co>
2020-05-07 17:27:00 +0200

    refactor: fully rewrite SfHeader

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -5,6 +5,7 @@ import {
   select,
   boolean,
   object,
+  optionsKnob as options,
 } from "@storybook/addon-knobs";
 import { SfHeader } from "@storefront-ui/vue";
 const StoriesPlaceholder = {
@@ -33,6 +34,19 @@ storiesOf("Organisms|Header", module)
   .add("Common", () => ({
     components: { SfHeader, StoriesPlaceholder },
     props: {
+      customClass: {
+        default: options(
+          "CSS modifiers",
+          {
+            "sf-header--has-mobile-search": "sf-header--has-mobile-search",
+            "sf-header--has-mobile-navigation": "sf-header--has-mobile-navigation",
+            "sf-header--multiline": "sf-header--multiline",
+          },
+          "",
+          { display: "multi-select" },
+          "CSS Modifiers"
+        ),
+      },
       title: {
         default: text("title", "Storefront UI", "Props"),
       },
@@ -53,123 +67,6 @@ storiesOf("Organisms|Header", module)
           "account",
           "Props"
         ),
-      },
-      hasMobileSearch: {
-        default: boolean("hasMobileSearch", false, "Props"),
-      },
-      hasMobileNavigation: {
-        default: boolean("hasMobileNavigation", false, "Props"),
-      },
-      isSticky: {
-        default: boolean("isSticky", true, "Props"),
-      },
-      searchPlaceholder: {
-        default: text("searchPlaceholder", "Search for items", "Props"),
-      },
-      cartIcon: {
-        default: text("cartIcon", "empty_cart", "Props"),
-      },
-      wishlistIcon: {
-        default: text("wishlistIcon", "heart", "Props"),
-      },
-      accountIcon: {
-        default: text("accountIcon", "profile", "Props"),
-      },
-      cartItemsQty: {
-        default: text("cartItemsQty", "0", "Props"),
-      },
-    },
-    data() {
-      return {
-        isMobile: false,
-        navigation: ["women", "man", "kids", "women", "man", "kids"],
-        searchValue: "",
-      };
-    },
-    computed: {
-      spacer() {
-        return;
-      },
-    },
-    mounted() {
-      this.isMobile =
-        Math.max(document.documentElement.clientWidth, window.innerWidth) <=
-        1023;
-      window.matchMedia("(max-width: 1023px)").addListener(this.mobileHandler);
-    },
-    beforeDestroy() {
-      window
-        .matchMedia("(max-width: 1023px)")
-        .removeListener(this.mobileHandler);
-    },
-    methods: {
-      alert(label) {
-        alert(label);
-      },
-      mobileHandler(event) {
-        this.isMobile = event.matches;
-      },
-    },
-    template: `<div>
-      <SfHeader
-          :title="title"
-          :logo="logo"
-          :active-icon="activeIcon"
-          :has-mobile-search="hasMobileSearch"
-          :has-mobile-navigation="hasMobileNavigation"
-          :search-placeholder="searchPlaceholder"
-          :search-value="searchValue"
-          :cart-icon="cartIcon"
-          :wishlist-icon="wishlistIcon"
-          :is-sticky="isSticky"
-          :account-icon="accountIcon"
-          :style="spacer"
-          :cart-items-qty="cartItemsQty"
-          @click:cart="alert('@click:cart')"
-          @click:wishlist="alert('@click:wishlist')"
-          @click:account="alert('@click:account')"
-          @change:search="searchValue = $event"
-      >
-        <template #navigation>
-          <SfHeaderNavigationItem
-              v-for="item in navigation"
-              :key="item">
-            <a href="#" :style="{ display: 'flex',alignItems: 'center',height: '100%' }">{{item}}</a>
-          </SfHeaderNavigationItem>
-        </template>
-      </SfHeader>
-      <StoriesPlaceholder :mobile="isMobile"/>
-    </div>`,
-  }))
-  .add("with Search and Navigation items", () => ({
-    components: { SfHeader, StoriesPlaceholder },
-    props: {
-      title: {
-        default: text("title", "Storefront UI", "Props"),
-      },
-      logo: {
-        default: object(
-          "logo",
-          {
-            mobile: { url: "/assets/logo.svg" },
-            desktop: { url: "/assets/logo.svg" },
-          },
-          "Props"
-        ),
-      },
-      activeIcon: {
-        default: select(
-          "activeIcon",
-          ["", "account", "wishlist", "cart"],
-          "account",
-          "Props"
-        ),
-      },
-      hasMobileSearch: {
-        default: boolean("hasMobileSearch", true, "Props"),
-      },
-      hasMobileNavigation: {
-        default: boolean("hasMobileNavigation", true, "Props"),
       },
       isSticky: {
         default: boolean("isSticky", true, "Props"),
@@ -226,8 +123,109 @@ storiesOf("Organisms|Header", module)
           :title="title"
           :logo="logo"
           :active-icon="activeIcon"
-          :has-mobile-search="hasMobileSearch"
-          :has-mobile-navigation="hasMobileNavigation"
+          :search-placeholder="searchPlaceholder"
+          :search-value="searchValue"
+          :cart-icon="cartIcon"
+          :wishlist-icon="wishlistIcon"
+          :is-sticky="isSticky"
+          :account-icon="accountIcon"
+          :style="spacer"
+          :cart-items-qty="cartItemsQty"
+          @click:cart="alert('@click:cart')"
+          @click:wishlist="alert('@click:wishlist')"
+          @click:account="alert('@click:account')"
+          @change:search="searchValue = $event"
+      >
+        <template #navigation>
+          <SfHeaderNavigationItem
+              v-for="item in navigation"
+              :key="item">
+            <a href="#" :style="{ display: 'flex',alignItems: 'center',height: '100%' }">{{item}}</a>
+          </SfHeaderNavigationItem>
+        </template>
+      </SfHeader>
+      <StoriesPlaceholder :mobile="isMobile"/>
+    </div>`,
+  }))
+  .add("with Search and Navigation items", () => ({
+    components: { SfHeader, StoriesPlaceholder },
+    props: {
+      title: {
+        default: text("title", "Storefront UI", "Props"),
+      },
+      logo: {
+        default: object(
+          "logo",
+          {
+            mobile: { url: "/assets/logo.svg" },
+            desktop: { url: "/assets/logo.svg" },
+          },
+          "Props"
+        ),
+      },
+      activeIcon: {
+        default: select(
+          "activeIcon",
+          ["", "account", "wishlist", "cart"],
+          "account",
+          "Props"
+        ),
+      },
+      isSticky: {
+        default: boolean("isSticky", true, "Props"),
+      },
+      searchPlaceholder: {
+        default: text("searchPlaceholder", "Search for items", "Props"),
+      },
+      cartIcon: {
+        default: text("cartIcon", "empty_cart", "Props"),
+      },
+      wishlistIcon: {
+        default: text("wishlistIcon", "heart", "Props"),
+      },
+      accountIcon: {
+        default: text("accountIcon", "profile", "Props"),
+      },
+      cartItemsQty: {
+        default: text("cartItemsQty", "0", "Props"),
+      },
+    },
+    data() {
+      return {
+        isMobile: false,
+        navigation: ["women", "man", "kids"],
+        searchValue: "",
+      };
+    },
+    computed: {
+      spacer() {
+        return;
+      },
+    },
+    mounted() {
+      this.isMobile =
+        Math.max(document.documentElement.clientWidth, window.innerWidth) <=
+        1023;
+      window.matchMedia("(max-width: 1023px)").addListener(this.mobileHandler);
+    },
+    beforeDestroy() {
+      window
+        .matchMedia("(max-width: 1023px)")
+        .removeListener(this.mobileHandler);
+    },
+    methods: {
+      alert(label) {
+        alert(label);
+      },
+      mobileHandler(event) {
+        this.isMobile = event.matches;
+      },
+    },
+    template: `<div>
+      <SfHeader
+          :title="title"
+          :logo="logo"
+          :active-icon="activeIcon"
           :search-placeholder="searchPlaceholder"
           :search-value="searchValue"
           :cart-icon="cartIcon"
