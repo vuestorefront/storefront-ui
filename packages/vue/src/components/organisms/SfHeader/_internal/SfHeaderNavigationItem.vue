22191537566ef57d0660f8aaf905893c41c2cd5f
Przemek Spaczek <pspaczek@divante.pl>
2020-05-08 09:03:50 +0200

    refactor: multiline modifier for SfHeader and API changes (#1165)
    * chore: update stories for long nav
    
    * refactor: fully rewrite SfHeader
    
    * fix: sticky condition
    
    * fix: css variables
    
    * fix: styles and stories
    
    * fix: adjust for MegaMenu

--- packages/vue/src/components/organisms/SfHeader/_internal/SfHeaderNavigationItem.vue
+++ packages/vue/src/components/organisms/SfHeader/_internal/SfHeaderNavigationItem.vue
@@ -1,22 +1,10 @@
 <template>
-  <div :style="maxWidth" class="sf-header-navigation-item" v-on="$listeners">
+  <div class="sf-header-navigation-item" v-on="$listeners">
     <slot />
   </div>
 </template>
 <script>
 export default {
   name: "SfHeaderNavigationItem",
-  data() {
-    return {
-      maxWidth: {},
-    };
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.maxWidth = {
-        "--max-width": `${this.$el.offsetWidth}px`,
-      };
-    });
-  },
 };
 </script>
