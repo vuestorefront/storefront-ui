399232fcb41968e10a719e1b55b8897f7f3e50ab
Przemek Spaczek <pspaczek@divante.co>
2020-05-08 08:19:35 +0200

    fix: styles and stories

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -253,7 +253,7 @@ storiesOf("Organisms|Header", module)
     </div>`,
   }))
   .add("[slot] navigation", () => ({
-    components: { SfHeader },
+    components: { SfHeader, SfLink },
     props: {
       title: {
         default: text("title", "Storefront UI", "Props"),
@@ -346,7 +346,7 @@ storiesOf("Organisms|Header", module)
         @change:search="searchValue = $event"
     >
       <template #navigation>
-        <div :style="{margin: '0 0 0 1.25rem', display: 'flex', alignItems:'center', height: '100%'}">CUSTOM NAVIGATION</div>
+        <SfLink link="/">CUSTOM NAVIGATION</SfLink>
       </template>
     </SfHeader>`,
   }))
@@ -665,8 +665,8 @@ storiesOf("Organisms|Header", module)
       </template>
     </SfHeader>`,
   }))
-  .add("[slot] language-selector", () => ({
-    components: { SfHeader },
+  .add("[slot] aside", () => ({
+    components: { SfHeader, SfLink },
     props: {
       title: {
         default: text("title", "Storefront UI", "Props"),
@@ -748,18 +748,18 @@ storiesOf("Organisms|Header", module)
         :has-mobile-search="hasMobileSearch"
         :search-placeholder="searchPlaceholder"
         :search-value="searchValue"
-        :style="spacer"
         :cart-items-qty="cartItemsQty"
         @change:search="searchValue = $event"
+        class="sf-header--has-mobile-search sf-header--has-mobile-navigation"
     >
-      <template #language-selector>
-        <div :style="{margin: '0 0 0 1rem'}">LANGUAGE SELECTOR</div>
+      <template #aside>
+        <div :style="{margin: '0 0 0 1rem'}" class="mobile-only">ASIDE</div>
       </template>
       <template #navigation>
         <SfHeaderNavigationItem
             v-for="item in navigation"
             :key="item">
-          <a href="#" :style="{ display: 'flex',alignItems: 'center',height: '100%' }">{{item}}</a>
+          <SfLink href="#">{{item}}</SfLink>
         </SfHeaderNavigationItem>
       </template>
     </SfHeader>`,
