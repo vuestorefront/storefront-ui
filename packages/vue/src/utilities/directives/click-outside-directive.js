16a841db48ee3b7f206ce0ad8a22b0638e0cccd9
martaradziszewska <marta.radziszewskaa@gmail.com>
2020-05-08 10:54:49 +0200

    fix: remove excluding

--- packages/vue/src/utilities/directives/click-outside-directive.js
+++ packages/vue/src/utilities/directives/click-outside-directive.js
@@ -1,20 +1,10 @@
 export const clickOutside = {
   bind(el, binding, vnode) {
     binding.name = "click-outside";
-    el._outsideClickHandler = (e) => {
-      e.stopPropagation();
-      const { closeHandler, exclude } = binding.value;
-      let clickedOnExcludedEl = false;
-      if (exclude)
-        exclude.forEach((element) => {
-          if (!clickedOnExcludedEl) {
-            const excludedEl = vnode.context.$parent.$refs[element];
-            excludedEl.$el
-              ? (clickedOnExcludedEl = excludedEl.$el.contains(e.target))
-              : (clickedOnExcludedEl = excludedEl.contains(e.target));
-          }
-        });
-      if (!el.contains(e.target) && !clickedOnExcludedEl) {
+    const closeHandler = binding.value;
+    el._outsideClickHandler = function (event) {
+      if (!el.contains(event.target)) {
+        event.stopPropagation();
         closeHandler();
       }
     };
@@ -24,5 +14,6 @@ export const clickOutside = {
   unbind(el) {
     document.removeEventListener("click", el._outsideClickHandler);
     document.removeEventListener("touchstart", el._outsideClickHandler);
+    el._outsideClickHandler = null;
   },
 };
