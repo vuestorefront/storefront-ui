3025d9c1867eb40763e26f2687aa331fafbc72b9
Przemek Spaczek <pspaczek@divante.co>
2020-05-14 14:34:03 +0200

    fix: price direction

--- packages/vue/src/components/atoms/SfPrice/SfPrice.vue
+++ packages/vue/src/components/atoms/SfPrice/SfPrice.vue
@@ -4,13 +4,13 @@
     <slot name="regular" v-bind="{ regular, special }">
       <span v-if="!special" class="sf-price__regular">{{ regular }}</span>
     </slot>
-    <!--@slot Custom special price -->
-    <slot name="special" v-bind="{ special }">
-      <ins v-if="special" class="sf-price__special">{{ special }}</ins>
-    </slot>
     <!--@slot Custom old price (value from regular)-->
     <slot name="old" v-bind="{ regular, special }">
       <del v-if="special" class="sf-price__old">{{ regular }}</del>
+    </slot>
+    <!--@slot Custom special price -->
+    <slot name="special" v-bind="{ special }">
+      <ins v-if="special" class="sf-price__special">{{ special }}</ins>
     </slot>
   </div>
 </template>
