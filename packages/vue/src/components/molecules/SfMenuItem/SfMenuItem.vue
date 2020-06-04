e2085fb99678a3a80e2554e4a66cac12c164edb4
Adam Pawliński <apawlinski@divante.pl>
2020-06-04 08:54:53 +0200

    fix: remove role button and add sflink

--- packages/vue/src/components/molecules/SfMenuItem/SfMenuItem.vue
+++ packages/vue/src/components/molecules/SfMenuItem/SfMenuItem.vue
@@ -1,41 +1,39 @@
 <template>
-  <div
-    v-focus
-    class="sf-menu-item"
-    role="button"
-    tabindex="0"
-    v-on="$listeners"
-  >
-    <!-- @slot for menu item icon-->
-    <slot name="icon" />
-    <!-- @slot for menu item label-->
-    <slot name="label" v-bind="{ label }">
-      <span v-focus class="sf-menu-item__label">{{ label }}</span>
-    </slot>
-    <!-- @slot for items count -->
-    <slot name="count" v-bind="{ count }">
-      <span class="sf-menu-item__count">{{ count }}</span>
-    </slot>
-    <!-- @slot -->
-    <slot name="mobile-nav-icon" v-bind="{ icon }">
-      <SfIcon
-        v-if="icon"
-        :icon="icon"
-        class="sf-menu-item__mobile-nav-icon"
-        size="14px"
-      />
-    </slot>
+  <div class="sf-menu-item" v-on="$listeners">
+    <SfLink class="sf-menu-item__link" :link="link">
+      <!-- @slot for menu item icon-->
+      <slot name="icon" />
+      <!-- @slot for menu item label-->
+      <slot name="label" v-bind="{ label }">
+        <span class="sf-menu-item__label">{{ label }}</span>
+      </slot>
+      <!-- @slot for items count -->
+      <slot name="count" v-bind="{ count }">
+        <span class="sf-menu-item__count">{{ count }}</span>
+      </slot>
+      <!-- @slot -->
+      <slot name="mobile-nav-icon" v-bind="{ icon }">
+        <SfIcon
+          v-if="icon"
+          :icon="icon"
+          class="sf-menu-item__mobile-nav-icon"
+          size="14px"
+        />
+      </slot>
+    </SfLink>
   </div>
 </template>
 <script>
-import focus from "../../../utilities/directives";
+// import focus from "../../../utilities/directives";
 import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+import SfLink from "../../atoms/SfLink/SfLink.vue";
 export default {
   name: "SfMenuItem",
   components: {
     SfIcon,
+    SfLink,
   },
-  directives: { focus },
+  // directives: { focus },
   props: {
     label: {
       type: String,
@@ -47,6 +45,10 @@ export default {
     },
     count: {
       type: [String, Number],
+      default: "",
+    },
+    link: {
+      type: [String, Object],
       default: "",
     },
   },
