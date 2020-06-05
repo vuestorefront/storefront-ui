ee2d29873ed38ec624f21e783169069e48f73c1d
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-06-05 12:45:30 +0200

    feat: detect right behaviour for and use SfLink or SfButton

--- packages/vue/src/components/molecules/SfMenuItem/SfMenuItem.vue
+++ packages/vue/src/components/molecules/SfMenuItem/SfMenuItem.vue
@@ -1,55 +1,84 @@
 <template>
-  <div class="sf-menu-item" v-on="$listeners">
-    <SfLink class="sf-menu-item__link" :link="link">
-      <!-- @slot for menu item icon-->
-      <slot name="icon" />
-      <!-- @slot for menu item label-->
-      <slot name="label" v-bind="{ label }">
-        <span class="sf-menu-item__label">{{ label }}</span>
-      </slot>
-      <!-- @slot for items count -->
-      <slot name="count" v-bind="{ count }">
-        <span class="sf-menu-item__count">{{ count }}</span>
-      </slot>
-      <!-- @slot -->
-      <slot name="mobile-nav-icon" v-bind="{ icon }">
-        <SfIcon
-          v-if="icon"
-          :icon="icon"
-          class="sf-menu-item__mobile-nav-icon"
-          size="14px"
-        />
-      </slot>
-    </SfLink>
-  </div>
+  <component
+    :is="componentIs"
+    class="sf-menu-item"
+    v-bind="bind"
+    v-on="$listeners"
+  >
+    <!-- @slot for menu item icon-->
+    <slot name="icon" />
+    <!-- @slot for menu item label-->
+    <slot name="label" v-bind="{ label }">
+      <span class="sf-menu-item__label">{{ label }}</span>
+    </slot>
+    <!-- @slot for items count -->
+    <slot name="count" v-bind="{ count }">
+      <span class="sf-menu-item__count">{{ count }}</span>
+    </slot>
+    <!-- @slot -->
+    <slot name="mobile-nav-icon" v-bind="{ icon }">
+      <SfIcon
+        v-if="icon"
+        :icon="icon"
+        class="sf-menu-item__mobile-nav-icon"
+        size="14px"
+      />
+    </slot>
+  </component>
 </template>
 <script>
-// import focus from "../../../utilities/directives";
 import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
 import SfLink from "../../atoms/SfLink/SfLink.vue";
+import SfButton from "../../atoms/SfButton/SfButton.vue";
 export default {
   name: "SfMenuItem",
   components: {
     SfIcon,
     SfLink,
+    SfButton,
   },
-  // directives: { focus },
   props: {
+    /**
+     * Menu-item label
+     */
     label: {
       type: String,
       default: "",
     },
+    /**
+     * Menu-item icon (visible on mobile)
+     */
     icon: {
       type: [String, Array],
       default: "chevron_right",
     },
+    /**
+     * Menu-item count of items
+     */
     count: {
       type: [String, Number],
       default: "",
     },
+    /**
+     * Menu-item link (if is empty then SfMenuItem is SfButton)
+     */
     link: {
       type: [String, Object],
       default: "",
+    },
+  },
+  computed: {
+    bind() {
+      const bind = {};
+      if (this.link) {
+        bind.link = this.link;
+      } else {
+        bind.class = "sf-button--pure";
+      }
+      return bind;
+    },
+    componentIs() {
+      return this.link ? "sf-link" : "sf-button";
     },
   },
 };
