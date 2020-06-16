df774efd08c46ba11d564aaea128d232687853bf
Adam Pawliński <apawlinski@divante.pl>
2020-06-17 00:05:53 +0200

    feat: add sflink to button

--- packages/vue/src/components/atoms/SfButton/SfButton.stories.js
+++ packages/vue/src/components/atoms/SfButton/SfButton.stories.js
@@ -37,11 +37,64 @@ storiesOf("Atoms|Button", module)
       disabled: {
         default: boolean("disabled", false, "Props"),
       },
+      nativeButton: {
+        default: boolean("native button tag", true, "Props"),
+      },
+      href: {
+        default: text("link", "https://www.storefrontui.io/", "Props"),
+      }
     },
     components: { SfButton },
     template: `<SfButton
       :class="customClass"
-      :disabled="disabled">
+      :disabled="disabled"
+      :nativeButton="nativeButton"
+      :link="href">
+      {{customLabel}}
+    </SfButton>`,
+  }))
+  .add("Link as button", () => ({
+    props: {
+      customClass: {
+        default: options(
+          "CSS modifiers",
+          {
+            "sf-button--outline": "sf-button--outline",
+            "sf-button--underlined": "sf-button--underlined",
+            "sf-button--text": "sf-button--text",
+            "sf-button--full-width": "sf-button--full-width",
+            "sf-button--pure": "sf-button--pure",
+            "color-primary": "color-primary",
+            "color-secondary": "color-secondary",
+            "color-warning": "color-warning",
+            "color-danger": "color-danger",
+            "color-info": "color-info",
+            "color-success": "color-success",
+          },
+          "",
+          { display: "multi-select" },
+          "CSS Modifiers"
+        ),
+      },
+      customLabel: {
+        default: text("default", "Shop now", "Slots"),
+      },
+      disabled: {
+        default: boolean("disabled", false, "Props"),
+      },
+      nativeButton: {
+        default: boolean("native button tag", false, "Props"),
+      },
+      href: {
+        default: text("link", "https://www.storefrontui.io/", "Props"),
+      }
+    },
+    components: { SfButton },
+    template: `<SfButton
+      :class="customClass"
+      :disabled="disabled"
+      :nativeButton="nativeButton"
+      :link="href">
       {{customLabel}}
     </SfButton>`,
   }));
--- packages/vue/src/components/atoms/SfButton/SfButton.vue
+++ packages/vue/src/components/atoms/SfButton/SfButton.vue
@@ -1,19 +1,25 @@
 <template>
-  <button
+  <component
+    :is="tag"
     v-focus
     class="sf-button"
     v-bind="$attrs"
     :disabled="disabled"
+    :link="link"
     v-on="$listeners"
   >
     <!--@slot Use this slot to place content inside the button.-->
     <slot />
-  </button>
+  </component>
 </template>
 <script>
 import { focus } from "../../../utilities/directives";
+import SfLink from "../SfLink/SfLink.vue";
 export default {
   name: "SfButton",
+  components: {
+    SfLink,
+  },
   directives: {
     focus,
   },
@@ -24,6 +30,28 @@ export default {
     disabled: {
       type: Boolean,
       default: false,
+    },
+    /**
+     * To change for "a" tag chose false
+     */
+    nativeButton: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * Link for "a" tag
+     */
+    href: {
+      type: [String, Object],
+      default: "",
+    },
+  },
+  computed: {
+    tag() {
+      return this.nativeButton === true ? "button" : "SfLink";
+    },
+    link() {
+      return this.nativeButton === true ? undefined : this.href;
     },
   },
 };
