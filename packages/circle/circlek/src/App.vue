bd49b6245045e6c9388db7211a449c950f4dcdab
Adam Pawliński <apawlinski@divante.pl>
2020-06-25 00:26:12 +0200

    feat: wip add content

--- packages/circle/circlek/src/App.vue
+++ packages/circle/circlek/src/App.vue
@@ -1,17 +1,131 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SfHeader 
+      :logo="logo"
+    >
+      <template #navigation>
+        <SfHeaderNavigationItem
+            v-for="item in navigation"
+            :key="item">
+          <SfLink href="#">{{item}}</SfLink>
+        </SfHeaderNavigationItem>
+      </template>
+    </SfHeader>
+    <SfBreadcrumbs 
+      :breadcrumbs="breadcrumbs"
+    />
+    <SfHeading
+      title="Beverages"
+    />
+    <span>Sort by:</span>
+    <SfSelect>
+      <SfSelectOption v-for="(option, key) in selectOptions" :key="key" :value="option">
+        {{option}}
+      </SfSelectOption>
+    </SfSelect>
+
+    <SfProductCard
+      v-for="(product, key) in products" :key="key"
+    />
+    <SfSidebar>
+      <SfAccordion>
+        <SfAccordionItem
+            v-for="(accordion, i) in sidebarAccordion"
+            :key="i"
+            :header="accordion.header"
+          >
+          <template>
+              <SfList class="list">
+                <SfListItem
+                  v-for="(item, j) in accordion.items"
+                  :key="j"
+                  class="list__item"
+                >
+                  <SfMenuItem :label="item.label" :count="item.count" />
+                </SfListItem>
+              </SfList>
+            </template>
+        </SfAccordionItem>
+      </SfAccordion>
+    </SfSidebar>
+    <SfFooter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {SfHeader,
+    SfBreadcrumbs,
+    SfHeading,
+    SfSelect,
+    SfProductCard,
+    SfSidebar,
+    SfAccordion,
+    SfList,
+    SfFooter,
+    SfLink
+    } from '@storefront-ui/vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SfHeader,
+    SfBreadcrumbs,
+    SfHeading,
+    SfSelect,
+    SfProductCard,
+    SfSidebar,
+    SfAccordion,
+    SfList,
+    SfFooter,
+    SfLink
+  },
+  data() {
+    return {
+      logo: "./assets/logo.png",
+      navigation: ["home", "products", "our stores"],
+      breadcrumbs: [
+                    {
+                      "text": "Shop",
+                      "link": "#"
+                    },
+                    {
+                      "text": "Products",
+                      "link": "#"
+                    },
+                    {
+                      "text": "Beverages",
+                      "link": "#"
+                    }
+                  ],
+      selectOptions: [
+        "Price from high to low", "Price from high to low", "Price from high to low"
+      ],
+      products: [
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"},
+        {title: "5 Hour Energy Peach Mango",
+        price: "$ 50"}        
+      ],
+      sidebarAccordion: [
+        "Categories", "Brands", "Price", "Popular"
+      ]
+    }
   }
 }
 </script>
--- packages/circle/circlek/src/main.js
+++ packages/circle/circlek/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "@storefront-ui/vue/styles.scss";
 
 Vue.config.productionTip = false
 
