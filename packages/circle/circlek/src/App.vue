b8c1b0c701a8cf4b65e56bc3b01096ef47a6bbbd
Adam Pawliński <apawlinski@divante.pl>
2020-06-25 15:43:47 +0200

    feat: style header and upper bar

--- packages/circle/circlek/src/App.vue
+++ packages/circle/circlek/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="circle-category">
     <SfHeader 
       :logo="logo"
     >
@@ -14,14 +14,16 @@
     <SfBreadcrumbs 
       :breadcrumbs="breadcrumbs"
     />
-    <Heading title="Beverages"/>
-    <span>Sort by:</span>
-    <SfSelect>
-      <SfSelectOption v-for="(option, key) in selectOptions" :key="key" :value="option">
-        {{option}}
-      </SfSelectOption>
-    </SfSelect>
-
+    <div class="circle-category__upper-bar">
+      <SfHeading class="circle-category__heading" title="Beverages" :level="3"/>
+      <div class="circle-category__filter">
+        <SfSelect label="Sort by:" class="circle-category__filter-select" selected="Price from high to low">
+          <SfSelectOption v-for="(option, key) in selectOptions" :key="key" :value="option">
+            {{option}}
+          </SfSelectOption>
+        </SfSelect>
+      </div>
+    </div>
     <SfProductCard
       v-for="(product, key) in products" :key="key"
       :image="product.image"
@@ -31,24 +33,22 @@
       wishlistIcon="heart"
 
     />
-    <div :title="Beverages" :visible="true" :button="false" :overlay="false">
-      <SfAccordion multiple="true">
+    <div>
+      <SfAccordion :multiple="true" :showChevron="false">
         <SfAccordionItem
             v-for="(accordion, i) in sidebarAccordion"
             :key="i"
             :header="accordion.header"
           >
           <template>
-            <SfScrollable>
-              <SfList class="list">
+            <SfScrollable showText="more" hideText="less" maxContentHeight="4rem">
+              <SfList>
                 <SfListItem
-                  v-for="(item, j) in accordion.items"
-                  :key="j"
+                  v-for="item in accordion.items"
+                  :key="item"
                   class="list__item"
                 >
-                  <SfMenuItem >
-                    {{item}}
-                  </SfMenuItem>
+                  <SfMenuItem :label="item"/>
                 </SfListItem>
               </SfList>
             </SfScrollable>
@@ -75,7 +75,7 @@
 <script>
 import {SfHeader,
     SfBreadcrumbs,
-    // SfHeading,
+    SfHeading,
     SfSelect,
     SfProductCard,
     // SfSidebar,
@@ -84,7 +84,9 @@ import {SfHeader,
     SfFooter,
     SfLink,
     SfInput,
-    SfImage
+    SfImage,
+    SfMenuItem,
+    SfScrollable
     } from '@storefront-ui/vue'
 
 export default {
@@ -92,7 +94,7 @@ export default {
   components: {
     SfHeader,
     SfBreadcrumbs,
-    // SfHeading,
+    SfHeading,
     SfSelect,
     SfProductCard,
     // SfSidebar,
@@ -101,7 +103,9 @@ export default {
     SfFooter,
     SfLink,
     SfInput,
-    SfImage
+    SfImage,
+    SfMenuItem,
+    SfScrollable
   },
   data() {
     return {
@@ -201,13 +205,84 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+.circle-category {
+  font-family: Montserrat, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  max-width: 1280px;
+  margin: 0 auto;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .sf-header {
+    height:90px;
+    margin: 0 0 1.5rem 0;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 30px rgba(29, 31, 34, 0.08), 1px 10px 10px rgba(102, 62, 51, 0.02);
+    border-radius: 60px;
+    &-navigation-item {
+      --header-navigation-item-border: none;
+      --header-navigation-item-font-weight: bold;
+      --header-navigation-item-font-line-height: 1.0625rem;
+      .sf-link {
+        --header-navigation-item-color: #1D1F22;
+        &:hover,
+        &:active {
+          --header-navigation-item-color:#EE2E24;
+        }
+      }      
+    }
+    .sf-search-bar {
+      --search-bar-icon-right: 0.875rem;
+      --search-bar-input-text-align: left;
+      --search-bar-font: normal 14px Montserrat;
+      --search-bar-border: none;
+      --search-bar-padding: 0 2.1875rem 0 2.125rem;
+      
+      width: 21.75rem;
+      height: 3rem;
+      border: 2px solid #EE2E24;
+      box-sizing: border-box;
+      border-radius: 34px;
+      &__icons {
+        bottom: var(--spacer-base);
+        transform: translate3d(0, 50%, 0);
+      }
+    }
+  }
+  &__upper-bar {
+    display: flex;
+    justify-content: space-between;       
+  }
+  &__filter {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+      span {
+        font-weight: bold;
+      }
+      &-select {   
+        --select-label-top: 50%; 
+        --select-label-left: calc(var(--spacer-2xl) * -1);  
+        --select-label-font: var(--font-lg) Montserrat;  
+        width: 15rem;
+        .sf-select-option {
+          --select-option-background: #fff;          
+        }
+        &:hover,
+        &:focus {
+          --chevron-color: #000;
+        }
+      }
+    } 
+  &__heading {
+    --heading-title-color: #EE2E24; 
+    display: flex;
+    align-self: flex-end;     
+    font-weight: bold;
+    font-size: var(--font-lg);
+  }    
 }
 </style>
