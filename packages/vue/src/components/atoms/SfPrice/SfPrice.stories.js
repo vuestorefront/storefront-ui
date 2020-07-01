eeab39ff7bc4309576e27b48d7be27d394b9f26f
Justyna <jgieracka@divante.pl>
2020-07-01 09:11:18 +0200

    feat: old-text and special-text slots added

--- packages/vue/src/components/atoms/SfPrice/SfPrice.stories.js
+++ packages/vue/src/components/atoms/SfPrice/SfPrice.stories.js
@@ -1,6 +1,7 @@
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, text } from "@storybook/addon-knobs";
 import { SfPrice, SfBadge } from "@storefront-ui/vue";
+import "./SfPrice.stories.scss";
 storiesOf("Atoms|Price", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
@@ -77,6 +78,29 @@ storiesOf("Atoms|Price", module)
       :special="special">
       <template #regular="{regular, special}">
         <SfBadge>{{regular}}</SfBadge>
+      </template>
+    </SfPrice>`,
+  }))
+  .add("[slots] old-text, special-text", () => ({
+    components: {
+      SfPrice,
+    },
+    props: {
+      regular: {
+        default: text("regular", "$200.00", "Props"),
+      },
+      special: {
+        default: text("special", "$30", "Props"),
+      },
+    },
+    template: `<SfPrice
+      :regular="regular"
+      :special="special">
+      <template #old-text>
+        <p class="price--text-old">Was</p>
+      </template>
+      <template #special-text>
+        <p class="price--text-special">Now</p>
       </template>
     </SfPrice>`,
   }));
--- packages/vue/src/components/atoms/SfPrice/SfPrice.vue
+++ packages/vue/src/components/atoms/SfPrice/SfPrice.vue
@@ -4,10 +4,14 @@
     <slot name="regular" v-bind="{ regular, special }">
       <span v-if="!special" class="sf-price__regular">{{ regular }}</span>
     </slot>
+    <!--@slot Custom old price text -->
+    <slot name="old-text" />
     <!--@slot Custom old price (value from regular)-->
     <slot name="old" v-bind="{ regular, special }">
       <del v-if="special" class="sf-price__old">{{ regular }}</del>
     </slot>
+    <!--@slot Custom special price text -->
+    <slot name="special-text" />
     <!--@slot Custom special price -->
     <slot name="special" v-bind="{ special }">
       <ins v-if="special" class="sf-price__special">{{ special }}</ins>
