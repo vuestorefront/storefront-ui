22f6ba27d2a5a245efeee8f6620d107ee19649d9
Adam Pawliński <apawlinski@divante.pl>
2020-07-22 22:42:18 +0200

    fix: unit test fix

--- packages/vue/src/components/molecules/SfComponentSelect/SfComponentSelect.spec.js
+++ packages/vue/src/components/molecules/SfComponentSelect/SfComponentSelect.spec.js
@@ -3,6 +3,6 @@ import SfComponentSelect from "./SfComponentSelect.vue";
 describe("SfComponentSelect.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfComponentSelect);
-    expect(component.contains(".sf-select")).toBe(true);
+    expect(component.contains(".sf-component-select")).toBe(true);
   });
 });
