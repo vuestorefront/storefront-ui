d90d5999e08830c0f9ea8d65002b7bb4479f4881
Michał Dziedziński <32803679+Michal-Dziedzinski@users.noreply.github.com>
2020-07-28 11:12:48 +0200

    refactor: change test methods to class or find methods (#1313)
    * refactor: change test methods to class or find methods
    
    * refactor: add testing-library/jest-dom and replace deprecated methods

--- packages/vue/src/components/organisms/SfBottomNavigation/SfBottomNavigation.spec.js
+++ packages/vue/src/components/organisms/SfBottomNavigation/SfBottomNavigation.spec.js
@@ -3,6 +3,6 @@ import SfBottomNavigation from "./SfBottomNavigation.vue";
 describe("SfBottomNavigation.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfBottomNavigation);
-    expect(component.contains(".sf-bottom-navigation")).toBe(true);
+    expect(component.classes("sf-bottom-navigation")).toBe(true);
   });
 });
