454b9fb41ffc096a7b9eba89066faa849903556c
Adam Pawliński <32042425+AdamPawlinski@users.noreply.github.com>
2020-07-29 10:03:15 +0200

    feat: SfScrollable add slot for button (#1330)
    * fix: remove vsf-test from repo
    
    * feat: add slot for button and apropriate story
    
    * fix: remove conflict
    
    * fix: after CR fix, change button name

--- packages/vue/src/components/molecules/SfScrollable/SfScrollable.vue
+++ packages/vue/src/components/molecules/SfScrollable/SfScrollable.vue
@@ -3,14 +3,16 @@
     <Simplebar ref="content" class="sf-scrollable__content" :style="style">
       <slot />
     </Simplebar>
-    <SfButton
-      v-show="hasScroll"
-      class="sf-button--text sf-scrollable__button"
-      @click="isHidden = !isHidden"
-    >
-      <span v-if="isHidden">{{ showText }}</span>
-      <span v-else>{{ hideText }}</span>
-    </SfButton>
+    <slot name="view-all" v-bind="{ hasScroll, showText, hideText }">
+      <SfButton
+        v-show="hasScroll"
+        class="sf-button--text sf-scrollable__view-all"
+        @click="isHidden = !isHidden"
+      >
+        <span v-if="isHidden">{{ showText }}</span>
+        <span v-else>{{ hideText }}</span>
+      </SfButton>
+    </slot>
   </div>
 </template>
 <script>
