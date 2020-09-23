fc81a7d6d34d3d12a70469d5612d4cbc060c7490
Justyna <jgieracka@divante.pl>
2020-09-23 08:14:08 +0200

    fix: tests fixed

--- packages/vue/src/components/molecules/SfAddressPicker/SfAddressPicker.spec.js
+++ packages/vue/src/components/molecules/SfAddressPicker/SfAddressPicker.spec.js
@@ -4,6 +4,6 @@ import SfAddressPicker from "./SfAddressPicker.vue";
 describe("SfAddressPicker.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfAddressPicker);
-    expect(component.contains(".sf-address-picker")).toBe(true);
+    expect(component.classes("sf-address-picker")).toBe(true);
   });
 });
