74e32fdcf5bd7da08389a15c895cdd1d5e760b41
Justyna <jgieracka@divante.pl>
2020-10-26 14:43:39 +0100

    feat: w/o close btn

--- packages/vue/src/components/organisms/SfProductCard/SfProductCard.vue
+++ packages/vue/src/components/organisms/SfProductCard/SfProductCard.vue
@@ -37,7 +37,6 @@
           v-if="colors"
           class="sf-product-card__colors"
           label="Choose color"
-          :has-close="true"
           :is-open="!isMobile || openColorPicker"
           @click:toggle="toggleColorPicker"
         >
