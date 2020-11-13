efdb7e9eff8e024b425768d45434931c96611379
Adam Pawliński <apawlinski@divante.pl>
2020-11-13 17:53:36 +0100

    chore: test Ci workflow

--- packages/vue/src/components/organisms/SfHero/_internal/SfHeroItem.vue
+++ packages/vue/src/components/organisms/SfHero/_internal/SfHeroItem.vue
@@ -70,6 +70,7 @@ export default {
     style() {
       const image = this.image;
       const background = this.background;
+      console.log("test");
       return {
         "--_hero-item-background-image": image.mobile
           ? `url(${image.mobile})`
