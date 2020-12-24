f8a875d759c71caea9637c3752bf5a68a2f83e4f
Maya Shavin <maya@cloudinary.com>
2020-12-24 19:21:42 +0200

    fix: stories in transition, storybook settings

--- packages/vue/.storybook/main.js
+++ packages/vue/.storybook/main.js
@@ -3,10 +3,7 @@ const path = require('path');
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/components/atoms/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../src/components/molecules/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../src/components/organisms/**/*.stories.@(js|jsx|ts|tsx)",
-    // "../src/components/pages/**.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-essentials",
