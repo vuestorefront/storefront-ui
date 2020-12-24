b90fd9df19c4003d54d09d8f4410700602e304b8
Maya Shavin <maya@cloudinary.com>
2020-12-24 18:44:34 +0200

    fix: load organisms stories

--- packages/vue/.storybook/main.js
+++ packages/vue/.storybook/main.js
@@ -5,7 +5,7 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/components/atoms/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../src/components/molecules/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    // "../src/components/organisms/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/organisms/**/*.stories.@(js|jsx|ts|tsx)",
     // "../src/components/pages/**.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
