f25e50e4763e987733e3740af3c654f5a12580f5
Maya Shavin <maya@cloudinary.com>
2020-12-24 12:11:37 +0200

    fix: stories of grouped-product, header, hero

--- packages/vue/src/components/organisms/SfHero/SfHero.stories.js
+++ packages/vue/src/components/organisms/SfHero/SfHero.stories.js
@@ -7,6 +7,40 @@ import {
   optionsKnob as options,
 } from "@storybook/addon-knobs";
 import { SfHero } from "@storefront-ui/vue";
+export default {
+  title: "Molecules/AddressPicker",
+  component: SfAddressPicker,
+  argTypes: {
+    name: {
+      control: "text",
+      table: {
+        category: "Props",
+      },
+      defaultValue: 0,
+    },
+    current: {
+      control: "number",
+      defaultValue: 0,
+      table: {
+        category: "Props",
+      },
+    },
+    input: { action: "Address picked", table: { category: "Events" } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { SfAddressPicker },
+  props: Object.keys(argTypes),
+  template: `<SfBar
+  :title="title"
+  :back="back"
+  :close="close"
+/>`,
+});
+
+export const Common = Template.bind({});
+Common.args = {};
 storiesOf("Organisms|Hero", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
