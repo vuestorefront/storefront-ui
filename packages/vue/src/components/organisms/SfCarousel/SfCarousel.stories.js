97e3fe837a1ee45ebce08a3be8ffa2eb3bbabf23
Maya Shavin <maya@cloudinary.com>
2020-12-24 13:41:59 +0200

    fix: stories of carousel, collected-product

--- packages/vue/src/components/organisms/SfCarousel/SfCarousel.stories.js
+++ packages/vue/src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -1,79 +1,71 @@
-import { storiesOf } from "@storybook/vue";
 import { SfCarousel } from "@storefront-ui/vue";
 export default {
-  title: "Molecules/AddressPicker",
-  component: SfAddressPicker,
+  title: "Organisms/Carousel",
+  component: SfCarousel,
   argTypes: {
-    name: {
-      control: "text",
+    settings: {
+      control: "object",
       table: {
         category: "Props",
+        defaultValue: {
+          summary: `{}`,
+        }
       },
-      defaultValue: 0,
+      defaultValue: {},
     },
-    current: {
-      control: "number",
-      defaultValue: 0,
-      table: {
-        category: "Props",
-      },
-    },
-    input: { action: "Address picked", table: { category: "Events" } },
   },
 };
 
 const Template = (args, { argTypes }) => ({
-  components: { SfAddressPicker },
+  components: { SfCarousel },
   props: Object.keys(argTypes),
-  template: `<SfBar
-  :title="title"
-  :back="back"
-  :close="close"
-/>`,
+  template: `
+  <SfCarousel
+  :style="{maxWidth: '1140px', margin: 'auto'}"
+  >
+    <SfCarouselItem v-for="(item, i) in 12" :key="i">
+      <div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">
+        {{item}}
+      </div>
+    </SfCarouselItem>
+  </SfCarousel>`,
 });
 
 export const Common = Template.bind({});
 Common.args = {};
-storiesOf("Organisms|Carousel", module)
-  .add("Common", () => ({
-    components: { SfCarousel },
-    template: `<SfCarousel
-      :style="{maxWidth: '1140px', margin: 'auto'}"
-    >
-        <SfCarouselItem v-for="(item, i) in 12" :key="i">
-          <div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">
-            {{item}}
-          </div>
-        </SfCarouselItem>
-      </SfCarousel>`,
-  }))
-  .add("[slot] next", () => ({
-    components: { SfCarousel },
-    template: `<SfCarousel
-      :style="{maxWidth: '1140px', margin: 'auto'}"
-    >
-      <template #next="{go}">
-        <button @click="go">NEXT</button>
-      </template>
-      <SfCarouselItem v-for="(item, i) in 12" :key="i">
-        <div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">
-          {{item}}
-        </div>
-      </SfCarouselItem>
-    </SfCarousel>`,
-  }))
-  .add("[slot] prev", () => ({
-    components: { SfCarousel },
-    template: `<SfCarousel
-      :style="{maxWidth: '1140px', margin: 'auto'}"
-    >
-      <template #prev="{go}">
-        <button @click="go">PREV</button>
-      </template>
-      <SfCarouselItem v-for="(item, i) in 12" :key="i">
-        <div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">
-          {{item}}
-        </div>
-      </SfCarouselItem>
-    </SfCarousel>`,
-  }));
+
+export const UseNextSlot = (args, { argTypes }) => ({
+  components: { SfCarousel },
+  props: Object.keys(argTypes),
+  template: `
+  <SfCarousel
+  :style="{maxWidth: '1140px', margin: 'auto'}"
+  >
+    <template #prev="{go}">
+      <button @click="go">PREV</button>
+    </template>
+    <SfCarouselItem v-for="(item, i) in 12" :key="i">
+      <div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">
+        {{item}}
+      </div>
+    </SfCarouselItem>
+  </SfCarousel>`,
+});
+
+export const UsePrevSlot = (args, { argTypes }) => ({
+  components: { SfCarousel },
+  props: Object.keys(argTypes),
+  template: `
+  <SfCarousel
+  :style="{maxWidth: '1140px', margin: 'auto'}"
+  >
+    <template #next="{go}">
+      <button @click="go">NEXT</button>
+    </template>
+    <SfCarouselItem v-for="(item, i) in 12" :key="i">
+      <div style="display: flex; align-items: center; justify-content: center; height: 300px; background-color: #5ECE7B; color: #FFF; font-size: 2.5rem">
+        {{item}}
+      </div>
+    </SfCarouselItem>
+  </SfCarousel>`,
+});
