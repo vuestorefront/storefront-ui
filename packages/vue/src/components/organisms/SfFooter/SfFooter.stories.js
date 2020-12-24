7f1b1d22c6f4f309e0f5a4c45667abecc810abda
Maya Shavin <maya@cloudinary.com>
2020-12-24 12:10:32 +0200

    fix: stories of bottom navigation, carousel,footer

--- packages/vue/src/components/organisms/SfFooter/SfFooter.stories.js
+++ packages/vue/src/components/organisms/SfFooter/SfFooter.stories.js
@@ -9,6 +9,40 @@ import {
   SfButton,
   SfMenuItem,
 } from "@storefront-ui/vue";
+export default {
+  title: "Molecules/AddressPicker",
+  component: SfAddressPicker,
+  argTypes: {
+    name: {
+      control: "text",
+      table: {
+        category: "Props",
+      },
+      defaultValue: 0,
+    },
+    current: {
+      control: "number",
+      defaultValue: 0,
+      table: {
+        category: "Props",
+      },
+    },
+    input: { action: "Address picked", table: { category: "Events" } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { SfAddressPicker },
+  props: Object.keys(argTypes),
+  template: `<SfBar
+  :title="title"
+  :back="back"
+  :close="close"
+/>`,
+});
+
+export const Common = Template.bind({});
+Common.args = {};
 storiesOf("Organisms|Footer", module)
   .addDecorator(withKnobs)
   .add("Common", () => ({
