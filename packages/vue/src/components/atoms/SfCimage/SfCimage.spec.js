fb14f79ed5deee8b297e4a5626c97423fe0d8a29
Maya Shavin <maya@cloudinary.com>
2021-01-06 00:48:33 +0200

    fix: unit test

--- packages/vue/src/components/atoms/SfCimage/SfCimage.spec.js
+++ packages/vue/src/components/atoms/SfCimage/SfCimage.spec.js
@@ -3,7 +3,14 @@ import SfCimage from "./SfCimage.vue";
 
 describe("SfCimage.vue", () => {
   it("renders a component", () => {
-    const component = shallowMount(SfCimage);
+    const component = shallowMount(SfCimage, {
+      propsData: {
+        publicId: "example",
+        cloud: {
+          cloudName: "demo",
+        },
+      },
+    });
     expect(component.contains(".sf-cimage")).toBe(true);
   });
 });
