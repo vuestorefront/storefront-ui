59ce08c0dd1ec3b02ed7864a777640c9e9babd39
Justyna <jgieracka@vuestorefront.io>
2021-01-05 16:41:38 +0100

    fix: story fixed

--- packages/vue/src/components/molecules/SfTile/SfTile.stories.js
+++ packages/vue/src/components/molecules/SfTile/SfTile.stories.js
@@ -1,31 +1,47 @@
-import { storiesOf } from "@storybook/vue";
-import { withKnobs, text, color, object } from "@storybook/addon-knobs";
 import { SfTile } from "@storefront-ui/vue";
-storiesOf("Molecules|Tile", module)
-  .addDecorator(withKnobs)
-  .add("Common", () => ({
-    components: { SfTile },
-    props: {
-      title: {
-        default: text("title", "Bags", "Props"),
-      },
-      background: {
-        default: object(
-          "image",
-          {
-            mobile: "/assets/storybook/SfTile/tileB-m.jpg",
-            desktop: "/assets/storybook/SfTile/tileB.jpg",
-          },
-          "Props"
-        ),
-      },
-      link: {
-        default: text("link", "https://www.storefrontui.io/", "Props"),
+
+export default {
+  title: "Components/Molecules/Tile",
+  component: SfTile,
+  argTypes: {
+    title: {
+      control: "text",
+      table: {
+        category: "Props",
       },
     },
-    template: `<SfTile
-      :title="title"
-      :background="background"
-      :link="link"
-      />`,
-  }));
+    background: {
+      control: "object",
+      table: {
+        category: "Props",
+      },
+    },
+    link: {
+      control: "text",
+      table: {
+        category: "Props",
+      },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { SfTile },
+  props: Object.keys(argTypes),
+  template: `
+  <SfTile
+    :title="title"
+    :background="background"
+    :link="link"
+  />`,
+});
+
+export const Common = Template.bind({});
+Common.args = {
+  title: "Bags",
+  background: {
+    mobile: "/assets/storybook/SfTile/tileB-m.jpg",
+    desktop: "/assets/storybook/SfTile/tileB.jpg",
+  },
+  link: "https://www.storefrontui.io/",
+};
