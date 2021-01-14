5d607da8f926a1558aad17d957422ab82331bfc7
Justyna <jgieracka@vuestorefront.io>
2021-01-14 16:43:22 +0100

    feat: transition docs

--- packages/vue/src/utilities/transitions/transitions.stories.js
+++ packages/vue/src/utilities/transitions/transitions.stories.js
@@ -1,5 +1,68 @@
 import SfButton from "./../../../src/components/atoms/SfButton/SfButton.vue";
 
+const doscTransitions = `<div>
+  <p style="font-size: 16px; margin-top: 30px;">
+    Transitions are animations that help users understand the flow on a page. 
+    You can use our ready-to-use transition effects or modify them according to your needs.
+  </p>
+  <p style="font-size: 16px">
+    Some of our components already have their own transitions for example
+    <a
+    href="?path=/story/components-atoms-overlay--common"
+    target="_self"
+    >
+      <span>
+        SfOverlay, 
+      </span>
+    </a>
+    <a
+    href="?path=/story/components-molecules-notification--common"
+    target="_self"
+    >
+      <span>
+        SfNotification, 
+      </span>
+    </a>
+    <a
+    href="?path=/story/components-molecules-dropdown--common"
+    target="_self"
+    >
+      <span>
+        SfDropdown
+      </span>
+    </a>
+  </p>
+  <p style="font-size: 18px; margin-top: 40px;">
+    <b>Changing transitions</b>
+  </p>
+  <p style="font-size: 16px">
+  If you would like to change exsisting transition, 
+  you can override classes that are responsible for the effects: 
+  </p>
+  <ul>
+    <li>.slide-enter-active</li>
+    <li>.slide-leave-active</li>
+    <li>.slide-leave-active</li>
+    <li>.slide-leave-to </li>
+  </ul>
+  <p style="margin: 40px auto;">
+    <span>
+      If you want to learn more about vue transitions you can find useful information on the
+    </span>
+    <a
+    href="https://vuejs.org/v2/guide/transitions.html"
+    target="_blank"
+    >
+    <span>
+      Vue.js 
+    </span>
+    </a>
+  </p>
+  <p style="font-size: 16px;">
+    There are available transitions below. To see an exact effect click on a button.
+  </p>
+</div>`;
+
 export default {
   title: "Utilities/Transitions",
   argTypes: {
@@ -16,6 +79,13 @@ export default {
       },
       table: {
         category: "Class",
+      },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: doscTransitions,
       },
     },
   },
