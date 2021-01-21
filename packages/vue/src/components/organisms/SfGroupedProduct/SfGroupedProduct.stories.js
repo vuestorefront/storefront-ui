4b0d1aadffece478adc01e52d04d60ab18960666
adampawlinski <apawlinski@vuestorefront.io>
2021-01-21 16:01:11 +0100

    fix: story products and style

--- packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.stories.js
+++ packages/vue/src/components/organisms/SfGroupedProduct/SfGroupedProduct.stories.js
@@ -16,6 +16,7 @@ export default {
         category: "Props",
       },
     },
+    input: { action: "Text area input typing", table: { category: "Events" } },
   },
 };
 
@@ -24,7 +25,40 @@ const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   data() {
     return {
-      productQty: 1,
+      products: [
+        {
+          productQty: 1,
+          title: "Leave white brooch",
+          priceRegular: "$10.99",
+          image: "/assets/storybook/SfGroupedProduct/product-white.png",
+          imageWidth: 146,
+          imageHeight: 200,
+        },
+        {
+          productQty: 1,
+          title: "Leave green brooch",
+          priceRegular: "$13.89",
+          image: "/assets/storybook/SfGroupedProduct/product-green.png",
+          imageWidth: 146,
+          imageHeight: 200,
+        },
+        {
+          productQty: 1,
+          title: "Leave black brooch",
+          priceRegular: "$9.99",
+          image: "/assets/storybook/SfGroupedProduct/product-black.png",
+          imageWidth: 146,
+          imageHeight: 200,
+        },
+        {
+          productQty: 1,
+          title: "Leave white brooch extra",
+          priceRegular: "$15.99",
+          image: "/assets/storybook/SfGroupedProduct/product-white.png",
+          imageWidth: 146,
+          imageHeight: 200,
+        },
+      ],
     };
   },
   template: `
@@ -34,78 +68,29 @@ const Template = (args, { argTypes }) => ({
     :style="{maxWidth: '1140px', margin: 'auto'}"
   >
     <SfGroupedProductItem
-      :qty="productQty"
-      @input="productQty = $event"
-      :image="image"
-      :image-width="imageWidth"
-      :image-height="imageHeight"
+      v-for="(item, i) in products"
+      :key="i"
+      v-model="item.productQty"
+      @input="input"
+      :image="item.image"
+      :image-width="item.imageWidth"
+      :image-height="item.imageHeight"
       :image-lazy="imageLazy"
-      :title="title"
-      :price-regular="priceRegular"
+      :title="item.title"
+      :price-regular="item.priceRegular"
     >
       <template #details>
         <div :style="{color: '#72757E', fontSize: 'var(--font-size--base'}">MSD23-345-324</div>
         <SfProperty name="Color" value="White" :style="{margin: 'auto 0 0 0', width: '110px'}" />
       </template>
-    </SfGroupedProductItem>
-    <SfGroupedProductItem
-      :qty="productQty"
-      @input="productQty = $event"
-      :image="image"
-      :image-width="imageWidth"
-      :image-height="imageHeight"
-      :image-lazy="imageLazy"
-      :title="title"
-      :price-regular="priceRegular"
-    >
-      <template #details>
-        <div :style="{color: '#72757E', fontSize: 'var(--font-size--base'}">MSD23-345-324</div>
-        <SfProperty name="Color" value="White" :style="{margin: 'auto 0 0 0', width: '110px'}" />
-      </template>
-    </SfGroupedProductItem>
-    <SfGroupedProductItem
-      :qty="productQty"
-      @input="productQty = $event"
-      :image="image"
-      :image-width="imageWidth"
-      :image-height="imageHeight"
-      :image-lazy="imageLazy"
-      :title="title"
-      :price-regular="priceRegular"
-    >
-      <template #details>
-        <div :style="{color: '#72757E', fontSize: 'var(--font-size--base'}">MSD23-345-324</div>
-        <SfProperty name="Color" value="White" :style="{margin: 'auto 0 0 0', width: '110px'}" />
-      </template>
-    </SfGroupedProductItem>
-    <SfGroupedProductItem
-      :qty="productQty"
-      @input="productQty = $event"
-      :image="image"
-      :image-width="imageWidth"
-      :image-height="imageHeight"
-      :image-lazy="imageLazy"
-      :title="title"
-      :price-regular="priceRegular"
-    >
-      <template #details>
-        <div :style="{color: '#72757E', fontSize: 'var(--font-size--base'}">MSD23-345-324</div>
-        <SfProperty name="Color" value="White" :style="{margin: 'auto 0 0 0', width: '110px'}" />
-      </template>
-    </SfGroupedProductItem>
+    </SfGroupedProductItem>    
   </SfGroupedProduct>`,
 });
 
 export const Common = Template.bind({});
 Common.args = {
   settings: { type: "slider" },
-  image: "/assets/storybook/SfGroupedProduct/product-white.png",
-  imageWidth: 146,
-  imageHeight: 200,
   imageLazy: true,
-  title: "Leave white brooch",
-  priceRegular: "$10.99",
-  hasCarousel: false,
 };
 
 export const UseConfigurationSlot = (args, { argTypes }) => ({
