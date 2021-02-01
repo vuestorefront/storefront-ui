54bf4fb509835a90a35229f48e505c93054f9678
AdamPawlinski <apawlinski@vuestorefront.io>
2021-02-01 15:38:22 +0100

    fix: upgrade story for range

--- packages/vue/src/components/molecules/SfRange/SfRange.stories.js
+++ packages/vue/src/components/molecules/SfRange/SfRange.stories.js
@@ -1,117 +1,114 @@
-import { storiesOf } from "@storybook/vue";
-import {
-  withKnobs,
-  boolean,
-  object,
-} from "@storybook/addon-knobs";
-import SfRange from "./SfRange.vue";
-storiesOf("Molecules|Range", module)
-  .addDecorator(withKnobs)
-  .add("Common", () => ({
-    props: {
-      disabled: {
-        default: boolean("disabled", false, "Props"),
+import { SfRadio } from "@storefront-ui/vue";
+export default {
+  title: "Components/Molecules/Radio",
+  component: SfRadio,
+  argTypes: {
+    value: {
+      control: [Number, Array],
+      table: {
+        category: "Props",
       },
-      config: {
-        default: object("config",
-          {
-            start: [0, 1],
-            range: {
-              min: 0,
-              max: 10
-            },
-            step: 1,
-            connect: true,
-            direction: "ltr",
-            orientation: "horizontal",
-            behaviour: "tap-drag",
-            tooltips: true,
-            format: {
-              to: function (value) {
-                return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(value)
-              },
-              from: function (value) {
-                const parsedValue = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).formatToParts(value)
-                return parsedValue[0].value
-              }
-            },
-            keyboardSupport: true,
+      defaultValue: {
+        default: () => [0, 1],
+      },
+      description: "Disabled",
+    },
+    disabled: {
+      control: "boolean",
+      table: {
+        category: "Props",
+      },
+      defaultValue: {
+        summary: false,
+      },
+      description: "Disabled",
+    },
+    config: {
+      control: "object",
+      table: {
+        category: "Props",
+      },
+      defaultValue: {
+        start: [0, 1],
+        range: {
+          min: 0,
+          max: 10,
+        },
+        step: 1,
+        connect: true,
+        direction: "ltr",
+        orientation: "horizontal",
+        behaviour: "tap-drag",
+        tooltips: true,
+        format: {
+          to: function (value) {
+            return new Intl.NumberFormat("de-DE", {
+              style: "currency",
+              currency: "EUR",
+            }).format(value);
           },
-          "Props"),
-      }
-    },
-    data() {
-      return {
-        value: [0, 1],
-      }
-    },
-    methods: {
-      alert(value) {
-        console.log("slider current value:", value);
-      },
-    },
-    components: { SfRange },
-    template:
-      `<div style="padding: 100px">
-			<SfRange
-        v-model="value"
-        :disabled="disabled"
-        :config="config"        
-				@change="alert"
-			/>  
-		</div>`,
-  }))
-  .add("One slider", () => ({
-    props: {
-      disabled: {
-        default: boolean("disabled", false, "Props"),
-      },
-      config: {
-        default: object("config",
-          {
-            start: 0,
-            range: {
-              min: 0,
-              max: 10
-            },
-            step: 1,
-            connect: true,
-            direction: "ltr",
-            orientation: "horizontal",
-            behaviour: "tap-drag",
-            tooltips: true,
-            format: {
-              to: function (value) {
-                return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(value)
-              },
-              from: function (value) {
-                const parsedValue = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).formatToParts(value)
-                return parsedValue[0].value
-              }
-            },
-            keyboardSupport: true,
+          from: function (value) {
+            const parsedValue = new Intl.NumberFormat("de-DE", {
+              style: "currency",
+              currency: "EUR",
+            }).formatToParts(value);
+            return parsedValue[0].value;
           },
-          "Props"),
-      }
+        },
+        keyboardSupport: true,
+      },
+      description: "Configuration",
     },
-    data() {
-      return {
-        value: 0,
-      }
+    input: { action: "Toggle selection", table: { category: "Events" } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { SfRadio },
+  props: Object.keys(argTypes),
+  template: `
+  <div style="padding: 100px">
+    <SfRange
+      v-model="value"
+      :disabled="disabled"
+      :config="config"        
+      @change="alert"
+    />  
+  </div>`,
+});
+
+export const Common = Template.bind({});
+
+export const OneSlider = Template.bind({});
+OneSlider.args = {
+  value: 0,
+  config: {
+    start: 0,
+    range: {
+      min: 0,
+      max: 10,
     },
-    methods: {
-      alertValues(value) {
-        console.log("slider current value:", value);
+    step: 1,
+    connect: true,
+    direction: "ltr",
+    orientation: "horizontal",
+    behaviour: "tap-drag",
+    tooltips: true,
+    format: {
+      to: function (value) {
+        return new Intl.NumberFormat("de-DE", {
+          style: "currency",
+          currency: "EUR",
+        }).format(value);
+      },
+      from: function (value) {
+        const parsedValue = new Intl.NumberFormat("de-DE", {
+          style: "currency",
+          currency: "EUR",
+        }).formatToParts(value);
+        return parsedValue[0].value;
       },
     },
-    components: { SfRange },
-    template:
-      `<div style="padding: 100px">
-			<SfRange
-				v-model="value"
-				:disabled="disabled"
-				:config="config"
-        @change="alertValues"
-      />
-		</div>`,
-  }));
+    keyboardSupport: true,
+  },
+};
