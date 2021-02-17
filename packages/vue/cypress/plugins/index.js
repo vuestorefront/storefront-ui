4c1c932bf4ae26e8742940b49707a46264ed3686
AdamPawlinski <apawlinski@vuestorefront.io>
2021-02-17 15:52:29 +0100

    feat: cypress config and add first test

--- packages/vue/cypress/plugins/index.js
+++ packages/vue/cypress/plugins/index.js
@@ -15,7 +15,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
