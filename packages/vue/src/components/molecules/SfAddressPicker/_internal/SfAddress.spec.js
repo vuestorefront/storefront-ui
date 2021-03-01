636d58f010fa9bf1bd372e6998926857b6ade847
AdamPawlinski <apawlinski@vuestorefront.io>
2021-03-01 23:14:24 +0100

    fix: unit test warnings

--- packages/vue/src/components/molecules/SfAddressPicker/_internal/SfAddress.spec.js
+++ packages/vue/src/components/molecules/SfAddressPicker/_internal/SfAddress.spec.js
@@ -5,14 +5,10 @@ import SfAddressPicker from "../SfAddressPicker.vue";
 describe("SfAddress.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfAddress, {
-      parentComponent: SfAddressPicker,
-      inject: ["getSelectedValue", "setSelectedValue"],
-      provide() {
-        return {
-          getSelectedValue: this.getSelectedValue,
-          setSelectedValue: this.setSelectedValue,
-        };
-      },
+      provide: {
+        getSelectedValue: () => "example",
+        setSelectedValue: () => "example",
+      }
     });
     expect(component.classes("sf-address")).toBe(true);
   });
