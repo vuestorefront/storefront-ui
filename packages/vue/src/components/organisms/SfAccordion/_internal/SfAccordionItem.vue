d5290d6a562052976943172fe86262e4c4caf990
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-03-18 10:06:21 +0100

    feat: #1668 refactor Accordion to work without JS

--- packages/vue/src/components/organisms/SfAccordion/_internal/SfAccordionItem.vue
+++ packages/vue/src/components/organisms/SfAccordion/_internal/SfAccordionItem.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="sf-accordion-item">
+    <div class="accordion__single">
+      <input
+        :id="header"
+        class="accordion__single__hidden"
+        type="checkbox"
+        autocomplete="off"
+        checked
+      />
+      <label
+        class="accordion__single__question sf-accordion-item__header"
+        :for="header"
+        :aria-pressed="isOpen.toString()"
+        :aria-expanded="isOpen.toString()"
+        @click="accordionClick"
+      >
+        <span>{{ header }}</span>
+        <!-- @slot here you can add additional information about this item -->
+        <slot name="additional-info" />
+        <SfChevron tabindex="0" class="sf-accordion-item__chevron" />
+      </label>
+      <div class="accordion__single__answer sf-accordion-item__content">
+        <!-- @slot -->
+        <slot />
+      </div>
+    </div>
     <!-- @slot -->
-    <slot
+    <!-- <slot
       name="header"
       v-bind="{
         header,
@@ -16,39 +41,35 @@
         :class="{ 'is-open': isOpen }"
         class="sf-button--pure sf-accordion-item__header"
         @click="accordionClick"
-      >
-        {{ header }}
-        <!-- @slot here you can add additional information about this item -->
-        <slot name="additional-info" />
+      > -->
+    <!-- {{ header }} -->
+    <!-- @slot here you can add additional information about this item -->
+    <!-- <slot name="additional-info" />
         <SfChevron
           tabindex="0"
           class="sf-accordion-item__chevron"
           :class="{ 'sf-chevron--right': !isOpen }"
         />
-      </SfButton>
-    </slot>
-    <SfExpand :transition="$parent.transition">
-      <div v-if="isOpen">
-        <div class="sf-accordion-item__content">
-          <!-- @slot -->
-          <slot />
+      </SfButton> -->
+    <!-- </slot> -->
+    <!-- <SfExpand :transition="$parent.transition"> -->
+    <!-- <div v-if="isOpen">
+        <div class="sf-accordion-item__content"> -->
+    <!-- @slot -->
+    <!-- <slot />
         </div>
-      </div>
-    </SfExpand>
+      </div> -->
+    <!-- </SfExpand> -->
   </div>
 </template>
 <script>
 import { focus } from "../../../../utilities/directives";
-import SfExpand from "../../../../utilities/transitions/component/SfExpand";
 import SfChevron from "../../../atoms/SfChevron/SfChevron.vue";
-import SfButton from "../../../atoms/SfButton/SfButton.vue";
 export default {
   name: "SfAccordionItem",
   directives: { focus },
   components: {
     SfChevron,
-    SfButton,
-    SfExpand,
   },
   props: {
     header: {
