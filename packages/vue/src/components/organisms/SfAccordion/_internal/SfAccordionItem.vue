235637ebd408f4154c434a506180a5b1d90480c2
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-03-18 11:28:50 +0100

    feat: #1668 remove unused code

--- packages/vue/src/components/organisms/SfAccordion/_internal/SfAccordionItem.vue
+++ packages/vue/src/components/organisms/SfAccordion/_internal/SfAccordionItem.vue
@@ -1,73 +1,34 @@
 <template>
   <div class="sf-accordion-item">
-    <div class="accordion__single">
-      <input
-        :id="header"
-        class="accordion__single__hidden"
-        type="checkbox"
-        autocomplete="off"
-        checked
-      />
-      <label
-        class="accordion__single__question sf-accordion-item__header"
-        :for="header"
-        :aria-pressed="isOpen.toString()"
-        :aria-expanded="isOpen.toString()"
-        @click="accordionClick"
-      >
-        <span>{{ header }}</span>
-        <!-- @slot here you can add additional information about this item -->
-        <slot name="additional-info" />
-        <SfChevron tabindex="0" class="sf-accordion-item__chevron" />
-      </label>
-      <div class="accordion__single__answer sf-accordion-item__content">
-        <!-- @slot -->
-        <slot />
-      </div>
-    </div>
-    <!-- @slot -->
-    <!-- <slot
-      name="header"
-      v-bind="{
-        header,
-        isOpen,
-        accordionClick,
-        showChevron: $parent.showChevron,
-      }"
+    <input
+      :id="header"
+      class="sf-accordion-item__hidden"
+      type="checkbox"
+      autocomplete="off"
+      checked
+    />
+    <label
+      class="sf-accordion-item__header"
+      :for="header"
+      :aria-pressed="isOpen.toString()"
+      :aria-expanded="isOpen.toString()"
+      @click="accordionClick"
     >
-      <SfButton
-        :aria-pressed="isOpen.toString()"
-        :aria-expanded="isOpen.toString()"
-        :class="{ 'is-open': isOpen }"
-        class="sf-button--pure sf-accordion-item__header"
-        @click="accordionClick"
-      > -->
-    <!-- {{ header }} -->
-    <!-- @slot here you can add additional information about this item -->
-    <!-- <slot name="additional-info" />
-        <SfChevron
-          tabindex="0"
-          class="sf-accordion-item__chevron"
-          :class="{ 'sf-chevron--right': !isOpen }"
-        />
-      </SfButton> -->
-    <!-- </slot> -->
-    <!-- <SfExpand :transition="$parent.transition"> -->
-    <!-- <div v-if="isOpen">
-        <div class="sf-accordion-item__content"> -->
-    <!-- @slot -->
-    <!-- <slot />
-        </div>
-      </div> -->
-    <!-- </SfExpand> -->
+      <span>{{ header }}</span>
+      <!-- @slot here you can add additional information about this item -->
+      <slot name="additional-info" />
+      <SfChevron tabindex="0" class="sf-accordion-item__chevron" />
+    </label>
+    <div class="sf-accordion-item__content">
+      <!-- @slot -->
+      <slot />
+    </div>
   </div>
 </template>
 <script>
-import { focus } from "../../../../utilities/directives";
 import SfChevron from "../../../atoms/SfChevron/SfChevron.vue";
 export default {
   name: "SfAccordionItem",
-  directives: { focus },
   components: {
     SfChevron,
   },
