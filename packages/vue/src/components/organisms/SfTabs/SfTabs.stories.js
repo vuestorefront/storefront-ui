e5e4dd3ebaf8050a2448e3908453e5e48b81082c
Justyna <46591755+justyna-13@users.noreply.github.com>
2021-03-22 10:43:03 +0100

    feat: open tabs not only by clicking on SfTab (#1743)

--- packages/vue/src/components/organisms/SfTabs/SfTabs.stories.js
+++ packages/vue/src/components/organisms/SfTabs/SfTabs.stories.js
@@ -1,4 +1,4 @@
-import { SfTabs } from "@storefront-ui/vue";
+import { SfTabs, SfButton } from "@storefront-ui/vue";
 export default {
   title: "Components/Organisms/Tabs",
   component: SfTabs,
@@ -26,6 +26,12 @@ export default {
       table: {
         category: "Props",
       },
+    },
+    "click:tab": {
+      action: "Tab clicked",
+      table: { category: "Events" },
+      description:
+        "This event can be used on SfTabs when there is a need to change tabs in other way than clicking on a particular SfTab. For example by clicking on a button - this example can be found below in a story called `Change Tabs With Buttons`",
     },
   },
 };
@@ -60,6 +66,7 @@ const Template = (args, { argTypes }) => ({
     :tabMaxContentHeight="tabMaxContentHeight"
     :tabShowText="tabShowText"
     :tabHideText="tabHideText"
+    @click:tab="this['click:tab']"
   >
     <SfTab 
         v-for="tab in tabs" 
@@ -76,3 +83,55 @@ Common.args = {
   openTab: 1,
   tabMaxContentHeight: "6.25rem",
 };
+
+export const changeTabsWithButtons = (args, { argTypes }) => ({
+  components: { SfTabs, SfButton },
+  props: Object.keys(argTypes),
+  data() {
+    return {
+      open: 1,
+      tabs: [
+        {
+          title: "Description",
+          content:
+            "The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses. The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses. The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses. The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses. The Karissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses.",
+        },
+        {
+          title: "Read reviews",
+          content:
+            "The Larissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses.",
+        },
+        {
+          title: "Additional Information",
+          content:
+            "The Marissa V-Neck Tee features a semi-fitted shape that's flattering for every figure. You can hit the gym with confidence while it hugs curves and hides common 'problem' areas. Find stunning women's cocktail dresses and party dresses.",
+        },
+      ],
+    };
+  },
+  methods: {
+    change(tabNumber) {
+      this.open = tabNumber;
+    },
+  },
+  template: `<div>
+  <SfButton style="display: inline; margin: var(--spacer-xs)" @click="change(1)">open tab 1</SfButton>
+  <SfButton style="display: inline; margin: var(--spacer-xs)" @click="change(2)">open tab 2</SfButton>
+  <SfButton style="display: inline; margin: var(--spacer-xs)" @click="change(3)">open tab 3</SfButton>
+  <SfTabs
+    :open-tab="open"
+    :tabMaxContentHeight="tabMaxContentHeight"
+    :tabShowText="tabShowText"
+    :tabHideText="tabHideText"
+    @click:tab="change"
+  >
+    <SfTab
+        v-for="tab in tabs"
+        :key="tab.title"
+        :title="tab.title"
+    >
+        {{tab.content}}
+    </SfTab>
+  </SfTabs></div>`,
+});
+changeTabsWithButtons.args = { ...Common.args };
--- packages/vue/src/components/organisms/SfTabs/SfTabs.vue
+++ packages/vue/src/components/organisms/SfTabs/SfTabs.vue
@@ -12,7 +12,7 @@ Vue.component("SfTab", SfTab);
 export default {
   name: "SfTabs",
   props: {
-    /** Which tab should be open at the beginning  */
+    /** Which tab should be open  */
     openTab: {
       type: Number,
       default: 1,
@@ -38,6 +38,12 @@ export default {
       initialTabActivated: false,
     };
   },
+  watch: {
+    openTab(newValue, oldValue) {
+      if (newValue === oldValue) return;
+      this.toggle(this.$children[newValue - 1]._uid);
+    },
+  },
   mounted() {
     this.$on("toggle", this.toggle);
     if (this.openTab) this.openChild();
@@ -45,10 +51,11 @@ export default {
   methods: {
     toggle(id) {
       this.$children.forEach((child) => {
-        child._uid === id
-          ? (child.isActive = !child.isActive)
-          : (child.isActive = false);
+        child.isActive = child._uid === id;
       });
+      const activeTab =
+        this.$children.findIndex((child) => child.isActive === true) + 1;
+      this.$emit("click:tab", activeTab);
     },
     openChild() {
       if (this.openTab < this.$children.length + 1) {
