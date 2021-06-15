3fd1622e09a34a636486af69a4bfb8d86e522380
Niklas Wolf <wolfniklas94@web.de>
2021-06-15 09:28:34 +0200

    fix: respect size of options list set as a prop on SfComponentSelect

--- packages/vue/src/components/molecules/SfComponentSelect/SfComponentSelect.vue
+++ packages/vue/src/components/molecules/SfComponentSelect/SfComponentSelect.vue
@@ -182,7 +182,7 @@ export default {
     },
     maxHeight() {
       if (!this.options.length) return;
-      return `${this.optionHeight * this.options.length}px`;
+      return `${this.optionHeight * (this.size + 0.5)}px`;
     },
     isActive() {
       return this.open;
