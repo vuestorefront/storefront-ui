b02b4e8c085c2532b940373c0f8f49c0d6bcccba
AdamPawlinski <apawlinski@vuestorefront.io>
2021-06-17 11:31:50 +0200

    feat: add cart, category and product pages, fix package json

---
+++ packages/nuxt/pages/Cart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Cart />
+  </div>
+</template>
+
+<script>
+  import Cart from "@storefront-ui/vue/src/examples/cart/Cart.vue"
+  export default {
+    components: {
+      Cart
+    }
+  }
+</script>
