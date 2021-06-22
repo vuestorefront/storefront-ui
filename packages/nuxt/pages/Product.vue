41e1c85fea8645f79fb9834626bc5c74ff5ad193
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-06-22 16:27:59 +0200

    feat: merge lighthouse

---
+++ packages/nuxt/pages/Product.vue
@@ -0,0 +1,12 @@
+<template>
+  <Product />
+</template>
+
+<script>
+import Product from "@storefront-ui/vue/src/examples/pages/product/Product";
+export default {
+  components: {
+    Product
+  }
+};
+</script>
