5aca6e3eb25b9c5a1ccf9c3e1806a39d063efb4b
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-06-23 15:52:03 +0200

    revert sfsidebar without js (#1825)

--- packages/vue/src/components/organisms/SfSidebar/SfSidebar.vue
+++ packages/vue/src/components/organisms/SfSidebar/SfSidebar.vue
@@ -1,12 +1,9 @@
 <template>
-  <div
-    class="sf-sidebar"
-    :class="{ staticClass, className, jsEnabled: jsEnabled }"
-  >
+  <div class="sf-sidebar" :class="[staticClass, className]">
     <SfOverlay :visible="visibleOverlay" />
     <transition :name="transitionName">
       <aside
-        v-if="isMounted ? visible : true"
+        v-if="visible"
         ref="asideContent"
         v-focus-trap
         v-click-outside="checkPersistence"
@@ -14,25 +11,17 @@
       >
         <!--@slot Use this slot to place content inside the modal bar.-->
         <slot name="bar">
-          <SfBar :title="title" class="smartphone-only" @click:back="close">
-            <template #back>
-              <SfButton
-                :link="jsEnabled ? null : '#'"
-                aria-label="back"
-                class="sf-sidebar__bar-button sf-button--pure sf-bar__icon"
-                type="button"
-                @click="close"
-              >
-                <SfIcon icon="chevron_left" size="0.875rem" />
-              </SfButton>
-            </template>
-          </SfBar>
+          <SfBar
+            :title="title"
+            class="smartphone-only"
+            :back="true"
+            @click:back="close"
+          />
         </slot>
         <!--@slot Use this slot to replace close icon.-->
         <slot name="circle-icon" v-bind="{ close, button }">
           <SfCircleIcon
             v-if="button"
-            :link="jsEnabled ? null : '#'"
             icon-size="12px"
             aria-label="Close sidebar"
             icon="cross"
@@ -79,9 +68,6 @@ import SfBar from "../../molecules/SfBar/SfBar.vue";
 import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
 import SfOverlay from "../../atoms/SfOverlay/SfOverlay.vue";
 import SfHeading from "../../atoms/SfHeading/SfHeading.vue";
-import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
-import SfButton from "../../atoms/SfButton/SfButton.vue";
-
 export default {
   name: "SfSidebar",
   directives: { focusTrap, clickOutside },
@@ -90,8 +76,6 @@ export default {
     SfCircleIcon,
     SfOverlay,
     SfHeading,
-    SfIcon,
-    SfButton,
   },
   props: {
     /**
@@ -149,8 +133,6 @@ export default {
       position: "left",
       staticClass: null,
       className: null,
-      jsEnabled: false,
-      isMounted: false,
     };
   },
   computed: {
@@ -184,22 +166,13 @@ export default {
       immediate: true,
     },
   },
-  beforeMount() {
-    this.isMounted = true;
-  },
   mounted() {
     this.classHandler();
-    this.$nextTick(() => {
-      return (this.jsEnabled = true);
-    });
   },
   updated() {
     this.classHandler();
   },
   methods: {
-    isVisible() {
-      if (typeof this.jsEnabled === undefined) return false;
-    },
     close() {
       this.$emit("close");
     },
