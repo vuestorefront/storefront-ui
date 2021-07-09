9d59eb3f31d86759a9496e02589d9d49e0086320
Justyna <jgieracka@vuestorefront.io>
2021-07-09 08:17:38 +0200

    fix: static

--- packages/nuxt/cypress/integration/static.spec.js
+++ packages/nuxt/cypress/integration/static.spec.js
@@ -2,32 +2,32 @@ describe("Static page", () => {
   it('successfully loads', () => {
     cy.visit('/static')
   })
-	it("test SfTabs", () => {
-		cy.get(".sf-tabs__tab")
-			.its('length')
-			.should('eq', 3)
-			.get(".sf-tabs__tab")
-			.first()
+  it("test SfTabs", () => {
+    cy.get(".sf-tabs__tab")
+      .its('length')
+      .should('eq', 3)
+      .get(".sf-tabs__tab")
+      .first()
       .find("button")
-			.contains('Security')
-			.get(".sf-tabs__tab")
-			.eq(1)
-      .find("button")
-			.click({force: true})
-			.get(".sf-tabs__content__tab")
-			.contains("This privacy policy")
-	});
-	it("test Content pages list", () => {
-		cy.get(".sf-content-pages__list-item")
-			.its('length')
-			.should('eq', 7)
-			.get(".sf-content-pages__list-item")
+      .contains('Security')
+      .get(".sf-tabs__tab")
       .eq(1)
-			.click()
-			.get(".sf-tabs__tab")
       .find("button")
-			.first()
-			.contains('Size guide')
+      .click({force: true})
+      .get(".sf-tabs__content__tab")
+      .contains("This privacy policy")
+  });
+  it("test Content pages list", () => {
+    cy.get(".sf-content-pages__list-item")
+      .its('length')
+      .should('eq', 7)
+      .get(".sf-content-pages__list-item")
+      .eq(1)
+      .click()
+      .get(".sf-tabs__tab")
+      .find("button")
+      .first()
+      .contains('Size guide')
       .and("have.class", "is-active")
-	});
+  });
 });
