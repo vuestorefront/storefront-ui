60b4136739d4ebfe6fc42a8f6419ddf4ae12baad
Jakub Andrzejewski <37120330+Baroshem@users.noreply.github.com>
2021-08-17 12:53:19 +0200

    Revert "feat: Implement SfColorPicker in SfProductCard"

--- packages/vue/src/components/organisms/SfProductCard/SfProductCard.vue
+++ packages/vue/src/components/organisms/SfProductCard/SfProductCard.vue
@@ -32,24 +32,6 @@
           />
         </SfButton>
       </slot>
-      <slot name="colors" v-bind="{ colors }">
-        <SfColorPicker
-          v-if="colors.length"
-          class="sf-product-card__colors"
-          label="Choose color"
-          :is-open="!isMobile || openColorPicker"
-          @click:toggle="toggleColorPicker"
-        >
-          <SfColor
-            v-for="(color, i) in colors"
-            :key="color.value"
-            :color="color.color"
-            :selected="color.selected"
-            class="sf-product-card__color"
-            @click="handleSelectedColor(i)"
-          />
-        </SfColorPicker>
-      </slot>
       <slot name="badge" v-bind="{ badgeLabel, badgeColor }">
         <SfBadge
           v-if="badgeLabel"
@@ -173,13 +155,6 @@ import SfImage from "../../atoms/SfImage/SfImage.vue";
 import SfCircleIcon from "../../atoms/SfCircleIcon/SfCircleIcon.vue";
 import SfBadge from "../../atoms/SfBadge/SfBadge.vue";
 import SfButton from "../../atoms/SfButton/SfButton.vue";
-import SfColorPicker from "../../molecules/SfColorPicker/SfColorPicker.vue";
-import SfColor from "../../atoms/SfColor/SfColor.vue";
-import {
-  mapMobileObserver,
-  unMapMobileObserver,
-} from "../../../utilities/mobile-observer";
-
 export default {
   name: "SfProductCard",
   components: {
@@ -190,8 +165,6 @@ export default {
     SfCircleIcon,
     SfBadge,
     SfButton,
-    SfColorPicker,
-    SfColor,
   },
   props: {
     /**
@@ -231,14 +204,6 @@ export default {
     badgeColor: {
       type: String,
       default: "",
-    },
-    /**
-     * Product colors
-     * It should be an array of objects
-     */
-    colors: {
-      type: Array,
-      default: () => [],
     },
     /**
      * Product title
@@ -347,11 +312,9 @@ export default {
   data() {
     return {
       isAddingToCart: false,
-      openColorPicker: false,
     };
   },
   computed: {
-    ...mapMobileObserver(),
     isSFColors() {
       return SF_COLORS.includes(this.badgeColor.trim());
     },
@@ -372,22 +335,7 @@ export default {
       return `${defaultClass} ${this.isInWishlist ? "on-wishlist" : ""}`;
     },
   },
-  beforeDestroy() {
-    unMapMobileObserver();
-  },
   methods: {
-    handleSelectedColor(colorIndex) {
-      if (this.colors.length > 0) {
-        this.colors.map((color, i) => {
-          if (colorIndex === i) {
-            this.$emit("click:colors", color);
-          }
-        });
-      }
-    },
-    toggleColorPicker() {
-      this.openColorPicker = !this.openColorPicker;
-    },
     toggleIsInWishlist() {
       this.$emit("click:wishlist", !this.isInWishlist);
     },
