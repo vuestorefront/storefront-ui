b73a7e0c8705c29caa3b4391d38402b09e62f373
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-20 14:47:01 +0200

    fix: access to quantity

--- packages/vue/src/components/organisms/SfProductCardHorizontal/SfProductCardHorizontal.stories.js
+++ packages/vue/src/components/organisms/SfProductCardHorizontal/SfProductCardHorizontal.stories.js
@@ -196,7 +196,7 @@ const Template = (args, { argTypes }) => ({
         <SfButton
           class="sf-button--text desktop-only"
           @click="$emit('click:add-to-wishlist')"
-          style="margin: 0 0 1rem auto; display: block" 
+          style="margin: 0 0 1rem auto; display: block"
         >
           Save for later
         </SfButton>
