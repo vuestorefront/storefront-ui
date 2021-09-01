d866c14c36c3b069c3e1712f584171828635118f
Abhishek Jha <j7.abhi@gmail.com>
2021-09-01 19:14:08 +0530

    chore: reset playground.vue file

--- packages/vue/src/Playground.vue
+++ packages/vue/src/Playground.vue
@@ -1,45 +1,8 @@
 <template>
-  <div id="playground">
-    <sf-price regular="300.00" special="$200.00"></sf-price>
-    <SfOverlay :visible="true" />
-    <hr />
-    {{ fname }}
-    <sf-loader :loading="true">
-      <!-- <template #loader>Loading...</template> -->
-    </sf-loader>
-    <hr />
-    <SfProperty name="Material" value="Cotton" />
-  </div>
+  <div id="playground"></div>
 </template>
 
 <script>
 // Use this component to play with other components
-import { configureCompat } from "vue";
-import SfPrice from "../src/components/atoms/SfPrice/SfPrice.vue";
-import SfOverlay from "../src/components/atoms/SfOverlay/SfOverlay.vue";
-import SfLoader from "../src/components/atoms/SfLoader/SfLoader.vue";
-import SfProperty from "../src/components/atoms/SfProperty/SfProperty.vue";
-
-export default {
-  compatConfig: {
-    // COMPONENT_ASYNC: false, // features can also be toggled at component level
-    // COMPONENT_V_MODEL: false,
-  },
-  components: {
-    SfPrice,
-    SfOverlay,
-    SfLoader,
-    SfProperty,
-  },
-  data: function () {
-    return {
-      publicId: "sheep",
-      cloud: {
-        cloudName: "demo",
-      },
-      alt: "example",
-      fname: "Batman",
-    };
-  },
-};
+export default {};
 </script>
