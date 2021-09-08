36bd0da0b34eaba7a428741159577af29ac73675
AdamPawlinski <apawlinski@vuestorefront.io>
2021-09-08 21:08:35 +0200

    feat: sfordersummary story, test and md

---
+++ packages/vue/src/components/templates/SfOrderSummary/SfOrderSummary.stories.js
@@ -0,0 +1,192 @@
+import { SfOrderSummary } from "@storefront-ui/vue";
+
+const orderData = {
+  review: {
+    subtotal: "$150.00",
+    shipping: "$9.00",
+    total: "$159.00",
+  },
+  shipping: {
+    shippingMethod: {
+      isOpen: false,
+      price: "Free",
+      delivery: "Delivery from 3 to 7 business days",
+      label: "Pickup in the store",
+      value: "store",
+      description:
+        "Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted.",
+    },
+  },
+  products: [
+    {
+      title: "Cream Beach Bag",
+      image: "/assets/storybook/Home/productA.jpg",
+      price: { regular: "$50.00" },
+      configuration: [
+        { name: "Size", value: "XS" },
+        { name: "Color", value: "White" },
+      ],
+      qty: 1,
+      sku: "MSD23-345-324",
+    },
+    {
+      title: "Vila stripe maxi dress",
+      image: "/assets/storybook/Home/productB.jpg",
+      price: { regular: "$50.00", special: "$20.05" },
+      configuration: [
+        { name: "Size", value: "XS" },
+        { name: "Color", value: "White" },
+      ],
+      qty: 2,
+      sku: "MSD23-345-325",
+    },
+  ],
+};
+
+const paymentMethodsData = [
+  {
+    label: "Visa Debit",
+    value: "debit",
+  },
+  {
+    label: "MasterCard",
+    value: "mastercard",
+  },
+  {
+    label: "Visa Electron",
+    value: "electron",
+  },
+  {
+    label: "Cash on delivery",
+    value: "cash",
+  },
+  {
+    label: "Check",
+    value: "check",
+  },
+];
+
+const shippingMethodsData = [
+  {
+    isOpen: false,
+    price: "Free",
+    delivery: "Delivery from 3 to 7 business days",
+    label: "Pickup in the store",
+    value: "store",
+    description:
+      "Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted.",
+  },
+  {
+    isOpen: false,
+    price: "$9.90",
+    delivery: "Delivery from 4 to 6 business days",
+    label: "Delivery to home",
+    value: "home",
+    description:
+      "Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted.",
+  },
+  {
+    isOpen: false,
+    price: "$9.90",
+    delivery: "Delivery from 4 to 6 business days",
+    label: "Paczkomaty InPost",
+    value: "inpost",
+    description:
+      "Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted.",
+  },
+  {
+    isOpen: false,
+    price: "$11.00",
+    delivery: "Delivery within 48 hours",
+    label: "48 hours coffee",
+    value: "coffee",
+    description:
+      "Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted.",
+  },
+  {
+    isOpen: false,
+    price: "$14.00",
+    delivery: "Delivery within 24 hours",
+    label: "Urgent 24h",
+    value: "urgent",
+    description:
+      "Novelty! From now on you have the option of picking up an order in the selected InPack parceled. Just remember that in the case of orders paid on delivery, only the card payment will be accepted.",
+  },
+];
+
+const characteristicsData = [
+  {
+    title: "Safety",
+    description: "It carefully packaged with a personal touch",
+    icon: "safety",
+  },
+  {
+    title: "Easy shipping",
+    description: "You’ll receive dispatch confirmation and an arrival date",
+    icon: "shipping",
+  },
+  {
+    title: "Changed your mind?",
+    description: "Rest assured, we offer free returns within 30 days",
+    icon: "return",
+  },
+];
+
+export default {
+  title: "Components/Templates/OrderSummary",
+  component: SfOrderSummary,
+  argTypes: {
+    order: {
+      control: "object",
+      table: {
+        category: "Props",
+      },
+      defaultValue: orderData,
+    },
+    shippingMethods: {
+      control: "Array",
+      table: {
+        category: "Props",
+      },
+      defaultValue: shippingMethodsData,
+    },
+    paymentMethods: {
+      control: "Array",
+      table: {
+        category: "Props",
+      },
+      defaultValue: paymentMethodsData,
+    },
+    characteristics: {
+      control: "Array",
+      table: {
+        category: "Props",
+      },
+      defaultValue: characteristicsData,
+    },
+    buttonName: {
+      control: "String",
+      table: {
+        category: "Props",
+      },
+      defaultValue: "Place my order",
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { SfOrderSummary },
+  template: `
+  <SfOrderSummary 
+    :order="order"
+    :shipping-methods="shippingMethods"
+    :payment-methods="paymentMethods"
+    :characteristics="characteristics"
+    :button-name="buttonName"
+  >
+  </SfOrderSummary>`,
+});
+
+export const Common = Template.bind({});
+Common.args = {};
