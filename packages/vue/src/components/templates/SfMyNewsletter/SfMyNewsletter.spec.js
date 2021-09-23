503c5c15ba5ddf4f4385cb831b54ce80867b522f
Justyna <jgieracka@vuestorefront.io>
2021-09-23 12:39:56 +0200

    fix: test newsletter

--- packages/vue/src/components/templates/SfMyNewsletter/SfMyNewsletter.spec.js
+++ packages/vue/src/components/templates/SfMyNewsletter/SfMyNewsletter.spec.js
@@ -3,6 +3,6 @@ import SfMyNewsletter from "./SfMyNewsletter.vue";
 describe("SfMyNewsletter.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfMyNewsletter);
-    expect(component.classes("sf-my-account-content")).toBe(true);
+    expect(component.classes("sf-my-newsletter")).toBe(true);
   });
 });
