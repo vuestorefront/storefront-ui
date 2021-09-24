a806b2d20e0154b4fe3af7f83f0af53eee58367c
AdamPawlinski <apawlinski@vuestorefront.io>
2021-09-24 09:24:56 +0200

    feat: confirm order styles

--- packages/vue/src/components/templates/SfConfirmOrder/SfConfirmOrder.vue
+++ packages/vue/src/components/templates/SfConfirmOrder/SfConfirmOrder.vue
@@ -4,82 +4,105 @@
       <SfHeading
         :title="orderTitle"
         :level="orderTitleLevel"
-        class="sf-heading--left sf-heading--no-underline title"
+        class="
+          sf-heading--left sf-heading--no-underline
+          sf-confirm-order__heading
+        "
       />
     </slot>
     <slot name="table">
-      <SfTable class="sf-table--bordered table">
-        <SfTableHeading class="table__row">
-          <SfTableHeader class="table__header table__image">Item</SfTableHeader>
+      <SfTable class="sf-table--bordered sf-confirm-order__table">
+        <SfTableHeading class="sf-confirm-order__table-row">
+          <SfTableHeader
+            class="sf-confirm-order__table-header sf-confirm-order__table-image"
+            >Item</SfTableHeader
+          >
           <SfTableHeader
             v-for="tableHeader in tableHeaders"
             :key="tableHeader"
-            class="table__header"
-            :class="{ table__description: tableHeader === 'Description' }"
+            class="sf-confirm-order__table-header"
+            :class="{
+              'sf-confirm-order__table-description':
+                tableHeader === 'Description',
+            }"
             >{{ tableHeader }}
           </SfTableHeader>
         </SfTableHeading>
         <SfTableRow
           v-for="(product, index) in orderItems"
           :key="index"
-          class="table__row"
+          class="sf-confirm-order__table-row"
           data-testid="product-table-row"
         >
-          <SfTableData class="table__image">
+          <SfTableData class="sf-confirm-order__table-image">
             <SfImage
               :src="product.image"
               :alt="product.title"
               data-testid="product-image-table-data"
             />
           </SfTableData>
-          <SfTableData class="table__data"
+          <SfTableData class="sf-confirm-order__table-data"
             >{{ product.configuration[0].value }}
           </SfTableData>
           <SfTableData
-            class="table__description"
+            class="sf-confirm-order__table-description"
             data-testid="product-description-table-data"
           >
-            <div class="product-title">{{ product.title }}</div>
-            <div class="product-sku">{{ product.sku }}</div>
+            <div class="sf-confirm-order__product-title">
+              {{ product.title }}
+            </div>
+            <div class="sf-confirm-order__product-sku">{{ product.sku }}</div>
           </SfTableData>
-          <SfTableData class="table__data">{{ product.qty }}</SfTableData>
+          <SfTableData class="sf-confirm-order__table-data">{{
+            product.qty
+          }}</SfTableData>
 
-          <SfTableData class="table__data"
+          <SfTableData class="sf-confirm-order__table-data"
             >{{ product.configuration[1].value }}
           </SfTableData>
-          <SfTableData class="table__data">
+          <SfTableData class="sf-confirm-order__table-data">
             <SfPrice
               :regular="product.price.regular"
               :special="product.price.special"
-              class="product-price"
+              class="sf-confirm-order__product-price"
             />
           </SfTableData>
         </SfTableRow>
       </SfTable>
     </slot>
-    <div class="totals">
-      <slot name="desktop-summary">
+    <div class="sf-confirm-order__totals">
+      <slot name="summary">
         <SfProperty
           name="Subtotal"
           :value="subtotal"
-          class="sf-property--full-width property property__subtotal"
+          class="
+            sf-property--full-width
+            sf-confirm-order__property sf-confirm-order__property-subtotal
+          "
         >
         </SfProperty>
         <SfProperty
           name="Shipping"
           :value="shippingMethod.price"
-          class="sf-property--full-width property"
+          class="sf-property--full-width sf-confirm-order__property"
         >
         </SfProperty>
-        <SfDivider class="divider" />
+        <SfDivider class="sf-confirm-order__divider" />
         <SfProperty
           name="Total price"
           :value="total"
-          class="sf-property--full-width sf-property--large property__total"
+          class="
+            sf-property--full-width sf-property--large
+            sf-confirm-order__property-total
+          "
         >
         </SfProperty>
         <slot name="checkbox">
-          <SfCheckbox v-model="terms" name="terms" class="totals__terms">
+          <SfCheckbox
+            v-model="terms"
+            name="terms"
+            class="sf-confirm-order__totals-terms"
+          >
             <template #label>
               <div class="sf-checkbox__label">
                 I agree to <SfLink href="#">Terms and conditions</SfLink>
@@ -171,149 +194,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-@import "~@storefront-ui/vue/styles";
-.title {
-  --heading-padding: var(--spacer-xl) 0 var(--spacer-base);
-  --heading-title-font-weight: var(--font-weight--bold);
-  @include for-desktop {
-    --heading-title-font-size: var(--h3-font-size);
-    --heading-title-font-weight: var(--font-weight--semibold);
-    --heading-padding: var(--spacer-xl) 0;
-  }
-}
-.table {
-  --table-row-padding: var(--spacer-sm) 0 var(--spacer-xs);
-  &__header:last-child,
-  :nth-of-type(5) {
-    text-align: right;
-  }
-  &__header:nth-of-type(odd),
-  .sf-table__data:nth-of-type(odd) {
-    padding-bottom: var(--spacer-sm);
-  }
-  &__row {
-    justify-content: space-between;
-  }
-  &__data:nth-of-type(5) {
-    text-align: right;
-  }
-  &__image {
-    --image-width: 2.75rem;
-    text-align: left;
-  }
-  @include for-desktop {
-    margin: 0 0 var(--spacer-base) 0;
-    --table-heading-padding: var(--spacer-sm) 0;
-    &__header {
-      &:last-of-type {
-        margin-right: var(--spacer-xs);
-      }
-      &:nth-of-type(5) {
-        text-align: left;
-      }
-      &__description {
-        order: -1;
-      }
-    }
-    &__header:nth-of-type(odd),
-    .sf-table__data:nth-of-type(odd) {
-      padding-bottom: 0;
-    }
-    &__data {
-      &:nth-of-type(5) {
-        text-align: left;
-      }
-      &:last-of-type {
-        margin-right: var(--spacer-xs);
-      }
-    }
-    &__description {
-      text-align: left;
-      flex: 0 0 15rem;
-      order: -1;
-    }
-    &__image {
-      --image-width: 5.125rem;
-      order: -1;
-      text-align: center;
-      margin: 0 var(--spacer-xl) 0 0;
-    }
-  }
-}
-.product-sku {
-  font-family: var(--font-family--primary);
-  color: var(--c-text-muted);
-  @include for-desktop {
-    margin-top: var(--spacer-sm);
-  }
-}
-.product-price {
-  --price-regular-font-size: var(--font-size--base);
-  --price-regular-font-weight: var(--font-weight--normal);
-  --price-special-font-weight: var(--font-weight--normal);
-  text-align: right;
-  display: flex;
-  flex-direction: column;
-  ::v-deep .sf-price__old {
-    display: none;
-  }
-}
-.totals {
-  &__terms {
-    margin-top: var(--spacer-xl);
-    --link-color: var(--c-link);
-    --link-font-family: var(--font-family--primary);
-    --link-font-weight: var(--font-weight--normal);
-  }
-  &__element:first-child {
-    margin-bottom: var(--spacer-base);
-  }
-}
-.property {
-  margin: 0 0 var(--spacer-base) 0;
-  --property-name-font-weight: var(--font-weight--medium);
-  --property-name-font-size: var(--font-size--lg);
-  --property-value-font-weight: var(--font-weight--bold);
-  --property-value-font-size: var(--h4-font-size);
-  &__total {
-    margin: 0 0 var(--spacer-xl) 0;
-    --property-name-color: var(--c-text);
-  }
-  @include for-desktop {
-    margin: 0 0 var(--spacer-sm) 0;
-    &__subtotal {
-      margin: var(--spacer-xl) 0 var(--spacer-base);
-    }
-    &__total {
-      --property-name-font-weight: var(--font-weight--bold);
-      --property-name-font-size: var(--h4-font-size);
-      --property-value-font-weight: var(--font-weight--bold);
-      --property-value-font-size: var(--h4-font-size);
-      padding: var(--spacer-base) 0 0 0;
-    }
-  }
-}
-.divider {
-  --divider-border-color: var(--c-primary);
-  --divider-width: 100%;
-  --divider-margin: 0 0 var(--spacer-base) 0;
-}
-.summary {
-  &__content {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: var(--spacer-xl) 0;
-  }
-  &__terms {
-    margin: 0 0 0 var(--spacer-xs);
-  }
-}
-.content {
-  margin: 0 0 var(--spacer-base) 0;
-  color: var(--c-text);
-  &__label {
-    font-weight: var(--font-weight--normal);
-  }
-}
+@import "~@storefront-ui/shared/styles/components/templates/SfConfirmOrder.scss";
 </style>
