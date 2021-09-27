09859b2fc5a66e166ae070c9b9f16280969ec52b
AdamPawlinski <apawlinski@vuestorefront.io>
2021-09-27 09:30:25 +0200

    feat: move order review styling to shared package

--- packages/vue/src/components/templates/SfOrderReview/SfOrderReview.vue
+++ packages/vue/src/components/templates/SfOrderReview/SfOrderReview.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="sf-order-review review" data-testid="review">
+  <div class="sf-order-review" data-testid="review">
     <slot name="heading">
       <SfHeading
         :title="reviewTitle"
         :level="reviewTitleLevel"
-        class="sf-heading--left sf-heading--no-underline title"
+        class="
+          sf-heading--left sf-heading--no-underline
+          sf-order-review__heading
+        "
       />
     </slot>
     <slot name="personal-details">
-      <div class="review__header">
-        <p class="review__title">Personal details</p>
+      <div class="sf-order-review__header">
+        <p class="sf-order-review__title">Personal details</p>
         <SfButton
           class="sf-button--text"
           data-testid="personal-edit-button"
@@ -17,22 +20,24 @@
           >Edit
         </SfButton>
       </div>
-      <p class="content">{{ order.firstName }} {{ order.lastName }}<br /></p>
-      <p class="content">
+      <p class="sf-order-review__content">
+        {{ order.firstName }} {{ order.lastName }}<br />
+      </p>
+      <p class="sf-order-review__content">
         {{ shipping.streetName }} {{ shipping.apartment }} <br />
         {{ shipping.zipCode }}, {{ shipping.city }}, {{ shipping.country }}
       </p>
       <br />
-      <p class="content">
+      <p class="sf-order-review__content">
         {{ order.email }}
       </p>
-      <p class="content">
+      <p class="sf-order-review__content">
         {{ shipping.phoneNumber }}
       </p>
     </slot>
     <slot name="shipping-details">
-      <div class="review__header">
-        <p class="review__title">Shipping details</p>
+      <div class="sf-order-review__header">
+        <p class="sf-order-review__title">Shipping details</p>
         <SfButton
           class="sf-button--text"
           data-testid="shipping-edit-button"
@@ -41,18 +46,20 @@
           Edit
         </SfButton>
       </div>
-      <p class="content">
-        <span class="content__label content__shipping">{{
-          shippingMethod
-        }}</span
+      <p class="sf-order-review__content">
+        <span
+          class="
+            sf-order-review__content-label sf-order-review__content-shipping
+          "
+          >{{ shippingMethod }}</span
         ><br />
         {{ shipping.streetName }} {{ shipping.apartment }} <br />
         {{ shipping.zipCode }}, {{ shipping.city }}, {{ shipping.country }}
       </p>
     </slot>
     <slot name="billing-details">
-      <div class="review__header">
-        <p class="review__title">Billing address</p>
+      <div class="sf-order-review__header">
+        <p class="sf-order-review__title">Billing address</p>
         <SfButton
           class="sf-button--text"
           data-testid="billing-edit-button"
@@ -61,23 +68,25 @@
           Edit
         </SfButton>
       </div>
-      <p v-if="payment.sameAsShipping" class="content">
+      <p v-if="payment.sameAsShipping" class="sf-order-review__content">
         Same as shipping address
       </p>
       <template v-else>
-        <p class="content">
-          <span class="content__label">{{ payment.shippingMethod }}</span
+        <p class="sf-order-review__content">
+          <span class="sf-order-review__content-label">{{
+            payment.shippingMethod
+          }}</span
           ><br />
           {{ payment.streetName }} {{ payment.apartment }}, {{ payment.zipCode
           }}<br />
           {{ payment.city }}, {{ payment.country }}
         </p>
-        <p class="content">{{ payment.phoneNumber }}</p>
+        <p class="sf-order-review__content">{{ payment.phoneNumber }}</p>
       </template>
     </slot>
     <slot name="payment-details">
-      <div class="review__header">
-        <p class="review__title">Payment method</p>
+      <div class="sf-order-review__header">
+        <p class="sf-order-review__title">Payment method</p>
         <SfButton
           class="sf-button--text"
           data-testid="payment-edit-button"
@@ -86,18 +95,18 @@
           Edit
         </SfButton>
       </div>
-      <p class="content">{{ paymentMethod }}</p>
+      <p class="sf-order-review__content">{{ paymentMethod }}</p>
     </slot>
-    <div class="promo-code">
+    <div class="sf-order-review__promo-code">
       <slot name="promo">
         <SfInput
           v-model="promoCode"
           name="promoCode"
           label="Enter promo code"
-          class="sf-input--filled promo-code__input"
+          class="sf-input--filled sf-order-review__promo-code-input"
         />
         <SfButton
-          class="promo-code__button"
+          class="sf-order-review__promo-code-button"
           data-testid="apply-button"
           @click="$emit('click:promo-code-apply')"
         >
@@ -105,7 +114,7 @@
         </SfButton>
       </slot>
     </div>
-    <div class="characteristics">
+    <div class="sf-order-review__characteristics">
       <slot name="characteristics">
         <SfCharacteristic
           v-for="characteristic in characteristics"
@@ -114,7 +123,7 @@
           :description="characteristic.description"
           :icon="characteristic.icon"
           color-icon="green-primary"
-          class="characteristics__item"
+          class="sf-order-review__characteristics-item"
         />
       </slot>
     </div>
@@ -196,77 +205,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-@import "~@storefront-ui/vue/styles";
-.title {
-  --heading-title-margin: 0 0 var(--spacer-xl) 0;
-  --heading-title-font-weight: var(--font-weight--bold);
-  border-bottom: 1px solid var(--c-white);
-}
-.review {
-  box-sizing: border-box;
-  width: 100%;
-  &__header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  &__title {
-    font-family: var(--font-family--secondary);
-    font-weight: var(--font-weight--medium);
-    font-size: var(--font-size--base);
-  }
-}
-.promo-code {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  padding-top: var(--spacer-lg);
-  margin-top: var(--spacer-lg);
-  border-top: var(--c-white) solid 1px;
-  &__input {
-    --input-background: var(--c-white);
-    --input-label-font-size: var(--font-size--base);
-    flex: 1;
-  }
-  &__button {
-    --button-height: 1.875rem;
-  }
-}
-.promo-code {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  padding: var(--spacer-lg) 0 var(--spacer-base) 0;
-  margin-top: var(--spacer-lg);
-  border-top: var(--c-white) solid 1px;
-  &__input {
-    --input-background: var(--c-white);
-    flex: 1;
-  }
-  &__button {
-    --button-height: 30px;
-  }
-}
-.characteristics {
-  &__item {
-    margin: var(--spacer-base) 0;
-    &:last-of-type {
-      margin-bottom: 0;
-    }
-  }
-}
-.content {
-  font-family: var(--font-family--primary);
-  font-size: var(--font-size--sm);
-  line-height: 1.6;
-  font-weight: var(--font-weight--normal);
-  margin: 0;
-  color: var(--c-dark-variant);
-  &__label {
-    color: var(--c-text);
-  }
-  &__shipping {
-    font-weight: var(--font-weight--bold);
-  }
-}
+@import "~@storefront-ui/shared/styles/components/templates/SfOrderReview.scss";
 </style>
