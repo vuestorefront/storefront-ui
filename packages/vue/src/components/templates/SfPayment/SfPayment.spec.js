fbef46618f04d95c89f8379bd9a24380e45f2f3e
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-09-28 13:57:12 +0200

    fix: test

--- packages/vue/src/components/templates/SfPayment/SfPayment.spec.js
+++ packages/vue/src/components/templates/SfPayment/SfPayment.spec.js
@@ -1,15 +1,15 @@
-import { shallowMount } from "@vue/test-utils"
-const countries = ["United States", "Germany", "Poland"]
-import SfPayment from "./SfPayment.vue"
+import { shallowMount } from "@vue/test-utils";
+const countries = ["United States", "Germany", "Poland"];
+import SfPayment from "./SfPayment.vue";
 describe("SfPayment.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(SfPayment, {
       methods: {
         getCountries: () => {
-          return countries
+          return countries;
         },
       },
-    })
-    expect(component.classes("sf-payment")).toBe(true)
-  })
-})
+    });
+    expect(component.classes("sf-payment")).toBe(true);
+  });
+});
