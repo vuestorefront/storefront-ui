ec23ed877e01d1f8166b65bd61fda493ccae845c
AdamPawlinski <apawlinski@vuestorefront.io>
2021-09-28 13:02:44 +0200

    fix: unknown tytpes in docs

--- packages/vue/src/components/templates/SfOrderSummary/SfOrderSummary.stories.js
+++ packages/vue/src/components/templates/SfOrderSummary/SfOrderSummary.stories.js
@@ -88,6 +88,7 @@ export default {
       control: "object",
       table: {
         category: "Props",
+        type: { summary: "array" },
         defaultValue: {
           summary: `[
             {
