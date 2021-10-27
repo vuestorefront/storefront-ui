f447c1fd1882446d3ee69bd365ffd390af05f89f
Anna Musial <ania.musial13@gmail.com>
2021-10-27 13:59:50 +0200

    feat: updated tests for cimage

--- packages/vue/src/components/atoms/SfBullets/SfBullets.vue
+++ packages/vue/src/components/atoms/SfBullets/SfBullets.vue
@@ -81,7 +81,7 @@ export default {
   props: {
     total: {
       type: Number,
-      default: 0,
+      default: 3,
     },
     current: {
       type: Number,
--- packages/vue/src/components/atoms/SfCimage/SfCimage.spec.js
+++ packages/vue/src/components/atoms/SfCimage/SfCimage.spec.js
@@ -1,9 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
+import { getConfig } from "cloudinary-build-url";
 import SfCimage from "./SfCimage.vue";
 
 describe("SfCimage.vue", () => {
   it("renders a component with publicId and cloud", () => {
-    const component = shallowMount(SfCimage, {
+    const wrapper = shallowMount(SfCimage, {
       propsData: {
         publicId: "example",
         cloud: {
@@ -12,10 +13,10 @@ describe("SfCimage.vue", () => {
         alt: "example",
       },
     });
-    expect(component.classes("sf-cimage")).toBe(true);
+    expect(wrapper.classes("sf-cimage")).toBe(true);
   });
   it("renders a component with placeholder", () => {
-    const component = shallowMount(SfCimage, {
+    const wrapper = shallowMount(SfCimage, {
       propsData: {
         publicId: "example",
         cloud: {
@@ -25,6 +26,23 @@ describe("SfCimage.vue", () => {
         placeholder: "blur",
       },
     });
-    expect(component.classes("sf-cimage")).toBe(true);
+    expect(wrapper.classes("sf-cimage")).toBe(true);
+  });
+  it("throws an error when public id is not provided", () => {
+    expect(() => shallowMount(SfCimage)).toThrow(
+      "PublicId is required to use this component."
+    );
+  });
+  it("throws an error when cloud is not provided", () => {
+    jest.mock("cloudinary-build-url", () => ({
+      getConfig: jest.fn({ cloudName: undefined }),
+    }));
+    expect(() =>
+      shallowMount(SfCimage, {
+        propsData: {
+          publicId: "test",
+        },
+      })
+    ).toThrow("cloudName is required to use this component.");
   });
 });
