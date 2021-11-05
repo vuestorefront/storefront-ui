994ff3faaa5e9eaedee29e5b4091035dfeddaac1
Anna Musial <ania.musial13@gmail.com>
2021-11-05 16:10:51 +0100

    feat: heading level change

--- packages/vue/src/components/atoms/SfHeading/SfHeading.vue
+++ packages/vue/src/components/atoms/SfHeading/SfHeading.vue
@@ -47,7 +47,7 @@ export default {
     return !!descriptionProp || slots().description;
   },
   headingClass(headingLevel) {
-    return headingLevel < 8 ? `h${headingLevel}` : `h2`;
+    return headingLevel <= 6 ? `h${headingLevel}` : `h2`;
   },
 };
 </script>
