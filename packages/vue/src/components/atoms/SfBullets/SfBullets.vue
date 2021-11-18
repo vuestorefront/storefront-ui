71a466a42bcf345ba538bb99cbca5a7f6c33aa03
Anna Musial <ania.musial13@gmail.com>
2021-11-18 12:48:41 +0100

    2058 - Atoms - Missing tests & improved code coverage (#2079)
    * feat: added tests for arrow, breadcrumbs and bullets
    
    * feat: added tests for arrow, badge, color, cimage
    
    * feat: added tests for heading, input, link and skeleton
    
    * feat: updated tests for cimage
    
    * feat: added tests for quantity selector input icon
    
    * feat: added tests for input and quantity selector
    
    * feat: added tests for textarea
    
    * feat: added heading level conditional
    
    * feat: heading level change
    
    * fix: cleared console errors
    
    * chore: remove unused propsdata
    
    * chore: updated changelog

--- packages/vue/src/components/atoms/SfBullets/SfBullets.vue
+++ packages/vue/src/components/atoms/SfBullets/SfBullets.vue
@@ -81,7 +81,7 @@ export default {
   props: {
     total: {
       type: Number,
-      default: 0,
+      default: 3,
     },
     current: {
       type: Number,
@@ -93,9 +93,6 @@ export default {
   },
   inactiveLeft(total, current) {
     return total - (total - 1 - current) - 1;
-  },
-  go(listeners, index) {
-    listeners.click && listeners.click(index);
   },
 };
 </script>
