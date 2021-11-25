d3bef010f9693d6aa243abd5f24f797762eb3d28
Anna Musial <ania.musial13@gmail.com>
2021-11-25 16:39:02 +0100

    feat: added html validator for nuxt

--- packages/nuxt/nuxt.config.js
+++ packages/nuxt/nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ['@nuxtjs/html-validator'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
