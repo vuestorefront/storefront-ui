7ebfff17318609b7eb2370ad196be0f5c1ded18b
AdamPawlinski <apawlinski@vuestorefront.io>
2022-01-21 11:11:09 +0100

    fix: header width on desktop

--- packages/vue/src/components/organisms/SfHeader/SfHeader.vue
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.vue
@@ -1,109 +1,116 @@
 <template>
   <header ref="header" v-click-outside="closeHandler" class="sf-header">
     <!--@slot Use this slot to replace logo with text or image-->
-    <slot name="logo" v-bind="{ logo, title }">
-      <SfLink link="/">
-        <SfImage v-if="logo" :src="logo" :alt="title" class="sf-header__logo" />
-        <SfHeading
-          v-else
-          :title="title"
-          :level="1"
-          class="sf-header__title"
-        ></SfHeading>
-      </SfLink>
-    </slot>
-    <div class="sf-header__aside">
-      <!-- @slot Use this slot for language or currency selector -->
-      <slot name="aside" />
-    </div>
-    <div class="sf-header__actions">
-      <nav class="sf-header__navigation">
-        <slot name="navigation">
-          <div
-            class="sf-header__menu"
-            :class="{ 'display-none': isVisibleOnMobile }"
-          >
-            <SfButton
-              v-for="(item, i) in menuItems"
-              :key="`button-menu-item-${i}`"
-              class="
-                sf-header__menu-item sf-header__menu-item--desktop
-                sf-button--pure
-              "
-              v-on="$listeners"
-              @mouseenter="$emit('mouseenter:button', item)"
-              @mouseleave="$emit('mouseleave:button', '')"
-              @click="$emit('click:button', item)"
+    <div class="sf-header__header">
+      <slot name="logo" v-bind="{ logo, title }">
+        <SfLink link="/">
+          <SfImage
+            v-if="logo"
+            :src="logo"
+            :alt="title"
+            class="sf-header__logo"
+          />
+          <SfHeading
+            v-else
+            :title="title"
+            :level="1"
+            class="sf-header__title"
+          ></SfHeading>
+        </SfLink>
+      </slot>
+      <div class="sf-header__aside">
+        <!-- @slot Use this slot for language or currency selector -->
+        <slot name="aside" />
+      </div>
+      <div class="sf-header__actions">
+        <nav class="sf-header__navigation">
+          <slot name="navigation">
+            <div
+              class="sf-header__menu"
+              :class="{ 'display-none': isVisibleOnMobile }"
             >
-              {{ item }}
+              <SfButton
+                v-for="(item, i) in menuItems"
+                :key="`button-menu-item-${i}`"
+                class="
+                  sf-header__menu-item sf-header__menu-item--desktop
+                  sf-button--pure
+                "
+                v-on="$listeners"
+                @mouseenter="$emit('mouseenter:button', item)"
+                @mouseleave="$emit('mouseleave:button', '')"
+                @click="$emit('click:button', item)"
+              >
+                {{ item }}
+              </SfButton>
+            </div>
+            <SfSidebar
+              :visible="isVisibleOnMobile && openSidebar"
+              :persistent="true"
+              :overlay="false"
+              class="sf-header__menu--sidebar"
+              @close="$emit('close')"
+            >
+              <SfMenuItem
+                v-for="(item, i) in menuItems"
+                :key="`menu-item-${i}`"
+                :label="item"
+                class="sf-header__menu-item sf-header__menu-item--mobile"
+                @click="$emit('click:button', item)"
+              />
+            </SfSidebar>
+            <div v-if="openContent" class="sf-header__menu-content">
+              <template v-for="item in menuItems">
+                <slot :name="item" />
+              </template>
+            </div>
+          </slot>
+        </nav>
+        <!--@slot Use this slot to replace default search bar-->
+        <slot
+          v-if="isVisible"
+          name="search"
+          v-bind="{ searchValue, searchPlaceholder }"
+        >
+          <SfSearchBar
+            :value="searchValue"
+            :placeholder="searchPlaceholder"
+            aria-label="Search"
+            class="sf-header__search"
+            @input="$emit('change:search', $event)"
+            @keyup.enter="$emit('enter:search', $event)"
+          />
+        </slot>
+        <!--@slot Use this slot to replace default header icons with custom content-->
+        <slot
+          name="header-icons"
+          v-bind="{
+            activeIcon,
+            icons,
+            isVisible,
+          }"
+        >
+          <div v-if="isVisible" class="sf-header__icons">
+            <SfButton
+              v-for="(icon, item) in icons"
+              :key="`icon-${item}`"
+              class="sf-button--pure sf-header__action"
+              :class="`icon-${item}`"
+              :data-testid="`icon-${item}`"
+              @click="$emit('click:icon', item)"
+            >
+              <slot :name="`icon-${item}`" v-bind="{ activeIcon }">
+                <SfIcon
+                  :icon="icon"
+                  size="1.25rem"
+                  class="sf-header__icon"
+                  :class="{ 'is-active': activeIcon === item }"
+                />
+              </slot>
             </SfButton>
           </div>
-          <SfSidebar
-            :visible="isVisibleOnMobile && openSidebar"
-            :persistent="true"
-            :overlay="false"
-            class="sf-header__menu--sidebar"
-            @close="$emit('close')"
-          >
-            <SfMenuItem
-              v-for="(item, i) in menuItems"
-              :key="`menu-item-${i}`"
-              :label="item"
-              class="sf-header__menu-item sf-header__menu-item--mobile"
-              @click="$emit('click:button', item)"
-            />
-          </SfSidebar>
-          <div v-if="openContent" class="sf-header__menu-content">
-            <template v-for="item in menuItems">
-              <slot :name="item" />
-            </template>
-          </div>
         </slot>
-      </nav>
-      <!--@slot Use this slot to replace default search bar-->
-      <slot
-        v-if="isVisible"
-        name="search"
-        v-bind="{ searchValue, searchPlaceholder }"
-      >
-        <SfSearchBar
-          :value="searchValue"
-          :placeholder="searchPlaceholder"
-          aria-label="Search"
-          class="sf-header__search"
-          @input="$emit('change:search', $event)"
-          @keyup.enter="$emit('enter:search', $event)"
-        />
-      </slot>
-      <!--@slot Use this slot to replace default header icons with custom content-->
-      <slot
-        name="header-icons"
-        v-bind="{
-          activeIcon,
-          icons,
-          isVisible,
-        }"
-      >
-        <div v-if="isVisible" class="sf-header__icons">
-          <SfButton
-            v-for="(icon, item) in icons"
-            :key="`icon-${item}`"
-            class="sf-button--pure sf-header__action"
-            :class="`icon-${item}`"
-            :data-testid="`icon-${item}`"
-            @click="$emit('click:icon', item)"
-          >
-            <slot :name="`icon-${item}`" v-bind="{ activeIcon }">
-              <SfIcon
-                :icon="icon"
-                size="1.25rem"
-                class="sf-header__icon"
-                :class="{ 'is-active': activeIcon === item }"
-              />
-            </slot>
-          </SfButton>
-        </div>
-      </slot>
+      </div>
     </div>
   </header>
 </template>
