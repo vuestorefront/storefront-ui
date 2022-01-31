cde904590b285d19d67b3cb49b1ce558b3ea2329
shatanov <timur-shatanov.work@mail.ru>
2022-01-31 20:48:59 +0300

    fix('Category'): create and add 'navbar__view-button' to SfButton #2220

--- packages/vue/src/components/pages/category/Category.vue
+++ packages/vue/src/components/pages/category/Category.vue
@@ -42,7 +42,7 @@
         <div class="navbar__view">
           <span class="navbar__view-label desktop-only">View</span>
           <SfButton
-            class="sf-button--pure"
+            class="sf-button--pure navbar__view-button"
             aria-label="Change to grid view"
             :aria-pressed="isGridView"
             @click="isGridView = true"
@@ -55,7 +55,7 @@
             />
           </SfButton>
           <SfButton
-            class="sf-button--pure"
+            class="sf-button--pure navbar__view-button"
             aria-label="Change to list view"
             :aria-pressed="!isGridView"
             @click="isGridView = false"
@@ -828,6 +828,16 @@ export default {
       order: 0;
     }
     &-icon {
+      cursor: pointer;
+      margin: 0 var(--spacer-base) 0 var(--spacer-sm);
+      @include for-desktop {
+        margin: 0 var(--spacer-base) 0 0;
+      }
+      &:last-child {
+        margin: 0;
+      }
+    }
+    &-button {
       cursor: pointer;
       margin: 0 var(--spacer-base) 0 var(--spacer-sm);
       @include for-desktop {
