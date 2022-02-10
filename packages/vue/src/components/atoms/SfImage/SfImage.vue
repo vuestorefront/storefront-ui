46b559a7d315c24297abb53494da9340c9e04c30
Adam Pawliński <apawlinski@vuestorefront.io>
2022-02-10 12:39:57 +0100

    fix: image-tag set to img for sfimage on nuxt (#2253)

--- packages/vue/src/components/atoms/SfImage/SfImage.vue
+++ packages/vue/src/components/atoms/SfImage/SfImage.vue
@@ -138,7 +138,7 @@ export default {
       }
     },
     imageComponentTag() {
-      return !this.$nuxt ? "img" : this.imageTag;
+      return !this.$nuxt ? "img" : this.imageTag || "img";
     },
     isPlaceholderVisible() {
       return (
