5cbf6ee06bbde1f6c4a9cc0565419e0bfd457768
shatanov <timur-shatanov.work@mail.ru>
2022-02-23 17:33:36 +0300

    test('SfButton'): add test for aria-label props

--- packages/vue/src/components/atoms/SfButton/SfButton.spec.js
+++ packages/vue/src/components/atoms/SfButton/SfButton.spec.js
@@ -38,6 +38,14 @@ describe("SfButton.vue", () => {
     });
     expect(wrapper.classes("is-disabled--link")).toBe(true);
   });
+  it("renders component as button with aria-label", () => {
+    const wrapper = shallowMount(SfButton, {
+      propsData: {
+        ariaLabel: "Go to ...",
+      },
+    });
+    expect(wrapper.attributes("aria-label")).toBe("Go to ...");
+  });
   it("renders content via default slot", () => {
     const msg = "HelloWorld";
     const wrapper = shallowMount(SfButton, {
