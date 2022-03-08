6946cf78ffb735c1772c1c247481edfbf865b999
Justyna <jgieracka@vuestorefront.io>
2022-03-08 14:52:55 +0100

    fix: e2e test hero

--- packages/nuxt/cypress/integration/home.spec.js
+++ packages/nuxt/cypress/integration/home.spec.js
@@ -5,7 +5,8 @@ describe("Home page", () => {
     cy.visit('/')
   })
   it("test SfHero component", () => {
-    cy.get(selectors.hero.items)
+    cy.get(".sf-hero")
+      .find(selectors.hero.items)
       .first()
       .should("be.visible")
       .get(selectors.hero.prevArrowButton)
@@ -25,9 +26,6 @@ describe("Home page", () => {
       .find("button")
       .eq(2)
       .click()
-      .get(selectors.hero.items)
-      .eq(2)
-      .should("be.visible")
   });
   it("test SfBannerGrid component", () => {
     cy.get(selectors.bannerGrid.ctaButton)
