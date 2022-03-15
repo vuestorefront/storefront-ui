1b0f50b6134af5f07fd7c93b4f6f5c72e6337883
AdamPawlinski <apawlinski@vuestorefront.io>
2022-03-15 15:17:08 +0100

    feat: change-breakpoint script simplified

--- packages/vue/scripts/change-breakpoint.js
+++ packages/vue/scripts/change-breakpoint.js
@@ -1,49 +1,25 @@
 const fs = require("fs");
-const glob = require("glob");
 const path = require("path");
 const breakpointValue = process.argv[2];
 
-const srcPath = path.join(__dirname, "..", "/src/components/");
-const pathsVueFiles = glob.sync("*/*/*{.vue,/*.vue}", {
-  cwd: srcPath,
-});
-const styleSrcPath = path.join(
-  __dirname,
-  "../..",
-  "/shared/styles/components/"
-);
-const pathsStyleFiles = glob.sync("*/Sf*.scss", {
-  cwd: styleSrcPath,
-});
-const styleFiles = pathsStyleFiles.map(file => path.join(styleSrcPath, file));
-const vueFiles = pathsVueFiles.map(file => path.join(srcPath, file));
-const transitionFile = path.join(__dirname, "..", "/src/utilities/transitions/_transitions.scss");
+const transitionFile = path.join(__dirname, "../..", "/shared/styles/helpers/_breakpoints.scss");
 
 function updateBreakpoint() {
-  for (const file of styleFiles) {
-    addBreakpointValue(file, breakpointValue);
-  }
-  for (const file of vueFiles) {
-    addBreakpointValue(file, breakpointValue);
-  }
   addBreakpointValue(transitionFile, breakpointValue);
 }
 
 function addBreakpointValue(file, value) {    
   const data = fs.readFileSync(file, 'utf8', function (err) {
     if (err) {
-      return console.log('Cannot read file.', err);
+      return console.log('Cannot read breakpoints.scss file.', err);
     }    
   });  
-  const addBreakpoint = mixin => {
-    const valueString = !!value ? value.toString().concat('px') : '$breakpoint';
-    return mixin.includes('for-desktop') ? `for-desktop(${valueString}) ` : `for-mobile(${valueString}) `
-  }
+  const valueString = !!value ? value.toString().concat('px') : '$desktop-min';
   let result = 
-    data.replace(/for\-(desktop|mobile)\(.+\)./g, addBreakpoint); 
+    data.replace(/\$breakpoint:.+/, `$breakpoint: ${valueString} !default;`);
   if (!result) return;
   fs.writeFileSync(file, result, 'utf8', function (err) {
-    if (err) return console.log('Cannot write file.', err);
+    if (err) return console.log('Cannot write breakpoints.scss file.', err);
   });
   return true;
 }
