57f5d8519ffbd5cc4cb881c38ead972d9f85762e
Justyna <jgieracka@vuestorefront.io>
2022-04-08 10:19:32 +0200

    fix: removed from nuxt config

--- boilerplates/nuxt/nuxt.config.js
+++ boilerplates/nuxt/nuxt.config.js
@@ -23,7 +23,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/utils/focus-directive.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
