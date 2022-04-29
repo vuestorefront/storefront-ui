94e22f57f0840ed3683f94282ab5cf6dc7ce16ac
shatanov <timur-shatanov.work@mail.ru>
2022-04-29 22:25:36 +0300

    feat('SfArrow'): add aria-label props

--- packages/vue/src/components/atoms/SfArrow/SfArrow.vue
+++ packages/vue/src/components/atoms/SfArrow/SfArrow.vue
@@ -3,6 +3,7 @@
     :is="injections.components.SfButton"
     :class="[data.class, data.staticClass, 'sf-arrow']"
     :style="[data.style, data.staticStyle]"
+    :aria-label="props.ariaLabel"
     type="button"
     v-bind="data.attrs"
     v-on="listeners"
@@ -27,6 +28,12 @@ export default {
   inject: {
     components: {
       default: { SfButton, SfIcon },
+    },
+  },
+  props: {
+    ariaLabel: {
+      type: String,
+      default: "Arrow label",
     },
   },
 };
