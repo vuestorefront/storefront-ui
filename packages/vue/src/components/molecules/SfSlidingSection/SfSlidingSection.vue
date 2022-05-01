52568447c9dc5efd484c2112a7780ea643d21dd3
shatanov <timur-shatanov.work@mail.ru>
2022-05-01 15:34:55 +0300

    fix('SfSlidingSection'): fix bug in e2e test

--- packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.vue
+++ packages/vue/src/components/molecules/SfSlidingSection/SfSlidingSection.vue
@@ -8,7 +8,7 @@
         <slot name="close" v-bind="{ closeHandler }">
           <SfButton
             class="sf-button--pure sf-sliding-section__close"
-            :aria-label="'Close'"
+            aria-label="Close"
             @click="closeHandler"
           >
             <SfIcon icon="cross" size="14px" />
