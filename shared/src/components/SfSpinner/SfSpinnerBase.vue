053c88af8f64316ac6bb78f9180ce14931551393
AdamPawlinski <apawlinski@vuestorefront.io>
2022-05-05 12:44:54 +0200

    feat: add a11y attrs and class

--- shared/src/components/SfSpinner/SfSpinnerBase.vue
+++ shared/src/components/SfSpinner/SfSpinnerBase.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="relative">
+  <div class="relative" role="presentation" aria-live="polite" aria-busy="true">
     <div class="absolute w-6 h-6 bg-transparent border-2 border-gray-200 rounded-full"></div>
-    <div class="absolute w-3 h-3 bg-transparent border-2 border-transparent rounded-tl-full animate-spin border-t-primary-600"></div>
+    <div class="w-3 h-3 bg-transparent border-2 border-transparent rounded-tl-full stopped-animation before:absolute animate-spin border-t-primary-600"></div>
   </div> 
 </template>
 
