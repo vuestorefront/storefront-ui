105d85cd372c01f3e067f8310a63d4dc4b77bb91
AdamPawlinski <apawlinski@vuestorefront.io>
2022-05-06 08:36:03 +0200

    fix: after CR

--- shared/src/components/SfSpinner/SfSpinner3xl.vue
+++ shared/src/components/SfSpinner/SfSpinner3xl.vue
@@ -6,7 +6,7 @@
     aria-busy="true"
   >
     <div class="absolute w-48 h-48 bg-transparent border-8 border-gray-200 rounded-[100%]" />
-    <div class="stopped-animation absolute w-24 h-24 bg-transparent border-8 border-transparent rounded-tl-[9999px] animate-spin border-t-primary-600" />
+    <div class="stopped-animation absolute w-24 h-24 bg-transparent border-8 border-transparent rounded-tl-[100%] animate-spin border-t-primary-600" />
   </div>
 </template>
 
