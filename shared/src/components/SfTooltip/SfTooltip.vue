0d7590a8eee528f81129caf234c540c44fa8c811
Anna Musial <ania.musial13@gmail.com>
2022-05-12 16:07:40 +0200

    feat: styling improvements for tooltip

--- shared/src/components/SfTooltip/SfTooltip.vue
+++ shared/src/components/SfTooltip/SfTooltip.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative mx-2">
-    <div class="flex justify-center z-50 bg-gray-500 max-w-xs font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5 right-0 bottom-full">
+    <div class="flex justify-center z-50 bg-gray-500 font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5 right-0 bottom-full">
       <span>Tooltip text</span>
       <div class="absolute w-2 h-2 bottom-0 -mb-1 rotate-45 bg-gray-500" />
     </div>
--- shared/src/components/SfTooltip/SfTooltipRight.vue
+++ shared/src/components/SfTooltip/SfTooltipRight.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative mx-2">
-    <div class="flex items-center z-50 bg-gray-500 max-w-xs font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5 right-0 bottom-full">
+    <div class="flex justify-center items-center z-50 bg-gray-500 font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5 right-0 bottom-full">
       <span>Tooltip text</span>
       <div class="absolute w-2 h-2 right-0 -mr-1 rotate-45 bg-gray-500" />
     </div>
