9626b2a9a511ffbac3094e5f64373d4c3f5f3ab1
Anna Musial <ania.musial13@gmail.com>
2022-05-25 18:10:08 +0200

    Update shared/src/components/Tooltip/TooltipBase.vue
    Co-authored-by: RafalDobrowolski <rafal.dobrowolski77@gmail.com>

--- shared/src/components/Tooltip/TooltipBase.vue
+++ shared/src/components/Tooltip/TooltipBase.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative flex flex-col items-center group">
     <ButtonPrimary aria-describedby="tooltip-base">Hover me</ButtonPrimary>
-    <div class="absolute bottom-0 justify-center items-center hidden mb-14 group-hover:flex z-50 bg-gray-500 font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5">
+    <div  id="tooltip-base" role="tooltip" class="absolute bottom-0 justify-center items-center hidden mb-14 group-hover:flex z-50 bg-gray-500 font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5">
       <div>Tooltip text</div>
       <div class="absolute w-2 h-2 bottom-0 -mb-1 rotate-45 bg-gray-500" />
     </div>
