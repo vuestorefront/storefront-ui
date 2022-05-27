42e6c2ccad1794cc3baeb333c0c5cd9fb51dee0b
Justyna <jgieracka@vuestorefront.io>
2022-05-27 15:23:52 +0200

    feat: added logic

--- shared/src/stories/Breadcrumbs/Breadcrumbs.stories.js
+++ shared/src/stories/Breadcrumbs/Breadcrumbs.stories.js
@@ -1,24 +1,62 @@
 
-import BreadcrumbsBase from "../../components/Breadcrumbs/BreadcrumbsBase.vue";
+import BreadcrumbsBase from '../../components/Breadcrumbs/BreadcrumbsBase.vue';
+import BreadcrumbsWithIcon from '../../components/Breadcrumbs/BreadcrumbsWithIcon.vue';
 
 export default {
-  title: "SFUI Components/Breadcrumbs",
+  title: 'SFUI Components/Breadcrumbs',
   component: BreadcrumbsBase,
   parameters: {
     docs: {
       description: {
         component:
-          "The breadcrumbs component. [link to component docs on the page]",
-      },
-    },
+          'The breadcrumbs component. [link to component docs on the page]'
+      }
+    }
   },
+  argTypes: {
+    breadcrumbs: {
+      control: 'object',
+      defaultValue: [],
+      description: 'Array of objects. Objects need to have `name` and `link` keys'
+    }
+  }
 };
 
 const Template = (args, { argTypes }) => ({
   components: { BreadcrumbsBase },
+  props: Object.keys(argTypes),
   template: `
-    <BreadcrumbsBase />
+    <BreadcrumbsBase :breadcrumbs="breadcrumbs" />
     `
 });
 
 export const Common = Template.bind({});
+
+Common.args = {
+  breadcrumbs: [
+    { name: 'Home', link: '/' },
+    { name: 'Electronics', link: '/' },
+    { name: 'iPad & Tablets', link: '/' },
+    { name: 'Apple iPad Accessories', link: '/' },
+    { name: 'iPad Keyboards', link: '/' }
+  ]
+};
+
+const BreadcrumbsIcon = (args, { argTypes }) => ({
+  components: { BreadcrumbsWithIcon },
+  props: Object.keys(argTypes),
+  template: `
+    <BreadcrumbsWithIcon :breadcrumbs="breadcrumbs" />
+    `
+});
+
+export const WithIcon = BreadcrumbsIcon.bind({});
+
+WithIcon.args = {
+  breadcrumbs: [
+    { name: 'Electronics', link: '/' },
+    { name: 'iPad & Tablets', link: '/' },
+    { name: 'Apple iPad Accessories', link: '/' },
+    { name: 'iPad Keyboards', link: '/' }
+  ]
+};
