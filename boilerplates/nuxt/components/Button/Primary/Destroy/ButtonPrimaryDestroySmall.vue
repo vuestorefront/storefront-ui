75ba9dd408e3c580b7f3001f87bcdccb7802f151
Justyna <jgieracka@vuestorefront.io>
2022-06-22 11:23:12 +0200

    fix: listeners for buttons

--- boilerplates/nuxt/components/Button/Primary/Destroy/ButtonPrimaryDestroySmall.vue
+++ boilerplates/nuxt/components/Button/Primary/Destroy/ButtonPrimaryDestroySmall.vue
@@ -2,6 +2,7 @@
   <button
     v-focus
     class="inline-flex items-center justify-center cursor-pointer bg-rose-600 hover:bg-rose-700 active:bg-rose-800 disabled:bg-gray-200 text-white disabled:text-gray-500 disabled:opacity-50 uppercase leading-5 px-3 py-[6px] text-sm rounded-md font-body font-medium shadow-base hover:shadow-medium disabled:shadow-none disabled:cursor-not-allowed outline-violet"
+    v-on="$listeners"
   >
     <slot />
   </button>
