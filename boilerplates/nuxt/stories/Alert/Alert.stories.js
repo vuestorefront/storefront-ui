ae815a1c11a4b90fa3a2eb15d4c09fb86852c6f7
Justyna <jgieracka@vuestorefront.io>
2022-06-24 13:21:56 +0200

    feat: new props for alert

--- boilerplates/nuxt/stories/Alert/Alert.stories.js
+++ boilerplates/nuxt/stories/Alert/Alert.stories.js
@@ -1,8 +1,5 @@
 
 import AlertBase from '../../components/Alert/AlertBase.vue';
-import AlertSimple from '../../components/Alert/AlertSimple.vue';
-import AlertMultiline from '../../components/Alert/AlertMultiline.vue';
-import AlertWithButton from '../../components/Alert/AlertWithButton.vue';
 
 export default {
   title: 'SFUI Components/Alert',
@@ -24,6 +21,14 @@ export default {
       },
       description: 'Persistence of the Alert. Default value is false.'
     },
+    icon: {
+      control: 'boolean',
+      defaultValue: false,
+      table: {
+        category: 'Props'
+      },
+      description: 'Visibility of icon.'
+    },
     type: {
       control: 'select',
       defaultValue: 'info',
@@ -41,38 +46,8 @@ const Template = (args, { argTypes }) => ({
   components: { AlertBase },
   props: Object.keys(argTypes),
   template: `
-    <AlertBase :persistent="persistent" :type="type" />
+    <AlertBase :icon="icon" :persistent="persistent" :type="type" />
     `
 });
 
 export const Base = Template.bind({});
-
-const SimpleAlert = (args, { argTypes }) => ({
-  components: { AlertSimple },
-  props: Object.keys(argTypes),
-  template: `
-  <AlertSimple :type="type" />
-  `
-});
-
-export const Simple = SimpleAlert.bind({});
-
-const MultilineAlert = (args, { argTypes }) => ({
-  components: { AlertMultiline },
-  props: Object.keys(argTypes),
-  template: `
-  <AlertMultiline :type="type" />
-  `
-});
-
-export const Multiline = MultilineAlert.bind({});
-
-const WithButtonAlert = (args, { argTypes }) => ({
-  components: { AlertWithButton },
-  props: Object.keys(argTypes),
-  template: `
-  <AlertWithButton :type="type" />
-  `
-});
-
-export const WithButton = WithButtonAlert.bind({});
