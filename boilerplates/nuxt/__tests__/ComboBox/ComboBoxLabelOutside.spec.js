1c62b638d730f233d9dacd6098df3805902bbcde
Justyna <jgieracka@vuestorefront.io>
2022-07-05 10:37:39 +0200

    fix: tests updated

--- boilerplates/nuxt/__tests__/ComboBox/ComboBoxLabelOutside.spec.js
+++ boilerplates/nuxt/__tests__/ComboBox/ComboBoxLabelOutside.spec.js
@@ -3,8 +3,38 @@ import ComboBoxLabelOutside from '../../components/ComboBox/ComboBoxLabelOutside
 let wrapper;
 
 describe('ComboBoxLabelOutside.vue', () => {
+  const filteredOptions = [
+    'red',
+    'red-orange',
+    'orange',
+    'yellow',
+    'yellow-green',
+    'green',
+    'sky blue',
+    'blue',
+    'violet',
+    'brown',
+    'black',
+    'white',
+    'gray',
+    'magenta',
+    'pink',
+    'light-blue',
+    'aqua-green',
+    'jade-green',
+    'peach',
+    'golden-yellow',
+    'yellow-orange',
+    'mahogany',
+    'tan',
+    'light brown'
+  ];
   beforeEach(() => {
-    wrapper = shallowMount(ComboBoxLabelOutside);
+    wrapper = shallowMount(ComboBoxLabelOutside, {
+      propsData: {
+        filteredOptions
+      }
+    });
   });
   it('render component correctly', () => {
     expect(wrapper.html()).toMatchSnapshot();
