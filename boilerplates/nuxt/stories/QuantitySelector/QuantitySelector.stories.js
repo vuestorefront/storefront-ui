47b78bba8a0b8917da2eab915109d3302802beef
Adam Pawliński <apawlinski@vuestorefront.io>
2022-07-07 13:23:40 +0200

    98 feat: create quantity selector (#111)

---
+++ boilerplates/nuxt/stories/QuantitySelector/QuantitySelector.stories.js
@@ -0,0 +1,104 @@
+
+import QuantitySelector from '../../components/QuantitySelector/QuantitySelector.vue';
+
+export default {
+  title: 'SFUI Components/QuantitySelector',
+  component: QuantitySelector,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The Quantity Selector component. [link to component docs on the page]'
+      }
+    }
+  },
+  argTypes: {
+    value: {
+      control: 'text',
+      defaultValue: '1',
+      table: {
+        category: 'Props',
+        defaultValue: {
+          summary: '1'
+        }
+      },
+      description: 'Input value'
+    },
+    disabled: {
+      control: 'boolean',
+      defaultValue: false,
+      table: {
+        category: 'Props'
+      },
+      description:
+        'Native input disabled attribute'
+    },
+    quantityInStock: {
+      control: 'number',
+      defaultValue: '5',
+      table: {
+        category: 'Props',
+        defaultValue: {
+          summary: '0'
+        }
+      },
+      description: 'Input value'
+    },
+    minQuantity: {
+      control: 'number',
+      defaultValue: '1',
+      table: {
+        category: 'Props',
+        defaultValue: {
+          summary: '1'
+        }
+      },
+      description: 'Input value'
+    },
+    size: {
+      control: 'radio',
+      options: ['base', 'lg'],
+      defaultValue: 'base',
+      table: {
+        category: 'Props',
+        defaultValue: {
+          summary: 'base'
+        }
+      },
+      description: 'Change size of component'
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { QuantitySelector },
+  props: Object.keys(argTypes),
+  data() {
+    return {
+      quantity: this.value,
+      disabledState: this.disabled,
+      inStock: this.quantityInStock
+    };
+  },
+  watch: {
+    inStock: {
+      handler(newVal) {
+        if (newVal <= 0) { this.disabledState = true; }
+      },
+      immediate: true
+    }
+  },
+  template: `
+  <div>
+    <QuantitySelector 
+      v-model="quantity"
+      :quantityInStock="quantityInStock"
+      :minQuantity="minQuantity"
+      :disabled="disabledState"
+      :size="size"
+    />
+    </div>
+    `
+});
+
+export const Base = Template.bind({});
