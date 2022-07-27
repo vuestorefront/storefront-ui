5c52a63de8e0bee1ecb27aac036cbdc439f898d9
Justyna <jgieracka@vuestorefront.io>
2022-07-27 06:20:11 +0200

    feat: tests

---
+++ boilerplates/nuxt/__tests__/ProductCard/ProductCardWithButton.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+import ProductCardWithButton from '../../components/ProductCard/ProductCardWithButton.vue';
+
+let wrapper;
+
+describe('ProductCardWithButton.vue', () => {
+  beforeEach(() => {
+    wrapper = shallowMount(ProductCardWithButton);
+  });
+  it('render component correctly', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
