a073e1245ff6f2e2eda93755dcac570c0b010bc5
Dawid Ziobro <dziobro@vuestorefront.io>
2022-08-05 13:18:08 +0200

    chore: added tailwind to remix

--- boilerplates/remix/app/root.tsx
+++ boilerplates/remix/app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
--- boilerplates/remix/app/routes/index.tsx
+++ boilerplates/remix/app/routes/index.tsx
@@ -3,7 +3,7 @@ import Button from '~/output/blocks/Button.lite';
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-6xl font-bold text-white text-center mt-28">Welcome to Remix</h1>
       <Button text="test button"/>
     </div>
   );
