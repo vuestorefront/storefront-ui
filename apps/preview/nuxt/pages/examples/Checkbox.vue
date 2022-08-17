4e55f7f4d2f4f50f661175b45af853f5c3bf01b4
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-17 14:00:21 +0200

    chore: cleanup

--- apps/preview/nuxt/pages/examples/Checkbox.vue
+++ apps/preview/nuxt/pages/examples/Checkbox.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="e-page">
     <div class="e-page-component">
-      <checkbox-base name="chkbx" :label="'label'" :rightCheckbox="true"/>
       <Checkbox :name="'custom'" label="label" :rightCheckbox="true" :indeterminate="true"></Checkbox>
     </div>
     <div class="e-page-controls">
@@ -10,14 +9,12 @@
 </template>
 
 <script>
-import CheckboxBase from '../../components/CheckboxBase.vue';
 import Checkbox from '../../output/blocks/Checkbox.vue';
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: 'CheckboxExample',
   components: {
-    CheckboxBase,
     Checkbox
   }
 });
