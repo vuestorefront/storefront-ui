e56cb6b3a22ff137b788eda2ff0db41f4b06af66
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2022-08-17 15:47:41 +0200

    fix(remix): clean up scripts, load fonts

--- apps/preview/nuxt/pages/examples.vue
+++ apps/preview/nuxt/pages/examples.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="e-page-examples">
-    <div class="sidebar" :class="isOpen && 'sidebar-collapsed'" v-if="!isDocs">
+    <div class="sidebar" :class="!isOpen && 'sidebar-collapsed'" v-if="!isDocs">
       <button class="sidebar-toggle" :class="isOpen && 'sidebar-toggle-collapsed'" @click="isOpen = !isOpen">&lt;&lt;</button>
       <h2 class="sidebar-heading">Components</h2>
       <ul class="sidebar-list">
@@ -17,6 +17,6 @@ import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 const { currentRoute } = useRouter();
 
-const isOpen = ref();
+const isOpen = ref(true);
 const isDocs = computed(() => currentRoute.value.query.docs)
 </script>
