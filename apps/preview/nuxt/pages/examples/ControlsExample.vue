bf2652beef8874a161721c3d4c71bdbe5bfd9c1c
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2022-08-23 10:43:03 +0200

    feat: create Control component for each framework

---
+++ apps/preview/nuxt/pages/examples/ControlsExample.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="e-page">
+    <div class="e-page-component">
+      <b>Text</b>
+      <div>textModel - {{ textModel }}</div>
+      <b>Boolean</b>
+      <div>booleanModel - {{ booleanModel }}</div>
+      <b>Range</b>
+      <div>rangeModel - {{ rangeModel }}</div>
+      <b>Select</b>
+      <div>selectModel - {{ selectModel }}</div>
+      <div>selectModel2 - {{ selectModel2 }}</div>
+      <b>Checkbox</b>
+      <div>checkboxModel - {{ checkboxModel }}</div>
+      <div>checkboxModel2 - {{ checkboxModel2 }}</div>
+      <div>checkboxModelTrueFalse - {{ checkboxModelTrueFalse }}</div>
+      <b>Radio</b>
+      <div>radioModel - {{ radioModel }}</div>
+      <div>radioModel2 - {{ radioModel2 }}</div>
+    </div>
+    <div class="e-page-controls">
+      <Controls v-bind="bind"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import Controls, { prepareControls } from '../../components/utils/Controls.vue';
+
+export default {
+  name: 'ControlsExample',
+  components: { Controls },
+  setup() {
+    return prepareControls([
+        {
+          title: 'Text',
+          type: 'text',
+          modelName: 'textModel',
+        },
+        {
+          title: 'Boolean',
+          type: 'boolean',
+          modelName: 'booleanModel',
+        },
+        {
+          title: 'Range Default',
+          type: 'range',
+          modelName: 'rangeModel',
+        },
+        {
+          title: 'Range step 10',
+          type: 'range',
+          modelName: 'rangeModel',
+          options: [{
+            bind: {
+              step: 10,
+            },
+          }],
+        },
+        {
+          title: 'Select options object',
+          type: 'select',
+          modelName: 'selectModel',
+          options: [{
+            label: 'Label1',
+            value: 'value1',
+          }, {
+            label: 'Label2',
+            value: 'value2',
+          }],
+        },
+        {
+          title: 'Select options Array',
+          type: 'select',
+          modelName: 'selectModel2',
+          options: ['Label1', 'Label2'],
+        },
+        {
+          title: 'Checkbox option object',
+          type: 'checkbox',
+          modelName: 'checkboxModel',
+          options: [{
+            label: 'Label1',
+            value: 'value1',
+          }, {
+            label: 'Label2',
+            value: 'value2',
+          }, {
+            label: 'Disabled',
+            value: 'value3',
+            bind: {
+              disabled: true
+            }
+          }],
+        },
+        {
+          title: 'Checkbox options Array',
+          type: 'checkbox',
+          modelName: 'checkboxModel2',
+          options: ['Label1', 'Label2'],
+        },
+        {
+          title: 'Checkbox true/false from string',
+          type: 'checkbox',
+          modelName: 'checkboxModelTrueFalse',
+          options: ['SomeString'],
+        },
+        {
+          title: 'Radio options object',
+          type: 'radio',
+          modelName: 'radioModel',
+          options: [{
+            label: 'Label1',
+            value: 'value1',
+          }, {
+            label: 'Label2',
+            value: 'value2',
+          }],
+        },
+        {
+          title: 'Radio options Array',
+          type: 'radio',
+          modelName: 'radioModel2',
+          options: ['Label1', 'Label2'],
+        },
+      ], {
+        textModel: ref('Default'),
+        booleanModel: ref(false),
+        rangeModel: ref(10),
+        selectModel: ref(),
+        selectModel2: ref(),
+        checkboxModel: ref([]),
+        checkboxModel2: ref([]),
+        checkboxModelTrueFalse: ref(''),
+        radioModel: ref(),
+        radioModel2: ref()
+      })
+  }
+};
+</script>
--- apps/preview/nuxt/pages/examples/VsfProgress.vue
+++ apps/preview/nuxt/pages/examples/VsfProgress.vue
@@ -1,165 +1,62 @@
 <template>
   <div class="e-page">
     <div class="e-page-component">
-      <VsfProgress :value="valueModel" :variant="variantModel" :type="typeModel" />
-
-      {{ dummyModel }}
-      {{ dummyModel2 }}
-      <div>dummyInputModel {{ dummyInputModel }}</div>
-      <div>dummyInputModel2 {{ dummyInputModel2 }}</div>
-      <div>dummyRadioModel {{ dummyRadioModel }}</div>
-      <div>dummyRadioModel2 {{ dummyRadioModel2 }}</div>
-      <div>textModel {{ textModel }}</div>
-      <div>boolModel {{ boolModel }}</div>
+      <VsfProgress :value="valueModel" :variant="variantModel" :type="typeModel" :label="labelModel" />
     </div>
     <div class="e-page-controls">
-      <ExampleStory
-        v-bind="bind"
-        v-on="on"
-      />
+      <Controls v-bind="bind"/>
     </div>
   </div>
 </template>
 
-<script>
-import { ref } from '@vue/composition-api';
+<script lang="ts">
+import { ref } from 'vue';
 // eslint-disable-next-line import/no-relative-packages, import/no-unresolved
 import VsfProgress, { VsfProgressVariants, VsfProgressTypes } from '../../output/blocks/VsfProgress/VsfProgress.vue';
-import ExampleStory from '../../utils/exampleStory.vue';
-
-const prepareControls = (controlsObject, models) => {
-  const on = {};
-  const controls = controlsObject.map((control) => {
-    on[`update:${control.modelName}`] = (e) => { models[control.modelName] = e; };
-    return { ...control, model: models[control.modelName] };
-  });
-
-  return {
-    bind: {
-      controls,
-    },
-    on,
-    ...toRefs(reactive(models)),
-  };
-};
-
+import Controls, { prepareControls } from '../../components/utils/Controls.vue';
 
 export default {
   name: 'VsfProgressExample',
-  components: { VsfProgress },
+  components: { VsfProgress, Controls },
   setup() {
-    return {
-      VsfProgressVariants,
-      VsfProgressTypes,
-      propValue: ref(0),
-      variantModel: ref(),
-      typeModel: ref(),
-      ...prepareControls([
+    return prepareControls([
         {
-          title: 'Text',
+          title: 'Label',
           type: 'text',
-          modelName: 'textModel',
-        },
-        {
-          title: 'Boolean',
-          type: 'boolean',
-          modelName: 'boolModel',
+          modelName: 'labelModel',
+          description: 'Prop used only to set aria-label attribute',
+          propDefaultValue: 'Loading',
+          propType: 'string',
         },
         {
           title: 'Value',
           type: 'range',
           modelName: 'valueModel',
-        },
-        {
-          title: 'Dummy range',
-          type: 'range',
-          modelName: 'valueModel',
-          options: [{
-            bind: {
-              step: 10,
-            },
-          }],
+          propDefaultValue: 0,
+          propType: 'number'
         },
         {
           title: 'Variant',
           type: 'select',
           modelName: 'variantModel',
           options: Object.keys(VsfProgressVariants),
-        },
-        {
-          title: 'Dummy select',
-          type: 'select',
-          modelName: 'dummyModel',
-          options: [{
-            label: 'dimmy1',
-            value: 'dummy1',
-          }, {
-            label: 'dimmy2',
-            value: 'dummy2',
-          }],
-        },
-        {
-          title: 'Dummy select Array',
-          type: 'select',
-          modelName: 'dummyModel2',
-          options: ['dimmy1', 'dimmy2'],
-        },
-        {
-          title: 'Dummy checkbox',
-          type: 'checkbox',
-          modelName: 'dummyInputModel',
-          options: [{
-            label: 'dimmy1',
-            value: 'dummy1',
-          }, {
-            label: 'dimmy2',
-            value: 'dummy2',
-          }],
-        },
-        {
-          title: 'Dummy checkbox',
-          type: 'checkbox',
-          modelName: 'dummyInputModel2',
-          options: ['dimmy1', 'dimmy2'],
+          propDefaultValue: VsfProgressVariants.base,
+          propType: 'VsfProgressVariants'
         },
         {
           title: 'Type',
-          type: 'radio',
+          type: 'select',
           modelName: 'typeModel',
           options: Object.keys(VsfProgressTypes),
-        },
-        {
-          title: 'Dummy radio',
-          type: 'radio',
-          modelName: 'dummyRadioModel',
-          options: [{
-            label: 'radio1',
-            value: 'radio1',
-          }, {
-            label: 'radio2',
-            value: 'radio2',
-          }],
-        },
-        {
-          title: 'Dummy radio2',
-          type: 'radio',
-          modelName: 'dummyRadioModel2',
-          options: ['radio1', 'radio2'],
+          propDefaultValue: VsfProgressTypes.circle,
+          propType: 'VsfProgressTypes'
         },
       ], {
-        boolModel: ref(),
+        labelModel: ref(),
         valueModel: ref(),
-        dummyModel: ref(),
-        dummyModel2: ref(),
-        dummyInputModel: ref([]),
-        dummyInputModel2: ref([]),
-        dummyRadioModel: ref([]),
-        dummyRadioModel2: ref([]),
         variantModel: ref(),
         typeModel: ref(),
-        textModel: ref(''),
-      }),
-    };
-  },
+      })
+  }
 };
 </script>
