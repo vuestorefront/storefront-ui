acb7542670b79c9c02d2372932146af69f7848ef
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-23 09:33:20 +0200

    feat: nuxt auto-import components

--- apps/preview/nuxt/pages/examples.vue
+++ apps/preview/nuxt/pages/examples.vue
@@ -4,7 +4,7 @@
       <button class="sidebar-toggle" :class="isOpen && 'sidebar-toggle-collapsed'" @click="isOpen = !isOpen">&lt;&lt;</button>
       <h2 class="sidebar-heading">Components</h2>
       <ul class="sidebar-list" >
-        <li v-for="component in components"><NuxtLink :to="`/examples/${component}`">{{component}}</NuxtLink></li>
+        <li v-for="(component, idx) in components" :key="idx"><NuxtLink :to="`/examples/${component}`">{{component}}</NuxtLink></li>
       </ul>
     </div>
     <NuxtPage />
@@ -16,7 +16,9 @@ import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 const { currentRoute } = useRouter();
 
+const files = import.meta.glob('./examples/*.vue');
+const components = Object.keys(files).map((file) => file.match(/([\w\d_-]*)\.?[^\\\/]*$/i)[1]).sort()
+
 const isOpen = ref();
 const isDocs = computed(() => currentRoute.value.query.docs);
-const components = ['VsfProgress'].sort(); //Generate component list, because we need sorted list
 </script>
