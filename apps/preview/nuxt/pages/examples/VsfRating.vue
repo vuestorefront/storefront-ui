2e61752192fbd89cb4d2b5711787351c2532aca3
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-23 14:58:57 +0200

    feat: rating base

---
+++ apps/preview/nuxt/pages/examples/VsfRating.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="e-page">
+    <div class="e-page-component">
+      <RatingBase />
+    </div>
+    <div class="e-page-controls"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import RatingBase from "../../../../../.stale/src/components/Rating/RatingBase/RatingBase.vue";
+
+export default defineComponent({
+  name: 'VsfRatingExample',
+  components: {
+    RatingBase
+  },
+  data() {
+    return {
+    };
+  },
+});
+</script>
