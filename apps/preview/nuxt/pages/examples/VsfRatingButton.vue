672b528940f6838fa46b4a17e733d9d406d7ddc4
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-24 14:21:11 +0200

    feat: rating-button styles

---
+++ apps/preview/nuxt/pages/examples/VsfRatingButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="e-page">
+    <div class="e-page-component">
+      <VsfRatingButton v-model="valueModel" :max="10"  size="lg" />
+      <p>{{ valueModel }}</p>
+    </div>
+    <div class="e-page-controls"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import VsfRatingButton from '../../output/blocks/VsfRatingButton/VsfRatingButton.vue';
+
+export default defineComponent({
+  name: 'VsfRatingButtonExample',
+  components: {
+    VsfRatingButton,
+  },
+  setup() {
+    return {
+      valueModel: ref(1),
+      handler: (item) => {
+        console.log(item);
+      },
+    };
+  },
+});
+</script>
