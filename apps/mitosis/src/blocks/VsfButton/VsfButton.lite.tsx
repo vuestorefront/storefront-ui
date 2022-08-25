02290dca43f09ce039d8a5dbf7c03e0bba82c5f9
Anna Musial <ania.musial13@gmail.com>
2022-08-25 11:02:12 +0200

    chore: code review improvements

--- apps/mitosis/src/blocks/VsfButton/VsfButton.lite.tsx
+++ apps/mitosis/src/blocks/VsfButton/VsfButton.lite.tsx
@@ -45,12 +45,12 @@ export default function VsfButton(props: VsfButtonProps) {
       return classStringFromArray(['inline-flex items-center justify-center border rounded-md cursor-pointer font-body disabled:cursor-not-allowed outline-violet disabled:text-gray-500 disabled:opacity-50',
       props.size === VsfButtonSizes.sm ? 'leading-5 px-3 py-[6px] text-sm' : props.size === VsfButtonSizes.base ? 'px-4 py-2' : 'px-6 py-3',
       props.variant === VsfButtonVariants.tertiary || props.variant === VsfButtonVariants['destroy-tertiary'] ? 'font-normal bg-transparent disabled:bg-transparent' : 'font-medium uppercase shadow-base disabled:bg-gray-200 disabled:shadow-none',
-      props.variant === VsfButtonVariants.primary ? 'bg-primary-500 text-white hover:bg-primary-600 hover:shadow-medium active:bg-primary-700' : '',
-      props.variant === VsfButtonVariants.secondary ? 'bg-white border border-gray-200 text-primary-500 hover:border-primary-300 hover:bg-green-100 hover:text-primary-600 hover:shadow-medium active:border-primary-400 active:bg-green-200 active:text-primary-700 ' : '',
-      props.variant === VsfButtonVariants.tertiary ? 'bg-transparent text-primary-500 hover:bg-green-100 hover:text-primary-600 active:text-primary-700 active:bg-green-200' : '',
-      props.variant === VsfButtonVariants['destroy-primary'] ? 'text-base text-white bg-rose-600 hover:bg-rose-700 active:bg-rose-800 hover:shadow-medium' : '',
-      props.variant === VsfButtonVariants['destroy-secondary'] ? 'text-base text-rose-600 bg-white border border-rose-400 disabled:border-0 hover:bg-rose-100 active:bg-rose-200 hover:text-rose-600 active:text-rose-700 hover:shadow-medium' : '',
-      props.variant === VsfButtonVariants['destroy-tertiary'] ? 'text-base text-rose-600 hover:bg-rose-100 hover:text-rose-700 active:bg-rose-200 active:text-rose-800' : '',
+      props.variant === VsfButtonVariants.primary && 'bg-primary-500 text-white hover:bg-primary-600 hover:shadow-medium active:bg-primary-700',
+      props.variant === VsfButtonVariants.secondary && 'bg-white border border-gray-200 text-primary-500 hover:border-primary-300 hover:bg-green-100 hover:text-primary-600 hover:shadow-medium active:border-primary-400 active:bg-green-200 active:text-primary-700 ',
+      props.variant === VsfButtonVariants.tertiary && 'bg-transparent text-primary-500 hover:bg-green-100 hover:text-primary-600 active:text-primary-700 active:bg-green-200',
+      props.variant === VsfButtonVariants['destroy-primary'] && 'text-base text-white bg-rose-600 hover:bg-rose-700 active:bg-rose-800 hover:shadow-medium',
+      props.variant === VsfButtonVariants['destroy-secondary'] && 'text-base text-rose-600 bg-white border border-rose-400 disabled:border-0 hover:bg-rose-100 active:bg-rose-200 hover:text-rose-600 active:text-rose-700 hover:shadow-medium',
+      props.variant === VsfButtonVariants['destroy-tertiary'] && 'text-base text-rose-600 hover:bg-rose-100 hover:text-rose-700 active:bg-rose-200 active:text-rose-800',
   ]);
     },
   });
