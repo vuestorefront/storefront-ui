147130c29abd2d02815f06498cf2083f39cc5fdc
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-25 15:14:00 +0200

    feat: tooltip types

--- apps/mitosis/src/blocks/VsfTooltip/VsfTooltip.lite.tsx
+++ apps/mitosis/src/blocks/VsfTooltip/VsfTooltip.lite.tsx
@@ -1,22 +1,71 @@
-import { Show, useStore, useRef, For, onUnMount, onUpdate, onMount } from '@builder.io/mitosis';
+import { useStore } from '@builder.io/mitosis';
 
+export const VsfTooltipTypes = Object.freeze({
+  top: 'top',
+  bottom: 'bottom',
+  left: 'left',
+  right: 'right',
+});
+
+type VsfTooltipTypesKeys = keyof typeof VsfTooltipTypes;
 
 export interface VsfTooltipProps {
+  children?: Element | Element[] | string;
+  label?: string;
+  type?: VsfTooltipTypesKeys;
 }
 
 const DEFAULT_VALUES = {
+  label: '',
+  type: VsfTooltipTypes.top,
 };
 
 export default function VsfTooltip(props: VsfTooltipProps) {
-
   const state = useStore({
-
+    get useLabelProp() {
+      return props.label || DEFAULT_VALUES.label;
+    },
+    get useTypeProp() {
+      return props.type || DEFAULT_VALUES.type;
+    },
+    get tooltipClass() {
+      switch (state.useTypeProp) {
+        case VsfTooltipTypes.bottom:
+          return 'left-1/2 -translate-x-1/2 translate-y-2 top-full';
+        case VsfTooltipTypes.left:
+          return '-left-full -translate-x-2 -translate-y-1/2 top-1/2';
+        case VsfTooltipTypes.right:
+          return 'left-full translate-x-2 -translate-y-1/2 top-1/2';
+        default:
+          return 'left-1/2 -translate-x-1/2 translate-y-2 -top-full';
+      }
+    },
+    get tooltipCaretClass() {
+      switch (state.useTypeProp) {
+        case VsfTooltipTypes.bottom:
+          return 'bottom-full -mb-1';
+        case VsfTooltipTypes.left:
+          return 'left-full -ml-1';
+        case VsfTooltipTypes.right:
+          return 'left-0 -ml-1';
+        default:
+          return 'bottom-0 -mb-1';
+      }
+    },
   });
-
 
   return (
     <>
-     <div></div>
+      <div class="relative inline-flex flex-col items-center group">
+        {props.children}
+        <div
+          role="tooltip"
+          class={`absolute justify-center items-center hidden mb-14 group-hover:flex z-50 bg-gray-500 font-body font-medium text-white text-sm leading-5 rounded-md px-3 py-1.5 ${state.tooltipClass}`}
+        >
+          <div>{state.useLabelProp}</div>
+          <div class={`absolute w-2 h-2 rotate-45 bg-gray-500 ${state.tooltipCaretClass}`}></div>
+        </div>
+      </div>
     </>
   );
 }
--- apps/preview/nuxt/pages/examples/VsfTooltip.vue
+++ apps/preview/nuxt/pages/examples/VsfTooltip.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="e-page">
-    <div class="e-page-component">
-      <VsfTooltip />
+    <div class="e-page-component pt-12">
+      <VsfTooltip label="Tooltip" type="left">
+        <button class="p-4 font-semibold text-sm bg-primary-600 text-white rounded-none shadow-sm uppercase">hover me</button>
+      </VsfTooltip>
     </div>
     <div class="e-page-controls"></div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import VsfTooltip from "../../output/blocks/VsfTooltip/VsfTooltip.vue";
+import { defineComponent } from 'vue';
+import VsfTooltip from '../../output/blocks/VsfTooltip/VsfTooltip.vue';
 
 export default defineComponent({
   name: 'VsfTooltipExample',
   components: {
-    VsfTooltip
+    VsfTooltip,
   },
 });
 </script>
