fb687a88789e328e8a3312a65da24cc5341f6e18
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-25 08:42:56 +0200

    feat: tooltip

---
+++ apps/mitosis/src/blocks/VsfTooltip/VsfTooltip.lite.tsx
@@ -0,0 +1,22 @@
+import { Show, useStore, useRef, For, onUnMount, onUpdate, onMount } from '@builder.io/mitosis';
+
+
+export interface VsfTooltipProps {
+}
+
+const DEFAULT_VALUES = {
+};
+
+export default function VsfTooltip(props: VsfTooltipProps) {
+
+  const state = useStore({
+
+  });
+
+
+  return (
+    <>
+     <div></div>
+    </>
+  );
+}
---
+++ apps/preview/nuxt/pages/examples/VsfTooltip.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="e-page">
+    <div class="e-page-component">
+      <VsfTooltip />
+    </div>
+    <div class="e-page-controls"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import VsfTooltip from "../../output/blocks/VsfTooltip/VsfTooltip.vue";
+
+export default defineComponent({
+  name: 'VsfTooltipExample',
+  components: {
+    VsfTooltip
+  },
+});
+</script>
