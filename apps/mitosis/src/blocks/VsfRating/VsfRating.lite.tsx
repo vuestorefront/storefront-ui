28a6d91a00ee900e0ed0805be1e6cccf84bf7d70
Andrzej Kurek <akurek@vuestorefront.io>
2022-08-25 10:48:24 +0200

    refactor: extract component shared parts

--- apps/mitosis/src/blocks/VsfRating/VsfRating.lite.tsx
+++ apps/mitosis/src/blocks/VsfRating/VsfRating.lite.tsx
@@ -82,31 +82,28 @@ export default function VsfRating(props: VsfRatingProps) {
         '2xl': 'text-lg',
       }[state.useSizeProp];
     },
+    get isBaseType() {
+      return state.useTypeProp === VsfRatingTypes.base;
+    },
   });
 
   return (
     <>
-      <Show
-        when={state.useTypeProp === VsfRatingTypes.base}
-        else={
-          <div
-            class={`flex items-center h-6 font-normal pointer-events-none fill-warning-500 font-body ${state.textClass}`}
-            role="img"
-          >
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class={state.sizeClass}>
-              <path d="M12.0072 17.6975L16.1572 20.2075C16.9172 20.6675 17.8472 19.9875 17.6472 19.1275L16.5472 14.4075L20.2172 11.2275C20.8872 10.6475 20.5272 9.5475 19.6472 9.4775L14.8172 9.0675L12.9272 4.6075C12.5872 3.7975 11.4272 3.7975 11.0872 4.6075L9.19715 9.0575L4.36715 9.4675C3.48715 9.5375 3.12715 10.6375 3.79715 11.2175L7.46715 14.3975L6.36715 19.1175C6.16715 19.9775 7.09715 20.6575 7.85715 20.1975L12.0072 17.6975Z" />
-            </svg>
-            <span class="mx-1 font-medium">{state.ratingValue}</span>
-            <Show when={state.useReviewsProp >= 0}>
-              <span class="text-gray-500">({state.useReviewsProp})</span>
-            </Show>
-          </div>
-        }
+      <div
+        class={`flex items-center h-6 font-normal pointer-events-none fill-warning-500 font-body ${state.textClass}`}
+        role="img"
+        aria-label={`Rating: ${state.useValueProp} out of ${state.useMaxProp} stars`}
       >
-        <div
-          class="flex items-center h-6 font-normal pointer-events-none fill-warning-500 font-body"
-          role="img"
-          aria-label={`Rating: ${state.useValueProp} out of ${state.useMaxProp} stars`}
+        <Show
+          when={state.isBaseType}
+          else={
+            <>
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class={state.sizeClass}>
+                <path d="M12.0072 17.6975L16.1572 20.2075C16.9172 20.6675 17.8472 19.9875 17.6472 19.1275L16.5472 14.4075L20.2172 11.2275C20.8872 10.6475 20.5272 9.5475 19.6472 9.4775L14.8172 9.0675L12.9272 4.6075C12.5872 3.7975 11.4272 3.7975 11.0872 4.6075L9.19715 9.0575L4.36715 9.4675C3.48715 9.5375 3.12715 10.6375 3.79715 11.2175L7.46715 14.3975L6.36715 19.1175C6.16715 19.9775 7.09715 20.6575 7.85715 20.1975L12.0072 17.6975Z" />
+              </svg>
+              <span class="mx-1">{state.ratingValue}</span>
+            </>
+          }
         >
           <For each={[...Array(state.filled)]}>
             {(_, index) => (
@@ -139,11 +136,11 @@ export default function VsfRating(props: VsfRatingProps) {
               </svg>
             )}
           </For>
-          <Show when={state.useReviewsProp >= 0}>
-            <span class={`ml-1 text-gray-500 ${state.textClass}`}>({state.useReviewsProp})</span>
-          </Show>
-        </div>
-      </Show>
+        </Show>
+        <Show when={state.useReviewsProp >= 0}>
+          <span class={`text-gray-500 ` + (state.isBaseType ? 'ml-1' : '')}>({state.useReviewsProp})</span>
+        </Show>
+      </div>
     </>
   );
 }
