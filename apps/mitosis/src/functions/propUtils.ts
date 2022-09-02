d928096174cb9d7eb5bfa606f50bd55f6fb45234
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2022-09-02 11:50:50 +0200

    chore: update mitosis version

--- apps/mitosis/src/functions/propUtils.ts
+++ apps/mitosis/src/functions/propUtils.ts
@@ -1,14 +1,8 @@
-export function validator(valid, value, defaultValue) {
+export function validator<T>(
+  valid: string[],
+  value: T | undefined,
+  defaultValue: T
+) {
   if (!value) return defaultValue;
-  return valid.includes(value) ? value : defaultValue;
-}
-
-// keep it typed when supported
-// export function validator<T>(
-//   valid: string[],
-//   value: T | undefined,
-//   defaultValue: T
-// ) {
-//   if (!value) return defaultValue;
-//   return (valid as unknown as T[]).includes(value) ? value : defaultValue;
-// };
+  return (valid as unknown as T[]).includes(value) ? value : defaultValue;
+};
