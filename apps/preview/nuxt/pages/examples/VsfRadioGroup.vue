33ce39031570435a92fa74af065dd28c72d2f902
ljedrasik <ljedrasik@vuestorefront.io>
2022-09-14 11:38:21 +0200

    chore: add radioGroup component

--- apps/preview/nuxt/pages/examples/VsfRadioGroup.vue
+++ apps/preview/nuxt/pages/examples/VsfRadioGroup.vue
@@ -1,10 +1,27 @@
-
 <template>
   <div class="e-page">
     <div class="e-page-component">
       <VsfRadioGroup
-
+          :direction="directionModel"
+          :required="requiredModel"
+          :requiredText="requiredTextModel"
+          :errorText="errorTextModel"
+          :helpText="helpTextModel"
+          :isInvalid="invalidModel"
+          :variant="variantModel"
       >
+        <VsfRadio
+          v-for="radio in radioModel"
+          :key="radio.id"
+          :id="radio.id"
+          :value="radio.value"
+          v-model="valueModel"
+          :variant="variantModel"
+          helpText=""
+          :selected="selectedModel"
+          :invalid="invalidModel"
+          @change="selectedModel = $event.target.value"
+        />
       </VsfRadioGroup>
     </div>
     <div class="e-page-controls">
@@ -15,20 +32,100 @@
 
 <script>
 import { ref } from 'vue';
-import VsfRadioGroup from '../../output/blocks/VsfRadioGroup/VsfRadioGroup.vue'
+import VsfRadioGroup, { VsfRadioGroupVariants, VsfRadioGroupDirection } from '../../output/blocks/VsfRadioGroup/VsfRadioGroup.vue'
 import Controls, { prepareControls } from '../../components/utils/Controls.vue';
+import VsfRadio, { VsfRadioVariants } from "../../output/blocks/VsfRadio/VsfRadio";
 
 export default {
   name: 'VsfRadioGroupExample',
   components: {
+    VsfRadio,
     Controls,
     VsfRadioGroup
   },
   setup() {
     return {
       ...prepareControls([
-          ], {
-        variantModel: ref()
+        {
+          title: 'Direction',
+          type: 'select',
+          modelName: 'directionModel',
+          options: Object.keys(VsfRadioGroupDirection),
+          propDefaultValue: VsfRadioGroupDirection.vertical,
+          propType: 'VsfRadioVariants',
+        },
+        {
+          title: 'Variant',
+          type: 'select',
+          modelName: 'variantModel',
+          options: Object.keys(VsfRadioVariants),
+          propDefaultValue: VsfRadioVariants.base,
+          propType: 'VsfRadioVariants',
+        },
+        {
+          title: 'Required',
+          type: 'boolean',
+          modelName: 'requiredModel',
+          propType: 'boolean',
+          propDefaultValue: 'false',
+        },
+        {
+          title: 'Required Text',
+          type: 'text',
+          modelName: 'requiredTextModel',
+          propType: 'string',
+          propDefaultValue: '*Required',
+        },
+        {
+          title: 'Error Text',
+          type: 'text',
+          modelName: 'errorTextModel',
+          propType: 'string',
+          propDefaultValue: 'Error message',
+        },
+        {
+          title: 'HelpText',
+          type: 'text',
+          modelName: 'helpTextModel',
+          propType: 'string',
+          propDefaultValue: 'Help text',
+        },
+        {
+          title: 'Value',
+          type: 'text',
+          modelName: 'valueModel',
+          propType: 'string',
+          propDefaultValue: 'string',
+          description: "v-model prop",
+        },
+        {
+          title: 'Selected',
+          type: 'text',
+          modelName: 'selectedModel',
+          propType: 'string',
+          propDefaultValue: '',
+        },
+        {
+          title: 'Invalid',
+          type: 'boolean',
+          modelName: 'invalidModel',
+          propType: 'boolean',
+          propDefaultValue: 'false',
+        },
+      ], {
+        radioModel: ref([
+          { id: 'radio-1',  name: VsfRadioGroupVariants.base ? 'radio-group-1' : `radio-${state.useVariantProp}-group-1`, value: 'one'},
+          { id: 'radio-2',  name: VsfRadioGroupVariants.base ? 'radio-group-2' : `radio-${state.useVariantProp}-group-2`, value: 'two'},
+        ]),
+        directionModel: ref(VsfRadioGroupDirection.vertical),
+        variantModel: ref(VsfRadioVariants.base),
+        requiredModel: ref(false),
+        requiredTextModel: ref('*Required'),
+        errorTextModel: ref('Error message'),
+        helpTextModel: ref('Help text'),
+        selectedModel: ref(''),
+        valueModel: ref(''),
+        invalidModel: ref(false),
       })
     }
   }
--- apps/preview/remix/app/routes/examples/VsfRadioGroup.tsx
+++ apps/preview/remix/app/routes/examples/VsfRadioGroup.tsx
@@ -1,19 +1,112 @@
-
-import VsfRadioGroup from '../../output/blocks/VsfRadioGroup/VsfRadioGroup.lite';
+import VsfRadioGroup, { VsfRadioGroupVariants, VsfRadioGroupDirection } from '../../output/blocks/VsfRadioGroup/VsfRadioGroup.lite';
 import Controls, { prepareControls } from '../../components/utils/Controls';
+import VsfRadio, { VsfRadioVariants } from "../../output/blocks/VsfRadio/VsfRadio";
 
 export default function VsfRadioGroupExample() {
   const { state, controls } = prepareControls([
-      ], {
-      });
+    {
+      title: 'Direction',
+      type: 'select',
+      modelName: 'directionModel',
+      options: Object.keys(VsfRadioGroupDirection),
+      propDefaultValue: VsfRadioGroupDirection.vertical,
+      propType: 'VsfRadioVariants',
+    },
+    {
+      title: 'Variant',
+      type: 'select',
+      modelName: 'variantModel',
+      options: Object.keys(VsfRadioVariants),
+      propDefaultValue: VsfRadioVariants.base,
+      propType: 'VsfRadioVariants',
+    },
+    {
+      title: 'Required',
+      type: 'boolean',
+      modelName: 'requiredModel',
+      propType: 'boolean',
+      propDefaultValue: 'false',
+    },
+    {
+      title: 'Required Text',
+      type: 'text',
+      modelName: 'requiredTextModel',
+      propType: 'string',
+      propDefaultValue: '*Required',
+    },
+    {
+      title: 'Error Text',
+      type: 'text',
+      modelName: 'errorTextModel',
+      propType: 'string',
+      propDefaultValue: 'Error message',
+    },
+    {
+      title: 'HelpText',
+      type: 'text',
+      modelName: 'helpTextModel',
+      propType: 'string',
+      propDefaultValue: 'Help text',
+    },
+    {
+      title: 'Selected',
+      type: 'text',
+      modelName: 'selectedModel',
+      propType: 'string',
+      propDefaultValue: '',
+    },
+    {
+      title: 'Invalid',
+      type: 'boolean',
+      modelName: 'invalidModel',
+      propType: 'boolean',
+      propDefaultValue: 'false',
+    },
+  ], {
+    radioModel: [
+      { id: 'radio-1',  name: VsfRadioGroupVariants.base ? 'radio-group-1' : `radio-${state.useVariantProp}-group-1`, value: 'one'},
+      { id: 'radio-2',  name: VsfRadioGroupVariants.base ? 'radio-group-2' : `radio-${state.useVariantProp}-group-2`, value: 'two'},
+    ],
+    directionModel: VsfRadioGroupDirection.vertical,
+    variantModel: VsfRadioVariants.base,
+    requiredModel: false,
+    requiredTextModel: '*Required',
+    errorTextModel: 'Error message',
+    helpTextModel: 'Help text',
+    selectedModel: '',
+    invalidModel: false,
+  });
+  function onChangeHandler(event) {
+    state.set({
+      ...state.get,
+      selectedModel: event.target.value
+    });
+  }
   
   return (
     <div className="e-page">
       <div className="e-page-component">
         <VsfRadioGroup
-
+          direction={state.get.directionModel}
+          required={state.get.requiredModel}
+          requiredText={state.get.requiredTextModel}
+          errorText={state.get.errorTextModel}
+          helpText={state.get.helpTextModel}
+          isInvalid={state.get.invalidModel}
+          variant={state.get.variantModel}
         >
-
+          {state.get.radioModel.map(radio => (
+            <VsfRadio
+              key={radio.id}
+              id={radio.id}
+              value={radio.value}
+              variant={state.get.variantModel}
+              helpText=""
+              selected={state.get.selectedModel}
+              invalid={state.get.invalidModel}
+              onChange={onChangeHandler}
+            />
+          ))}
         </VsfRadioGroup>
       </div>
       <div className="e-page-controls">
