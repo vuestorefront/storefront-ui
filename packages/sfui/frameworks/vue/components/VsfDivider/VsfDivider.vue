20895b918633d8bf1b54bd1ddf02315a00264aad
Andrzej Kurek <akurek@vuestorefront.io>
2022-09-27 15:47:15 +0200

    fix: cr

--- packages/sfui/frameworks/vue/components/VsfDivider/VsfDivider.vue
+++ packages/sfui/frameworks/vue/components/VsfDivider/VsfDivider.vue
@@ -1,10 +1,3 @@
 <template>
   <hr class="vsf-divider" />
 </template>
-
-<script lang="ts" setup>
-import {} from 'vue';
-import type { VsfDividerProps } from './types';
-
-const props = defineProps({});
-</script>
