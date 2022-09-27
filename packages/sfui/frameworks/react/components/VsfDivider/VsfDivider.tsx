e3263df9638c5b79276dde51b311ab98049cc600
Andrzej Kurek <akurek@vuestorefront.io>
2022-09-27 15:55:26 +0200

    feat: classnames lib

--- packages/sfui/frameworks/react/components/VsfDivider/VsfDivider.tsx
+++ packages/sfui/frameworks/react/components/VsfDivider/VsfDivider.tsx
@@ -1,5 +1,6 @@
 import type { VsfDividerProps } from './types';
+import classnames from 'classnames';
 
-export default function VsfDivider({ className = '', ...attributes }: VsfDividerProps) {
-  return <hr className={`vsf-divider ${className}`} {...attributes} />;
+export default function VsfDivider({ className, ...attributes }: VsfDividerProps) {
+  return <hr className={classnames('vsf-divider', className)} {...attributes} />;
 }
