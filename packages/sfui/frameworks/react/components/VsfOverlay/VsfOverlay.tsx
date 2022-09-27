35a01e385775f5540cd4482f4188ddd66ae4e716
Andrzej Kurek <akurek@vuestorefront.io>
2022-09-27 17:26:32 +0200

    fix: missing class

--- packages/sfui/frameworks/react/components/VsfOverlay/VsfOverlay.tsx
+++ packages/sfui/frameworks/react/components/VsfOverlay/VsfOverlay.tsx
@@ -2,6 +2,6 @@ import classNames from 'classnames';
 import type { VsfOverlayProps } from './types';
 
 export default function VsfOverlay({ onClick, visible, absolute, className, ...attributes }: VsfOverlayProps) {
-  const overlayClasses = classNames('', absolute ? 'absolute' : 'fixed', className);
+  const overlayClasses = classNames('vsf-overlay', absolute ? 'absolute' : 'fixed', className);
   return <>{visible && <div onClick={onClick} className={overlayClasses} {...attributes} />}</>;
 }
