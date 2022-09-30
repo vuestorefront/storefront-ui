e77b5f7b7188efdf2be689127cd00f723f564d31
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2022-09-30 09:52:03 +0200

    chore: update after rebase

--- apps/preview/next/pages/examples/VsfOverlay.tsx
+++ apps/preview/next/pages/examples/VsfOverlay.tsx
@@ -1,45 +1,43 @@
 import VsfOverlay from '@sfui/sfui/frameworks/react/components/VsfOverlay';
 import Controls, { prepareControls } from '../../components/utils/Controls';
 import { ExamplePageLayout } from '../examples';
+import type { VsfOverlayProps } from '@sfui/sfui/frameworks/react/components/VsfOverlay/types';
 
-function Example() {
-  const { state, controls } = prepareControls(
+const Example = () => {
+  const { state, controls } = prepareControls<VsfOverlayProps>(
     [
       {
-        title: 'Visible',
         type: 'boolean',
         propType: 'boolean',
-        modelName: 'visibleModel',
+        modelName: 'visible',
       },
       {
-        title: 'Absolute',
         type: 'boolean',
         propType: 'boolean',
-        modelName: 'absoluteModel',
+        modelName: 'absolute',
         description:
           'Set `position:absolute` to component (require `position:relative` style attribute in parent component)',
       },
     ],
     {
-      absoluteModel: false,
-      visibleModel: false,
+      absolute: false,
+      visible: false,
     },
   );
   function onClickHandler() {
-    // eslint-disable-next-line no-console
     console.log('VsfOverlay clicked!');
   }
   return (
     <div className="e-page">
       <div className="e-page-component relative">
-        <VsfOverlay absolute={state.get.absoluteModel} onClick={onClickHandler} visible={state.get.visibleModel} />
+        <VsfOverlay absolute={state.get.absolute} onClick={onClickHandler} visible={state.get.visible} />
       </div>
       <div className="e-page-controls">
         <Controls {...{ state, controls }} />
       </div>
     </div>
   );
-}
+};
 
 Example.getLayout = ExamplePageLayout;
 export default Example;
