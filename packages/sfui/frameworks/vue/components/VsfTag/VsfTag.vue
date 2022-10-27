9de94c8ac05c9f3bbc9a736b2cb65ebf4430bfba
Anna Musial <ania.musial13@gmail.com>
2022-10-27 15:46:16 +0200

    chore: tag improvements

--- packages/sfui/frameworks/vue/components/VsfTag/VsfTag.vue
+++ packages/sfui/frameworks/vue/components/VsfTag/VsfTag.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { PropType } from 'vue';
-import type { VsfTagSizes, VsfTagVariants } from './types';
+import { VsfTagSizes, VsfTagVariants } from './types';
 
-const props = defineProps({
+defineProps({
   label: {
     type: String,
     default: '',
@@ -23,15 +23,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <div
-    :class="[
-      'vsf-tag',
-      `vsf-tag--size-${props.size}`,
-      `vsf-tag--variant-${props.variant}`,
-      props.strong ? `vsf-tag--strong` : null,
-    ]"
-  >
-    <div v-if="$slots.icon" class="vsf-tag__icon">
+  <div :class="['vsf-tag', `vsf-tag--size-${size}`, `vsf-tag--variant-${variant}`, strong ? `vsf-tag--strong` : null]">
+    <div v-if="$slots.icon" :class="['vsf-tag__icon', label ? 'ml-1 mr-0.5' : null]">
       <slot name="icon" />
     </div>
     <span v-if="label" class="vsf-tag__label">{{ label }}</span>
