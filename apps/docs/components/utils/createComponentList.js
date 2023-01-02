4be2f2460e33b5a1e08384a7a6afba40d5e01c43
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-01-02 11:41:48 +0100

    docs: remove unnecessary index entry in component json (#387)

--- apps/docs/components/utils/createComponentList.js
+++ apps/docs/components/utils/createComponentList.js
@@ -1,28 +1,20 @@
 const glob = require('glob');
 const fs = require('fs');
 
-const vueDocs = glob.sync('./vue/components/*.md').map((path) => path.split('/').pop().split('.')[0]);
+const removeIndexEntry = paths => paths.filter(path => path !== 'index');
+const componentNameFromPath = paths => paths.map((path) => path.split('/').pop().split('.')[0]);
 
-const vueFiles = glob
-  .sync('../../../packages/sfui/frameworks/vue/components/**/*.vue')
-  .map((path) => path.split('/').pop().split('.')[0])
-  .filter((name) => {
-    return vueDocs.includes(name.replace('Vsf', '').toLowerCase());
-  });
-
-const reactDocs = glob.sync('./vue/components/*.md').map((path) => path.split('/').pop().split('.')[0]);
-
-const reactFiles = glob
-  .sync('../../../packages/sfui/frameworks/react/components/**/*.tsx')
-  .map((path) => path.split('/').pop().split('.')[0])
-  .filter((name) => {
-    return reactDocs.includes(name.replace('Vsf', '').toLowerCase());
-  });
-
+const getFrameworkFiles = (frameworkName, componentsExt) => {
+  const componentNames = componentNameFromPath(glob.sync(`./${frameworkName}/components/*.md`));
+  const existingDocsNames = removeIndexEntry(componentNames);
+  return componentNameFromPath(
+    glob.sync(`../../../packages/sfui/frameworks/${frameworkName}/components/**/*.${componentsExt}`)
+  ).filter(name => existingDocsNames.includes(name.replace('Vsf', '').toLowerCase()));
+}
 
 const components = {
-  vue: vueFiles,
-  react: reactFiles,
+  vue: getFrameworkFiles('vue', 'vue'),
+  react: getFrameworkFiles('react', 'tsx'),
 };
 
 fs.writeFileSync('./utils/components.json', JSON.stringify(components, null, 2));
