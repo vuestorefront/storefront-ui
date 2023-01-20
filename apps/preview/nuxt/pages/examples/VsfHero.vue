dc118b410780b793088bde333a6887fd77c9af78
Jakub Freisler <kubaef@gmail.com>
2023-01-20 16:38:14 +0100

    fix: unify Hero examples (#479)

--- apps/preview/nuxt/pages/examples/VsfHero.vue
+++ apps/preview/nuxt/pages/examples/VsfHero.vue
@@ -16,7 +16,7 @@
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            ea commodo consequat.
           </p>
         </div>
         <VsfButton type="button">Buy now</VsfButton>
