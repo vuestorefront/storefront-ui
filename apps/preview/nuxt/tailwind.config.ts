ee01a4d6dd34130ca96b7eda5fb8302b164b942a
Adam Pawliński <apawlinski@vuestorefront.io>
2023-02-24 13:58:15 +0100

    chore: (sfui2-703) adjust tailwind config (#579)
    * chore: adjust tailwind config
    
    * Update apps/preview/next/tailwind.config.js
    
    Co-authored-by: Jakub Freisler <jakub@frsource.org>
    
    * chore: nuxt config
    
    ---------
    
    Co-authored-by: Jakub Freisler <jakub@frsource.org>

--- apps/preview/nuxt/tailwind.config.ts
+++ apps/preview/nuxt/tailwind.config.ts
@@ -3,4 +3,5 @@ import config from '@storefront-ui/tailwind-config/index.json';
 
 export default <Config>{
   ...(config as unknown as Config),
+  content: [...config.content, '../../../node_modules/@storefront-ui/vue/**/*.{ts,vue}'],
 };
