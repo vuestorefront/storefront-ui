915e72fc1bcbb3031c7ee859cefadae7f03d6c84
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-03-09 10:36:13 +0100

    fix: transition drawer showcase vue (#624)

--- apps/preview/nuxt/pages/showcases/Drawer/TransitionAndCloseButton.vue
+++ apps/preview/nuxt/pages/showcases/Drawer/TransitionAndCloseButton.vue
@@ -1,32 +1,41 @@
 <template>
   <VsfButton type="button" @click="open = $event"> Open Drawer </VsfButton>
 
-  <VsfDrawer
-    ref="drawerRef"
-    v-model="open"
-    :placement="placement"
-    :class="[
-      'bg-neutral-50',
-      'border',
-      'border-gray-300',
-      { 'max-w-[370px]': placement === VsfDrawerPlacement.left || placement === VsfDrawerPlacement.right },
-    ]"
+  <transition
+    enter-active-class="transition duration-500 ease-in-out"
+    leave-active-class="transition duration-500 ease-in-out"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-from-class="translate-x-0"
+    leave-to-class="-translate-x-full"
   >
-    <header className="bg-primary-700 flex justify-between py-6 px-10 items-center">
-      <div className="flex items-center text-white"><VsfIconFavorite class="mr-2" /> Your favorite items</div>
-      <VsfButton square variant="tertiary" class="text-white" @click="open = false">
-        <VsfIconClose />
-      </VsfButton>
-    </header>
-    <div class="p-5 px-10">
-      <p className="mb-2">
-        Focus is trapped inside this Drawer and <strong>tab</strong> button cycle through
-        <a href="#" class="underline text-primary-700"> Focusable Element </a> and another
-        <a href="#" class="underline text-primary-700"> Focusable Element </a>
-      </p>
-      <strong>You can close Drawer by clicking outside or focus and use ESC button</strong>
-    </div>
-  </VsfDrawer>
+    <VsfDrawer
+      ref="drawerRef"
+      v-model="open"
+      :placement="placement"
+      :class="[
+        'bg-neutral-50',
+        'border',
+        'border-gray-300',
+        { 'max-w-[370px]': placement === VsfDrawerPlacement.left || placement === VsfDrawerPlacement.right },
+      ]"
+    >
+      <header className="bg-primary-700 flex justify-between py-6 px-10 items-center">
+        <div className="flex items-center text-white"><VsfIconFavorite class="mr-2" /> Your favorite items</div>
+        <VsfButton square variant="tertiary" class="text-white" @click="open = false">
+          <VsfIconClose />
+        </VsfButton>
+      </header>
+      <div class="p-5 px-10">
+        <p className="mb-2">
+          Focus is trapped inside this Drawer and <strong>tab</strong> button cycle through
+          <a href="#" class="underline text-primary-700"> Focusable Element </a> and another
+          <a href="#" class="underline text-primary-700"> Focusable Element </a>
+        </p>
+        <strong>You can close Drawer by clicking outside or focus and use ESC button</strong>
+      </div>
+    </VsfDrawer>
+  </transition>
 </template>
 
 <script lang="ts" setup>
