06d5bfa2344afaca169f0b2f05aba8aadeb5b620
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-03-10 13:09:23 +0100

    fix: radio weird behaviour on disabled, invalid and radio selected (#631)
    * fix: radio disabled state with invalid
    
    * fix: disabled in props

--- packages/sfui/frameworks/react/components/VsfRadio/VsfRadio.tsx
+++ packages/sfui/frameworks/react/components/VsfRadio/VsfRadio.tsx
@@ -2,19 +2,22 @@ import classNames from 'classnames';
 import { forwardRef } from 'react';
 import { VsfRadioProps } from './types';
 
-export default forwardRef<HTMLInputElement, VsfRadioProps>(
-  ({ invalid, className, ...attributes }: VsfRadioProps, ref) => (
+export default forwardRef<HTMLInputElement, VsfRadioProps>((props, ref) => {
+  const { invalid, className, disabled, ...attributes } = props;
+
+  return (
     <input
       ref={ref}
       type="radio"
+      disabled={disabled}
       className={classNames(
         'min-w-[20px] h-5 border-2 border-neutral-500 rounded-full appearance-none cursor-pointer focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 active:border-primary-900 hover:border-primary-700 checked:bg-primary-700 checked:border-primary-700 checked:shadow-inset hover:checked:bg-primary-800 hover:checked:border-primary-800 active:checked:bg-primary-900 active:checked:border-primary-900 disabled:border-disabled-500 disabled:opacity-40 disabled:cursor-not-allowed disabled:checked:bg-disabled-500 disabled:checked:border-disabled-500',
         {
-          '!border-negative-700 hover:!border-negative-800 active:!border-negative-900': invalid,
+          '!border-negative-700 hover:!border-negative-800 active:!border-negative-900': invalid && !disabled,
         },
         className,
       )}
       {...attributes}
     />
-  ),
-);
+  );
+});
--- packages/sfui/frameworks/vue/components/VsfRadio/VsfRadio.vue
+++ packages/sfui/frameworks/vue/components/VsfRadio/VsfRadio.vue
@@ -43,7 +43,7 @@ const proxyChecked = computed({
     :value="value"
     :class="[
       'min-w-[20px] h-5 border-2 border-neutral-500 rounded-full appearance-none cursor-pointer focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 active:border-primary-900 hover:border-primary-700 checked:bg-primary-700 checked:border-primary-700 checked:shadow-inset hover:checked:bg-primary-800 hover:checked:border-primary-800 active:checked:bg-primary-900 active:checked:border-primary-900 disabled:border-disabled-500 disabled:opacity-40 disabled:cursor-not-allowed disabled:checked:bg-disabled-500 disabled:checked:border-disabled-500',
-      { '!border-negative-700 hover:!border-negative-800 active:!border-negative-900': invalid },
+      { '!border-negative-700 hover:!border-negative-800 active:!border-negative-900': invalid && !disabled },
     ]"
     :disabled="disabled"
   />
