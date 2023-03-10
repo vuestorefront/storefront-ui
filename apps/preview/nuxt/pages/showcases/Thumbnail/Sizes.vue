050c3cff4c81a283f17207e21ca243360f487f54
Anna Musial <ania.musial13@gmail.com>
2023-03-10 12:58:23 +0100

    fix: correct class naming in thumbnail sizes showcase (#640)
    * fix: correct class naming in thumbnail sizes showcase
    
    * chore: removed unused file

--- apps/preview/nuxt/pages/showcases/Thumbnail/Sizes.vue
+++ apps/preview/nuxt/pages/showcases/Thumbnail/Sizes.vue
@@ -1,9 +1,9 @@
 <template>
   <div className="flex items-center justify-around max-w-sm">
-    <VsfThumbnail size="sm" class-name="bg-primary-500 mr-2" />
-    <VsfThumbnail size="base" class-name="bg-primary-500 mr-2" />
-    <VsfThumbnail size="lg" class-name="bg-primary-500 mr-2" />
-    <VsfThumbnail size="xl" class-name="bg-primary-500" />
+    <VsfThumbnail size="sm" class="bg-primary-500 mr-2" />
+    <VsfThumbnail size="base" class="bg-primary-500 mr-2" />
+    <VsfThumbnail size="lg" class="bg-primary-500 mr-2" />
+    <VsfThumbnail size="xl" class="bg-primary-500" />
   </div>
 </template>
 
