53a789bf1d193465d141cc399300da85513e632d
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-03-13 13:04:10 +0100

    feat: checkout - delivery options (#649)

---
+++ apps/docs/components/showcases/Checkout.md
@@ -0,0 +1,23 @@
+---
+layout: ComponentLayout
+hideBreadcrumbs: true
+description: Checkout page in one of the most important pages in e-commerce. Usually it contains information about delivery destination, shipping options and payment methods.
+---
+# Checkout page
+
+{{ $frontmatter.description }}
+
+## Delivery options
+
+Present possible delivery options in a way where your customers can easily see differences and choose the best one for their needs.
+
+<Showcase showcase-name="Checkout/CheckoutDeliveryOptions" style="height: 250px">
+
+<!-- vue -->
+<<<../../preview/nuxt/pages/showcases/Checkout/CheckoutDeliveryOptions.vue
+<!-- end vue -->
+<!-- react -->
+<<<../../preview/next/pages/showcases/Checkout/CheckoutDeliveryOptions.tsx
+<!-- end react -->
+
+</Showcase>
---
+++ apps/preview/nuxt/pages/showcases/Checkout/CheckoutDeliveryOptions.vue
@@ -0,0 +1,36 @@
+<template>
+  <VsfListItemMenu
+    v-for="({ name, cost, date }, index) in deliveryOptions"
+    :key="index"
+    tag="label"
+    class="!items-start max-w-sm border rounded-md border-neutral-200 first-of-type:mr-4 first-of-type:mb-4"
+  >
+    {{ name }}
+    <span className="text-xs text-gray-500 break-words">{{ date }}</span>
+    <template #prefix>
+      <VsfRadio v-model="radioModel" :value="name" name="delivery-options" />
+    </template>
+    <template #suffix>
+      <span className="text-gray-900">{{ cost }}</span>
+    </template>
+  </VsfListItemMenu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { VsfRadio } from '@storefront-ui/vue/components/VsfRadio/index';
+import { VsfListItemMenu } from '@storefront-ui/vue/components/VsfListItemMenu/index';
+const deliveryOptions = [
+  {
+    name: 'Standard',
+    cost: 'Free',
+    date: 'Thursday, September 15',
+  },
+  {
+    name: 'Express',
+    cost: '$5.00',
+    date: 'Tomorrow, September 12',
+  },
+];
+const radioModel = ref('');
+</script>
