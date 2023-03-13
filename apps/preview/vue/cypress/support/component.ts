3e1aead5ba76a7f5b42669d56f0f601278195d45
Jakub Freisler <jakub@frsource.org>
2023-03-13 21:14:09 +0100

    fix: vue tests styling (#654)
    add typography build dep

--- apps/preview/vue/cypress/support/component.ts
+++ apps/preview/vue/cypress/support/component.ts
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
-import "../../src/assets/main.css";
+import "../../src/assets/global.css";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
--- apps/preview/vue/tailwind.config.js
+++ apps/preview/vue/tailwind.config.js
@@ -1,10 +1,21 @@
+const vsfTypography = require("@storefront-ui/typography");
 /** @type {import('tailwindcss').Config} */
-
 const { tailwindConfig } = require("@storefront-ui/tailwind-config");
 module.exports = {
   presets: [tailwindConfig],
   content: [
+    "./**/*.vue",
+    "./**/*.ts",
+    "./**/*.tsx",
     "../../../node_modules/@storefront-ui/vue/shared/**/*.ts",
     "../../../node_modules/@storefront-ui/vue/components/**/*.{ts,vue}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Red Hat Text, sans-serif",
+      },
+    },
+  },
+  plugins: [vsfTypography],
 };
