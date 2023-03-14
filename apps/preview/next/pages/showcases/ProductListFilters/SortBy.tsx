6fd608fb82c00788ca7ef02e3df3e55ab1c0482d
Anna Musial <ania.musial13@gmail.com>
2023-03-14 14:12:14 +0100

    feat: (sfui2-763) sort by block (#653)
    * feat: created sort by block
    
    * feat: added styling for sort by heading
    
    * chore: removed unused class
    
    * chore: removed unused span

---
+++ apps/preview/next/pages/showcases/ProductListFilters/SortBy.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { VsfSelect } from '@storefront-ui/react/components/VsfSelect';
+import { ShowcasePageLayout } from '../../showcases';
+
+const options = [
+  { label: 'Relevance', value: 'relevance' },
+  { label: 'Price: Low to High', value: 'price low to high' },
+  { label: 'Price: High to Low', value: 'price high to low' },
+  { label: 'New Arrivals', value: 'new arrivals' },
+  { label: 'Customer Rating', value: 'customer rating' },
+  { label: 'Bestsellers', value: 'bestsellers' },
+];
+
+export function Showcase() {
+  return (
+    <>
+      <h6 className="bg-neutral-100 mb-4 px-4 py-2 rounded uppercase typography-headline-6 font-bold tracking-widest">
+        Sort by
+      </h6>
+      <div className="px-4">
+        <VsfSelect aria-label="Sort by">
+          {options.map((option) => (
+            <option value={option.value} key={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </VsfSelect>
+      </div>
+    </>
+  );
+}
+
+Showcase.getLayout = ShowcasePageLayout;
+export default Showcase;
---
+++ apps/preview/nuxt/pages/showcases/ProductListFilters/SortBy.vue
@@ -0,0 +1,27 @@
+<template>
+  <h6 class="bg-neutral-100 mb-4 px-4 py-2 rounded uppercase typography-headline-6 font-bold tracking-widest">
+    Sort by
+  </h6>
+  <div class="px-4">
+    <VsfSelect v-model="selected" aria-label="Sort by">
+      <option v-for="{ value, label } in options" :key="value" :value="value">
+        {{ label }}
+      </option>
+    </VsfSelect>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { VsfSelect } from '@storefront-ui/vue/components/VsfSelect/index';
+
+const options = ref([
+  { label: 'Relevance', value: 'relevance' },
+  { label: 'Price: Low to High', value: 'price low to high' },
+  { label: 'Price: High to Low', value: 'price high to low' },
+  { label: 'New Arrivals', value: 'new arrivals' },
+  { label: 'Customer Rating', value: 'customer rating' },
+  { label: 'Bestsellers', value: 'bestsellers' },
+]);
+const selected = ref('');
+</script>
