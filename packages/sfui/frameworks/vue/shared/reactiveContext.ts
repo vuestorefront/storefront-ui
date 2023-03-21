27adf7dd776196a916159a377c306b72fce9071d
Adam Pawliński <apawlinski@vuestorefront.io>
2023-03-21 12:50:01 +0100

    fix: (sfui2-838)  Button slots gap (#713)
    * fix: button slots gap
    
    * fix: button slots reactivity bug
    
    Signed-off-by: Jakub Freisler <jakub@frsource.org>
    
    ---------
    
    Signed-off-by: Jakub Freisler <jakub@frsource.org>
    Co-authored-by: Jakub Freisler <jakub@frsource.org>

---
+++ packages/sfui/frameworks/vue/shared/reactiveContext.ts
@@ -0,0 +1,14 @@
+import { useSlots, ref, onBeforeUpdate } from 'vue';
+
+export const useSlotsRef = () => {
+  const slots = ref(useSlots());
+
+  // slots need to be updated manually, becuase useSlots returns not a reactive object
+  // see: https://vuejs.org/api/composition-api-setup.html#setup-context
+  // see: https://github.com/vuejs/core/issues/6202#issuecomment-1278776852
+  onBeforeUpdate(() => {
+    slots.value = useSlots();
+  });
+
+  return slots;
+};
