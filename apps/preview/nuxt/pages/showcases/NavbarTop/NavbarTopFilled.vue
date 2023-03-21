754fcc72ba670869f22ba7f57d136d7605f4831f
Adam Pawliński <apawlinski@vuestorefront.io>
2023-03-21 12:15:40 +0100

    fix: (sfui2-828) NavbarTop logo and menu styling (#711)
    * fix: logo and menu styling
    
    * fix: remove unnecessary logo image

--- apps/preview/nuxt/pages/showcases/NavbarTop/NavbarTopFilled.vue
+++ apps/preview/nuxt/pages/showcases/NavbarTop/NavbarTopFilled.vue
@@ -1,63 +1,59 @@
 <template>
   <div class="w-full h-full bg-neutral-50">
     <header class="flex justify-center w-full text-white border-0 bg-primary-700 h-14 md:h-20 border-neutral-200">
-      <div class="flex items-center flex-row flex-nowrap justify-between h-full max-w-[1536px] w-full px-4 md:px-10">
+      <div class="flex items-center flex-row flex-nowrap justify-start h-full max-w-[1536px] w-full px-4 md:px-10">
         <a aria-label="VSF Homepage" class="inline-block h-8 text-white md:h-10 lg:h-7 text-primary-700">
-          <img :src="brandLogo" alt="VSF Logo" class="hidden large:block w-[12.5rem] h-[1.75rem]" />
-          <VsfIconVuestorefront size="xl" class="hidden medium:block large:hidden" />
-          <VsfIconVuestorefront size="lg" class="block medium:hidden" />
+          <img :src="brandLogo" alt="VSF Logo" class="hidden large:block mr-10 w-[12.5rem] h-[1.75rem]" />
+          <img :src="brandLogoSign" alt="Vsf Logo" className="hidden w-10 h-10 mr-4 md:block lg:hidden" />
+          <img :src="brandLogoSign" alt="Vsf Logo" className="block w-8 h-8 mr-4 min-w-8 md:hidden" />
         </a>
         <VsfButton
-          class="block text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white font-body"
+          class="block !px-2 mr-auto text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white font-body"
           type="button"
           variant="tertiary"
         >
           <template #prefix>
             <VsfIconMenu />
           </template>
-          Menu
+          <span className="hidden md:inline-flex">Categories</span>
         </VsfButton>
-        <nav class="flex flex-row flex-nowrap">
-          <VsfButton
-            v-for="actionItem in actionItems"
-            :key="actionItem.ariaLabel"
-            :class="[
-              'mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white',
-              { 'lg:hidden': actionItem.label === 'Log in' },
-            ]"
-            :aria-label="actionItem.ariaLabel"
-            variant="tertiary"
-            square
-          >
-            <template #prefix>
-              <Component :is="actionItem.icon" />
-            </template>
-          </VsfButton>
-          <VsfButton
-            class="hidden lg:inline-flex mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
-            :aria-label="actionItems[2].ariaLabel"
-            variant="tertiary"
-          >
-            <template #prefix>
-              <Component :is="actionItems[2].icon" />
-            </template>
-            Log in
-          </VsfButton>
+        <nav>
+          <div class="flex flex-row flex-nowrap">
+            <VsfButton
+              v-for="actionItem in actionItems"
+              :key="actionItem.ariaLabel"
+              :class="[
+                'mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white',
+                { 'lg:hidden': actionItem.label === 'Log in' },
+              ]"
+              :aria-label="actionItem.ariaLabel"
+              variant="tertiary"
+              square
+            >
+              <template #prefix>
+                <Component :is="actionItem.icon" />
+              </template>
+            </VsfButton>
+            <VsfButton
+              class="hidden lg:inline-flex mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
+              :aria-label="actionItems[2].ariaLabel"
+              variant="tertiary"
+            >
+              <template #prefix>
+                <Component :is="actionItems[2].icon" />
+              </template>
+              Log in
+            </VsfButton>
+          </div>
         </nav>
       </div>
     </header>
   </div>
 </template>
 <script lang="ts" setup>
-import {
-  VsfButton,
-  VsfIconShoppingCart,
-  VsfIconFavorite,
-  VsfIconPerson,
-  VsfIconMenu,
-  VsfIconVuestorefront,
-} from '@storefront-ui/vue';
+import { VsfButton, VsfIconShoppingCart, VsfIconFavorite, VsfIconPerson, VsfIconMenu } from '@storefront-ui/vue';
 import brandLogo from '@assets/vsf_logo_white.svg';
+import brandLogoSign from '@assets/vsf_logo_sign_white.svg';
 
 const actionItems = [
   {
