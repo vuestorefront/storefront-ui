ba1d1c23bbf1aca033633efbef87130e247864c3
Jakub Freisler <jakub@frsource.org>
2023-03-22 14:19:19 +0100

    chore: button refine [SFUI2-843] (#721)
    button slots gap
    button slots reactivity bug
    remove classNames join

--- apps/preview/next/pages/showcases/Button/ButtonSizes.tsx
+++ apps/preview/next/pages/showcases/Button/ButtonSizes.tsx
@@ -1,18 +1,16 @@
-import { VsfButton, VsfButtonSize } from '@storefront-ui/react';
+import { VsfButton } from '@storefront-ui/react';
 import { ShowcasePageLayout } from '../../showcases';
 
 export function Showcase() {
   return (
     <div className="flex flex-col items-center space-y-4 xs:block xs:space-x-4">
-      <VsfButton type="button" size={VsfButtonSize.sm}>
+      <VsfButton type="button" size="sm">
         Hello
       </VsfButton>
 
-      <VsfButton type="button" size={VsfButtonSize.base}>
-        Hello
-      </VsfButton>
+      <VsfButton type="button">Hello</VsfButton>
 
-      <VsfButton type="button" size={VsfButtonSize.lg}>
+      <VsfButton type="button" size="lg">
         Hello
       </VsfButton>
     </div>
--- apps/preview/nuxt/pages/showcases/Button/ButtonSizes.vue
+++ apps/preview/nuxt/pages/showcases/Button/ButtonSizes.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col items-center space-y-4 xs:block xs:space-x-4">
-    <VsfButton type="button" :size="VsfButtonSize.sm"> Hello </VsfButton>
+    <VsfButton type="button" size="sm">Hello</VsfButton>
 
-    <VsfButton type="button" :size="VsfButtonSize.base"> Hello </VsfButton>
+    <VsfButton type="button">Hello</VsfButton>
 
-    <VsfButton type="button" :size="VsfButtonSize.lg"> Hello </VsfButton>
+    <VsfButton type="button" size="lg">Hello</VsfButton>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { VsfButton, VsfButtonSize } from '@storefront-ui/vue';
+import { VsfButton } from '@storefront-ui/vue';
 </script>
