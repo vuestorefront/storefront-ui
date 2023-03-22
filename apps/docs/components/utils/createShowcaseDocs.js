e8c4bb4eca661d396eefea0be2bd9ae3818bc8d7
Matt Maribojoc <mmaribojoc@vuestorefront.io>
2023-03-22 17:51:50 -0400

    docs: block thumbnails (#732)
    * docs: update base components thumbnails
    
    * docs: add blocks thumbnails
    
    * docs: add block count
    
    * docs: fix framework select spacing
    
    * docs: clean up utils.js
    
    * docs: rename Filters.png

--- apps/docs/components/utils/createShowcaseDocs.js
+++ apps/docs/components/utils/createShowcaseDocs.js
@@ -1,5 +1,5 @@
 const glob = require('glob');
-const { docsSplit, removePart, saveDoc } = require('./utils');
+const { docsSplit, removePart, saveDoc, updateFrontmatter } = require('./utils');
 const fs = require('fs');
 const commandLineArgs = require('command-line-args');
 
@@ -10,8 +10,8 @@ const mdFiles = glob.sync('./blocks/*.md');
 function generateDocs() {
   if (options.event === 'change') {
     let docsContent = fs.readFileSync(options.path, 'utf8');
-    const reactDocContent = removePart(docsContent, 'vue');
-    const vueDocContent = removePart(docsContent, 'react');
+    const reactDocContent = updateFrontmatter(removePart(docsContent, 'vue'), options.path, 'blocks');
+    const vueDocContent = updateFrontmatter(removePart(docsContent, 'react'), options.path, 'blocks');
 
     saveDoc(`./react/${options.path}`, reactDocContent);
     saveDoc(`./vue/${options.path}`, vueDocContent);
--- apps/docs/components/utils/utils.js
+++ apps/docs/components/utils/utils.js
@@ -2,67 +2,87 @@ const path = require('path');
 const fs = require('fs');
 
 function addRepoPathToFrontmatter(content, fileName) {
-    const frontmatterStartIndex = content.indexOf('---') + '---'.length;
-    const link = `/components/${fileName}`;
+  const frontmatterStartIndex = content.indexOf('---') + '---'.length;
+  const link = `/components/${fileName}`;
 
-    return content.slice(0, frontmatterStartIndex) + `\nrepoPath: ${link}` + content.slice(frontmatterStartIndex);
+  return content.slice(0, frontmatterStartIndex) + `\nrepoPath: ${link}` + content.slice(frontmatterStartIndex);
+}
+
+function addNumberOfBlocksToFrontmatter(content) {
+  const frontmatterStartIndex = content.indexOf('---') + '---'.length;
+  const numberOfShowcases = content.match(/showcase-name/g)?.length;
+
+  return (
+    content.slice(0, frontmatterStartIndex) +
+    `\nblockCount: ${numberOfShowcases}` +
+    content.slice(frontmatterStartIndex)
+  );
+}
+
+function updateFrontmatter(content, fileName, type) {
+  let docContent = addRepoPathToFrontmatter(content, fileName);
+  if (type === 'blocks') {
+    docContent = addNumberOfBlocksToFrontmatter(docContent);
+  }
+  return docContent;
 }
 
 function removePart(content, framework) {
-    const removeRegex = new RegExp(`<!-- ${framework} -->.*?<!-- end ${framework} -->(?:\n|$)?`, 'gs');
-    const docsWithout = content.replace(removeRegex, '');
-    const removeIfsRegex = /<!--(.*?)-->\n?/g;
-    const docsWithoutIfs = docsWithout.replace(removeIfsRegex, '');
-    return docsWithoutIfs;
+  const removeRegex = new RegExp(`<!-- ${framework} -->.*?<!-- end ${framework} -->(?:\n|$)?`, 'gs');
+  const docsWithout = content.replace(removeRegex, '');
+  const removeIfsRegex = /<!--(.*?)-->\n?/g;
+  const docsWithoutIfs = docsWithout.replace(removeIfsRegex, '');
+  return docsWithoutIfs;
 }
 
 function saveDoc(filepath, docsContent) {
-    const pathWithoutFilename = path.dirname(filepath);
-    if (!fs.existsSync(pathWithoutFilename)) {
-        fs.mkdir(pathWithoutFilename, { recursive: true }, (e) => {
-            console.warn(`WARN: Cannot create directory: ${e}`);
-        });
-        console.log('Docs directory created');
-    }
-    fs.writeFile(filepath, docsContent, (e) => {
-        if (e) {
-            console.warn(`WARN: skipping write md file: ${e.message}`);
-        } else {
-            console.log(`Doc file successfully written ${filepath}`);
-        }
+  const pathWithoutFilename = path.dirname(filepath);
+  if (!fs.existsSync(pathWithoutFilename)) {
+    fs.mkdir(pathWithoutFilename, { recursive: true }, (e) => {
+      console.warn(`WARN: Cannot create directory: ${e}`);
     });
-    return true;
+    console.log('Docs directory created');
+  }
+  fs.writeFile(filepath, docsContent, (e) => {
+    if (e) {
+      console.warn(`WARN: skipping write md file: ${e.message}`);
+    } else {
+      console.log(`Doc file successfully written ${filepath}`);
+    }
+  });
+  return true;
 }
 
 module.exports = {
-    removePart,
-    saveDoc,
-    docsSplit(mdFiles, type) {
-        for (const file of mdFiles) {
-            let docsContent;
-            try {
-                docsContent = fs.readFileSync(file, 'utf8');
-            } catch (e) {
-                console.warn(`WARN: skipping read md file: ${e.message}`);
-                continue;
-            }
+  removePart,
+  saveDoc,
+  updateFrontmatter,
+  docsSplit(mdFiles, type) {
+    for (const file of mdFiles) {
+      let docsContent;
+      try {
+        docsContent = fs.readFileSync(file, 'utf8');
+      } catch (e) {
+        console.warn(`WARN: skipping read md file: ${e.message}`);
+        continue;
+      }
 
-            const fileName = path.basename(file);
-            const reactDocContent = addRepoPathToFrontmatter(removePart(docsContent, 'vue'), fileName);
-            const vueDocContent = addRepoPathToFrontmatter(removePart(docsContent, 'react'), fileName);
+      const fileName = path.basename(file);
+      const reactDocContent = updateFrontmatter(removePart(docsContent, 'vue'), fileName, type);
+      const vueDocContent = updateFrontmatter(removePart(docsContent, 'react'), fileName, type);
 
-            const reactDirectory = `./react/${type}`;
-            const vueDirectory = `./vue/${type}`;
+      const reactDirectory = `./react/${type}`;
+      const vueDirectory = `./vue/${type}`;
 
-            if (!fs.existsSync(reactDirectory)) {
-                fs.mkdirSync(reactDirectory);
-            }
-            if (!fs.existsSync(vueDirectory)) {
-                fs.mkdirSync(vueDirectory);
-            }
+      if (!fs.existsSync(reactDirectory)) {
+        fs.mkdirSync(reactDirectory);
+      }
+      if (!fs.existsSync(vueDirectory)) {
+        fs.mkdirSync(vueDirectory);
+      }
 
-            saveDoc(`${reactDirectory}/${fileName}`, reactDocContent);
-            saveDoc(`${vueDirectory}/${fileName}`, vueDocContent);
-        }
+      saveDoc(`${reactDirectory}/${fileName}`, reactDocContent);
+      saveDoc(`${vueDirectory}/${fileName}`, vueDocContent);
     }
-}
+  },
+};
