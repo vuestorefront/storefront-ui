d096751fb4b843bdaea61e1effcbf47059cea60c
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-03-22 14:51:48 +0100

    fix: proper image and labels for product card vertical (#730)

--- apps/preview/next/pages/showcases/ProductCard/ProductCardVertical.tsx
+++ apps/preview/next/pages/showcases/ProductCard/ProductCardVertical.tsx
@@ -1,5 +1,5 @@
 import { VsfButton, VsfRating, VsfCounter, VsfLink, VsfIconShoppingCart, VsfIconFavorite } from '@storefront-ui/react';
-import card from '@assets/card.png';
+import productImage from '@assets/sneakers.png';
 import Image from 'next/image';
 import { ShowcasePageLayout } from '../../showcases';
 
@@ -9,7 +9,7 @@ export function Showcase() {
       <div className="relative">
         <VsfLink href="#">
           <Image
-            src={card.src}
+            src={productImage.src}
             alt="Great product"
             className="object-cover h-auto rounded-md aspect-square"
             width={300}
@@ -29,7 +29,7 @@ export function Showcase() {
       </div>
       <div className="p-4 border-t border-neutral-200">
         <VsfLink href="#" variant="secondary" className="no-underline">
-          The standard chunk of Lorem Ipsum
+          Athletic mens walking sneakers
         </VsfLink>
         <div className="flex items-center pt-1">
           <VsfRating size="xs" value={5} max={5} />
@@ -39,7 +39,7 @@ export function Showcase() {
           </VsfLink>
         </div>
         <p className="block py-2 font-normal typography-text-sm text-neutral-700">
-          There are many variations of passages of Lorem Ipsum available
+          Lightweight • Non slip • Flexible outsole • Easy to wear on and off
         </p>
         <span className="block pb-2 font-bold typography-text-lg">$2345,99</span>
         <VsfButton type="button" size="sm" slotPrefix={<VsfIconShoppingCart size="sm" />}>
