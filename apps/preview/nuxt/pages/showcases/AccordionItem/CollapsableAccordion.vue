1c69adadb23731fae9d477682f2310301d89c5ba
Kacper Dziub <34419118+dkacper@users.noreply.github.com>
2023-03-23 16:30:43 +0100

    fix: improve AccordionItem to match the design (#766)

--- apps/preview/nuxt/pages/showcases/AccordionItem/CollapsableAccordion.vue
+++ apps/preview/nuxt/pages/showcases/AccordionItem/CollapsableAccordion.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="border rounded divide-y">
+  <div class="border border-neutral-200 rounded-md divide-y text-neutral-900">
     <SfAccordionItem
       v-for="{ id, summary, details } in accordionItems"
       :key="id"
-      :class="[{ 'bg-gray-100': isOpen(id) }]"
       :model-value="isOpen(id)"
       @update:model-value="toggle(id, $event)"
     >
       <template #summary>
-        <p class="p-2">{{ summary }}</p>
+        <div class="flex justify-between p-4 font-medium hover:bg-neutral-100 active:neutral-100">
+          <p>{{ summary }}</p>
+          <SfIconChevronLeft :class="['text-neutral-500', { 'rotate-90': isOpen(id), '-rotate-90': !isOpen(id) }]" />
+        </div>
       </template>
-      <p class="p-2">{{ details }}</p>
+      <p class="p-4">{{ details }}</p>
     </SfAccordionItem>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { SfAccordionItem } from '@storefront-ui/vue';
+import { SfAccordionItem, SfIconChevronLeft } from '@storefront-ui/vue';
 
 const opened = ref<string | null>(null);
 
@@ -34,20 +36,21 @@ const toggle = (id: string, open: boolean) => {
 const accordionItems = [
   {
     id: 'acc-1',
-    summary: 'One',
+    summary: 'Where is my order?',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'We will inform you about the expected delivery time of your order in checkout and in your order confirmation email.',
   },
   {
     id: 'acc-2',
-    summary: 'Two',
+    summary: 'What if an item is out of stock?',
     details:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      "If an item you're interested in is sold out, you can register to be notified when your size is back in stock.",
   },
   {
     id: 'acc-3',
-    summary: 'Three',
-    details: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    summary: 'How do I cancel my order?',
+    details:
+      "If you made a mistake or simply changed your mind after placing an order, there's no need to fuss. As long as your parcel has yet to be picked and packed in our warehouse, you'll have the option to cancel.",
   },
 ];
 </script>
