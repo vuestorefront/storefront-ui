6a87b0dd3be54644ef4392a60d322e3ea6905ccb
Kacper Dziub <34419118+dkacper@users.noreply.github.com>
2023-03-23 16:30:43 +0100

    fix: improve AccordionItem to match the design (#766)

--- apps/preview/next/pages/showcases/AccordionItem/BasicAccordion.tsx
+++ apps/preview/next/pages/showcases/AccordionItem/BasicAccordion.tsx
@@ -2,28 +2,29 @@ import { ShowcasePageLayout } from '../../showcases';
 
 // #region source
 import { useState } from 'react';
-import classNames from 'classnames';
 import { SfAccordionItem } from '@storefront-ui/react';
 
 const accordionItems = [
   {
     id: 'acc-1',
-    summary: 'One',
+    summary: 'Where is my order?',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'We will inform you about the expected delivery time of your order in checkout and in your order confirmation email.',
   },
   {
     id: 'acc-2',
-    summary: 'Two',
+    summary: 'What if an item is out of stock?',
     details:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      "If an item you're interested in is sold out, you can register to be notified when your size is back in stock.",
   },
   {
     id: 'acc-3',
-    summary: 'Three',
-    details: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    summary: 'How do I cancel my order?',
+    details:
+      "If you made a mistake or simply changed your mind after placing an order, there's no need to fuss. As long as your parcel has yet to be picked and packed in our warehouse, you'll have the option to cancel.",
   },
 ];
+
 export default function BasicAccordion() {
   const [opened, setOpened] = useState<string[]>([]);
 
@@ -38,16 +39,15 @@ export default function BasicAccordion() {
   };
 
   return (
-    <div className="border rounded divide-y">
+    <div className="border border-neutral-200 rounded-md divide-y text-neutral-900">
       {accordionItems.map(({ id, summary, details }) => (
         <SfAccordionItem
           key={id}
-          summary={<p className="p-2">{summary}</p>}
+          summary={<p className="p-4 font-medium hover:bg-neutral-100 active:neutral-100">{summary}</p>}
           onToggle={handleToggle(id)}
           open={isOpen(id)}
-          className={classNames({ 'bg-gray-100': isOpen(id) })}
         >
-          <p className="p-2">{details}</p>
+          <p className="p-4">{details}</p>
         </SfAccordionItem>
       ))}
     </div>
--- apps/preview/nuxt/pages/showcases/AccordionItem/BasicAccordion.vue
+++ apps/preview/nuxt/pages/showcases/AccordionItem/BasicAccordion.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="border rounded divide-y">
-    <SfAccordionItem
-      v-for="({ id, summary, details }, index) in accordionItems"
-      :key="id"
-      v-model="opened[index]"
-      :class="[{ 'bg-gray-100': opened[index] }]"
-    >
+  <div class="border border-neutral-200 rounded-md divide-y text-neutral-900">
+    <SfAccordionItem v-for="({ id, summary, details }, index) in accordionItems" :key="id" v-model="opened[index]">
       <template #summary>
-        <p class="p-2">{{ summary }}</p>
+        <p class="p-4 font-medium hover:bg-neutral-100 active:neutral-100">{{ summary }}</p>
       </template>
-      <p class="p-2">{{ details }}</p>
+      <p class="p-4">{{ details }}</p>
     </SfAccordionItem>
   </div>
 </template>
@@ -23,20 +18,21 @@ const opened = ref<boolean[]>([]);
 const accordionItems = [
   {
     id: 'acc-1',
-    summary: 'One',
+    summary: 'Where is my order?',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'We will inform you about the expected delivery time of your order in checkout and in your order confirmation email.',
   },
   {
     id: 'acc-2',
-    summary: 'Two',
+    summary: 'What if an item is out of stock?',
     details:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      "If an item you're interested in is sold out, you can register to be notified when your size is back in stock.",
   },
   {
     id: 'acc-3',
-    summary: 'Three',
-    details: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    summary: 'How do I cancel my order?',
+    details:
+      "If you made a mistake or simply changed your mind after placing an order, there's no need to fuss. As long as your parcel has yet to be picked and packed in our warehouse, you'll have the option to cancel.",
   },
 ];
 </script>
