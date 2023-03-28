61f7b600173d06d2947792d3b3fec836be6c9153
AdamPawlinski <apawlinski@vuestorefront.io>
2023-03-28 11:37:21 +0200

    fix: usedisclosure

--- packages/sfui/frameworks/vue/composables/useDisclosure/useDisclosure.ts
+++ packages/sfui/frameworks/vue/composables/useDisclosure/useDisclosure.ts
@@ -1,15 +1,20 @@
 import { syncRefs } from '@vueuse/core';
-import { ref, isRef } from 'vue';
+import { ref, isRef, unref, Ref } from 'vue';
 import type { UseDisclosureOptions } from '@storefront-ui/vue';
 
-export function useDisclosure({ initialValue = false }: UseDisclosureOptions = {}) {
-  const isOpen = ref<boolean>();
-  const toggle = (value?: boolean) => (isOpen.value = value ?? !isOpen.value);
+export function useDisclosure({ initialValue = false }: UseDisclosureOptions = {}): {
+  isOpen: Ref<boolean>;
+  open: () => boolean;
+  close: () => boolean;
+  toggle: () => boolean;
+} {
+  const isOpen: Ref<boolean> = ref<boolean>(unref(initialValue));
+  const toggle: (value?: boolean | undefined) => boolean = (value?: boolean) => (isOpen.value = value ?? !isOpen.value);
+  console.log(isOpen.value)
 
-  if (isRef(initialValue)) syncRefs(initialValue, isOpen, { immediate: true });
+  if (isRef(initialValue)) syncRefs(initialValue, isOpen);
 
-  const open = () => toggle(true);
-  const close = () => toggle(false);
-
+  const open: () => boolean = () => toggle(true);
+  const close: () => boolean = () => toggle(false);
   return { isOpen, open, close, toggle };
 }
