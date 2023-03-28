ca80a280ed491698a0eb5ef3601e721923b35602
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-03-28 08:43:55 -0400

    docs: update config to make top navigation work

--- apps/docs/components/.vuepress/config.js
+++ apps/docs/components/.vuepress/config.js
@@ -128,6 +128,7 @@ module.exports = {
     DOCS_EXAMPLES_REACT_PATH,
     DOCS_EXAMPLES_VUE_PATH,
     FIGMA_URL,
+    coreDocs: false,
     title: 'Storefront UI',
     repo: 'https://github.com/vuestorefront/storefront-ui',
     docsRepoPath: 'https://github.com/vuestorefront/storefront-ui/tree/v2/apps/docs/components/', // used to generate direct edit links on docs pages.
