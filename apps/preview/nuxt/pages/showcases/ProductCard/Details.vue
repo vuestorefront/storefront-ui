cf0f1b9073caed3e028e8261a4c9aca1831b7a40
Jakub Freisler <jakub@frsource.org>
2023-03-28 11:14:14 +0200

    chore: redo details on firefox
    Signed-off-by: Jakub Freisler <jakub@frsource.org>

--- apps/preview/nuxt/pages/showcases/ProductCard/Details.vue
+++ apps/preview/nuxt/pages/showcases/ProductCard/Details.vue
@@ -47,7 +47,7 @@
               v-model="count"
               type="number"
               role="spinbutton"
-              class="grow appearance-none mx-2 text-center [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
+              class="grow appearance-none mx-2 w-8 text-center bg-transparent font-medium [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:display-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:display-none [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none disabled:placeholder-disabled-900 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
               :min="min"
               :max="max"
               @input="handleOnChange"
