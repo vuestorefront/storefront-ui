7f52b08232d71213202ba7c56de21bb65c8d1c69
AdamPawlinski <apawlinski@vuestorefront.io>
2023-03-31 11:25:14 +0200

    feat: vue and react add blocks

--- apps/preview/next/pages/showcases/Card/CategoryCard.tsx
+++ apps/preview/next/pages/showcases/Card/CategoryCard.tsx
@@ -1,28 +1,52 @@
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import { ShowcasePageLayout } from '../../showcases';
 // #region source
 import { SfLink } from '@storefront-ui/react';
 import Image from 'next/image';
-import card from '@assets/smartwatch.png';
+import men from '@assets/men_category.png';
+import women from '@assets/women_category.png';
+import kid from '@assets/kid_category.png';
 
+const categories = [
+  {
+    title: `Women`,
+    image: women.src,
+  },
+  {
+    title: `Men`,
+    image: men.src,
+  },
+  {
+    title: `Kid`,
+    image: kid.src,
+  },
+];
 export default function CategoryCard() {
   return (
-    <div
-      tabIndex={0}
-      className="relative first-letter:flex flex-col w-[280px] outline-none justify-center overflow-hidden group focus-visible:outline focus-visible:outline-offset focus-visible:rounded-md"
-    >
-      <SfLink className="absolute z-10 w-full h-full" href="#" />
-      <Image
-        className="rounded-full bg-neutral-100 group-hover:shadow-xl group-active:shadow-none"
-        src={card.src}
-        alt="smartwatch"
-        width={280}
-        height={280}
-      />
-      <div className="flex justify-center">
-        <span className="mt-4 typography-text-base group-hover:underline group-hover:text-primary-800 group-active:text-primary-800">
-          Category
-        </span>
-      </div>
+    <div className="flex flex-wrap gap-4 lg:gap-6 lg:flex-no-wrap">
+      {categories.map(({ title, image }) => (
+        <div
+          className="relative first-letter:flex flex-col w-max-[280px] justify-center overflow-hidden group"
+          key={title}
+        >
+          <SfLink className="absolute w-full h-full z-1" href="/" aria-label={title} />
+          <Image
+            className="rounded-full bg-neutral-100 group-hover:shadow-xl group-active:shadow-none"
+            src={image}
+            alt={title}
+            width={280}
+            height={280}
+          />
+          <div className="flex justify-center">
+            <SfLink
+              variant="secondary"
+              className="mt-4 font-semibold no-underline !text-normal-900 typography-text-base group-hover:underline group-hover:text-primary-800 group-hover:font-normal group-active:text-primary-800 group-active:font-normal"
+            >
+              {title}
+            </SfLink>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- apps/preview/nuxt/pages/showcases/Card/CategoryCard.vue
+++ apps/preview/nuxt/pages/showcases/Card/CategoryCard.vue
@@ -1,27 +1,48 @@
 <template>
-  <div
-    tabindex="0"
-    class="relative first-letter:flex flex-col w-[280px] outline-none justify-center overflow-hidden group focus-visible:outline focus-visible:outline-offset focus-visible:rounded-md"
-  >
-    <SfLink class="absolute z-10 w-full h-full" href="#" />
-    <img
-      class="rounded-full bg-neutral-100 group-hover:shadow-xl group-active:shadow-none"
-      :src="card"
-      alt="smartwatch"
-      :width="280"
-      :height="280"
-    />
-    <div class="flex justify-center">
-      <span
-        class="mt-4 typography-text-base group-hover:underline group-hover:text-primary-800 group-active:text-primary-800"
-      >
-        Category
-      </span>
+  <div className="flex flex-wrap gap-4 lg:gap-6 lg:flex-no-wrap">
+    <div
+      v-for="{ title, image } in categories"
+      :key="title"
+      class="relative first-letter:flex flex-col w-[280px] justify-center overflow-hidden group focus-visible:outline focus-visible:outline-offset focus-visible:rounded-md"
+    >
+      <SfLink class="absolute w-full h-full z-1" href="/" :aria-label="title" />
+      <img
+        class="rounded-full bg-neutral-100 group-hover:shadow-xl group-active:shadow-none"
+        :src="image"
+        :alt="title"
+        :width="280"
+        :height="280"
+      />
+      <div class="flex justify-center">
+        <SfLink
+          variant="secondary"
+          class="mt-4 font-semibold no-underline !text-normal-900 typography-text-base group-hover:underline group-hover:text-primary-800 group-hover:font-normal group-active:text-primary-800 group-active:font-normal"
+        >
+          {{ title }}
+        </SfLink>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { SfLink } from '@storefront-ui/vue';
-import card from '@assets/smartwatch.png';
+import men from '@assets/men_category.png';
+import women from '@assets/women_category.png';
+import kid from '@assets/kid_category.png';
+
+const categories = [
+  {
+    title: `Women`,
+    image: women,
+  },
+  {
+    title: `Men`,
+    image: men,
+  },
+  {
+    title: `Kid`,
+    image: kid,
+  },
+];
 </script>
