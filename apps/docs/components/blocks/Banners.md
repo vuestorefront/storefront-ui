e3d7832e5c479908238a23a77fbfd95a5927666e
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-04-05 07:24:20 +0200

    feat: display horizontal (#2564)
    * feat: display horizontal
    
    * fix: reusable images and banners directory
    
    * fix: thumbnail image proper name
    
    * fix: remove unused class

---
+++ apps/docs/components/blocks/Banners.md
@@ -0,0 +1,24 @@
+---
+layout: DefaultLayout
+hideBreadcrumbs: true
+description: Display component acts like a layout for your banners. You can provide main image and any content.
+---
+# Display
+
+{{ $frontmatter.description }}
+
+## Display horizontal
+
+Example of layout for 3 horizontal displays.
+
+<Showcase showcase-name="Banners/DisplayHorizontal" style="min-height:800px">
+
+<!-- vue -->
+<<<../../preview/nuxt/pages/showcases/Banners/DisplayHorizontal.vue
+<!-- end vue -->
+<!-- react -->
+<<<../../preview/next/pages/showcases/Banners/DisplayHorizontal.tsx#source
+<!-- end react -->
+
+</Showcase>
+
---
+++ apps/preview/nuxt/pages/showcases/Banners/DisplayHorizontal.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="flex flex-col md:flex-row flex-wrap gap-6 max-w-[1540px]">
+    <div
+      v-for="{
+        image,
+        title,
+        subtitle,
+        description,
+        buttonText,
+        backgroundColor,
+        reverse,
+        titleClass,
+        subtitleClass,
+      } in displayDetails"
+      :key="title"
+      :class="[
+        'relative flex md:max-w-[1536px] md:[&:not(:first-of-type)]:flex-1 md:first-of-type:w-full',
+        backgroundColor,
+      ]"
+    >
+      <a
+        class="absolute w-full h-full z-1 focus-visible:outline focus-visible:rounded-lg"
+        :aria-label="title"
+        href="/"
+      />
+      <div :class="['flex justify-between overflow-hidden grow', { 'flex-row-reverse': reverse }]">
+        <div class="flex flex-col justify-center items-start p-6 lg:p-10 max-w-1/2">
+          <p :class="['uppercase typography-text-xs block font-bold tracking-widest', subtitleClass]">
+            {{ subtitle }}
+          </p>
+          <h2 :class="['mb-4 mt-2 font-bold typography-headline-3', titleClass]">
+            {{ title }}
+          </h2>
+          <p class="typography-text-base block mb-4">
+            {{ description }}
+          </p>
+          <SfButton class="!bg-black">{{ buttonText }}</SfButton>
+        </div>
+        <img :src="image" :alt="title" class="w-1/2 self-end object-contain" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { SfButton } from '@storefront-ui/vue';
+import sunglasses from '@assets/display.png';
+import backpack from '@assets/display-2.png';
+import winterHat from '@assets/display-3.png';
+
+const displayDetails = [
+  {
+    image: sunglasses,
+    title: 'Sunny Days Ahead',
+    subtitle: 'Be inspired',
+    description: 'Step out in style with our sunglasses collection',
+    buttonText: 'Discover now',
+    reverse: false,
+    backgroundColor: 'bg-negative-200',
+    titleClass: 'md:typography-headline-2',
+    subtitleClass: 'md:typography-headline-6',
+    descriptionClass: 'md:typography-text-lg',
+  },
+  {
+    image: backpack,
+    title: 'Pack it Up',
+    subtitle: 'Be active',
+    description: 'Explore the great outdoors with our backpacks',
+    buttonText: 'Discover now',
+    reverse: true,
+    backgroundColor: 'bg-warning-200',
+  },
+  {
+    image: winterHat,
+    title: 'Fresh and Bold',
+    subtitle: 'New collection',
+    description: 'Add a pop up color to your outfit',
+    buttonText: 'Discover now',
+    reverse: false,
+    backgroundColor: 'bg-secondary-200',
+  },
+];
+</script>
