59ce33f5ea11a8abfa21cd8db7757ab949e0f460
Anna Musial <ania.musial13@gmail.com>
2023-04-06 12:57:51 +0200

    chore: remove unused prop

--- packages/sfui/frameworks/react/components/SfDrawer/types.tsx
+++ packages/sfui/frameworks/react/components/SfDrawer/types.tsx
@@ -1,11 +1,10 @@
-import { SfDrawerPlacement, SfPopoverStrategy } from '@storefront-ui/shared';
+import { SfDrawerPlacement } from '@storefront-ui/shared';
 import type { PropsWithChildren } from 'react';
 import type { PropsWithStyle } from '@storefront-ui/react';
 
 export interface SfDrawerProps extends PropsWithStyle, PropsWithChildren {
   open: boolean;
   placement?: `${SfDrawerPlacement}`;
-  strategy?: `${SfPopoverStrategy}`;
   disableClickAway?: boolean;
   disableEsc?: boolean;
   onClose?: () => void;
