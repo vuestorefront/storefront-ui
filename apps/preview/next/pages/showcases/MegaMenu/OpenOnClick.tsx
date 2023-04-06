a9231ec7cfb2bf5890a9c7c647eaec1cb1669224
Anna Musial <ania.musial13@gmail.com>
2023-04-06 12:09:35 +0200

    feat: added styling adjustments

--- apps/preview/next/pages/showcases/MegaMenu/OpenOnClick.tsx
+++ apps/preview/next/pages/showcases/MegaMenu/OpenOnClick.tsx
@@ -5,18 +5,22 @@ import {
   SfIconFavorite,
   SfIconPerson,
   SfIconMenu,
+  SfIconChevronRight,
+  SfIconClose,
   SfButton,
   SfDrawer,
   SfListItem,
 } from '@storefront-ui/react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import classNames from 'classnames';
-import brandLogo from '@assets/vsf_logo.svg';
-import brandLogoSign from '@assets/vsf_logo_sign.svg';
-import smartwatch from '@assets/smartwatch-2.png';
+import brandLogo from '@assets/vsf_logo_white.svg';
+import brandLogoSign from '@assets/vsf_logo_sign_white.svg';
+import sneakers from '@assets/sneakers-2.png';
 
 export default function OpenOnClick() {
   const [open, setOpen] = useState(false);
+  const nodeRef = useRef(null);
   const actionItems = [
     {
       icon: <SfIconShoppingCart />,
@@ -34,11 +38,8 @@ export default function OpenOnClick() {
   ];
 
   const bannerDetails = {
-    image: smartwatch,
+    image: sneakers,
     title: 'Hot Deals. No sweat.',
-    subtitle: 'Special offer',
-    description: 'Stay connected. Stay ahead and stay stylish.',
-    buttonText: 'Browse offers.',
   };
 
   const categoriesContent = [
@@ -121,86 +122,119 @@ export default function OpenOnClick() {
 
   return (
     <div className="w-full h-full">
-      <header className="flex justify-center w-full bg-white border-b h-14 md:h-20 border-neutral-200">
+      <header className="relative flex justify-center w-full border-0 bg-primary-700 h-14 md:h-20 border-neutral-200">
         <div className="flex items-center flex-row flex-nowrap justify-start h-full max-w-[1536px] w-full px-4 md:px-10">
-          <a href="/" aria-label="SF Homepage" className="inline-block text-primary-700">
+          <a href="/" aria-label="SF Homepage" className="inline-block text-white mr-2 lg:mr-10">
             <img src={brandLogo.src} alt="Sf Logo" className="hidden lg:block mr-10 w-[12.5rem] h-[1.75rem]" />
-            <img src={brandLogoSign.src} alt="Sf Logo" className="block w-8 h-8 mr-4 md:w-10 md:h-10 lg:hidden" />
+            <img src={brandLogoSign.src} alt="Sf Logo" className="hidden w-10 h-10 mr-4 md:block lg:hidden" />
+            <img src={brandLogoSign.src} alt="Sf Logo" className="block w-8 h-8 mr-4 min-w-8 md:hidden" />
           </a>
-          <nav className="flex flex-row flex-nowrap">
+          <nav className="flex w-full justify-between flex-nowrap">
             <ul>
               <li>
                 <SfButton
-                  className="block !px-2 mr-auto"
+                  className="block !px-2 mr-auto text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white font-body"
                   type="button"
-                  slotPrefix={<SfIconMenu />}
+                  slotSuffix={
+                    <>
+                      <SfIconChevronRight className="hidden rotate-90 md:inline-flex" />
+                      <SfIconMenu className="inline-flex md:hidden" />
+                    </>
+                  }
                   variant="tertiary"
-                  onClick={() => setOpen(true)}
+                  onClick={() => {
+                    setOpen(!open);
+                  }}
                 >
-                  <span className="hidden md:inline-flex">Categories</span>
+                  <span className="hidden md:inline-flex">Browse all products</span>
                 </SfButton>
-                <SfDrawer
-                  open={open}
-                  onClose={() => setOpen(false)}
-                  placement="top"
-                  className="flex flex-col gap-6 lg:flex-row bg-white shadow-lg p-8"
+                <CSSTransition
+                  ref={nodeRef}
+                  in={open}
+                  timeout={500}
+                  unmountOnExit
+                  classNames={{
+                    enter: '-translate-x-full md:translate-y-0 md:translate-x-0',
+                    enterActive: 'translate-x-0 md:translate-y-0 transition duration-500 ease-in-out',
+                    exitDone: 'translate-x-0',
+                    exitActive: '-translate-x-full transition duration-500 ease-in-out',
+                  }}
                 >
-                  <div className="flex flex-col gap-6 flex-1 lg:flex-row">
+                  <SfDrawer
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    placement="top"
+                    className="grid grid-cols-1 gap-6 lg:grid-cols-4 !fixed md:!absolute md:!top-[5rem] bg-white max-w-xs md:max-w-full shadow-lg p-0 md:p-8"
+                  >
+                    <div className="flex items-center justify-between p-4 bg-primary-700 md:hidden">
+                      <div className="flex items-center typography-text-lg font-medium text-white">
+                        Browse all products
+                      </div>
+                      <SfButton
+                        square
+                        variant="tertiary"
+                        onClick={() => {
+                          setOpen(!open);
+                        }}
+                        className="text-white"
+                      >
+                        <SfIconClose />
+                      </SfButton>
+                    </div>
                     {categoriesContent.map(({ heading, items }) => (
-                      <section>
-                        <p className="typography-text-xs font-bold text-neutral-900 tracking-widest mb-3.5 px-4">
+                      <section className="flex-1/4">
+                        <p className="typography-text-xs font-bold text-neutral-900 tracking-widest whitespace-nowrap px-4 py-2">
                           {heading}
                         </p>
+                        <hr className="mb-3.5" />
                         <ul>
                           {items.map((item) => (
                             <SfListItem size="sm">
-                              <a href={item.link}>{item.title}</a>
+                              <a
+                                className="focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
+                                href={item.link}
+                              >
+                                {item.title}
+                              </a>
                             </SfListItem>
                           ))}
                         </ul>
                       </section>
                     ))}
-                  </div>
-                  <div className="flex bg-neutral-100 rounded-md border-neutral-300 justify-between overflow-hidden grow">
-                    <img
-                      src={bannerDetails.image.src}
-                      alt={bannerDetails.title}
-                      className="w-1/2 self-end object-contain"
-                    />
-                    <div className="flex flex-col justify-center items-start p-6 lg:p-10 max-w-1/2">
-                      <p className="uppercase typography-text-xs block font-bold tracking-widest">
-                        {bannerDetails.subtitle}
+                    <div className="flex flex-col items-center justify-center bg-neutral-100 rounded-md border-neutral-300 overflow-hidden grow">
+                      <img src={bannerDetails.image.src} alt={bannerDetails.title} className="object-contain" />
+                      <p className="mb-4 mt-4 px-4 text-center typography-text-base font-medium">
+                        {bannerDetails.title}
                       </p>
-                      <h2 className="mb-4 mt-2 font-bold typography-headline-3">{bannerDetails.title}</h2>
-                      <p className="typography-text-base hidden lg:block mb-4">{bannerDetails.description}</p>
-                      <SfButton className="hidden lg:inline-flex">{bannerDetails.buttonText}</SfButton>
                     </div>
-                  </div>
-                </SfDrawer>
+                  </SfDrawer>
+                </CSSTransition>
               </li>
             </ul>
-            {actionItems.map((actionItem) => (
+            <div>
+              {actionItems.map((actionItem) => (
+                <SfButton
+                  className={classNames(
+                    'mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white',
+                    actionItem.label === 'Log in' ? 'md:hidden' : null,
+                  )}
+                  key={actionItem.ariaLabel}
+                  aria-label={actionItem.ariaLabel}
+                  variant="tertiary"
+                  slotPrefix={actionItem.icon}
+                  square
+                />
+              ))}
               <SfButton
-                className={classNames(
-                  'mr-2 -ml-0.5 text-primary-700 hover:bg-primary-100 active:bg-primary-200 hover:text-primary-600 active:text-primary-700',
-                  actionItem.label === 'Log in' ? 'lg:hidden' : null,
-                )}
-                key={actionItem.ariaLabel}
-                aria-label={actionItem.ariaLabel}
+                className="hidden md:inline-flex mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
+                key={actionItems[2].ariaLabel}
+                aria-label={actionItems[2].ariaLabel}
                 variant="tertiary"
-                slotPrefix={actionItem.icon}
-                square
-              />
-            ))}
-            <SfButton
-              className="hidden lg:inline-flex mr-2 -ml-0.5 text-primary-700 hover:bg-primary-100 active:bg-primary-200 hover:text-primary-600 active:text-primary-700"
-              key={actionItems[2].ariaLabel}
-              aria-label={actionItems[2].ariaLabel}
-              variant="tertiary"
-              slotPrefix={actionItems[2].icon}
-            >
-              Log In
-            </SfButton>
+                slotPrefix={actionItems[2].icon}
+              >
+                Log In
+              </SfButton>
+            </div>
           </nav>
         </div>
       </header>
