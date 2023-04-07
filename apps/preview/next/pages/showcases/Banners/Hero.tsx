9493eb1b5ddf94f8b942b13b3952c5c201fca8f8
Justyna <jgieracka@vuestorefront.io>
2023-04-07 11:57:19 +0200

    fix: picture instead of background image

--- apps/preview/next/pages/showcases/Banners/Hero.tsx
+++ apps/preview/next/pages/showcases/Banners/Hero.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { ShowcasePageLayout } from '../../showcases';
 // #region source
 import { SfButton } from '@storefront-ui/react';
@@ -5,33 +6,34 @@ import headphones from '@assets/hero-headphones.png';
 import mobileBackground from '@assets/hero-bg-mobile.png';
 import desktopBackground from '@assets/hero-bg.png';
 
-const variables = {
-  '--sf-img-mobile': `url(${mobileBackground.src})`,
-  '--sf-img-desktop': `url(${desktopBackground.src})`,
-} as React.CSSProperties;
-
 export default function Hero() {
   return (
-    <div
-      style={variables}
-      className="md:flex md:flex-row-reverse min-h-[600px] max-w-[1536px] mx-auto bg-[url:var(--sf-img-mobile)] md:bg-[url:var(--sf-img-desktop)] bg-center bg-cover bg-no-repeat"
-    >
-      <div className="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
-        <img src={headphones.src} alt="Headphones" className="h-full object-cover object-left" />
-      </div>
-      <div className="p-4 md:p-10 md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
-        <p className="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500">
-          Feel the music
-        </p>
-        <h2 className="typography-headline-2 md:typography-headline-1 md:leading-[67.5px] font-bold mt-2 mb-4">
-          New Wireless Pro
-        </h2>
-        <p className="typography-text-base md:typography-text-lg">
-          Spatial audio. Adjustable ear cups. On-device controls. All-day battery.
-        </p>
-        <div className="flex flex-col md:flex-row gap-4 mt-6">
-          <SfButton> Order now </SfButton>
-          <SfButton variant="secondary"> Show more </SfButton>
+    <div className="relative min-h-[600px]">
+      <picture>
+        <source srcSet={mobileBackground.src} media="(max-width: 767px)" />
+        <source srcSet={desktopBackground.src} media="(min-width: 768px)" />
+        <img src={desktopBackground.src} className="absolute w-full h-full z-[-1] md:object-cover" />
+      </picture>
+      <div className="md:flex md:flex-row-reverse md:justify-center min-h-[600px] max-w-[1536px] mx-auto">
+        <div className="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
+          <img src={headphones.src} alt="Headphones" className="h-full object-cover object-left" />
+        </div>
+        <div className="p-4 md:p-10 md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
+          <p className="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500 uppercase">
+            Feel the music
+          </p>
+          <h2 className="typography-headline-2 md:typography-headline-1 md:leading-[67.5px] font-bold mt-2 mb-4">
+            New Wireless Pro
+          </h2>
+          <p className="typography-text-base md:typography-text-lg">
+            Spatial audio. Adjustable ear cups. On-device controls. All-day battery.
+          </p>
+          <div className="flex flex-col md:flex-row gap-4 mt-6">
+            <SfButton size="lg"> Order now </SfButton>
+            <SfButton size="lg" className="bg-white" variant="secondary">
+              Show more
+            </SfButton>
+          </div>
         </div>
       </div>
     </div>
--- apps/preview/nuxt/pages/showcases/Banners/Hero.vue
+++ apps/preview/nuxt/pages/showcases/Banners/Hero.vue
@@ -1,22 +1,28 @@
 <template>
-  <div
-    :style="variables"
-    class="md:flex md:flex-row-reverse min-h-[600px] max-w-[1536px] mx-auto bg-[url:var(--sf-img-mobile)] md:bg-[url:var(--sf-img-desktop)] bg-center bg-cover bg-no-repeat"
-  >
-    <div class="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
-      <img :src="headphones" alt="Headphones" class="h-full object-cover object-left" />
-    </div>
-    <div class="p-4 md:p-10 md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
-      <p class="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500">Feel the music</p>
-      <h2 class="typography-headline-2 md:typography-headline-1 md:leading-[67.5px] font-bold mt-2 mb-4">
-        New Wireless Pro
-      </h2>
-      <p class="typography-text-base md:typography-text-lg">
-        Spatial audio. Adjustable ear cups. On-device controls. All-day battery.
-      </p>
-      <div class="flex flex-col md:flex-row gap-4 mt-6">
-        <SfButton> Order now </SfButton>
-        <SfButton variant="secondary"> Show more </SfButton>
+  <div class="relative min-h-[600px]">
+    <picture>
+      <source :srcset="mobileBackground" media="(max-width: 767px)" />
+      <source :srcset="desktopBackground" media="(min-width: 768px)" />
+      <img :src="desktopBackground" class="absolute w-full h-full z-[-1] md:object-cover" />
+    </picture>
+    <div class="md:flex md:flex-row-reverse md:justify-center max-w[1536px] mx-auto md:min-h-[600px]">
+      <div class="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
+        <img :src="headphones" alt="Headphones" class="h-full object-cover object-left" />
+      </div>
+      <div class="p-4 md:p-10 md:max-w-[768px] md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
+        <p class="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500 uppercase">
+          Feel the music
+        </p>
+        <h2 class="typography-headline-2 md:typography-headline-1 md:leading-[67.5px] font-bold mt-2 mb-4">
+          New Wireless Pro
+        </h2>
+        <p class="typography-text-base md:typography-text-lg">
+          Spatial audio. Adjustable ear cups. On-device controls. All-day battery.
+        </p>
+        <div class="flex flex-col md:flex-row gap-4 mt-6">
+          <SfButton size="lg"> Order now </SfButton>
+          <SfButton size="lg" variant="secondary" class="bg-white"> Show more </SfButton>
+        </div>
       </div>
     </div>
   </div>
@@ -27,9 +33,4 @@ import { SfButton } from '@storefront-ui/vue';
 import headphones from '@assets/hero-headphones.png';
 import mobileBackground from '@assets/hero-bg-mobile.png';
 import desktopBackground from '@assets/hero-bg.png';
-
-const variables = {
-  '--sf-img-mobile': `url(${mobileBackground})`,
-  '--sf-img-desktop': `url(${desktopBackground})`,
-};
 </script>
