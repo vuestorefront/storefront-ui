8c85e5c8f8ec4945f0b716beccb2284327ae91c2
Justyna <jgieracka@vuestorefront.io>
2023-04-12 08:36:10 +0200

    fix: lint fixes

--- apps/preview/next/pages/showcases/Alert/AlertSecondary.tsx
+++ apps/preview/next/pages/showcases/Alert/AlertSecondary.tsx
@@ -5,12 +5,12 @@ import { SfIconInfo } from '@storefront-ui/react';
 export default function AlertSecondary() {
   return (
     <div
-    role="alert"
-    className="flex items-center bg-secondary-100 pr-2 pl-4 ring-1 ring-secondary-200 typography-text-sm md:typography-text-base py-1 rounded-md"
-  >
-    <SfIconInfo className="mr-2 text-secondary-700" />
-    <p className="py-2">Your cart will soon be full.</p>
-  </div>
+      role="alert"
+      className="flex items-center bg-secondary-100 pr-2 pl-4 ring-1 ring-secondary-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+    >
+      <SfIconInfo className="mr-2 text-secondary-700" />
+      <p className="py-2">Your cart will soon be full.</p>
+    </div>
   );
 }
 // #endregion source
