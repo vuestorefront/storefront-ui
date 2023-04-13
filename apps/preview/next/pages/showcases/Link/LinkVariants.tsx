04a995710f6a4e2946b4ef32c97e8468348d4d08
Kacper Dziub <34419118+dkacper@users.noreply.github.com>
2023-04-13 12:51:03 +0200

    fix: link examples (#2592)
    * fix: provide links for examples
    
    * fix: add docs query param to NuxtLink

--- apps/preview/next/pages/showcases/Link/LinkVariants.tsx
+++ apps/preview/next/pages/showcases/Link/LinkVariants.tsx
@@ -5,8 +5,12 @@ import { SfLink } from '@storefront-ui/react';
 export default function LinkVariants() {
   return (
     <div className="space-x-4">
-      <SfLink variant="primary"> Primary </SfLink>
-      <SfLink variant="secondary"> Secondary </SfLink>
+      <SfLink variant="primary" href="#">
+        Primary
+      </SfLink>
+      <SfLink variant="secondary" href="#">
+        Secondary
+      </SfLink>
     </div>
   );
 }
