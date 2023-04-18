3afe17e6f8c723f32da0dfffd879a9cfe028f379
AdamPawlinski <apawlinski@vuestorefront.io>
2023-04-18 10:47:23 +0200

    feat: gallery with bullets

--- apps/preview/next/pages/showcases/Gallery/GalleryWithBullets.tsx
+++ apps/preview/next/pages/showcases/Gallery/GalleryWithBullets.tsx
@@ -3,9 +3,8 @@
 import { ShowcasePageLayout } from '../../showcases';
 
 // #region source
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { SfScrollable, SfButton, SfIconChevronLeft, SfIconChevronRight } from '@storefront-ui/react';
-import { clamp } from '@storefront-ui/shared';
 import gallery1 from '@assets/gallery_1.png';
 import gallery2 from '@assets/gallery_2.png';
 import gallery3 from '@assets/gallery_3.png';
@@ -16,16 +15,6 @@ import gallery7 from '@assets/gallery_7.png';
 import gallery8 from '@assets/gallery_8.png';
 import gallery9 from '@assets/gallery_9.png';
 import gallery10 from '@assets/gallery_10.png';
-import gallery1_thumb from '@assets/gallery_1_thumb.png';
-import gallery2_thumb from '@assets/gallery_2_thumb.png';
-import gallery3_thumb from '@assets/gallery_3_thumb.png';
-import gallery4_thumb from '@assets/gallery_4_thumb.png';
-import gallery5_thumb from '@assets/gallery_5_thumb.png';
-import gallery6_thumb from '@assets/gallery_6_thumb.png';
-import gallery7_thumb from '@assets/gallery_7_thumb.png';
-import gallery8_thumb from '@assets/gallery_8_thumb.png';
-import gallery9_thumb from '@assets/gallery_9_thumb.png';
-import gallery10_thumb from '@assets/gallery_10_thumb.png';
 
 const images = [
   { image: gallery1.src, alt: 'backpack' },
@@ -40,30 +29,16 @@ const images = [
   { image: gallery10.src, alt: 'backpack' },
 ];
 
-const thumbImages = [
-  { image: gallery1_thumb.src, alt: 'backpack' },
-  { image: gallery2_thumb.src, alt: 'backpack' },
-  { image: gallery3_thumb.src, alt: 'backpack' },
-  { image: gallery4_thumb.src, alt: 'backpack' },
-  { image: gallery5_thumb.src, alt: 'backpack' },
-  { image: gallery6_thumb.src, alt: 'backpack' },
-  { image: gallery7_thumb.src, alt: 'backpack' },
-  { image: gallery8_thumb.src, alt: 'backpack' },
-  { image: gallery9_thumb.src, alt: 'backpack' },
-  { image: gallery10_thumb.src, alt: 'backpack' },
-];
-
 export default function GalleryWithBullets() {
   const [activeIndex, setActiveIndex] = useState(0);
-  const itemsLength = thumbImages.length;
+  const itemsLength = images.length;
   return (
-    <div className="relative flex flex-col h-full gap-1 scroll-smooth">
+    <div className="relative flex flex-col gap-1 scroll-smooth">
       <SfScrollable
-        className="after:block after:pt-[100%] flex-1 relative overflow-hidden w-full cursor-grab active:cursor-grabbing touch-pan-y max-h-[600px] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
+        className="items-center w-full max-h-[700px] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
         activeIndex={activeIndex}
         previousDisabled={activeIndex === 0}
         nextDisabled={activeIndex === itemsLength - 1}
-        drag
         onPrev={() => {
           setActiveIndex(() => activeIndex - 1);
         }}
@@ -72,47 +47,34 @@ export default function GalleryWithBullets() {
         }}
         slotPreviousButton={
           <SfButton
-            className="hidden group-hover:flex absolute !rounded-full z-10 left-4"
+            className="absolute !rounded-full !px-3 z-10 top-1/2 left-4"
             variant="secondary"
-            size="sm"
-            square
+            size="lg"
             slotPrefix={<SfIconChevronLeft />}
           />
         }
         slotNextButton={
           <SfButton
-            className="hidden group-hover:flex absolute !rounded-full z-10 right-4"
+            className="absolute !rounded-full !px-3 z-10 top-1/2 right-4"
             variant="secondary"
-            size="sm"
-            square
+            size="lg"
             slotPrefix={<SfIconChevronRight />}
-            onClick={({ preventDefault }) => {
-              preventDefault();
-              setActiveIndex(() => activeIndex + 1);
-            }}
           />
         }
       >
-        {/* <div
-          className="absolute top-0 left-0 flex w-full h-full transition-transform snap-x snap-mandatory [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] will-change-transform"
-          style={{
-            transform: `translate3d(${imgPosition * -100}%,0,0)`,
-          }}
-        > */}
         {images.map(({ image, alt }, index) => (
-          <div className="relative snap-center snap-always basis-full shrink-0 grow" key={`${alt}-${index}`}>
+          <div className="relative snap-center snap-always shrink-0 " key={`${alt}-${index}`}>
             <img className="object-contain" alt={alt} src={image} draggable="false" />
           </div>
         ))}
-        {/* </div> */}
       </SfScrollable>
       <div className="flex-shrink-0 overflow-hidden basis-auto">
-        <div className="flex-row w-full snap-both snap-mandatory flex gap-0.5 mt overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
-          {thumbImages.map(({ alt }, index) => (
+        <div className="flex-row w-full flex gap-0.5 mt overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
+          {images.map(({ alt }, index) => (
             <div
               aria-label={alt}
               key={`${index}-bullet`}
-              className={`w-[78px] relative pb-1 border-b-4 snap-start cursor-pointer transition-colors  ${
+              className={`w-[78px] relative pb-1 border-b-4 cursor-pointer transition-colors  ${
                 activeIndex === index ? 'border-primary-700' : 'border-gray-200'
               }`}
             />
