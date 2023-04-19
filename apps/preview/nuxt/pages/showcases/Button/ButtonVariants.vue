73a394a26f4b53945811eb53ed35d3c2f554bda8
Justyna <jgieracka@vuestorefront.io>
2023-04-19 11:45:29 +0200

    fix: remove enums from button examples

--- apps/preview/nuxt/pages/showcases/Button/ButtonVariants.vue
+++ apps/preview/nuxt/pages/showcases/Button/ButtonVariants.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col items-center space-y-4 xs:block xs:space-x-4">
-    <SfButton type="button" :variant="SfButtonVariant.primary"> Hello </SfButton>
+    <SfButton type="button" variant="primary"> Hello </SfButton>
 
-    <SfButton type="button" :variant="SfButtonVariant.secondary"> Hello </SfButton>
+    <SfButton type="button" variant="secondary"> Hello </SfButton>
 
-    <SfButton type="button" :variant="SfButtonVariant.tertiary"> Hello </SfButton>
+    <SfButton type="button" variant="tertiary"> Hello </SfButton>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { SfButton, SfButtonVariant } from '@storefront-ui/vue';
+import { SfButton } from '@storefront-ui/vue';
 </script>
