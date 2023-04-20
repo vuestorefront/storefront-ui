135d41a86203898a7c94f1ebe6e336c9aa2815b3
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-04-20 10:28:58 +0200

    fix: alert blocks styling

--- apps/preview/next/pages/showcases/Alert/AlertError.tsx
+++ apps/preview/next/pages/showcases/Alert/AlertError.tsx
@@ -11,17 +11,17 @@ export default function AlertError() {
       <p className="py-2 mr-2">The password change was failed.</p>
       <button
         type="button"
-        className="py-1.5 px-3 md:py-2 md:px-4 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900 ml-auto font-medium"
+        className="py-1.5 px-3 md:py-2 md:px-4 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900 ml-auto font-medium focus-visible:outline focus-visible:outline-offset"
       >
         Retry
       </button>
       <button
         type="button"
-        className="p-1.5 md:p-2 ml-2 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900"
+        className="p-1.5 md:p-2 ml-2 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900 focus-visible:outline focus-visible:outline-offset"
         aria-label="Close error alert"
       >
         <SfIconClose className="hidden md:block" />
-        <SfIconClose size="sm" className="md:hidden block" />
+        <SfIconClose size="sm" className="block md:hidden" />
       </button>
     </div>
   );
--- apps/preview/nuxt/pages/showcases/Alert/AlertError.vue
+++ apps/preview/nuxt/pages/showcases/Alert/AlertError.vue
@@ -6,17 +6,17 @@
     <p class="py-2 mr-2">The password change was failed.</p>
     <button
       type="button"
-      class="py-1.5 px-3 md:py-2 md:px-4 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900 ml-auto font-medium"
+      class="py-1.5 px-3 md:py-2 md:px-4 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900 ml-auto font-medium focus-visible:outline focus-visible:outline-offset"
     >
       Retry
     </button>
     <button
       type="button"
-      class="p-1.5 md:p-2 ml-2 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900"
+      class="p-1.5 md:p-2 ml-2 rounded-md text-negative-700 hover:bg-negative-200 active:bg-negative-300 hover:text-negative-800 active:text-negative-900 focus-visible:outline focus-visible:outline-offset"
       aria-label="Close error alert"
     >
       <SfIconClose class="hidden md:block" />
-      <SfIconClose size="sm" class="md:hidden block" />
+      <SfIconClose size="sm" class="block md:hidden" />
     </button>
   </div>
 </template>
