dd88c60263d2bdb4e57d25925ffc385e18e993ca
Anna Musial <ania.musial13@gmail.com>
2023-04-20 17:10:33 +0200

    feat: adjusted listitem structure in megamenu

--- apps/preview/nuxt/pages/showcases/MegaMenu/BaseMegaMenu.vue
+++ apps/preview/nuxt/pages/showcases/MegaMenu/BaseMegaMenu.vue
@@ -65,14 +65,17 @@
                     </h2>
                     <hr class="mb-3.5" />
                     <ul>
-                      <SfListItem v-for="item in items" :key="item.title" size="sm" role="none">
-                        <a
-                          class="focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
+                      <li v-for="item in items" :key="item.title">
+                        <SfListItem
+                          tag="a"
                           :href="item.link"
+                          size="sm"
+                          role="none"
+                          class="focus-visible:outline focus-visible:outline-offset focus-visible:rounded-md"
                         >
                           {{ item.title }}
-                        </a>
-                      </SfListItem>
+                        </SfListItem>
+                      </li>
                     </ul>
                   </div>
                   <div
