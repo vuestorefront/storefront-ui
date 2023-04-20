a1059a07ba54f0a81eced33ac0a062115f641522
AdamPawlinski <apawlinski@vuestorefront.io>
2023-04-20 10:23:59 +0200

    feat: vue with bullets

--- apps/preview/nuxt/pages/showcases/Gallery/GalleryWithBullets.vue
+++ apps/preview/nuxt/pages/showcases/Gallery/GalleryWithBullets.vue
@@ -1,34 +1,58 @@
 <template>
   <div class="relative flex flex-col h-full gap-1 scroll-smooth">
-    <div
-      ref="draggableRef"
-      class="after:block after:pt-[100%] flex-1 relative overflow-hidden w-full cursor-grab active:cursor-grabbing touch-pan-y max-h-[600px]"
-      @mousedown="pointerHandler"
-      @touchstart="pointerHandler"
+    <SfScrollable
+      class="group/scrollable items-center w-full max-h-[700px] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
+      :active-index="activeIndex"
+      is-active-index-centered
+      :previous-disabled="activeIndex === 0"
+      :next-disabled="activeIndex === itemsLength - 1"
+      buttons-placement="floating"
     >
+      <template #previousButton="defaultProps">
+        <SfButton
+          v-bind="defaultProps"
+          :disabled="activeIndex === 0"
+          class="absolute !rounded-full !p-3 z-10 top-1/2 left-4"
+          variant="secondary"
+          size="lg"
+          square
+        >
+          <SfIconChevronLeft />
+        </SfButton>
+      </template>
       <div
-        class="absolute top-0 left-0 flex w-full h-full transition-transform snap-x snap-mandatory [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] will-change-transform"
-        :style="`transform: translate3d(${imgPosition * -100}%, 0, 0) `"
+        class="flex w-full h-full [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] will-change-transform"
       >
         <div
           v-for="({ image, alt }, index) in images"
           :key="`${alt}-${index}`"
-          class="relative snap-center snap-always basis-full shrink-0 grow"
+          class="relative basis-full shrink-0 grow"
         >
           <img class="object-contain" :alt="alt" :src="image" draggable="false" />
         </div>
       </div>
-    </div>
+      <template #nextButton="defaultProps">
+        <SfButton
+          v-bind="defaultProps"
+          :disabled="activeIndex === itemsLength - 1"
+          class="absolute !rounded-full !p-3 z-10 top-1/2 right-4"
+          variant="secondary"
+          size="lg"
+          square
+        >
+          <SfIconChevronRight />
+        </SfButton>
+      </template>
+    </SfScrollable>
     <div class="flex-shrink-0 overflow-hidden basis-auto">
       <div
-        class="flex-row w-full snap-both snap-mandatory flex gap-0.5 mt overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
+        class="flex-row w-full flex gap-0.5 mt overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
       >
         <div
-          v-for="({ alt }, index) in thumbImages"
-          :key="`${alt}-${index}-thumbnail`"
-          :aria-label="alt"
+          v-for="(item, index) in images"
+          :key="`${index}-bullet`"
           :class="[
-            'w-[78px] relative pb-1 border-b-4 snap-start cursor-pointer transition-colors',
+            'w-[78px] relative pb-1 border-b-4 snap-start transition-colors',
             activeIndex === index ? 'border-primary-700' : 'border-gray-200',
           ]"
         />
@@ -37,9 +61,8 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, computed, watch } from 'vue';
-import { clamp } from '@storefront-ui/shared';
-
+import { SfScrollable, SfButton, SfIconChevronLeft, SfIconChevronRight } from '@storefront-ui/vue';
+import { ref } from 'vue';
 import gallery1 from '@assets/gallery_1.png';
 import gallery2 from '@assets/gallery_2.png';
 import gallery3 from '@assets/gallery_3.png';
@@ -50,16 +73,6 @@ import gallery7 from '@assets/gallery_7.png';
 import gallery8 from '@assets/gallery_8.png';
 import gallery9 from '@assets/gallery_9.png';
 import gallery10 from '@assets/gallery_10.png';
-import gallery1_thumb from '@assets/gallery_1_thumb.png';
-import gallery2_thumb from '@assets/gallery_2_thumb.png';
-import gallery3_thumb from '@assets/gallery_3_thumb.png';
-import gallery4_thumb from '@assets/gallery_4_thumb.png';
-import gallery5_thumb from '@assets/gallery_5_thumb.png';
-import gallery6_thumb from '@assets/gallery_6_thumb.png';
-import gallery7_thumb from '@assets/gallery_7_thumb.png';
-import gallery8_thumb from '@assets/gallery_8_thumb.png';
-import gallery9_thumb from '@assets/gallery_9_thumb.png';
-import gallery10_thumb from '@assets/gallery_10_thumb.png';
 
 const images = [
   { image: gallery1, alt: 'backpack' },
@@ -74,50 +87,6 @@ const images = [
   { image: gallery10, alt: 'backpack' },
 ];
 
-const thumbImages = [
-  { image: gallery1_thumb, alt: 'backpack' },
-  { image: gallery2_thumb, alt: 'backpack' },
-  { image: gallery3_thumb, alt: 'backpack' },
-  { image: gallery4_thumb, alt: 'backpack' },
-  { image: gallery5_thumb, alt: 'backpack' },
-  { image: gallery6_thumb, alt: 'backpack' },
-  { image: gallery7_thumb, alt: 'backpack' },
-  { image: gallery8_thumb, alt: 'backpack' },
-  { image: gallery9_thumb, alt: 'backpack' },
-  { image: gallery10_thumb, alt: 'backpack' },
-];
-
-const draggableRef = ref<HTMLElement>();
-const offsetPosition = ref(0);
 const activeIndex = ref(0);
-const isDragging = ref(false);
-
-const imgPosition = activeIndex.value + offsetPosition.value;
-
-const pointerHandler = (e: PointerEvent<HTMLDivElement>) => {
-  e.preventDefault();
-  if (!draggableRef.value) {
-    return;
-  }
-  draggableRef.value.setPointerCapture(e.pointerId);
-  const pointerDownOffset = computed(() => e.nativeEvent.offsetX);
-  isDragging.value = true;
-  const rect = draggableRef.value.getBoundingClientRect();
-  const pointerEventMethod = (event: PointerEvent) => {
-    offsetPosition.value = pointerDownOffset.value - event.offsetX / rect.width / 5;
-  };
-  draggableRef.value.addEventListener('pointermove', pointerEventMethod, { passive: false });
-  draggableRef.value.addEventListener('pointerup', () => {
-    isDragging.value = false;
-    draggableRef.value?.removeEventListener('pointermove', pointerEventMethod);
-  });
-};
-
-watch(isDragging, () => {
-  if (!isDragging.value) {
-    const stopVal = offsetPosition.value > 0 ? Math.ceil(imgPosition) : Math.floor(imgPosition);
-    activeIndex.value = clamp(stopVal, 0, images.length - 1);
-    offsetPosition.value = 0;
-  }
-});
+const itemsLength = images.length;
 </script>
