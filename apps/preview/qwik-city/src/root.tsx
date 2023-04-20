bbd094f955768ed24b84bb9d00152dd961a75706
gioboa <giorgiob.boa@gmail.com>
2023-04-20 17:32:17 +0200

    remove unused component

--- apps/preview/qwik-city/src/root.tsx
+++ apps/preview/qwik-city/src/root.tsx
@@ -1,6 +1,5 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
 import style from '../../../../packages/config/example-style/index.scss?inline';
@@ -18,7 +17,6 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />
