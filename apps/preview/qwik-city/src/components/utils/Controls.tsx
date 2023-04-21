dd8a05883699a1daee0b0535a9917874dbe7b8cf
gioboa <giorgiob.boa@gmail.com>
2023-04-21 09:25:35 +0200

    add different type of control

--- apps/preview/qwik-city/src/components/utils/Controls.tsx
+++ apps/preview/qwik-city/src/components/utils/Controls.tsx
@@ -1,13 +1,43 @@
-import { component$, useContext, useSignal } from '@builder.io/qwik';
+import { $, QwikChangeEvent, component$, useContext, useSignal } from '@builder.io/qwik';
 import { SfButton, SfButtonSize, SfButtonVariant, SfIconExpandLess, SfIconExpandMore } from '@storefront-ui/qwik';
 import { ControlOptionBind, ControlsType } from './types';
 import { EXAMPLES_STATE } from '~/routes/examples/layout';
 
-// TODO
-// <T extends { [k: string]: any }>({ controls, state, className }: ControlsProps<T>) {
 export const Controls = component$<any>(() => {
   const examplesState = useContext(EXAMPLES_STATE);
   const previewBottomOpenSignal = useSignal(false);
+
+  const handleOnChangeValue = $((e: QwikChangeEvent<HTMLSelectElement | HTMLInputElement>, name: string) => {
+    examplesState.data.state = { ...examplesState.data.state, [name]: e.target.value };
+  });
+
+  const handleJsonOnChangeValue = $((e: QwikChangeEvent<HTMLTextAreaElement>, name: string) => {
+    examplesState.data.state = { ...examplesState.data.state, [name]: JSON.parse(e.target.value) };
+  });
+
+  const handleCheckbox = $(
+    (e: QwikChangeEvent<HTMLInputElement>, name: string, currentValue: string | boolean | []) => {
+      const { value } = e.target;
+      if (Array.isArray(currentValue)) {
+        const newValue: string[] = [...currentValue];
+        const valueItemIndex = newValue.indexOf(value);
+        if (valueItemIndex === -1) {
+          newValue.push(value);
+        } else {
+          newValue.splice(valueItemIndex, 1);
+        }
+        examplesState.data.state = { ...examplesState.data.state, [name]: newValue };
+      } else {
+        examplesState.data.state = {
+          ...examplesState.data.state,
+          [name]: typeof currentValue === 'string' ? currentValue === value : !currentValue,
+        };
+      }
+    },
+  );
+
+  const checkboxValue = (option: ControlOptionBind | string) =>
+    typeof option === 'string' ? option : option.value || option.label;
 
   return (
     <div class={{ class: true, 'e-page-controls': true, 'e-page-controls--collapsed': !previewBottomOpenSignal.value }}>
@@ -48,7 +78,7 @@ export const Controls = component$<any>(() => {
                   <span id={control.modelName}>{control.modelName}</span>
                 </td>
                 <td class="value">
-                  {control.type === 'boolean' && (
+                  {control.type === 'boolean' ? (
                     <div class="switch-wrapper">
                       <label class="switch">
                         <input
@@ -67,17 +97,12 @@ export const Controls = component$<any>(() => {
                       </label>
                       <span class="ml-2">{examplesState.data.state[control.modelName].toString()}</span>
                     </div>
-                  )}
-
-                  {control.type === 'select' && (
+                  ) : control.type === 'select' ? (
                     <select
                       value={examplesState.data.state[control.modelName]}
                       aria-labelledby={control.modelName}
                       onChange$={(e) => {
-                        examplesState.data.state = {
-                          ...examplesState.data.state,
-                          [control.modelName]: e.target.value,
-                        };
+                        handleOnChangeValue(e, control.modelName);
                       }}
                     >
                       {(control?.options || ([{}] as NonNullable<ControlsType[number]['options']>)).map(
@@ -95,101 +120,77 @@ export const Controls = component$<any>(() => {
                         ),
                       )}
                     </select>
+                  ) : (
+                    (control?.options || ([{}] as NonNullable<ControlsType[number]['options']>)).map(
+                      (option, optionIndex) => (
+                        <div key={`${control.modelName}-${index}-${optionIndex}`} class="flex items-center">
+                          <label class="flex items-center">
+                            {(() => {
+                              switch (control.type) {
+                                case 'json':
+                                  return (
+                                    <textarea
+                                      rows={10}
+                                      {...(option as ControlOptionBind).bind}
+                                      class="border rounded-md"
+                                      aria-labelledby={control.modelName}
+                                      value={
+                                        JSON.stringify(
+                                          examplesState.data.state[control.modelName],
+                                          undefined,
+                                          2,
+                                        ) as string
+                                      }
+                                      onChange$={(e) => {
+                                        handleJsonOnChangeValue(e, control.modelName);
+                                      }}
+                                    />
+                                  );
+                                case 'range':
+                                case 'text':
+                                  return (
+                                    <input
+                                      {...(option as ControlOptionBind).bind}
+                                      value={examplesState.data.state[control.modelName] as number | string}
+                                      class="border rounded-md"
+                                      type={control.type}
+                                      aria-labelledby={control.modelName}
+                                      onChange$={(e) => {
+                                        handleOnChangeValue(e, control.modelName);
+                                      }}
+                                    />
+                                  );
+                                default:
+                                  return (
+                                    <input
+                                      {...(option as ControlOptionBind).bind}
+                                      value={checkboxValue(option)}
+                                      type={control.type}
+                                      aria-labelledby={control.modelName}
+                                      onChange$={(e) => {
+                                        control.type === 'checkbox'
+                                          ? handleCheckbox(
+                                              e,
+                                              control.modelName,
+                                              examplesState.data.state[control.modelName],
+                                            )
+                                          : handleOnChangeValue(e, control.modelName);
+                                      }}
+                                      name={`${control.modelName}-${index}`}
+                                    />
+                                  );
+                              }
+                            })()}
+                            {typeof option === 'string' ? (
+                              <span class="pl-2">{option}</span>
+                            ) : (
+                              'label' in option && <span class="pl-2">{(option as ControlOptionBind).label}</span>
+                            )}
+                          </label>
+                        </div>
+                      ),
+                    )
                   )}
-
-                  {/* {((): JSX.Element | JSX.Element[] => {
-                    switch (control.type) {
-                      case 'select':
-                        return (
-                          
-                        );
-                      case 'boolean':
-                        return (
-                          <div className="switch-wrapper">
-                            <label className="switch">
-                              <input
-                                checked={state.get[control.modelName]}
-                                value={state.get[control.modelName]}
-                                aria-labelledby={control.modelName}
-                                onChange={() =>
-                                  state.set((currentState) => ({
-                                    ...currentState,
-                                    [control.modelName]: !currentState[control.modelName],
-                                  }))
-                                }
-                                type="checkbox"
-                              />
-                              <span className="slider" />
-                            </label>
-                            <span className="ml-2">{state.get[control.modelName]?.toString()}</span>
-                          </div>
-                        );
-                      default:
-                        return (control?.options || ([{}] as NonNullable<ControlsType[number]['options']>)).map(
-                          (option, optionIndex) => (
-                            // eslint-disable-next-line react/no-array-index-key
-                            <div key={`${control.modelName}-${index}-${optionIndex}`} className="flex items-center">
-                              <label className="flex items-center">
-                                {(() => {
-                                  switch (control.type) {
-                                    case 'json':
-                                      return (
-                                        <textarea
-                                          rows={10}
-                                          {...(option as ControlOptionBind).bind}
-                                          className="border rounded-md"
-                                          aria-labelledby={control.modelName}
-                                          value={JSON.stringify(state.get[control.modelName], undefined, 2) as string}
-                                          onChange={(e) => handleJsonOnChangeValue(e, control.modelName)}
-                                        />
-                                      );
-                                    case 'range':
-                                    case 'text':
-                                      return (
-                                        <input
-                                          {...(option as ControlOptionBind).bind}
-                                          value={state.get[control.modelName] as number | string}
-                                          className="border rounded-md"
-                                          type={control.type}
-                                          aria-labelledby={control.modelName}
-                                          onChange={(e) => handleOnChangeValue(e, control.modelName)}
-                                        />
-                                      );
-                                    default:
-                                      return (
-                                        <input
-                                          {...(option as ControlOptionBind).bind}
-                                          value={checkboxValue(option)}
-                                          type={control.type}
-                                          aria-labelledby={control.modelName}
-                                          onChange={(e) => {
-                                            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-                                            control.type === 'checkbox'
-                                              ? handleCheckbox(
-                                                  e,
-                                                  control.modelName,
-                                                  state.get[control.modelName] as string | [],
-                                                )
-                                              : handleOnChangeValue(e, control.modelName);
-                                          }}
-                                          name={`${control.modelName}-${index}`}
-                                        />
-                                      );
-                                  }
-                                })()}
-                                {typeof option === 'string' ? (
-                                  <span className="pl-2">{option}</span>
-                                ) : (
-                                  'label' in option && (
-                                    <span className="pl-2">{(option as ControlOptionBind).label}</span>
-                                  )
-                                )}
-                              </label>
-                            </div>
-                          ),
-                        );
-                    }
-                  })()} */}
                 </td>
                 <td class="propType">
                   <span>{control.propType}</span>
