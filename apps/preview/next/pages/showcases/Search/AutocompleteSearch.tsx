6713a7187f7f5c7f7551f08f6e411a277b1e2b3b
dkacper <kacper.dziub@icloud.com>
2023-04-21 11:56:18 +0200

    feat: add search block React

---
+++ apps/preview/next/pages/showcases/Search/AutocompleteSearch.tsx
@@ -0,0 +1,214 @@
+import { ShowcasePageLayout } from '../../showcases';
+// #region source
+import { type ChangeEvent, type FormEvent, useState, useEffect, useRef } from 'react';
+import {
+  SfInput,
+  SfIconSearch,
+  SfIconCancel,
+  SfButton,
+  SfDropdown,
+  useDisclosure,
+  SfListItem,
+  SfLoaderCircular,
+} from '@storefront-ui/react';
+import { useDebounce } from 'react-use';
+
+interface Product {
+  id: string;
+  name: string;
+}
+
+const mockProducts: Product[] = [
+  {
+    id: 'ip-14',
+    name: 'iPhone 14',
+  },
+  {
+    id: 'ip-14-p',
+    name: 'iPhone 14 Pro',
+  },
+  {
+    id: 'ip-14-pm',
+    name: 'iPhone 14 Pro Max',
+  },
+  {
+    id: 'ip-14-p',
+    name: 'iPhone 14 Plus',
+  },
+  {
+    id: 'ip-13',
+    name: 'iPhone 13',
+  },
+  {
+    id: 'ip-13-m',
+    name: 'iPhone 13 mini',
+  },
+  {
+    id: 'ip-12',
+    name: 'iPhone 12',
+  },
+  {
+    id: 'ip-11',
+    name: 'iPhone 11',
+  },
+  {
+    id: 'mb-a',
+    name: 'MacBook Air',
+  },
+  {
+    id: 'mb-p-13',
+    name: 'MacBook Pro 13"',
+  },
+  {
+    id: 'mb-p-14',
+    name: 'MacBook Pro 14"',
+  },
+  {
+    id: 'mb-p-16',
+    name: 'MacBook Pro 16"',
+  },
+];
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const mockSearchRequest = async (phrase: string) => {
+  console.log('SEARCH WITH PHRASE:', phrase);
+  alert(`Searching phrase: ${phrase}`);
+};
+
+const mockAutocompleteRequest = async (phrase: string) => {
+  // Just for presentation purposes. Replace it with the actual API call.
+  await delay(1000);
+  const results = mockProducts
+    .filter((product) => product.name.toLowerCase().startsWith(phrase.toLowerCase()))
+    .map((product) => {
+      const highlight = product.name.substring(0, phrase.length);
+      const rest = product.name.substring(phrase.length);
+      return [highlight, rest, product] as [string, string, Product];
+    });
+  return results;
+};
+
+export default function AutocompleteSearch() {
+  useRef();
+  const [searchValue, setSearchValue] = useState('');
+  const [debouncedSearchValue, setDebouncedSearchValue] = useState('');
+  const [snippets, setSnippets] = useState<[string, string, Product][]>([]);
+  const { isOpen, close, open } = useDisclosure();
+  const [isLoadingSnippets, setIsLoadingSnippets] = useState(false);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    mockSearchRequest(searchValue);
+    close();
+  };
+
+  const handleReset = () => {
+    setSearchValue('');
+    setDebouncedSearchValue('');
+    close();
+    setSnippets([]);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const phrase = event.target.value;
+    if (phrase) {
+      setSearchValue(phrase);
+    } else {
+      handleReset();
+    }
+  };
+
+  const handleSelect = (phrase: string) => () => {
+    setSearchValue(phrase);
+    setDebouncedSearchValue(phrase);
+    mockSearchRequest(phrase);
+    close();
+  };
+
+  useDebounce(
+    () => {
+      setDebouncedSearchValue(searchValue);
+    },
+    1000,
+    [searchValue],
+  );
+
+  useEffect(() => {
+    if (debouncedSearchValue) {
+      const getSnippets = async () => {
+        open();
+        setIsLoadingSnippets(true);
+
+        try {
+          const data = await mockAutocompleteRequest(debouncedSearchValue);
+          setSnippets(data);
+        } catch (error) {
+          console.error(error);
+        }
+
+        setIsLoadingSnippets(false);
+      };
+
+      getSnippets();
+    }
+  }, [debouncedSearchValue]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <SfDropdown
+          open={isOpen}
+          onClose={close}
+          className="!w-full"
+          placement="bottom-start"
+          trigger={
+            <label>
+              <SfInput
+                value={searchValue}
+                onChange={handleChange}
+                onFocus={open}
+                placeholder="Search"
+                slotPrefix={<SfIconSearch />}
+                slotSuffix={
+                  <SfButton onClick={handleReset} square variant="tertiary" aria-label="Reset search">
+                    <SfIconCancel />
+                  </SfButton>
+                }
+              />
+            </label>
+          }
+        >
+          {isLoadingSnippets ? (
+            <div className="w-full h-20 flex justify-center items-center border border-solid border-neutral-100 rounded-md drop-shadow-md bg-white py-2">
+              <SfLoaderCircular />
+            </div>
+          ) : (
+            snippets.length > 0 && (
+              <ul className="border border-solid border-neutral-100 rounded-md drop-shadow-md bg-white py-2">
+                {snippets.map(([highlight, rest, product]) => (
+                  <li key={product.id}>
+                    <SfListItem
+                      as="button"
+                      type="button"
+                      onClick={handleSelect(product.name)}
+                      className="flex justify-start"
+                    >
+                      <p className="text-left">
+                        <span>{highlight}</span>
+                        <span className="font-semibold">{rest}</span>
+                      </p>
+                    </SfListItem>
+                  </li>
+                ))}
+              </ul>
+            )
+          )}
+        </SfDropdown>
+      </form>
+    </>
+  );
+}
+
+// #endregion source
+AutocompleteSearch.getLayout = ShowcasePageLayout;
