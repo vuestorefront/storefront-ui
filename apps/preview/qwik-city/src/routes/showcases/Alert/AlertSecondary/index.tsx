498f44a981af2a9ac000bbd2a125d1654724247b
gioboa <giorgiob.boa@gmail.com>
2023-04-24 20:21:19 +0200

    add Alert showcases

---
+++ apps/preview/qwik-city/src/routes/showcases/Alert/AlertSecondary/index.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+import { SfIconInfo } from "@storefront-ui/qwik";
+
+export default component$(() => {
+  return (
+    <div
+      role="alert"
+      class="flex items-center max-w-[600px] shadow-md bg-secondary-100 pr-2 pl-4 ring-1 ring-secondary-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+    >
+      <SfIconInfo class="mr-2 text-secondary-700 shrink-0" />
+      <p class="py-2">Your cart will soon be full.</p>
+    </div>
+  );
+})
