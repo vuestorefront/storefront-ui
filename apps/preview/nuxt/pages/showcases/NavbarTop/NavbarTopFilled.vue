75721d3d5ecc8ea90e2d569d13ec468f367b331b
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-04-24 12:29:27 +0200

    fix: navbartop styling issues (#2619)

--- apps/preview/nuxt/pages/showcases/NavbarTop/NavbarTopFilled.vue
+++ apps/preview/nuxt/pages/showcases/NavbarTop/NavbarTopFilled.vue
@@ -2,7 +2,7 @@
   <div class="w-full h-full bg-neutral-50">
     <header class="flex justify-center w-full text-white border-0 bg-primary-700 h-14 md:h-20 border-neutral-200">
       <div class="flex items-center flex-row flex-nowrap justify-start h-full max-w-[1536px] w-full px-4 md:px-10">
-        <a aria-label="SF Homepage" class="inline-block text-white md:h-10 lg:h-7">
+        <a aria-label="SF Homepage" class="inline-block text-white">
           <picture>
             <source :srcset="brandLogo" media="(min-width: 767px)" />
             <img :src="brandLogoSign" alt="Sf Logo" class="w-8 h-8 mr-4 md:w-[12.5rem] md:h-[1.75rem]" />
@@ -18,32 +18,20 @@
           </template>
           <span class="hidden md:inline-flex">Categories</span>
         </SfButton>
-        <nav>
-          <div class="flex flex-row flex-nowrap">
-            <SfButton
-              v-for="actionItem in actionItems"
-              :key="actionItem.ariaLabel"
-              class="mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
-              :aria-label="actionItem.ariaLabel"
-              variant="tertiary"
-              square
-            >
-              <template #prefix>
-                <Component :is="actionItem.icon" />
-              </template>
-              <span v-if="actionItem.role === 'login'" class="hidden md:inline-flex">{{ actionItem.label }}</span>
-            </SfButton>
-            <SfButton
-              class="hidden lg:inline-flex mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
-              :aria-label="actionItems[2].ariaLabel"
-              variant="tertiary"
-            >
-              <template #prefix>
-                <Component :is="actionItems[2].icon" />
-              </template>
-              Log in
-            </SfButton>
-          </div>
+        <nav class="flex flex-row flex-nowrap">
+          <SfButton
+            v-for="actionItem in actionItems"
+            :key="actionItem.ariaLabel"
+            class="mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
+            :aria-label="actionItem.ariaLabel"
+            variant="tertiary"
+            square
+          >
+            <template #prefix>
+              <Component :is="actionItem.icon" />
+            </template>
+            <span v-if="actionItem.role === 'login'" class="hidden md:inline-flex">{{ actionItem.label }}</span>
+          </SfButton>
         </nav>
       </div>
     </header>
