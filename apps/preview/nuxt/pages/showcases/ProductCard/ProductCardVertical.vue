ce387266b287038dcce1267dcf9a594695ee9751
Maya Shavin <dpnminh@gmail.com>
2023-05-03 10:25:59 +0300

    fix: code block for productCard

--- apps/preview/nuxt/pages/showcases/ProductCard/ProductCardVertical.vue
+++ apps/preview/nuxt/pages/showcases/ProductCard/ProductCardVertical.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="border border-neutral-200 rounded-md hover:shadow-lg max-w-[300px]">
+  <article class="border border-neutral-200 rounded-md hover:shadow-lg max-w-[300px]">
     <div class="relative">
       <SfLink href="#">
         <img
-          :src="productImage"
-          alt="Great product"
+          :src="product.image"
+          :alt="product.title"
           class="block object-cover h-auto rounded-md aspect-square"
           :width="300"
           :height="300"
@@ -22,18 +22,18 @@
       </SfButton>
     </div>
     <div class="p-4 border-t border-neutral-200">
-      <SfLink href="#" variant="secondary" class="no-underline"> Athletic mens walking sneakers </SfLink>
+      <SfLink href="#" variant="secondary" class="no-underline"> {{ product.title }} </SfLink>
       <div class="flex items-center pt-1">
-        <SfRating size="xs" :value="5" :max="5" />
+        <SfRating size="xs" :value="product.ratings" :max="5" />
 
         <SfLink href="#" variant="secondary" class="pl-1 no-underline">
-          <SfCounter size="xs">123</SfCounter>
+          <SfCounter size="xs">{{ product.reviews.length}}</SfCounter>
         </SfLink>
       </div>
       <p class="block py-2 font-normal leading-5 typography-text-sm text-neutral-700">
-        Lightweight • Non slip • Flexible outsole • Easy to wear on and off
+        {{ product.shortDescription }}
       </p>
-      <span class="block pb-2 font-bold typography-text-lg">$2345,99</span>
+      <span class="block pb-2 font-bold typography-text-lg">{{ product.currency_code }}{{ product.price }}</span>
       <SfButton type="button" size="sm">
         <template #prefix>
           <SfIconShoppingCart size="sm" />
@@ -41,11 +41,21 @@
         Add to cart
       </SfButton>
     </div>
-  </div>
+  </article>
 </template>
 
 <script lang="ts" setup>
 import { SfRating, SfCounter, SfLink, SfButton, SfIconShoppingCart, SfIconFavorite } from '@storefront-ui/vue';
-
 import productImage from '@assets/sneakers.png';
+
+const product = {
+  title: 'Athletic mens walking sneakers',
+  ratings: 4,
+  reviews: ['some reviews', 'some reviews', 'some reviews'],
+  shortDescription: 'Lightweight • Non slip • Flexible outsole • Easy to wear on and off',
+  price: '2345,99',
+  currency_code: '$',
+  image: productImage,
+}
+
 </script>
