2b2513ae6d1a727d6f5665c2e40af9f0cfc9058b
Runyasak Chaengnaimuang <world@odds.team>
2023-05-07 00:28:58 +0700

    fix: remove unused variables

--- apps/preview/angular/src/app/app.component.ts
+++ apps/preview/angular/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SfButtonComponent } from '@storefront-ui/angular';
-import { SfButtonSize } from '@storefront-ui/shared';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +10,4 @@ import { SfButtonSize } from '@storefront-ui/shared';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular';
-
-  buttonSize = signal(SfButtonSize.lg);
-
-  toggleSize() {
-    this.buttonSize.set(SfButtonSize.base);
-  }
-}
+export class AppComponent {}
