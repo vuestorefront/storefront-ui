0cb5136841415b7d8a4880201c8d98c6078ff15c
Maciej Wolowski <103260041+MaciejWolowski@users.noreply.github.com>
2023-05-08 18:07:09 +0200

    test: homepage + section content (#2635)
    * test: homepage + section content
    
    * chore: modify test with same patterns we have in component testing
    
    ---------
    
    Co-authored-by: Szymon Dziewoński <szymon.dziewonski@gmail.com>

--- apps/docs/tests/cypress.config.ts
+++ apps/docs/tests/cypress.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  fixturesFolder: './fixtures',
   viewportHeight: 1080,
   viewportWidth: 1920,
   defaultCommandTimeout: 20000,
   video: false,
   chromeWebSecurity: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-    },
-    baseUrl: 'https://docs.storefrontui.io/v2/vue/customization',
+    baseUrl: 'http://localhost:8080/v2/',
     specPattern: '../tests/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: '../tests/support/index.ts'
-  }
+    supportFile: '../tests/support/index.ts',
+  },
 });
