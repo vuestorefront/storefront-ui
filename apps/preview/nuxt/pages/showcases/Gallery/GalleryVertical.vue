a6fd9f100dff9c061f298c3cd7b8ada0e33a3cfe
AdamPawlinski <apawlinski@vuestorefront.io>
2023-05-10 15:19:25 +0200

    fix: ui bugs

--- apps/preview/nuxt/pages/showcases/Gallery/GalleryVertical.vue
+++ apps/preview/nuxt/pages/showcases/Gallery/GalleryVertical.vue
@@ -34,7 +34,7 @@
         type="button"
         :aria-label="alt"
         :aria-current="activeIndex === index"
-        :class="`md:w-[78px] md:h-auto relative shrink-0 pb-1 mx-4 border-b-4 snap-start cursor-pointer focus-visible:outline focus-visible:outline-offset transition-colors flex-grow md:flex-grow-0  ${
+        :class="`md:w-[78px] md:h-auto relative shrink-0 pb-1 mx-2 border-b-4 snap-start cursor-pointer focus-visible:outline focus-visible:outline-offset transition-colors flex-grow md:flex-grow-0  ${
           activeIndex === index ? 'border-primary-700' : 'border-transparent'
         }`"
         @mouseover="activeIndex = index"
@@ -59,6 +59,7 @@
     <SfScrollable
       class="w-full h-full snap-x snap-mandatory [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
       :active-index="activeIndex"
+      direction="vertical"
       wrapper-class="h-full"
       is-active-index-centered
       buttons-placement="none"
