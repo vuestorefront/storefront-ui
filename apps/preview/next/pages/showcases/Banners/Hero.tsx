3c3b72f0a8e7a666886d2c2569fd9a1267301275
Jakub Freisler <jakub@frsource.org>
2023-05-16 15:12:12 +0200

    chore: lint Hero
    Signed-off-by: Jakub Freisler <jakub@frsource.org>

--- apps/preview/next/pages/showcases/Banners/Hero.tsx
+++ apps/preview/next/pages/showcases/Banners/Hero.tsx
@@ -7,12 +7,22 @@ export default function Hero() {
   return (
     <div className="relative min-h-[600px]">
       <picture>
-        <source srcSet="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/fixture/hero-bg.png" media="(min-width: 768px)" />
-        <img src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/fixture/hero-bg-mobile.png" className="absolute w-full h-full z-[-1] md:object-cover" />
+        <source
+          srcSet="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/fixture/hero-bg.png"
+          media="(min-width: 768px)"
+        />
+        <img
+          src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/fixture/hero-bg-mobile.png"
+          className="absolute w-full h-full z-[-1] md:object-cover"
+        />
       </picture>
       <div className="md:flex md:flex-row-reverse md:justify-center min-h-[600px] max-w-[1536px] mx-auto">
         <div className="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
-          <img src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/fixture/hero-headphones.png" alt="Headphones" className="h-full object-cover object-left" />
+          <img
+            src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/fixture/hero-headphones.png"
+            alt="Headphones"
+            className="h-full object-cover object-left"
+          />
         </div>
         <div className="p-4 md:p-10 md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
           <p className="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500 uppercase">
