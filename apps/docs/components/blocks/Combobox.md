3aa42ea91905eef5aa2205a9332913d3a94ef5b2
AdamPawlinski <apawlinski@vuestorefront.io>
2023-05-16 11:12:44 +0200

    feat: init combobox block

---
+++ apps/docs/components/blocks/Combobox.md
@@ -0,0 +1,31 @@
+---
+layout: DefaultLayout
+hideBreadcrumbs: true
+description: The Combobox is a specialized input field designed for text-based searching on a website.
+hideToc: true
+---
+
+# Combobox
+
+The Search is a specialized input field designed for text-based searching on a website. In the provided example, the block includes an additional feature that displays live hints suggestions (autocomplete) as the user types.
+
+The Search input is primarily utilized in the global navigation (see NavBarTop block). However, variants without autocomplete functionality are commonly employed on results pages.
+
+## Accessibility notes
+
+The Search fully supports the use of the keyboard. The transition from the search input to the hints list is handled by Tab key.
+
+## Basic search
+
+Simple search with an autocomplete functionality. Give your users hints of what they may look for. In this example we use mock autocomplete example, make sure you provide real data.
+
+<Showcase showcase-name="Combobox/ComboboxBasic" style="min-height: 350px">
+
+<!-- vue -->
+<<<../../preview/nuxt/pages/showcases/Combobox/ComboboxBasic.vue
+<!-- end vue -->
+<!-- react -->
+<<<../../preview/next/pages/showcases/Combobox/ComboboxBasic.tsx#source
+<!-- end react -->
+
+</Showcase>
---
+++ apps/preview/next/pages/showcases/Combobox/ComboboxBasic.tsx
@@ -0,0 +1,176 @@
+import { ShowcasePageLayout } from '../../showcases';
+// #region source
+import { type ChangeEvent, type FormEvent, type KeyboardEvent, useState, useRef, useId, useEffect } from 'react';
+import { offset } from '@floating-ui/react-dom';
+import { SfInput, SfIconExpandMore, useDisclosure, SfListItem, useTrapFocus, useDropdown } from '@storefront-ui/react';
+import classNames from 'classnames';
+
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+const options: SelectOption[] = [
+  {
+    label: 'Startup',
+    value: 'startup',
+  },
+  {
+    label: 'Business',
+    value: 'business',
+  },
+  {
+    label: 'Buses',
+    value: 'buses',
+  },
+  {
+    label: 'Enterprise',
+    value: 'enterprise',
+  },
+];
+
+const mockAutocompleteRequest = (phrase: string) => {
+  const results = options.filter((option) => option.value.toLowerCase().startsWith(phrase.toLowerCase()));
+  return results;
+};
+
+export default function ComboboxBasic() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState<string | undefined>(undefined);
+  const { isOpen, close, open, toggle } = useDisclosure();
+  const [snippets, setSnippets] = useState<{ label: string; value: string }[]>([]);
+  const { refs, style } = useDropdown({ isOpen, onClose: close, placement: 'bottom-start', middleware: [offset(4)] });
+  const id = useId();
+  const listId = useId();
+  useTrapFocus(refs.floating, { arrowKeysOn: true, activeState: isOpen, initialFocus: false });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(event);
+    close();
+  };
+
+  const handleFocusInput = () => {
+    inputRef.current?.focus();
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const phrase = event.target.value;
+    if (phrase) {
+      setSearchValue(phrase);
+    } else {
+      setSearchValue(undefined);
+      setSnippets([]);
+      close();
+    }
+  };
+
+  const selectOption = (event: FormEvent, option: SelectOption) => {
+    setSearchValue(option.value);
+    handleSubmit(event);
+    close();
+    handleFocusInput();
+  };
+
+  const handleOptionItemKeyDown = (event: KeyboardEvent<HTMLButtonElement>, option: SelectOption) => {
+    if (event.key === ' ' || event.key === 'Enter') selectOption(event, option);
+  };
+
+  useEffect(() => {
+    if (searchValue) {
+      const getSnippets = async () => {
+        open();
+        try {
+          const data = await mockAutocompleteRequest(searchValue);
+          setSnippets(data);
+        } catch (error) {
+          close();
+          console.error(error);
+        }
+      };
+
+      getSnippets();
+    }
+  }, [searchValue]);
+
+  return (
+    <form ref={refs.setReference} className="relative">
+      <label className="font-medium typography-label-sm" htmlFor={id}>
+        Country
+      </label>
+      <SfInput
+        ref={inputRef}
+        id={id}
+        value={searchValue}
+        onChange={handleChange}
+        aria-label="Choose country"
+        placeholder="Choose country"
+        aria-controls={listId}
+        aria-autocomplete="list"
+        aria-expanded={isOpen}
+        aria-activedescendant=""
+        onClick={toggle}
+        slotSuffix={
+          <SfIconExpandMore
+            onClick={toggle}
+            className={classNames('ml-auto text-neutral-500 transition-transform ease-in-out duration-300', {
+              'rotate-180': isOpen,
+            })}
+          />
+        }
+      />
+      <div ref={refs.setFloating} style={style} className="left-0 right-0">
+        {isOpen && (
+          <ul
+            id={listId}
+            role="listbox"
+            aria-label="Country list"
+            className="py-2 bg-white border border-solid rounded-md border-neutral-100 drop-shadow-md"
+          >
+            {(snippets.length > 0 &&
+              snippets.map((option) => (
+                <li key={option.value}>
+                  <SfListItem
+                    as="button"
+                    type="button"
+                    onClick={(event) => selectOption(event, option)}
+                    onKeyDown={(event) => handleOptionItemKeyDown(event, option)}
+                    className="flex justify-start"
+                  >
+                    <p className="text-left">
+                      <span>{option.label}</span>
+                    </p>
+                  </SfListItem>
+                </li>
+              ))) ||
+              (searchValue && (
+                <SfListItem className="flex justify-start">
+                  <p className="text-left">
+                    <span>No options</span>
+                  </p>
+                </SfListItem>
+              )) ||
+              options.map((option) => (
+                <div key={option.value}>
+                  <SfListItem
+                    as="button"
+                    type="button"
+                    onClick={() => selectOption(option)}
+                    onKeyDown={(event) => handleOptionItemKeyDown(event, option)}
+                    className="flex justify-start"
+                  >
+                    <p className="text-left">
+                      <span>{option.label}</span>
+                    </p>
+                  </SfListItem>
+                </div>
+              ))}
+          </ul>
+        )}
+      </div>
+    </form>
+  );
+}
+
+// #endregion source
+ComboboxBasic.getLayout = ShowcasePageLayout;
