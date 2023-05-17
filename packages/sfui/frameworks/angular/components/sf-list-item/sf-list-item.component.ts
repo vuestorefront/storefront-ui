12cbd78d9c6372ab9ec582a296987944a2173900
Runyasak Chaengnaimuang <world@odds.team>
2023-05-17 22:58:05 +0700

    feat: complete all props to sf-list-item

--- packages/sfui/frameworks/angular/components/sf-list-item/sf-list-item.component.ts
+++ packages/sfui/frameworks/angular/components/sf-list-item/sf-list-item.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SfListItemSize } from '@storefront-ui/shared';
 
 @Component({
   selector: '[sf-list-item]',
@@ -8,4 +9,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './sf-list-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SfListItemComponent {}
+export class SfListItemComponent {
+  @Input() size: SfListItemSize = SfListItemSize.base;
+
+  @Input() disabled = false;
+
+  @Input() selected = false;
+
+  sizeClasses = {
+    [SfListItemSize.sm]: 'text-sm px-4 py-1',
+    [SfListItemSize.base]: 'px-4 py-2',
+    [SfListItemSize.lg]: 'p-4',
+  };
+
+  @HostBinding('disabled') get hostDisabled() {
+    return this.disabled;
+  }
+
+  @HostBinding('class') get hostClass() {
+    return [
+      'inline-flex items-center gap-2 w-full hover:bg-neutral-100 active:bg-neutral-200 cursor-pointer focus-visible:outline focus-visible:outline-offset focus-visible:relative focus-visible:z-10',
+      this.sizeClasses[this.size],
+      { 'cursor-not-allowed pointer-events-none text-disabled-900': this.disabled, 'font-medium': this.selected },
+    ].join(' ');
+  }
+
+  @HostBinding('attr.data-testid') dataTestId = 'list-item';
+}
