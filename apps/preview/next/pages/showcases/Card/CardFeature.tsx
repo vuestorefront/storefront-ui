635b6ed7da49e93fb8bcdce15c3dcb7b3fdaa551
Anna Musial <ania.musial13@gmail.com>
2023-05-24 14:42:03 +0200

    fix: adjusted width of cards in car feature block (#2739)

--- apps/preview/next/pages/showcases/Card/CardFeature.tsx
+++ apps/preview/next/pages/showcases/Card/CardFeature.tsx
@@ -32,7 +32,7 @@ export default function CardFeature() {
   return (
     <div className="flex flex-wrap gap-4 lg:gap-6 lg:flex-nowrap">
       {cardDetails.map(({ icon, title, description, buttonText, isDisabled }) => (
-        <div key={title} className="flex flex-col min-w-[325px] w-[375px] lg:w-[496px] items-center">
+        <div key={title} className="flex flex-col w-full max-w-[325px] sm:w-[375px] lg:w-[496px] items-center">
           <span className={classNames(isDisabled && 'text-disabled-900')}>{icon}</span>
           <div className="p-4 flex flex-col items-center">
             <p className={classNames('font-medium typography-text-base', { 'text-disabled-900': isDisabled })}>
