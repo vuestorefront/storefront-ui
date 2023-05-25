fcb16f0ae39b11f6fc7547e0090c8bc05185b262
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-05-25 11:42:32 +0200

    Update useScrollable.ts

--- packages/sfui/frameworks/react/hooks/useScrollable/useScrollable.ts
+++ packages/sfui/frameworks/react/hooks/useScrollable/useScrollable.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import { type UseScrollableOptions, Scrollable, composeHandlers, createPropsGetter } from '@storefront-ui/react';
 
 export function useScrollable<TElement extends HTMLElement>({
