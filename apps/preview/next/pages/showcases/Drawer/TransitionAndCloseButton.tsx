79d1f2a87ce0d22926908922ae55888357f772f1
Anna Musial <ania.musial13@gmail.com>
2023-06-23 13:39:36 +0200

    feat: added transition for drawer from left

--- apps/preview/next/pages/showcases/Drawer/TransitionAndCloseButton.tsx
+++ apps/preview/next/pages/showcases/Drawer/TransitionAndCloseButton.tsx
@@ -2,7 +2,8 @@ import { ShowcasePageLayout } from '../../showcases';
 // #region source
 import { SfDrawer, SfButton, SfIconFavorite, SfIconClose, useTrapFocus } from '@storefront-ui/react';
 import { useState, useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { Transition } from 'react-transition-group';
+import classNames from 'classnames';
 
 export default function DrawerWithTransition() {
   const [open, setOpen] = useState(false);
@@ -17,54 +18,53 @@ export default function DrawerWithTransition() {
         Open Drawer
       </SfButton>
 
-      <CSSTransition
-        ref={nodeRef}
-        in={open}
-        timeout={500}
-        unmountOnExit
-        classNames={{
-          enter: '-translate-x-full',
-          enterActive: 'translate-x-0 transition duration-500 ease-in-out',
-          exitDone: 'translate-x-0',
-          exitActive: '-translate-x-full transition duration-500 ease-in-out',
-        }}
-      >
-        <SfDrawer
-          ref={drawerRef}
-          open
-          onClose={() => setOpen(false)}
-          className="bg-neutral-50 border border-gray-300 max-w-[370px]"
-        >
-          <header className="flex items-center justify-between px-10 py-6 bg-primary-700">
-            <div className="flex items-center text-white">
-              <SfIconFavorite className="mr-2" /> Your favorite items
+      <Transition ref={nodeRef} in={open} timeout={500} mountOnEnter>
+        {(state) => (
+          <SfDrawer
+            ref={drawerRef}
+            open
+            onClose={() => setOpen(false)}
+            className={classNames(
+              'bg-neutral-50 border border-gray-300 max-w-[370px] duration-500 transition ease-in-out',
+              {
+                'transition-x-0': state === 'entered',
+                '-translate-x-full': state === 'entering',
+                '-translate-x-0': state === 'exiting',
+                '-translate-x-full': state === 'exited',
+              },
+            )}
+          >
+            <header className="flex items-center justify-between px-10 py-6 bg-primary-700">
+              <div className="flex items-center text-white">
+                <SfIconFavorite className="mr-2" /> Your favorite items
+              </div>
+              <SfButton
+                square
+                variant="tertiary"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+                className="text-white"
+              >
+                <SfIconClose />
+              </SfButton>
+            </header>
+            <div className="p-5 px-10">
+              <p className="mb-2">
+                Focus is trapped inside this Drawer and <strong>tab</strong> button cycle through{' '}
+                <a href="https://docs.storefrontui.io/v2/" className="underline text-primary-700">
+                  Focusable Element
+                </a>{' '}
+                and another{' '}
+                <a href="https://docs.storefrontui.io/v2/" className="underline text-primary-700">
+                  Focusable Element
+                </a>
+              </p>
+              <strong>You can close Drawer by clicking outside or focus and use ESC button</strong>
             </div>
-            <SfButton
-              square
-              variant="tertiary"
-              onClick={() => {
-                setOpen(!open);
-              }}
-              className="text-white"
-            >
-              <SfIconClose />
-            </SfButton>
-          </header>
-          <div className="p-5 px-10">
-            <p className="mb-2">
-              Focus is trapped inside this Drawer and <strong>tab</strong> button cycle through{' '}
-              <a href="https://docs.storefrontui.io/v2/" className="underline text-primary-700">
-                Focusable Element
-              </a>{' '}
-              and another{' '}
-              <a href="https://docs.storefrontui.io/v2/" className="underline text-primary-700">
-                Focusable Element
-              </a>
-            </p>
-            <strong>You can close Drawer by clicking outside or focus and use ESC button</strong>
-          </div>
-        </SfDrawer>
-      </CSSTransition>
+          </SfDrawer>
+        )}
+      </Transition>
     </>
   );
 }
