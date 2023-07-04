5ef410662473b150cf57183e4df356d2564f67d8
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-07-04 19:47:08 +0200

    test: add local setup for docs e2e tests (#2897)

--- apps/docs/tests/cypress.config.ts
+++ apps/docs/tests/cypress.config.ts
@@ -6,11 +6,10 @@ export default defineConfig({
   viewportWidth: 1920,
   defaultCommandTimeout: 20000,
   video: false,
-  
   e2e: {
     baseUrl: 'http://localhost:8080/v2',
     specPattern: '../tests/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: '../tests/support/index.ts',
   },
-  chromeWebSecurity: false
+  chromeWebSecurity: false,
 });
