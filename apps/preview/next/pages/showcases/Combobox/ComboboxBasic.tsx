e430d4dd0a870e51a0da94804c65eb8028adb227
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-07-10 12:08:35 +0200

    fix: remove required form combobox react (#2911)

--- apps/preview/next/pages/showcases/Combobox/ComboboxBasic.tsx
+++ apps/preview/next/pages/showcases/Combobox/ComboboxBasic.tsx
@@ -322,7 +322,6 @@ export default function ComboboxBasic() {
         <p className="text-negative-700 typography-text-sm font-medium mt-0.5">No option selected</p>
       )}
       <p className="text-xs mt-0.5 text-neutral-500">Help text</p>
-      <p className="mt-2 text-neutral-500 typography-text-sm">*Required</p>
       <DisableSwitch
         className={classNames({ hidden: isOpen })}
         onChangeHandler={onDisabledChangeHandler}
