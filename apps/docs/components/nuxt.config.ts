9ef54eb4c4e3d59b85f72dd4e73b9cdac9734d50
Matt Maribojoc <matthewmaribojoc@gmail.com>
2024-03-25 11:43:33 -0400

    docs: add figma redirect

--- apps/docs/components/nuxt.config.ts
+++ apps/docs/components/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['sf-docs-base'],
+  app: {
+    baseURL: process.env.VITE_DOCS_BASEPATH ? new URL(process.env.VITE_DOCS_BASEPATH).pathname : '',
+  },
   // fix via https://github.com/nuxt/content/issues/2254
   alias: {
     'micromark/lib/preprocess.js': 'micromark',
@@ -12,6 +15,11 @@ export default defineNuxtConfig({
       DOCS_EXAMPLES_VUE_PATH: process.env.DOCS_EXAMPLES_VUE_PATH,
       DOCS_EXAMPLES_REACT_PATH: process.env.DOCS_EXAMPLES_REACT_PATH,
     },
+  },
+  nitro: {
+    routeRules: {
+      '/figma': { redirect: 'https://www.figma.com/file/Q7Nr1NvjPdcGVhykkfDg2k/Storefront-UI-%7C-Design-Kit-v2.5-(public)?type=design&node-id=22913-91045&mode=design'}
+    }
   },
   vite: {
     server: {
