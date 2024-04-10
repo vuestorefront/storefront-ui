fefc4fa286a5f87717ea60ccc3d6010c7ae9d5b5
Matt Maribojoc <matthewmaribojoc@gmail.com>
2024-04-10 09:45:21 -0400

    docs: update site name

--- apps/docs/components/nuxt.config.ts
+++ apps/docs/components/nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
       storefrontUi: true,
       DOCS_EXAMPLES_VUE_PATH: process.env.VITE_DOCS_EXAMPLES_VUE_PATH,
       DOCS_EXAMPLES_REACT_PATH: process.env.VITE_DOCS_EXAMPLES_REACT_PATH,
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://docs.storefrontui.io',
+      siteName: 'Storefront UI',
+      siteDescription: 'Fast, accessible, and fully customizable components built for e-commerce.',
     },
   },
   nitro: {
