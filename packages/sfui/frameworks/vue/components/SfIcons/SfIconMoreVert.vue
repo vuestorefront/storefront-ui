6cb560ccdacb9966ac2880de7f5932d5e441f4c3
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2024-04-19 15:46:53 +0200

    chore: update dependencies part 1

--- packages/sfui/frameworks/vue/components/SfIcons/SfIconMoreVert.vue
+++ packages/sfui/frameworks/vue/components/SfIcons/SfIconMoreVert.vue
@@ -1,7 +1,7 @@
 <template>
   <SfIconBase :size="size" viewBox="0 0 24 24" data-testid="more-vert"
     ><path
-      d="M14 12a2 2 0 1 1-4.001-.001A2 2 0 0 1 14 12Zm-2-4a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 8Zm0 8a2 2 0 1 0 .001 4.001A2 2 0 0 0 12 16Z"
+      d="M14 12a2 2 0 1 1-4.001-.001A2 2 0 0 1 14 12m-2-4a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 8m0 8a2 2 0 1 0 .001 4.001A2 2 0 0 0 12 16"
   /></SfIconBase>
 </template>
 <script lang="ts" setup>
