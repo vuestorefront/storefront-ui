8df1b3902ee5d083fbab8876a3c48b1298173fad
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2024-05-06 16:58:10 +0200

    floating-ui

--- packages/sfui/frameworks/react/hooks/usePopover/types.ts
+++ packages/sfui/frameworks/react/hooks/usePopover/types.ts
@@ -1,4 +1,4 @@
-import type { UseFloatingProps } from '@floating-ui/react-dom';
+import type { Middleware } from '@floating-ui/react-dom';
 import { SfPopoverPlacement, SfPopoverStrategy, type SfPopoverBasePlacement } from '@storefront-ui/shared';
 
 export { SfPopoverPlacement, SfPopoverStrategy, type SfPopoverBasePlacement };
@@ -6,6 +6,6 @@ export { SfPopoverPlacement, SfPopoverStrategy, type SfPopoverBasePlacement };
 export interface UsePopoverOptions {
   isOpen?: boolean;
   placement?: `${SfPopoverPlacement}`;
-  middleware?: UseFloatingProps['middleware'];
+  middleware?: Array<Middleware | null | undefined | false>;
   strategy?: `${SfPopoverStrategy}`;
 }
