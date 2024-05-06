e399e63db25541cefac0ddd3f1c36975a7531d04
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2024-05-06 09:43:23 +0200

    chore: update dependencies (#3123)

--- packages/sfui/frameworks/react/components/SfIcons/SfIconThumbDown.tsx
+++ packages/sfui/frameworks/react/components/SfIcons/SfIconThumbDown.tsx
@@ -4,7 +4,7 @@ import { SfIconBase, SfIconSize } from '@storefront-ui/react';
 export default function SfIconThumbDown({ size = SfIconSize.base, viewBox = '0 0 24 24', ...attributes }: SfIconProps) {
   return (
     <SfIconBase size={size} viewBox={viewBox} data-testid="thumb-down" {...attributes}>
-      <path d="M3 16c-.533 0-1-.2-1.4-.6-.4-.4-.6-.867-.6-1.4v-2c0-.117.017-.242.05-.375s.067-.258.1-.375l3-7.05c.15-.333.4-.617.75-.85C5.25 3.117 5.617 3 6 3h11v13l-6 5.95c-.25.25-.546.396-.887.437a1.646 1.646 0 0 1-.988-.187 1.58 1.58 0 0 1-.7-.7 1.28 1.28 0 0 1-.1-.925L9.45 16zm12-.85V5H6l-3 7v2h9l-1.35 5.5zM20 3c.55 0 1.021.196 1.413.587.391.392.587.863.587 1.413v9c0 .55-.196 1.02-.587 1.412A1.927 1.927 0 0 1 20 16h-3v-2h3V5h-3V3z" />
+      <path d="M3 16q-.8 0-1.4-.6T1 14v-2q0-.175.05-.375t.1-.375l3-7.05q.225-.5.75-.85T6 3h11v13l-6 5.95q-.375.375-.887.437a1.65 1.65 0 0 1-.988-.187 1.58 1.58 0 0 1-.7-.7q-.225-.45-.1-.925L9.45 16zm12-.85V5H6l-3 7v2h9l-1.35 5.5zM20 3q.825 0 1.413.587Q22 4.175 22 5v9q0 .825-.587 1.412A1.93 1.93 0 0 1 20 16h-3v-2h3V5h-3V3z" />
     </SfIconBase>
   );
 }
--- packages/sfui/frameworks/react/components/SfIcons/SfIconViewList.tsx
+++ packages/sfui/frameworks/react/components/SfIcons/SfIconViewList.tsx
@@ -4,7 +4,7 @@ import { SfIconBase, SfIconSize } from '@storefront-ui/react';
 export default function SfIconViewList({ size = SfIconSize.base, viewBox = '0 0 24 24', ...attributes }: SfIconProps) {
   return (
     <SfIconBase size={size} viewBox={viewBox} data-testid="view-list" {...attributes}>
-      <path d="M3 17V7c0-.55.196-1.02.587-1.412A1.927 1.927 0 0 1 5 5h14c.55 0 1.021.196 1.413.588.391.391.587.862.587 1.412v10a1.93 1.93 0 0 1-.587 1.413A1.928 1.928 0 0 1 19 19H5a1.93 1.93 0 0 1-1.413-.587A1.928 1.928 0 0 1 3 17m2-8h2V7H5zm4 0h10V7H9zm0 4h10v-2H9zm0 4h10v-2H9zm-4 0h2v-2H5zm0-4h2v-2H5z" />
+      <path d="M3 17V7q0-.824.587-1.412A1.93 1.93 0 0 1 5 5h14q.825 0 1.413.588Q21 6.175 21 7v10q0 .825-.587 1.413A1.93 1.93 0 0 1 19 19H5q-.825 0-1.413-.587A1.93 1.93 0 0 1 3 17m2-8h2V7H5zm4 0h10V7H9zm0 4h10v-2H9zm0 4h10v-2H9zm-4 0h2v-2H5zm0-4h2v-2H5z" />
     </SfIconBase>
   );
 }
