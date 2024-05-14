844cbb0a664ba47be9312b1a5065952b54d7c05c
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2024-05-14 11:51:59 +0200

    chore: adjust nuxt playground

--- packages/sfui/frameworks/nuxt/playground/nuxt.config.ts
+++ packages/sfui/frameworks/nuxt/playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  modules: ['../dist/module.mjs'],
+  modules: ['../src/module.ts'],
   tailwindcss: {
     config: {
       content: ['../../../../node_modules/@storefront-ui/vue/**/*.{js,mjs}'],
