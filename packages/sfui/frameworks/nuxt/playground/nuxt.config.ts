15d6ddb197e8166d50d3fe61c6dc8c030af50c10
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2024-10-07 14:24:59 +0200

    chore: migrate nuxt module to HMR version

--- packages/sfui/frameworks/nuxt/playground/nuxt.config.ts
+++ packages/sfui/frameworks/nuxt/playground/nuxt.config.ts
@@ -1,5 +1,9 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
+// How to test? check if variable is overwritten with current priority table
+// 1. `tailwind.config.ts` file
+// 2. `nuxt.config.ts` file with `tailwindcss` property
+// 3. default configuration inside module
 export default defineNuxtConfig({
   modules: ['../src/module.ts'],
   // modules: ['../dist/module.mjs'], //For testing bundle
@@ -9,6 +13,9 @@ export default defineNuxtConfig({
         extend: {
           colors: {
             brand: 'red',
+            secondary: {
+              200: 'green',
+            },
           },
         },
       },
