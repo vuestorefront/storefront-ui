377231631dc7b31024105b7384c7ffb0e95812c6
Jakub Freisler <jakub@frsource.org>
2025-07-31 22:18:31 +0200

    feat: redesign hero block
    add second hero example

---
+++ apps/preview/next/pages/showcases/Banners/HeroWithImage.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { ShowcasePageLayout } from '../../showcases';
+// #region source
+import { SfButton } from '@storefront-ui/react';
+
+export default function Hero() {
+  return (
+    <div className="relative min-h-[576px]">
+      <picture>
+        <source srcSet="http://localhost:3100/@assets/hero-bg.png" media="(min-width: 768px)" />
+        <img
+          src="http://localhost:3100/@assets/hero-bg-mobile.png"
+          className="absolute w-full h-full z-[-1] object-cover"
+        />
+      </picture>
+      <div className="md:flex md:flex-row-reverse md:justify-center min-h-[576px] max-w-[1536px] mx-auto">
+        <div className="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
+          <img
+            src="http://localhost:3100/@assets/hero-headphones.png"
+            alt="Headphones"
+            className="h-full object-cover object-left"
+          />
+        </div>
+        <div className="p-4 md:p-10 md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
+          <p className="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-900 uppercase">
+            Feel the music
+          </p>
+          <h1 className="typography-display-2 md:typography-display-1 md:leading-[67.5px] font-bold mt-2 mb-4">
+            New Wireless Pro
+          </h1>
+          <p className="typography-text-base md:typography-text-lg">
+            Spatial audio. Adjustable ear cups. On-device controls. All-day battery.
+          </p>
+          <div className="flex flex-col md:flex-row gap-4 mt-6">
+            <SfButton size="lg"> Order now </SfButton>
+            <SfButton size="lg" className="bg-white" variant="secondary">
+              Show more
+            </SfButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+// #endregion source
+Hero.getLayout = ShowcasePageLayout;
--- apps/preview/nuxt/pages/showcases/Banners/Hero.vue
+++ apps/preview/nuxt/pages/showcases/Banners/Hero.vue
@@ -1,35 +1,24 @@
 <template>
-  <div class="relative min-h-[576px]">
-    <picture>
-      <source srcset="http://localhost:3100/@assets/hero-bg.png" media="(min-width: 768px)" />
+  <div class="relative h-dvh w-dvh">
+    <picture class="before:absolute before:inset-0 before:bg-primary-900 before:-z-10">
+      <source srcset="http://localhost:3100/@assets/hero-bg-2.png" media="(min-width: 768px)" />
       <img
-        src="http://localhost:3100/@assets/hero-bg-mobile.png"
-        class="absolute w-full h-full z-[-1] object-cover"
+        src="http://localhost:3100/@assets/hero-bg-mobile-2.png"
+        class="absolute w-full h-full z-[-1] object-cover opacity-50"
         alt="hero"
       />
     </picture>
-    <div class="md:flex md:flex-row-reverse md:justify-center max-w[1536px] mx-auto min-h-[576px]">
-      <div class="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
-        <img
-          src="http://localhost:3100/@assets/hero-headphones.png"
-          alt="Headphones"
-          class="h-full w-full object-cover object-left"
-        />
-      </div>
-      <div class="p-4 md:p-10 md:max-w-[768px] md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
-        <p class="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500 uppercase">
-          Feel the music
-        </p>
-        <h1 class="typography-display-2 md:typography-display-1 md:leading-[67.5px] font-bold mt-2 mb-4">
-          New Wireless Pro
-        </h1>
-        <p class="typography-text-base md:typography-text-lg">
-          Spatial audio. Adjustable ear cups. On-device controls. All-day battery.
-        </p>
-        <div class="flex flex-col md:flex-row gap-4 mt-6">
-          <SfButton size="lg"> Order now </SfButton>
-          <SfButton size="lg" variant="secondary" class="bg-white"> Show more </SfButton>
-        </div>
+    <div
+      class="h-full w-full max-w-[1536px] mx-auto p-4 md:px-10 flex flex-col justify-end md:justify-center text-white"
+    >
+      <p class="typography-text-xs md:typography-text-sm font-bold tracking-widest uppercase">New collection</p>
+      <h1 class="typography-display-2 md:typography-display-1 md:leading-[67.5px] font-bold mt-2 mb-4">
+        Made to move. Built to win.
+      </h1>
+      <p class="typography-text-base md:typography-text-lg">The new sport collection is now in store.</p>
+      <div class="flex flex-col md:flex-row gap-4 mt-6">
+        <SfButton size="lg" variant="secondary" class="bg-white"> Order now </SfButton>
+        <SfButton size="lg"> Show more </SfButton>
       </div>
     </div>
   </div>
