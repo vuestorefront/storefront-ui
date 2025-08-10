03a9589420dc9ffbd2dfe596093648db4d82edcf
Jakub Freisler <jakub@frsource.org>
2025-08-10 23:44:17 +0200

    chore: remove unnecessary ring-1 class

--- packages/sfui/frameworks/react/components/SfInput/SfInput.tsx
+++ packages/sfui/frameworks/react/components/SfInput/SfInput.tsx
@@ -21,7 +21,7 @@ const SfInput = polymorphicForwardRef<typeof defaultWrapperTag, SfInputProps>(
     return (
       <WrapperTag
         className={classNames([
-          'flex items-center gap-2 px-4 bg-white rounded-md ring-1 text-neutral-500 hover:ring-primary-700 focus-within:caret-primary-700 active:caret-primary-700 active:ring-primary-700 active:ring-2 focus-within:ring-primary-700 focus-within:ring-2',
+          'flex items-center gap-2 px-4 bg-white rounded-md text-neutral-500 hover:ring-primary-700 focus-within:caret-primary-700 active:caret-primary-700 active:ring-primary-700 active:ring-2 focus-within:ring-primary-700 focus-within:ring-2',
           {
             'ring-2 ring-negative-700': invalid,
             'ring-1 ring-neutral-300': !invalid,
