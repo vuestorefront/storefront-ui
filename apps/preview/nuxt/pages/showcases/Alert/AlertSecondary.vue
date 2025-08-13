5245cbd5cd7c5aa53f49744ec695b9c03f68cd71
Jakub Freisler <jakub@frsource.org>
2025-08-14 01:26:14 +0200

    feat: migrate alert & modal

--- apps/preview/nuxt/pages/showcases/Alert/AlertSecondary.vue
+++ apps/preview/nuxt/pages/showcases/Alert/AlertSecondary.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     role="alert"
-    class="flex items-center max-w-[600px] shadow-md bg-secondary-100 pr-2 pl-4 ring-1 ring-secondary-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+    class="flex items-center max-w-[600px] shadow-md bg-secondary-200 pr-2 pl-4 ring-1 ring-secondary-200 typography-text-sm md:typography-text-base py-1 rounded-xl"
   >
     <SfIconInfo class="mr-2 text-secondary-700 shrink-0" />
     <p class="py-2">Your cart will soon be full.</p>
