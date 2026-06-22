c248140107f145c82219fec7e77fc1e90b9229dc
Jakub Freisler <jakub@frsource.org>
2026-06-22 13:49:50 +0200

    fix: useFocusVisible SSR leak [ES-2268] (#3464)

--- packages/sfui/frameworks/vue/composables/useFocusVisible/useFocusVisible.ts
+++ packages/sfui/frameworks/vue/composables/useFocusVisible/useFocusVisible.ts
@@ -7,32 +7,32 @@ const manager = focusVisibleManager();
 export const useFocusVisible = (props: FocusVisibleProps = {}): FocusVisibleResult => {
   const isTextInput = computed(() => unref(props.isTextInput));
   const isFocusVisible = ref(props.autoFocus || manager.isFocusVisible());
-
-  onMounted(() => {
-    manager.setupGlobalFocusEvents();
-  });
-
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   let onCleanup = () => {};
-  watch(
-    isTextInput,
-    () => {
-      onCleanup();
+  
+  onMounted(() => {
+    manager.setupGlobalFocusEvents();
 
-      const handler = (modality: FocusModality, e: FocusHandlerEvent) => {
-        if (!manager.isKeyboardFocusEvent(isTextInput?.value, modality, e)) {
-          return;
-        }
-        isFocusVisible.value = manager.isFocusVisible();
-      };
-      manager.changeHandlers.add(handler);
-
-      onCleanup = () => {
-        manager.changeHandlers.delete(handler);
-      };
-    },
-    { immediate: true },
-  );
+    watch(
+      isTextInput,
+      () => {
+        onCleanup();
+  
+        const handler = (modality: FocusModality, e: FocusHandlerEvent) => {
+          if (!manager.isKeyboardFocusEvent(isTextInput?.value, modality, e)) {
+            return;
+          }
+          isFocusVisible.value = manager.isFocusVisible();
+        };
+        manager.changeHandlers.add(handler);
+  
+        onCleanup = () => {
+          manager.changeHandlers.delete(handler);
+        };
+      },
+      { immediate: true },
+    );
+  });
 
   onUnmounted(() => {
     onCleanup();
