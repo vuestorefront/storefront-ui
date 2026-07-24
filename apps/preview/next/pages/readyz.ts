a844ec0af2fe618d6f93100668293fb93d3e3e9a
Jakub Freisler <jakub@frsource.org>
2026-07-24 12:55:46 +0200

    feat: add /healthz and /readyz endpoints to deployed apps (#3466)

---
+++ apps/preview/next/pages/readyz.ts
@@ -0,0 +1,12 @@
+import type { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('OK');
+  return { props: {} };
+};
+
+export default function Readyz() {
+  return null;
+}
